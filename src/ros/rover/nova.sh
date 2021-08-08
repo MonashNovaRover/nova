10272595dec61576e23e5daec2bfe62dbfa4258f
Harrison Verrios <harrison.v1@live.com>
2021-08-08 13:47:08 +1000

    Added in the setup bash file

--- src/ros/rover/nova.sh
+++ src/ros/rover/nova.sh
@@ -1,2 +1,10 @@
-source /opt/ros/eloquent/setup.bash
-alias build='. ~/nova_ws/core/macros/build.sh'
+# Sourcing ROS
+source /opt/ros/eloquent/setup.bash               # Sources ROS 2 Eloquent
+. ~/nova_ws/install/setup.bash                    # Runs the workspace setup file
+
+# Macro Aliases
+alias build='. ~/nova_ws/core/macros/build.sh'    # Runs colcon build
+
+# Directory Aliases
+alias nova='cd ~/nova_ws'
+alias core='cd ~/nova_ws/core'
