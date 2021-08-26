c4e2ed0774d0a5b13ed9fe2362c89c0ad8d98dee
Harrison Verrios <harrison.v1@live.com>
2021-08-26 21:45:01 +1000

    Fixed up the ReadMe for the core repo

--- src/ros/rover/nova.sh
+++ src/ros/rover/nova.sh
@@ -11,23 +11,8 @@
 #
 # +--------------------------------------------+
 
-# Macro Aliases
-alias build='. ~/nova_ws/src/core/macros/build.sh' # Runs colcon build
-alias setup='. ~/nova_ws/install/setup.bash'       # Sets up the ROS repository when scripts change
-alias pull='. ~/nova_ws/src/core/macros/pull.sh'   # Runs a GitHub pull on all repositories
-
-# Directory Aliases
-alias nova='cd ~/nova_ws'
-alias core='cd ~/nova_ws/src/core'
-alias control='cd ~/nova_ws/src/control'
-alias electronics='cd ~/nova_ws/src/electronics'
-alias elec=electronics
-alias science='cd ~/nova_ws/src/science'
-alias cameras='cd ~/nova_ws/src/cameras'
-alias cams=cameras
-alias autonomous='cd ~/nova_ws/src/autonomous'
-alias auto=autonomous
-alias gui='cd ~/nova_ws/src/gui'
+# Source the aliases
+source ~/nova_ws/src/core/macros/alias.sh
 
 # Calls bash executions
 setup                                              # Sets up the bash file
