8b707ef34b33429948025bbca41d718b7a73f369
Marcel <marcel.masques@gmail.com>
2021-09-27 12:00:45 +1000

    added empty LED_transceiver

---
+++ src/ros/rover/electronics/LED_transceiver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+'''
+NOVA ROVER TEAM
+This script is a ros service that handles communicating commands to the LED Lights.
+Authors: Max Tory and Marcel Masque
+Last Modified: 27/09/2021 By Max Tory and Marcel Masque
+'''
+
+
