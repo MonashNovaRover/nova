3d8898b8884ce2dbacadb635cd1605da57100c30
Emily Kuo <emilykuo5@hotmail.com>
2021-09-30 02:06:30 -0700

    fix some bugs

--- src/ros/rover/electronics/radio_monitor.py
+++ src/ros/rover/electronics/radio_monitor.py
@@ -69,80 +69,81 @@ class RadioMonitor(Node):
       super().__init__("radio_monitor")
       self.radio_publisher = self.create_publisher(RadioStatus, "/electronics/radio_status", 10)
 
-    def connect_to_radio(self):
-      '''
-      Initiates ssh connection to the radio
-      '''
-      self.ssh_connection.open()
+  def connect_to_radio(self):
+    '''
+    Initiates ssh connection to the radio
+    '''
+    print("Connecting to radio")
+    self.ssh_connection.open()
+
+  def loop_function(self):
+    '''
+    Main function
+    Runs all the radio monitor commands
+    Prints/ publishes the output
+    '''
+    sent, recv = self.get_bandwidth()
+    ping = self.get_ping()
+    signal = self.get_signal()
+
+    print("Signal strength: %0.0fdb" % signal)
+    print("Sent: %0.0f kbits" % sent)
+    print("Received: %0.0f kbits" % recv)
+    print("Pinging %s, Time elapsed: %0.0fms \n" % (self.dest_IP, ping))
+
+    if self.is_ros:
+      radio_msg = RadioStatus()
+      radio_msg.signal = signal
+      radio_msg.sent = sent
+      radio_msg.recv = recv
+      radio_msg.ping = ping
+      self.radio_publisher.publish(radio_msg)
+  
+  def get_bandwidth(self):
+    '''
+    Monitors data transfer over the ethernet port over one second
+    Returns sent, received bandwidth (kbits)
+    '''
+    sent_old = psutil.net_io_counters(pernic=True)[self.interface][0]
+    recv_old = psutil.net_io_counters(pernic=True)[self.interface][1]
+    time.sleep(1)
+    sent_new = psutil.net_io_counters(pernic=True)[self.interface][0]
+    recv_new = psutil.net_io_counters(pernic=True)[self.interface][1]
+
+    sent = ((sent_new-sent_old) * 8)/1024 #converts to kbits
+    recv = ((recv_new-recv_old) * 8)/1024 
+
+    return sent, recv
+        
+  def get_signal(self):
+    '''
+    Sshs into the base station radio to monitor connection to the rover
+    Returns signal strength (dB) 
+    -96dB indicates that the two radios are disconnected
+    '''
+    raw_output = self.ssh_connection.run('mca-status | grep -Ei "signal"', hide=True)
+    msg = "{0.stdout}"
+    signal_string = msg.format(raw_output)
+    initial_split = signal_string.split('=')
+    signal_split = initial_split[1].split('\r')[0]
+    signal = int(signal_split)
+
+    return signal
+
+  def get_ping(self):
+    '''
+    Pings destination IP three times and averages the result
+    Returns ping (ms)
+    '''
+    try: 
+      output = subprocess.check_output(self.command, shell=True)
+      matches = re.findall(" time=([\d.]+) ms", output)
+      matches = [float(match) for match in matches]
+      ms = sum(matches)/len(matches)
+    except: 
+      ms=9999 #returns this if there is a ping error
     
-    def loop_function(self):
-      '''
-      Main function
-      Runs all the radio monitor commands
-      Prints/ publishes the output
-      '''
-      sent, recv = self.get_bandwidth()
-      ping = self.get_ping()
-      signal = self.get_signal()
-
-      print("Signal strength: %0.0fdb" % signal)
-      print("Sent: %0.0f kbits" % sent)
-      print("Received: %0.0f kbits" % recv)
-      print("Pinging %s, Time elapsed: %0.0fms \n" % (self.dest_IP, ping))
-
-      if self.is_ros:
-        radio_msg = RadioStatus()
-        radio_msg.signal = signal
-        radio_msg.sent = sent
-        radio_msg.recv = recv
-        radio_msg.ping = ping
-        self.radio_publisher.publish(radio_msg)
-    
-    def get_bandwidth(self):
-      '''
-      Monitors data transfer over the ethernet port over one second
-      Returns sent, received bandwidth (kbits)
-      '''
-      sent_old = psutil.net_io_counters(pernic=True)[self.interface][0]
-      recv_old = psutil.net_io_counters(pernic=True)[self.interface][1]
-      time.sleep(1)
-      sent_new = psutil.net_io_counters(pernic=True)[self.interface][0]
-      recv_new = psutil.net_io_counters(pernic=True)[self.interface][1]
-
-      sent = ((sent_new-sent_old) * 8)/1024 #converts to kbits
-      recv = ((recv_new-recv_old) * 8)/1024 
-
-      return sent, recv
-          
-    def get_signal(self):
-      '''
-      Sshs into the base station radio to monitor connection to the rover
-      Returns signal strength (dB) 
-      -96dB indicates that the two radios are disconnected
-      '''
-      raw_output = self.ssh_connection.run('mca-status | grep -Ei "signal"', hide=True)
-      msg = "{0.stdout}"
-      signal_string = msg.format(raw_output)
-      initial_split = signal_string.split('=')
-      signal_split = initial_split[1].split('\r')[0]
-      signal = int(signal_split)
-
-      return signal
-
-    def get_ping(self):
-      '''
-      Pings destination IP three times and averages the result
-      Returns ping (ms)
-      '''
-      try: 
-        output = subprocess.check_output(self.command, shell=True)
-        matches = re.findall(" time=([\d.]+) ms", output)
-        matches = [float(match) for match in matches]
-        ms = sum(matches)/len(matches)
-      except: 
-        ms=9999 #returns this if there is a ping error
-      
-      return ms
+    return ms
 
 def main():
   try:
@@ -161,14 +162,14 @@ if __name__=="__main__":
       for dev in dev_info.keys():
           print("\t" + dev)
       print("Using Metabox as the default settings.")
-      device = "Metabox"
+      device = "METABOX"
 
   # Otherwise take the parameter
   else:
       # If found an invalid parameter
       if sys.argv[1].upper() not in dev_info.keys():
           print("Using Metabox as the default settings.")
-          device = "Metabox"
+          device = "METABOX"
       # Else set the device
       else:
           device = sys.argv[1].upper()
