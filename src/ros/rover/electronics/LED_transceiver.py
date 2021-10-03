9752f55c06f1ae78bc58919707af82dcc14e248e
Marcel <marcel.masques@gmail.com>
2021-10-03 12:54:47 +1100

    added barebones status update based on changes to mode

--- src/ros/rover/electronics/LED_transceiver.py
+++ src/ros/rover/electronics/LED_transceiver.py
@@ -20,24 +20,100 @@ Goal 2: whenever the rover is running, check the mode and update
 color on changes 
      - check_update + callback?
 """
-
-class LEDTransceiverServiceNode(Node):
-
+class CanLEDCommunicator():
+     """Handles communication with LED
+     """
+     def set_LED(self, r, g, b):
+          print(f"Setting LED to [R: {r}, G: {g}, B: {b}]")
+          """TODO: actually interface with CAN
+          """
+     
+class LEDTransceiverServiceNode(Node,CanLEDCommunicator):
+     """This class' job is to receive requests for setting the LED colour, and 
+     passing the request on via CAN. 
+     TODO: Pass the request on via CAN (currently prints out requests)
+     """
      def __init__(self):
           super().__init__('LED_transceiver_node')
           # The service that listens for LED setting requests
           self.service = self.create_service(LED, 'LED', self.callback)
      
      def callback(self, request, response):
-          print("Callback")
           self.get_logger().info(f"Service received request with data [R: {request.r}, G: {request.g}, B: {request.b}]")
-          response.sent_status = 1234
+          self.set_LED(request.r, request.g, request.b)
+          response.sent_status = True
+
           return response
+
+class LEDStatusUpdateNode(Node,CanLEDCommunicator):
+     """class: its job is to check the status of the rover and update the LED via can on changes
+     TODO: update the LED via can on changes
+
+     Note: this is a temporary approach. Really, we should be putting heartbeat as a mode
+     in a topic with the other mode types, and then this should be a subscriber to that topic.
+     Then, a callback function will get executed periodically as the topic is published to. 
+     """ 
+     def __init__(self):
+          super().__init__('LED_status_update_node')
+          # these should not be here! they should be in a file/params? and then 
+          # can be pulled here
+          self.modes = {
+               0: "off",
+               1: "xbox", 
+               2: "phone", 
+               3: "auto"
+          }
+          self.mode_colours = {
+               "off":[0,0,0],
+               "xbox":[0,255,0],
+               "phone":[255,200,0],
+               "auto":[0,0,255],
+               "heartbeat":[255,0,0]
+          }
+          self.mode = 'off'
+     
+     def get_rover_heartbeat(self):
+          heartbeat = True    # TODO actually get heartbeat
+          return heartbeat
+
+     def get_rover_mode(self):
+          """ Gets the rover's current mode according to publisher/param and heartbeat
+          TODO: get mode from publisher
+          """
+          heartbeat = self.get_rover_heartbeat()
+          if heartbeat:  # hearbeat established
+               mode = 1 # TODO: get the mode from param/publisher
+               return self.modes[mode]
+          else:     # no hearbeat, so we are in heartbeat mode
+               return 'heartbeat'
+
+     def get_mode_RGB(self):
+          """Gets RGB for the mode
+          """
+          return self.mode_colours[self.mode]
+
+     def update_LED_status(self):
+          """Monitors for changes in the LED status, and asks the LED 
+          to update its colours when status changes occur
+          """
+          # get the current rover mode
+          updated_mode = self.get_rover_mode()
+
+          # the mode changed, so update the RGB status 
+          if updated_mode != self.mode:
+               print("mode changed")
+               self.mode = updated_mode
+               colours = self.get_mode_RGB()
+               self.set_LED(*colours)
+          
 
 def main (args = None):
      rclpy.init(args=args)
      service = LEDTransceiverServiceNode()
-     rclpy.spin(service)
+     LEDupdater = LEDStatusUpdateNode()
+     while rclpy.ok():
+          LEDupdater.update_LED_status()
+          rclpy.spin_once(service)
      service.destroy_node()
      rclpy.shutdown()
 
