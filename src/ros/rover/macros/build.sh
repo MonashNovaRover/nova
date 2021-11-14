ebc7761414967c5d8a30cea23b890e7440f29c39
harrisonv789 <harrison.v1@live.com>
2021-11-14 13:44:14 +1100

    Added Gamepad Input message for joystick code

--- src/ros/rover/macros/build.sh
+++ src/ros/rover/macros/build.sh
@@ -7,10 +7,22 @@
 # Build script builds the ROS workspace.
 # This can be called from any script and any
 #   package.
+# To build one package, add the package name as 
+#   an argument to the console script.
+#
+#           e.g 'build core'
 #
 # +--------------------------------------------+
 
 cwd=$(pwd);     # Save the current directory
 cd ~/nova_ws;   # Navigate to the workspace directory
-colcon build;   # Build the workspace
+setup           # Call the setup macro
+
+# Check if a keyword used
+if [[ -z $1 ]]; then
+    colcon build;   # Build the workspace
+else
+    colcon build --packages-select $1
+fi
+
 cd $cwd;        # Return back to the previous directory
