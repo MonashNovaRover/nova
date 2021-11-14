b04a00b9be637cc3d42626f4c8c903c9b9ff35a0
harrisonv789 <harrison.v1@live.com>
2021-11-14 14:42:10 +1100

    Finished formatting the joystick class

--- src/joystick.cpp
+++ src/joystick.cpp
@@ -1,57 +1,46 @@
 /*
- *--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--
- * A joystick class to handle xbox/arm joystick inputs 
- *
- * Author: Marcel Masque (marcel.masques@gmail.com)
- *
- * Date last updated: 29/01/20 by Marcel
- *--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
- */
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Monash Nova Rover Team
+
+PACKAGE: 	control
+AUTHOR(S):	Marcel Masque, Harrison Verrios
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
 #include "joystick.h"
 
 /*
- *--**--..--**----**--..--**--..--**--..--**--..--**--..--**--..--**--
- * Constructor used when offset is needed
- *    Initialises controller position values, offsets and controller boolean settings
- *    Initialises controller instance 
- *--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
- */
+    Constructor used when offset is needed
+    Initialises controller position values, offsets and controller boolean settings
+    Initialises controller instance 
+*/
 Joystick::Joystick(const GAMEPAD_DEVICE controller, const float offset) {
 
-
+    // Set the controller and offset instance variables
     this->offset = offset;
+    this->controller = controller;
 
+    // Reset the axis inputs
     stick_lx = 0.0;
     stick_ly = 0.0;
     stick_rx = 0.0;
     stick_ry = 0.0;
     twist_lock = true;
     hat_lock = true;
-    this->controller = controller;
 }
-/*
- *--**--..--**----**--..--**--..--**--..--**--..--**--..--**--..--**--
- * Constructor used when offset is NOT  needed
- *    Initialises controller position values, controller boolean settings
- *    Initialises controller instance 
- *--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
- */
-Joystick::Joystick(const GAMEPAD_DEVICE controller) {
 
-	stick_lx = 0.0;
-	stick_ly = 0.0;
-	stick_rx = 0.0;
-	stick_ry = 0.0;
-    offset = 0;
-    twist_lock = true;
-    hat_lock = true;
-    this->controller = controller;
-}
 /*
- *--**--..--**----**--..--**--..--**--..--**--..--**--..--**--..--**--
- * Fetches stick values, corrects for deadzone and sets message values.
- *--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
- */
+    Constructor used when offset is NOT  needed
+    Initialises controller position values, controller boolean settings
+    Initialises controller instance 
+*/
+Joystick::Joystick(const GAMEPAD_DEVICE controller) : 
+    Joystick(controller, 0.0) {
+}
+
+/*
+    Fetches stick values, corrects for deadzone and sets message values.
+*/
 void Joystick::Update() {
 
 	// grab stick values
@@ -63,27 +52,30 @@ void Joystick::Update() {
 	// set all message values
     SetMessageValues();
 }
+
 /*
- *--**--..--**----**--..--**--..--**--..--**--..--**--..--**--..--**--
- * The gamepad sticks have a deadzone - which means for a small amount of
- * movement of the stick, the reading remains at zero. This means as soon
- * as you move the stick out of the deadzone, the reading will jump from 
- * zero to some higher value. The calculations here account for this and 
- * rescale the values to remove this jump.
- *--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
- */
+    The gamepad sticks have a deadzone - which means for a small amount of
+    movement of the stick, the reading remains at zero. This means as soon
+    as you move the stick out of the deadzone, the reading will jump from 
+    zero to some higher value. The calculations here account for this and 
+    rescale the values to remove this jump.
+*/
 void Joystick::CorrectDeadzone() {
 
-    
-    stick_lx_f = Sign(stick_lx)*((float) abs(stick_lx) - GAMEPAD_DEADZONE_LEFT_STICK)/STICK_MAX_L;
-    stick_ly_f = Sign(stick_ly)*((float) abs(stick_ly) - GAMEPAD_DEADZONE_LEFT_STICK)/STICK_MAX_L;
+    // Updates the left stick float values
+    stick_lx_f = Sign(stick_lx) * ((float) abs(stick_lx) - GAMEPAD_DEADZONE_LEFT_STICK) / STICK_MAX_L;
+    stick_ly_f = Sign(stick_ly) * ((float) abs(stick_ly) - GAMEPAD_DEADZONE_LEFT_STICK) / STICK_MAX_L;
 
-    stick_rx_f = Sign(stick_rx)*((float) abs(stick_rx) - GAMEPAD_DEADZONE_RIGHT_STICK)/STICK_MAX_R;
-    stick_ry_f = Sign(stick_ry)*((float) abs(stick_ry) - GAMEPAD_DEADZONE_RIGHT_STICK)/STICK_MAX_R;
-    
-    
+    // Updates the right stick float values
+    stick_rx_f = Sign(stick_rx) * ((float) abs(stick_rx) - GAMEPAD_DEADZONE_RIGHT_STICK) / STICK_MAX_R;
+    stick_ry_f = Sign(stick_ry) * ((float) abs(stick_ry) - GAMEPAD_DEADZONE_RIGHT_STICK) / STICK_MAX_R;
 }
 
+/*
+    Returns the state of a particular button input based on how the button
+    has been pressed. If no input, it will be a 0. If triggered, 1. If the
+    button has been held, then 2 and when the button is released, 3.
+*/
 int Joystick::GetButtonState (const GAMEPAD_BUTTON button) {
     if (GamepadButtonTriggered(controller, button))
         return 1;
@@ -92,27 +84,28 @@ int Joystick::GetButtonState (const GAMEPAD_BUTTON button) {
     else if (GamepadButtonDown(controller, button))
         return 2;
     
+    // No input for this button
     return 0;
 }
 
 /*
- *--**--..--**----**--..--**--..--**--..--**--..--**--..--**--..--**--
- * Fetches rest of controller values and updates the message object
- *--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
- */
+    Fetches rest of controller values and updates the message object
+*/
 void Joystick::SetMessageValues() {
 
-    msg.connected = GamepadIsConnected(controller); // Check controller connection
+    // Checks if the gamepad is currently connected
+    msg.connected = GamepadIsConnected(controller);
+
+    // If the gamepad is connected
     if (msg.connected)
-    {   
-        
-        // Set the values in the ROS msg
+    {          
+        // Set the values in the ROS msg to the axis values
         msg.ax_stick_l_x = stick_lx_f; 
         msg.ax_stick_l_y = stick_ly_f;   
         msg.ax_stick_r_x = stick_rx_f; 
         msg.ax_stick_r_y = stick_ry_f;
 
-        // Set the state messages
+        // Set the state messages of each of the buttons
         msg.btn_a_state = GetButtonState(BUTTON_A);
         msg.btn_b_state = GetButtonState(BUTTON_B);
         msg.btn_x_state = GetButtonState(BUTTON_X);
@@ -130,73 +123,73 @@ void Joystick::SetMessageValues() {
         msg.btn_dpad_d_state = GetButtonState(BUTTON_DPAD_DOWN );
 
         
-        //left
-        if (twist_lock and GamepadTriggerLength(controller, TRIGGER_LEFT) < 0.1)
-        {
+        // Left trigger
+        if (twist_lock && GamepadTriggerLength(controller, TRIGGER_LEFT) < 0.1)
             msg.trg_l_val = 0.0;
-        }
+
         else
         {
             msg.trg_l_val = GamepadTriggerLength(controller, TRIGGER_LEFT) - offset;
             msg.trg_l_val = (msg.trg_l_val > 0.0) ? msg.trg_l_val/(1 - offset): msg.trg_l_val/(offset);
           
+            // Look for invalid input
             if ((msg.trg_l_val < 0.01 && msg.trg_l_val > -0.01) || isnan(msg.trg_l_val))
-            {
                 msg.trg_l_val = 0.0;
-            }
 
             twist_lock = false;
         }
-        // right
-        if (hat_lock and GamepadTriggerLength(controller, TRIGGER_RIGHT) < 0.1)
-        {
+
+        // Right trigger
+        if (hat_lock && GamepadTriggerLength(controller, TRIGGER_RIGHT) < 0.1)
             msg.trg_r_val = 0.0;
-        }
+
         else
         {
             msg.trg_r_val = GamepadTriggerLength(controller, TRIGGER_RIGHT)-offset;
             msg.trg_r_val = (msg.trg_r_val>0.0) ? msg.trg_r_val/(1-offset): msg.trg_r_val/(offset); //Re-scale OFFSET value
+            
+            // Look for invalid input
             if ((msg.trg_r_val < 0.01 && msg.trg_r_val > -0.01) || isnan(msg.trg_r_val)) // Get rid of tiny floats
-            { 
                 msg.trg_r_val = 0.0;
-            }
+            
             hat_lock = false;
         }
 
-        //When the joystick is first connected the twist and hat give a 0.0 until moved, whereas their actual centre is 0.435. This ensures that they have been moved first so they don't make a full negative power to twist and hat on connection
+        // When the joystick is first connected the twist and hat give a 0.0 until moved, whereas their actual centre is 0.435. This ensures that they have been moved first so they don't make a full negative power to twist and hat on connection
         msg.btn_trigger_l_down = GamepadTriggerDown(controller, TRIGGER_LEFT);
         msg.btn_trigger_r_down = GamepadTriggerDown(controller, TRIGGER_RIGHT);
 
+        // Get DPAD inputs
         bool dpad_l = GamepadButtonDown(controller, BUTTON_DPAD_LEFT);
         bool dpad_r = GamepadButtonDown(controller, BUTTON_DPAD_RIGHT);
-
         bool dpad_u = GamepadButtonDown(controller, BUTTON_DPAD_UP);
         bool dpad_d = GamepadButtonDown(controller, BUTTON_DPAD_DOWN);
 
+        // Calculate the DPAD data
         msg.ax_dpad_x = dpad_r - dpad_l; // Left -1, none/both 0, right 1
         msg.ax_dpad_y = dpad_u - dpad_d; // Down -1, none/both 0, up 1
         
     }
+
+    // If the input is not connected, lock the inputs and reset the message
     else
     {
-        msg.ax_stick_l_x = 0.0;
-        msg.ax_stick_l_y = 0.0;
-        msg.ax_stick_r_x = 0.0;
-        msg.ax_stick_r_y = 0.0;
+        msg = core::msg::InputGamepad();
         twist_lock = true;
         hat_lock = true;
     }
 }
 
+/*
+    Returns the message object from the instance
+*/
 core::msg::InputGamepad Joystick::GetMessage() {
     return msg;
 }
 
-//--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--
-// sgn():
-//
-//    Returns the sign of the input float (-1, 0 or 1).
-//--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
+/*
+    Returns the sign of the input float (-1 or 1).
+*/
 int Joystick::Sign(const float val) {
     return (int)(0.0 < val) - (val < 0.0);
 }
