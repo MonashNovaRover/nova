edc05f5214a6ff40b47b158e5e5c923b371c0368
harrisonv789 <harrison.v1@live.com>
2021-11-16 15:00:28 +1100

    Added in other repos to pull from the pull macro too

--- macros/pull.sh
+++ macros/pull.sh
@@ -15,4 +15,9 @@ cd ~/nova_ws/src;   # Navigate to the Nova workspace
 # Find all the files and pull
 find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;
 
+cd ~/nova_ws/other;   # Navigate to the other repositories workspace
+
+# Find all the files from other repos and pull
+find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;
+
 cd $cwd;            # Return back to the previous directory
