92218052b97f658ff4833d4fadd6ad5d5bcfeb71
harrisonv789 <harrison.v1@live.com>
2021-11-19 14:37:01 +1100

    Thrustmasters now have access to every single button on the controller

--- include/gamepad/gamepad.h
+++ include/gamepad/gamepad.h
@@ -61,6 +61,12 @@ enum GAMEPAD_BUTTON {
 	BUTTON_X				= 14,	/**< X button */
 	BUTTON_Y				= 15,	/**< Y button */
     BUTTON_XBOX             = 16,   /**< Xbox button */
+	BUTTON_EXTRA_1			= 17,	/**< Extra button 1*/
+	BUTTON_EXTRA_2			= 18,	/**< Extra button 1*/
+	BUTTON_EXTRA_3			= 19,	/**< Extra button 1*/
+	BUTTON_EXTRA_4			= 20,	/**< Extra button 1*/
+	BUTTON_EXTRA_5			= 21,	/**< Extra button 1*/
+	BUTTON_EXTRA_6			= 22,	/**< Extra button 1*/
 
 	BUTTON_COUNT					/**< Maximum number of supported buttons */
 };
