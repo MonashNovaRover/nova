bb1fb54d018062f822e2f84d88c697ff0325ceec
MaxTory2001 <torymax01@gmail.com>
2021-12-07 21:51:14 +1100

    added publish and some other minor methods

--- autonomous/Controller.py
+++ autonomous/Controller.py
@@ -30,6 +30,14 @@ from rclpy.node import Node
 from controller_math import *
 import controller_params
 from core.msg import DriveCmd, RoverPose, Waypoint
+import sys
+
+"""
+TODO: publish drive commands
+TODO: yaw_to method
+TODO: update led according to distance?
+TODO: go_to_waypoint
+"""
 
 class Controller(Node):
     """
@@ -44,7 +52,7 @@ class Controller(Node):
         self.waypoints = []
         self.max_distance = 0.0001      # furthest distance to an object? not sure
 
-        self.drive_cmd_publisher = self.create_publisher(DriveCmd, "autonomous_drive_commands", 10)
+        self.drive_cmd_publisher = self.create_publisher(DriveCmd, "auto_drive_commands", 10)
         self.pose_subscriber = self.create_subscription(Pose, "auto_command_pose_updates", self.update_pose, 10)
         self.waypt_subscriber = self.create_subscription(Waypoint, "auto_command_waypoints", self.add_waypoint, 10)
 
@@ -60,8 +68,48 @@ class Controller(Node):
 
     def add_waypoint(self, msg):
         """
-        Appends the x-y position of a waypoint to the back of the waypoints list
+        Callback that appends the x-y position of a waypoint to the back of the waypoints list
         """
         self.waypoints.append([msg.data.x, msg.data.y])
 
+    def __publish(self, drive_fraction, angular_fraction):
+        """
+        Publishes drive commands to the auto_drive_commands topic
+        :param: drive_fraction: [-1:1]
+        :param: angular_fraction: [-1:1]
+        """
+
+        # construct message to publish
+        drive_cmd_msg = DriveCmd()
+
+        drive_cmd_msg.speed = drive_fraction
+
+        drive_cmd_msg.steer = angular_fraction
+
+        # publish to public topic
+        self.drive_cmd_publisher.publish(drive_cmd_msg)
+
     @staticmethod
+    def print_update(action_msg, heading_to, yaw_diff, dist):
+        pad = 10
+        sys.stdout.write("\r" + "Action: " + action_msg.ljust(pad) + " | heading to: " + str(heading_to).ljust(pad)
+                          + " | yaw diff: " + str(round(yaw_diff, 4)).ljust(pad) + " | distance: " + str(round(dist, 4)).ljust(pad))
+        sys.stdout.flush()
+
+    def __clear_waypoints(self):
+        """
+        empties the waypoints list - prevents further coordinates from being travelled to and allows path planning to be reset
+        """
+        self.waypoints = []
+
+    def yaw_to(self, way_point):
+        """
+        TODO
+        """
+        pass
+
+    def go_to_waypoint(self, waypoint, ask_for_input=False):
+        pass
+
+    def control(self):
+        pass
