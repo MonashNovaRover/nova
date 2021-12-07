3c5c255b6b637df74a928d6309f8d0f1db449da9
liamwhittle <lwhi0020@student.monash.edu>
2021-12-07 12:12:01 +1100

    translation and transformation looks good - except for some weird flipping of certain objects!

--- src/ros/rover/autonomous/transform.py
+++ src/ros/rover/autonomous/transform.py
@@ -2,6 +2,21 @@
 
 import numpy as np
 
+"""
+On the tracking camera:
+    x : forward
+    y : right
+    z : up
+
+On the depth camera:
+    z : forward
+    x : right
+    y : down
+
+so we should have swapped the depth value indexes like so, and made the last value negative
+[2, 0, 1]
+"""
+
 class Q:
     def __init__(self, x, y, z, w):
         self.x = x
@@ -12,18 +27,9 @@ class Q:
 def camera_extrinsics():
     # according to librealsense, we need to have the 2nd and 3rd ones negative (why not first and 2nd? I thought z was depth...)
     m = \
-    [[1, 0, 0, 0],
-     [0, 1, 0, 0],
-     [0, 0, 1, 0],
-     [0, 0, 0, 1]]
-    return np.array(m)
-
-def camera_translation(x, y, z):
-    m = \
-    [[0, 0, 0, z],
-     [0, 0, 0, y],
-     [0, 0, 0, x],
-     [0, 0, 0, 0]]
+    [[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]]
     return np.array(m)
 
 def quat2mat(q):
@@ -31,13 +37,10 @@ def quat2mat(q):
     Assume q is a quaternion with y
     """
     m = \
-    [[1 - 2 * q.y * q.y - 2 * q.z * q.z, 2 * q.x * q.y - 2 * q.z * q.w, 2 * q.x * q.z + 2 * q.y * q.w, 0.0],
-     [2 * q.x * q.y + 2 * q.z * q.w, 1 - 2 * q.x * q.x - 2 * q.z * q.z, 2 * q.y * q.z - 2 * q.x * q.w, 0.0],
-     [2 * q.x * q.z - 2 * q.y * q.w, 2 * q.y * q.z + 2 * q.x * q.w, 1 - 2 * q.x * q.x - 2 * q.y * q.y, 0.0],
-     [0.0, 0.0, 0.0, 1.0]]
-    nm = np.array(m)
-    print(nm)
-    return nm
+    [[1 - 2 * q.y * q.y - 2 * q.z * q.z, 2 * q.x * q.y - 2 * q.z * q.w, 2 * q.x * q.z + 2 * q.y * q.w],
+     [2 * q.x * q.y + 2 * q.z * q.w, 1 - 2 * q.x * q.x - 2 * q.z * q.z, 2 * q.y * q.z - 2 * q.x * q.w],
+     [2 * q.x * q.z - 2 * q.y * q.w, 2 * q.y * q.z + 2 * q.x * q.w, 1 - 2 * q.x * q.x - 2 * q.y * q.y]]
+    return np.array(m)
 
 def get_pc_transformation(pose_msg):
     """
@@ -54,9 +57,5 @@ def get_pc_transformation(pose_msg):
 
     q = Q(qx, qy, qz, qw)
     
-    final = np.matmul(camera_extrinsics(), quat2mat(q)) + camera_translation(x, y, z) 
-    # final = quat2mat(q) + camera_translation(x, y, z) 
-    # final = camera_translation(x, y, z) + camera_extrinsics()
-    # final = np.matmul(quat2mat(q), camera_extrinsics()) + camera_translation(x, y, z) 
-    print(final)
-    return final
+    return np.matmul(camera_extrinsics(), quat2mat(q))
+
