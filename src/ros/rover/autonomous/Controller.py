a06919413be28eb00d39e833e408d2e578eb0ad8
MaxTory2001 <torymax01@gmail.com>
2021-12-08 18:41:12 +1100

    refactored to run on regular clock - fixed spinning weirdness

--- src/ros/rover/autonomous/Controller.py
+++ src/ros/rover/autonomous/Controller.py
@@ -26,6 +26,7 @@ EDITED:         07/12/2021
 """
 
 import rclpy
+from time import sleep
 from rclpy.node import Node
 from controller_math import *
 from controller_params import *
@@ -50,36 +51,37 @@ class Controller(Node):
 
         self.state = State()     # from controller_math
         self.waypoints = []
+        self.target_waypoint = None
+        self.previously_turned = False
         self.max_distance = 0.0001      # furthest distance to an object? not sure
 
         self.drive_cmd_publisher = self.create_publisher(DriveCmd, "auto_drive_commands", 10)
         self.pose_subscriber = self.create_subscription(RoverPose, "autonomous/pose", self.update_pose, 10)
         self.waypt_subscriber = self.create_subscription(Waypoint, "autonomous/goals", self.add_waypoint, 10)
 
-        # Controls the rate at which drive commands are sent - sleeps for the necessary time to maintain the frequency given
-        # I think this is a ros2 implementation of the ros1 Rate object - need to check it works
-        self.loop_rate = self.create_rate(controller_ros_rate, self.get_clock())
+        # Controls the rate at which drive commands are sent - sleeps for the necessary time to maintain the rate given
+        self.timer = self.create_timer(controller_ros_rate, self.control)
 
     def update_pose(self, msg):
         """
         Callback function that updates the current pose of the rover from data in the auto_command_pose_updates topic
         """
-        self.state.x = msg.data.x
-        self.state.y = msg.data.y
-        self.state.yaw = msg.data.yaw
-        self.state.velocity = msg.data.velocity
-        self.state.angular_velocity = msg.data.angular_velocity
+        self.state.x = msg.x
+        self.state.y = msg.y
+        self.state.yaw = msg.yaw
+        self.state.velocity = msg.velocity
+        self.state.angular_velocity = msg.angular_velocity
 
         # testing pose subscriber
-        print("new pose: x = %.2f, y = %.2f, yaw = %.2f, vel = %.2f, omega = %.2f" % self.state.x, self.state.y, self.state.yaw, self.state.velocity, self.state.angular_velocity)
+        print("new pose: x = %.2f, y = %.2f, yaw = %.2f, vel = %.2f, omega = %.2f" % (self.state.x, self.state.y, self.state.yaw, self.state.velocity, self.state.angular_velocity))
 
     def add_waypoint(self, msg):
         """
         Callback that appends the x-y position of a waypoint to the back of the waypoints list
         """
-        self.waypoints.append([msg.data.x, msg.data.y])
+        self.waypoints.append([msg.x, msg.y])
 
-        print("new waypoint: x = %.2f, y = %.2f" % msg.data.x, msg.data.y)
+        print("new waypoint: x = %.2f, y = %.2f" % (msg.x, msg.y))
 
     def __publish(self, drive_fraction, angular_fraction):
         """
@@ -110,78 +112,103 @@ class Controller(Node):
         empties the waypoints list - prevents further coordinates from being travelled to and allows path planning to be reset
         """
         self.waypoints = []
+        self.target_waypoint = None
 
-    def go_to_waypoint(self, waypoint):
+    def tank_turn(self, yaw_diff, target_yaw):
         """
-        Drives to a given waypoint in a straight line. Begins by tank turning (turning in place with 0 drive fraction)
-        until facing in the direction of the target waypoint. Then drives forwards until waypoint is reached. Continually
-        re-calculates target yaw and yaw_diff with each iteration to ensure the rover is not going off course.
+        turns the rover to face towards the target waypoint
+        :returns: True if the rover needed to turn, False if it was already on course
         """
-        while distance((self.state.x, self.state.y), waypoint) >= min_waypoint_distance:
-            # calculate target yaw and signed yaw difference using the controller_math module
-            target_yaw = desired_heading((self.state.x, self.state.y), waypoint)
-            yaw_diff = yaw_difference(self.state.yaw, target_yaw)
 
-            completed_turn = False
+        # re-adjusts yaw to ensure we aren't going off track
+        if abs(yaw_diff) >= (min_yaw_difference / 2.0):
+            steer_fraction = tank_turn_target_yaw_rate(self.state.yaw, target_yaw)
 
-            # re-adjusts yaw to ensure we aren't going off track
-            while abs(yaw_diff) >= (min_yaw_difference / 2.0):
-                # Steering the rover to face in the desired direction before driving forward
-                target_yaw = desired_heading((self.state.x, self.state.y), waypoint)
-                yaw_diff = yaw_difference(self.state.yaw, target_yaw)
+            self.__publish(0.0, steer_fraction)
 
-                steer_fraction = tank_turn_target_yaw_rate(self.state.yaw, target_yaw)
+            Controller.print_update("yawing", self.target_waypoint, yaw_diff, distance((self.state.x, self.state.y), self.target_waypoint))
+            
+            self.previously_turned = True
 
-                self.__publish(0, steer_fraction)
+            return True
 
-                Controller.print_update("yawing", waypoint, yaw_diff, distance((self.state.x, self.state.y), waypoint))
+        return False
 
-                completed_turn = True
+    def go_to_target(self):
+        """
+        Publishes a single drive commmand to navigate to the current target waypoint. 
+        Called every tick by the control method. Turns in place to face towards the waypoint, 
+        or drives towards it in a straight line. If the rover has just finished turning, a
+        single zero drive command is sent before driving begins.
+        """
+        # calculate target yaw and signed yaw difference using the controller_math module
+        target_yaw = desired_heading((self.state.x, self.state.y), self.target_waypoint)
+        yaw_diff = yaw_difference(self.state.yaw, target_yaw)
 
-                # waits until 0.1 seconds since last sleep to publish next command
-                self.loop_rate.sleep()
-                
-            if completed_turn:
-                # need to send a zero wheel command after turning before we drive
-                self.__publish(0, 0)
-                self.loop_rate.sleep()
+        if self.tank_turn(yaw_diff, target_yaw): return # turns the rover if necessary
+            
+        elif self.previously_turned:
+            # need to send a zero wheel command after turning before we drive
+            self.__publish(0.0, 0.0)
+            return
 
+        else:
             # drive in straight line toward waypoint
-            drive_fraction = crow_fly_target_velocity((self.state.x, self.state.y), waypoint)
-            self.__publish(drive_fraction, 0)
-            self.loop_rate.sleep()
+            drive_fraction = crow_fly_target_velocity((self.state.x, self.state.y), self.target_waypoint)
+            self.__publish(drive_fraction, 0.0)
 
-            Controller.print_update("heading", waypoint, yaw_diff, distance((self.state.x, self.state.y), waypoint))
+            Controller.print_update("heading", self.target_waypoint, yaw_diff, distance((self.state.x, self.state.y), self.target_waypoint))
 
-        print("Reached way-point: " + str(waypoint))
-        for _ in range(5):
-            self.publish(0.0, 0.0)
-            self.loop_rate.sleep()
 
-    def control(self, ask_for_input=False):
+    def control(self):
         """
-        Pops waypoints from the waypoints list and navigates to them consecutively by calling go_to_waypoint
+        Called once every tick by the node's timer. Identifies the next target waypoint
+        and calls navigate_to_waypoint, and determines when the rover has arrived
         """
-        while rclpy.ok():
+        if self.target_waypoint == None:
+            # There is currently no target - take the first waypoint on the list
             if self.waypoints:
+                self.target_waypoint = self.waypoints.pop(0)
+            else:
+                return
+
+        if distance((self.state.x, self.state.y), self.target_waypoint) >= min_waypoint_distance:
+            # we have not yet arrived at the waypoint
+            self.go_to_target()
+        
+        else:
+            # If distance to the waypoint is lower than the threshold distance, we have arrived
+            print("Reached way-point: " + str(self.target_waypoint))
+            self.target_waypoint = None
+            
+            for _ in range(5):
+                # stop for 5 seconds at waypoint
+                self.publish(0.0, 0.0)
+                sleep(1)
+
+    """def control(self, ask_for_input=False):
+        
+        Pops waypoints from the waypoints list and navigates to them consecutively by calling go_to_waypoint
+        
+        while rclpy.ok():
+            self.__get_updates()
+
+            if self.waypoints and self.target_waypoint != None:
                 # get permission to go to next waypoint
-                if ask_for_input and self.way_points:
-                    input("Please press enter before heading to way-point: " + str(self.way_points[0]))
+                if ask_for_input:
+                    input("Please press enter before heading to way-point: " + str(self.waypoints[0]))
 
                 self.go_to_waypoint(self.waypoints.pop(0))
-            else:
-                break
 
-        self.__publish(0,0)
+        self.__publish(0.0,0.0)"""
 
 
 def main(args = None):
     rclpy.init(args=args)
     controller = Controller()
 
-    while rclpy.ok():
-        controller.control(True)
-        rclpy.spin_once(controller)
+    rclpy.spin(controller)
+
     controller.destroy_node()
     rclpy.shutdown()
 
