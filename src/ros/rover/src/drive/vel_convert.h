482c45d5b86995a3ef12e5125b819219b2e14c64
harrisonv789 <harrison.v1@live.com>
2021-12-18 15:46:16 +1100

    Done drive convert!

--- src/ros/rover/src/drive/vel_convert.h
+++ src/ros/rover/src/drive/vel_convert.h
@@ -23,6 +23,7 @@ EDITED:		08/12/2021
 // Include ROS packages
 #include "rclcpp/rclcpp.hpp"
 #include "core/msg/drive_input.hpp"
+#include "core/msg/drive_vel.hpp"
 
 #include <iostream>
 
@@ -31,20 +32,18 @@ using namespace std;
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
-// The minimum and maximum multipliers
-const float MIN_MULTIPLIER       = 0.1;  // The minimum multiplier value
-const float conversion_intercept = 0.0305;
-const float CONVERSION_FACTOR    = 1.3759;
-const float STEER_FACTOR         = 0.5;
-
-
-
 
 // Main publisher class that sends input data for the gamepad and joysticks
 class DriveVelocity : public rclcpp::Node {
 
     //------------------------------------------------------------//
     private:
+
+    // The minimum and maximum multipliers
+    const float MIN_MULTIPLIER       = 0.1;  // The minimum multiplier value
+    const float CONVERSION_INTERCEPT = 0.0305;
+    const float CONVERSION_FACTOR    = 1.3759;
+    const float STEER_FACTOR         = 0.5;
 
 
     // Stores the publisher for the drive commands
