fbcc293c63e58a4ce7d7815fec48e4a6dcf47fb2
Himsara <manurahimsara@gmail.com>
2021-12-18 15:26:00 +1100

    Completed Vel convert

--- src/ros/rover/src/drive/vel_convert.cpp
+++ src/ros/rover/src/drive/vel_convert.cpp
@@ -10,64 +10,29 @@ AUTHOR(S):  Himsara Gallege
 // Include the header file
 #include "vel_convert.h"
 
-// Include standard output messages
-#include <iostream>
-
-
-// Publishes the velocity
-void VelConvert::publish_cmds () {
-    auto message = core::msg::DriveCmd();
-
-    message.speed = .5 * data.linear_vel / (self.conversion_factor / 60.0 * 0.785398 * 0.95) 
-    message.steer = .5 * data.angular_vel * self.steer_factor * 100
+// Receives velocity converters
+void DriveVelocity::velocity_callback (const core::msg::DriveVel::SharedPtr msg) {
     
+    // Create the message
+    auto message = core::msg::DriveInput();
+
+    message.speed = .5 * msg->linear_vel / (conversion_factor / 60.0 * 0.785398 * 0.95);
+    message.steer = .5 * msg->angular_vel * steer_factor;
+
     // Publish the drive commands
     publisher->publish(message);
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Receives input from the gamepad
-void DrivePublisher::input_callback (const core::msg::InputGamepad::SharedPtr msg) {
-
-    linear_vel = msg->linear_vel;
-    angular_vel = msg->angular_vel;
-
-
-
-
-
-
-
-
-
-
-//----------------------------------------------------------------------------------------------------
 
 // Main constructor that sets up the node
-DrivePublisher::DrivePublisher() 
-  : Node("vel_pub"), count(0) {
+DriveVelocity::DriveVelocity() 
+  : Node("drive_velocity"), count(0) {
 
     // Creates the publisher
-    publisher = this->create_publisher<core::msg::DriveCmd>("/control/drive_cmds", 10);
+    publisher = this->create_publisher<core::msg::DriveInput>("/autonomous/drive_inputs", 10);
     
     // Creates the input subscription
     subscription = this->create_subscription<core::msg::DriveVel>(
-        "/control/drive_vel", 10, std::bind(&DrivePublisher::input_callback, this, _1));
-}
+        "/autonomous/drive_velocity", 10, std::bind(&DriveVelocity::velocity_callback, this, _1));
 
 
 //  Main function called when the script execution begins
@@ -77,7 +42,7 @@ int main(int argc, char **argv)
     rclcpp::init(argc, argv);
 
     // Runs the Publisher class
-    rclcpp::spin(std::make_shared<DrivePublisher>());
+    rclcpp::spin(std::make_shared<DriveVelocity>());
 
     // Shutsdown ROS once complete
     rclcpp::shutdown();
--- src/ros/rover/src/drive/vel_convert.h
+++ src/ros/rover/src/drive/vel_convert.h
@@ -22,8 +22,7 @@ EDITED:		08/12/2021
 
 // Include ROS packages
 #include "rclcpp/rclcpp.hpp"
-//#include "core/msg/input_gamepad.hpp"
-#include "core/msg/drive_cmd.hpp"
+#include "core/msg/drive_input.hpp"
 
 #include <iostream>
 
@@ -34,34 +33,33 @@ using std::placeholders::_1;
 
 // The minimum and maximum multipliers
 const float MIN_MULTIPLIER       = 0.1;  // The minimum multiplier value
-const float conversion_intercept = 0.0305
-const float CONVERSION_FACTOR    = 1.3759
-const float STEER_FACTOR         = 0.5
+const float conversion_intercept = 0.0305;
+const float CONVERSION_FACTOR    = 1.3759;
+const float STEER_FACTOR         = 0.5;
 
 
 
 
 // Main publisher class that sends input data for the gamepad and joysticks
-class VelConvert : public rclcpp::Node {
+class DriveVelocity : public rclcpp::Node {
 
     //------------------------------------------------------------//
     private:
 
-    // Stores the loop timer for the update function
-    rclcpp::TimerBase::SharedPtr timer;
 
     // Stores the publisher for the drive commands
-    rclcpp::Publisher<core::msg::DriveCmd>::SharedPtr publisher;
+    rclcpp::Publisher<core::msg::DriveInput>::SharedPtr publisher;
 
-    // Stores the subscriber to the gamepad inputs
     rclcpp::Subscription<core::msg::DriveVel>::SharedPtr subscription;
 
     // Stores a counter for each step
     size_t count;
     //------------------------------------------------------------//
     public:
-
+    
+    void velocity_callback (const core::msg::DriveVel::SharedPtr msg);
+    
     /// @brief      Default constructor function that starts up the node
-    DrivePublisher();
+    DriveVelocity();
     
 };
