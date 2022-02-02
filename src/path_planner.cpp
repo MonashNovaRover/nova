db5a990db31c4a2cda4a0f32bf227002a02c6545
MaxTory2001 <torymax01@gmail.com>
2022-02-02 15:49:09 +1100

    got a_star python wrapper working

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -3,16 +3,16 @@
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 #include <chrono>
+#include <tuple>
 #include <thread>
 
 namespace py = pybind11;
-
-float test() {
-  return 2.5;
+typedef std::pair<int, int> Pair;
+bool check_tuples(Pair pair) {
+    return (std::get<0>(pair) > 5);
 }
 
-
-float array_sum(std::vector<float> lst){
+float array_sum(std::array<float, 10> lst){
     
     float sum = 0;
 
@@ -23,8 +23,8 @@ float array_sum(std::vector<float> lst){
     return sum;
 }
 
-
-float array2d_sum(std::vector<std::vector<float>> lst){
+template <size_t ROW, size_t COL>
+float array2d_sum(std::array<std::array<float, COL>, ROW> lst, Pair p1, Pair p2){
     
     float sum = 0;
     
@@ -40,9 +40,9 @@ float array2d_sum(std::vector<std::vector<float>> lst){
 PYBIND11_MODULE(path_planner, module_handle) {
   
     module_handle.doc() = "Nova Rover PathPlanner C++ functions binded to Python3";
-    module_handle.def("test", &test);
+    module_handle.def("check_tuples", &check_tuples);
     module_handle.def("array_sum", &array_sum);
-    module_handle.def("array2d_sum", &array2d_sum);
-    
+    module_handle.def("array2d_sum", &array2d_sum<10, 10>);
+    module_handle.def("array2d_sum", &array2d_sum<11, 11>);
 }
 
