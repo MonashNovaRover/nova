38e15b15e25072dc527b3008c3d3ad79e2b20611
MaxTory2001 <torymax01@gmail.com>
2022-02-03 21:28:40 +1100

    grid modification not yet working

--- src/ros/rover/autonomous/test_cpp_astar.py
+++ src/ros/rover/autonomous/test_cpp_astar.py
@@ -7,30 +7,27 @@ import matplotlib.pyplot as plt
 
 def add_obs(array):
     
-    for i in range(20, 380):
-        array[100, i] = 0.0
-
     for i in range(100):
-        array[140, 399 - i] = 0.0
+        array[140, 399 - i] = 1.0
 
-    for i in range(399):
-        array[50, i] = 0.0
-    
-    for i in range(360):
-        array[300, 399 - i] = 0.0
+    for i in range(300):
+        array[300, 399 - i] = 1.0
 
     return array
 
-arr = np.ones((400, 400))
+arr = np.zeros((400, 400))
 p1, p2 = (0, 0), (399, 399)
 
 arr = add_obs(arr)
 
 t = time.perf_counter()
-a_star(arr, p1, p2)
+path = np.array(a_star(arr, p1, p2, 5.0))
 print(time.perf_counter() - t)
 
 plt.imshow(arr)
 plt.plot(0, 0, 'ro')
 plt.plot(399, 399, 'bo')
+plt.plot(path[:,1], path[:,0])
 plt.savefig("obstacles.png")
+
+plt.show()
