3277ac03cd0da95deef9abd5553fa2b30ef3edff
harrisonv789 <harrison.v1@live.com>
2022-02-09 10:49:12 +1100

    Ready to test the new turning speed on the rover

--- src/ros/rover/src/drive/driver.cpp
+++ src/ros/rover/src/drive/driver.cpp
@@ -7,22 +7,73 @@ AUTHOR(S):	Harrison Verrios, Josh Cherubino
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 
+// Include math library
+#include <math.h>
+
 // Include the header file
 #include "driver.h"
 #include "print/print.h"
+#include <iostream>
 
 // Sends commands to the wheels
 void Driver::send_commands (const core::msg::DriveInput::SharedPtr msg) {
     
     // Check if wheels should spin
     if (msg->speed != 0.0 || msg->steer != 0.0) {
-        // Spin the wheels
-        for (Wheel* wheel : wheels) {
-            wheel->spin(msg->speed, msg->steer);
-        }
 
         // Reset the zero flag
         stopped_sent = false;
+
+        // If no streer, just spin with speed
+        if (msg->steer == 0) {
+            for (Wheel* wheel : wheels)
+                wheel->spin(msg->speed);
+
+            return;
+        }
+
+        // Otherwise, calculate the speed
+
+        // Store a new array
+        float distances[NUM_WHEELS];
+        float tangents[NUM_WHEELS];
+
+        // Gets the locas distance
+        float locas = get_locas_distance(msg->steer);
+        float max_distance = 0;
+        float max_tangent = 0;
+
+        // Calculate the max distance to the wheels and store them
+        for (int i = 0; i < NUM_WHEELS; i++) {
+            float dist = get_wheel_distance(wheels[i]->get_id(), locas);
+            distances[i] = dist;
+            if (dist > max_distance) max_distance = dist;          
+
+            float tangent = get_tangent_scale(wheels[i]->get_id(), locas);
+            tangents[i] = tangent;
+            if (tangent > max_tangent) max_tangent = tangent;
+        }
+
+        // Calculate the speed based on the distance
+        //std::cout << distances[0] << ", " << distances[1] << ", " << distances[2] << ", " << distances[3] << ", " << distances[4] << ", " << distances[5] << std::endl;
+        //std::fflush(stdout);
+
+        for (int i = 0; i < NUM_WHEELS; i++) {
+            // Calculate the velocity of wheel
+            float vel = msg->speed * distances[i] / max_distance;
+            vel *= tangents[i] / max_tangent;
+
+            if (abs(locas) < CHASSIS_SEPARATION / 2.0) {
+                if (locas < 0)
+                    if (i > 2) vel *= -1.0;
+                if (locas > 0)
+                    if (i <= 2) vel *= -1.0;
+            }
+
+            // Send the velocities to the wheels
+            wheels[i]->spin(vel);
+        }
+
     }
 
     // Otherwise, if handbrake is on, send zeros
@@ -103,8 +154,8 @@ Driver::Driver()
 
     // Initialise the wheels in the correct direction
     for (int i = 0; i < NUM_WHEELS; i++) {
-        bool clockwise = i < NUM_WHEELS / 2;
-        wheels[i] = new Wheel (i + 1, clockwise);
+        bool left = i < NUM_WHEELS / 2;
+        wheels[i] = new Wheel (i + 1, left);
     }
 
     // TODO QoS Profiles
@@ -120,6 +171,47 @@ Driver::Driver()
     // Creates the input subscription
     subscription_inputs = this->create_subscription<core::msg::InputGamepad>(
         "/control/input_gamepad", 10, std::bind(&Driver::input_callback, this, _1));
+}
+
+
+
+float Driver::get_locas_distance (float steer) {
+    if (steer == 0) return 0; // Maximum and will be ignored
+
+    // Return the calculation
+    return (1.0 / steer) - ((steer < 0.0) ? -1.0 : 1.0);
+}
+
+
+float Driver::get_wheel_distance (int id, float locas) {
+    // Calculate the y component
+    float y = 0;
+    if (id == 1 || id == 4) y = WHEEL_SEPARATION;
+    if (id == 3 || id == 6) y = -WHEEL_SEPARATION;
+
+    float wheel_x = (CHASSIS_SEPARATION / 2.0) * ((id <= 3) ? -1.0 : 1.0);
+
+    // Calculate the x component
+    float x = locas - wheel_x;
+
+    // Find pythagorus distance
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+
+float Driver::get_tangent_scale (int id, float locas) {
+    // Calculate the y component
+    float y = 0;
+    if (id == 1 || id == 4) y = WHEEL_SEPARATION;
+    if (id == 3 || id == 6) y = -WHEEL_SEPARATION;
+
+    float wheel_x = (CHASSIS_SEPARATION / 2.0) * ((id <= 3) ? -1.0 : 1.0);
+
+    // Calculate the x component
+    float x = locas - wheel_x;
+
+    // Find pythagorus distance
+    return sqrt(1.0 + pow(y / x, 2));
 }
 
 
