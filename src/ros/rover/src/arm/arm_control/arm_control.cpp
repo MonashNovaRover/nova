894f5fad3921bf850dfaa37f5b9f718093a7b0af
jesshep <jhep0001@student.monash.edu>
2022-02-11 23:12:26 +1100

    fixed datapath from inputs to control to driver using standard msg types

--- src/ros/rover/src/arm/arm_control/arm_control.cpp
+++ src/ros/rover/src/arm/arm_control/arm_control.cpp
@@ -11,6 +11,8 @@ AUTHOR(S):	Jess Hepworth
 #include "arm_control.h"
 #include "print/print.h"
 
+#include "../hacky_defines.h"
+
 // Receives input from resolvers
 void ArmControl::resolver_callback (const sensor_msgs::msg::JointState::SharedPtr msg) {
     
@@ -39,20 +41,26 @@ void ArmControl::joint_vel_ik_callback (const sensor_msgs::msg::JointState::Shar
 void ArmControl::publish_CMD_outputs () {
     
     // Create a new message
-    auto message = sensor_msgs::msg::JointState();
+    // auto message = sensor_msgs::msg::JointState();
+
+    // Set the header
+    cmd_outputs.header.stamp = this->now();
 
     for (auto i = 0; i < NUM_JOINTS; i++) {
-        message.velocity[i]   = joint_velocity[i] + joint_velocity_ik[i];
+        cmd_outputs.velocity[i]   = joint_velocity[i] + joint_velocity_ik[i];
     }
 
     // Publish the arm inputs
-    CMD_outputs_publisher->publish(message);
+    CMD_outputs_publisher->publish(cmd_outputs);
 
 }
 
 // Main constructor that sets up the node
 ArmControl::ArmControl() 
   : Node("arm_control"), count(0) {
+
+    // Initialise arrays in internal data structures
+    cmd_outputs = ArmCore::get_empty_joint_state(hack::JOINT_NAMES);
 
     // Creates the CMD outputs publisher
     CMD_outputs_publisher = this->create_publisher<sensor_msgs::msg::JointState>("/control/cmd_outputs", 10);
@@ -76,6 +84,15 @@ ArmControl::ArmControl()
         joint_velocity[i] = 0; 
         joint_velocity_ik[i] = 0; 
     }
+
+    // Output set-up messages
+    Print::title("ARM CONTROL");
+    Print::print("Valid Topics:");
+    Print::print("/control/cmd_ouputs            [JointState]", 1);
+    Print::print("/control/joint_velocities      [JointState]", 1);
+    Print::print("/control/joint_velocities_ik   [JointState]", 1);
+    Print::print("/control/resolvers             [JointState]", 1);
+    Print::print("", true);
 
 }
 
