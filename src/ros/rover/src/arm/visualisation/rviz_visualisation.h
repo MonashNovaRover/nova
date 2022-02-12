02e8d9560f92e79bf356a979e239f36caa8163bd
Jory Braun <joryb98@gmail.com>
2022-02-13 00:35:29 +1100

    Added plotted path between coordinate frames

--- src/ros/rover/src/arm/visualisation/rviz_visualisation.h
+++ src/ros/rover/src/arm/visualisation/rviz_visualisation.h
@@ -4,13 +4,14 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Monash Nova Rover Team
 
-This class converts for arm coordinate data from a
-  MultiDOFJointState message to a PoseArray message
+This class gets the arm coordinate info and re-publishes
+  it with message types that can be visualised in Rviz
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 NODE: rviz_visualisation
 TOPICS:
   - /control/arm_coord_frames   [sensor_msgs/MultiDOFJointState]    [Subscribed]
   - /control/arm_poses          [geometry_msgs/PoseArray]           [Published]
+  - /control/arm_poses_path     [nav_msgs/Path]                     [Published]
 SERVICES: None
 ACTIONS: None
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -20,7 +21,7 @@ CREATION:	 12/02/2022
 EDITED:		 12/02/2022
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 TODO:
- - Make the thing
+ - 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 
@@ -29,6 +30,7 @@ TODO:
 // Include message types
 #include "sensor_msgs/msg/multi_dof_joint_state.hpp"
 #include "geometry_msgs/msg/pose_array.hpp"
+#include "nav_msgs/msg/path.hpp"
 
 // Use the standard namespaces
 // For publishers
@@ -45,11 +47,17 @@ class RvizVisualisation : public rclcpp::Node
     
     // Subscription to /control/arm_coord_frames
     rclcpp::Subscription<sensor_msgs::msg::MultiDOFJointState>::SharedPtr coord_frames_sub;
+    
     // Publisher to /control/arm_poses
+    // Use to display coordinate frames in Rviz
     rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr arm_poses_pub;
+    
+    // Publisher to /control/arm_poses_path
+    // Use to display lines between coordinate frames in Rviz
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr arm_path_pub;
 
     /// @brief  Callback for arm_coord_frames subscription
-    ///         Re-publishes the message as a PoseArray message
+    ///         Re-publishes the position info as a PoseArray and a Path message
     void coord_frames_callback(const sensor_msgs::msg::MultiDOFJointState::SharedPtr frames_msg);
     
     //------------------------------------------------------------//
