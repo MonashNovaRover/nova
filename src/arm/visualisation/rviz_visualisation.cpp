0623535ee6f48f9f157010cfaa14667f69c7f4f7
Jory Braun <joryb98@gmail.com>
2022-02-13 00:35:29 +1100

    Added plotted path between coordinate frames

--- src/arm/visualisation/rviz_visualisation.cpp
+++ src/arm/visualisation/rviz_visualisation.cpp
@@ -12,7 +12,7 @@ AUTHOR(S):	Jory Braun
 #include "print/print.h"
 
 
-RvizVisualisation::RvizVisualisation() : Node("pose_array_publisher")
+RvizVisualisation::RvizVisualisation() : Node("rviz_visualisation")
 {
 
     // Create subscription to arm_coord_frames
@@ -20,39 +20,70 @@ RvizVisualisation::RvizVisualisation() : Node("pose_array_publisher")
         "/control/arm_coord_frames", 10, std::bind(&RvizVisualisation::coord_frames_callback, this, _1)
     );
 
-    // Create publisher for pose_array
+    // Create publisher for arm_poses
     arm_poses_pub = this->create_publisher<geometry_msgs::msg::PoseArray>(
         "/control/arm_poses", 10
+    );
+
+    // Create publisher for arm_poses_path
+    arm_path_pub = this->create_publisher<nav_msgs::msg::Path>(
+        "/control/arm_poses_path", 10
     );
 
     // Output set-up messages
     Print::title("RVIZ VISUALISATION");
     Print::print("Published Topics:");
-    Print::print("/control/arm_poses   [geometry_msgs/PoseArray]", 1);
+    Print::print("/control/arm_poses         [geometry_msgs/PoseArray]", 1);
+    Print::print("/control/arm_poses_path    [nav_msgs/Path]", 1);
     Print::print("", true);
 
 }
 
-// Get data from the MultiDOFJointState message and re-publish as a PoseArray message
+// Get data from the MultiDOFJointState message and re-publish as PoseArray and Path messages
 void RvizVisualisation::coord_frames_callback(const sensor_msgs::msg::MultiDOFJointState::SharedPtr frames_msg)
 {
-    // Construct the message. Create from scratch in each iteration since has no common info
+    // Construct the PoseArray message
     geometry_msgs::msg::PoseArray poses_msg;
     poses_msg.poses = std::vector<geometry_msgs::msg::Pose> (frames_msg->transforms.size());
 
-    // Convert the data
+    // Construct the Path message
+    nav_msgs::msg::Path path_msg;
+    path_msg.poses = std::vector<geometry_msgs::msg::PoseStamped> (frames_msg->transforms.size());
+
+    // Construct a standard header for all message types
+    rclcpp::Time current_time = this->now();
+    std_msgs::msg::Header header;
+    header.stamp = current_time;
+    header.frame_id = "map"; 
+    
+    // Convert the data from Transform types to Pose types. Convert from mm to m
+    // Store in the constructed messages
     for (unsigned int i = 0; i < frames_msg->transforms.size(); i++){
+        
+        // Fill the PoseArray message
+        // Unpack Transform into Pose
         poses_msg.poses[i].position.x = frames_msg->transforms[i].translation.x / 1000;
         poses_msg.poses[i].position.y = frames_msg->transforms[i].translation.y / 1000;
         poses_msg.poses[i].position.z = frames_msg->transforms[i].translation.z / 1000;
+        // Copy Quaternion
         poses_msg.poses[i].orientation = frames_msg->transforms[i].rotation;
+        
+        // Fill the Path message
+        // Copy Pose
+        path_msg.poses[i].pose = poses_msg.poses[i];
+        // Add header
+        path_msg.poses[i].header = header;
+
     }
 
-    // Update the header
-    poses_msg.header.stamp = this->now();
-    poses_msg.header.frame_id = "map";
-    // Publish the message
+    // Update the top-level headers
+    poses_msg.header = header;
+    path_msg.header = header;  
+    
+    // Publish the messages
     arm_poses_pub->publish(poses_msg);
+    arm_path_pub->publish(path_msg);
+
 }
 
 
