9e57a02b110c566ccfae6524c26fb7e1f219a5cf
MaxTory2001 <torymax01@gmail.com>
2022-02-14 16:15:47 +1100

    heigh mapping obstacle detection works

--- src/ros/rover/src/height_mapper.cpp
+++ src/ros/rover/src/height_mapper.cpp
@@ -1,3 +1,8 @@
+/*
+ 
+*/
+
+
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -5,197 +10,91 @@
 #include <vector>
 #include <vector>
 #include <cmath>
+#include <opencv2/opencv.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 
 #include <bits/stdc++.h>
 
 //#include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
-const int XS = 200, YS = 200, ZS = 50;
-typedef std::array<std::array<std::array<float, ZS>, YS>, XS> Map3D;
-typedef std::array<std::array<float, YS>, XS> Map2D;
+namespace py = pybind11;
 
-class Vec3{
-public:
-    float x, y, z;
+const int XS = 400, YS = 400, ZS = 50;
+typedef py::array_t<uint16_t> PointCloud;
 
-    Vec3()
-        : x(0), y(0), z(0){}
+// pybind thinks every cv::Mat is of unsigned chars, plus they will save us space
+static const unsigned char c_neg_inf = 0;
+static const unsigned char c_inf = 255;
+const unsigned char MAP_BOTTOM = 1;
 
-    Vec3(float x, float y, float z) 
-        : x(x), y(y), z(z) {}
+cv::Mat shift(cv::Mat& original, float x, float y){
+    // shift a cv::Mat in the direction given by x and y.
+    float shifter[6] = {1, 0, x, 0, 1, y};
+    cv::Mat shift_mat(cv::Size(3, 2), CV_32FC1, shifter);
 
-    Vec3(const Vec3& other) 
-        : x(other.x), y(other.y), z(other.z) {}
+    cv::Mat shifted;
+    cv::warpAffine(original, shifted, shift_mat, original.size());
 
-	float dot(const Vec3& other) {
-        return x*other.x + y*other.y + z*other.z; 
-    }
-
-    Vec3 cross(const Vec3& other) {
-        float x_det = y*other.z - z*other.y;
-        float y_det = x*other.z - z*other.x;
-        float z_det = x*other.y - y*other.x;
-
-        return Vec3(x_det, -y_det, z_det);
-    }
-
-    Vec3 operator+(const Vec3& other) {
-        return Vec3 (x+other.x, y+other.y, z+other.z);
-    }
-
-    Vec3 operator-(const Vec3& other) {
-        return Vec3 (x-other.x, y-other.y, z-other.z);
-    }
-
-    Vec3 operator*(float other) {
-        return Vec3 (x*other, y*other, z*other);
-    }
-
-    Vec3 operator/(float other) {
-        return Vec3 (x/other, y/other, z/other);
-    }
-
-    float magnitude(){
-        return this->dot(*this);
-    }
-
-    Vec3 normalise(){
-        return Vec3(*this) / sqrt(this->magnitude());
-    }
-};
-
-class Plane{
-public:
-    int num_points;
-    Vec3 centroid, normal;
-
-
-    Plane(int num_points, Vec3 centroid, Vec3 normal) 
-        : num_points(num_points), centroid(centroid), normal(normal) {}
-
-    Plane(){}
-};
-
-const float CRITICAL_INC = 5.0;
-const float CRITICAL_DOT = std::sin(CRITICAL_INC);
-const int PIXELS_PER_PLANE = 5;
-constexpr int PIXELS_PER_PLANE2 = PIXELS_PER_PLANE * PIXELS_PER_PLANE;
-const Vec3 up(0, 0, 1);
-
-Map2D get_2d_map(Map3D& map3D){
-    double start = std::clock();
-
-    int num_planes_x = XS / PIXELS_PER_PLANE;
-    int num_planes_y = YS / PIXELS_PER_PLANE;
-
-    Map2D topHeightMap;
-    Map2D bottomHeightMap;
-
-    for (int i = 0; i < XS; i++) {
-        for (int j = 0; j < YS; j++) {
-            for (int k = 0; k < ZS; k++){
-                if (map3D[i][j][k]) {
-                    bottomHeightMap[i][j] = k;
-                    break;
-                }
-               
-            }
-            for (int k = ZS - 1; k > -1; k--) {
-                if (map3D[i][j][k]) {
-                    topHeightMap[i][j] = k;
-                    break;
-                }
-            }
-        }
-    }
-
-            Vec3 centroid = point_sum / PIXELS_PER_PLANE2;
-
-            double xx=0.0, yy=0.0, zz=0.0, xy=0.0, xz=0.0, yz=0.0;
-
-            for (Vec3 p : these_pts) {
-                Vec3 r = p - centroid;
-                xx += r.x*r.x;
-                xy += r.x*r.y;
-                xz += r.x*r.z;
-                yy += r.y*r.y;
-                yz += r.y*r.z;
-                zz += r.z*r.z; 
-            }
-
-            xx /= PIXELS_PER_PLANE2;
-            xy /= PIXELS_PER_PLANE2;
-            xz /= PIXELS_PER_PLANE2;
-            yy /= PIXELS_PER_PLANE2;
-            yz /= PIXELS_PER_PLANE2;
-            zz /= PIXELS_PER_PLANE2;
-
-            Vec3 weighted_dir, axis_dir;
-            double weight;
-
-            double det_x = yy*zz - yz*yz;
-            double det_y = xx*zz - xz*xz;
-            double det_z = xx*yy - xy*xy;
-
-            // linear regression in x direction
-            axis_dir = Vec3(det_x, xz*yz - xy*zz, xy*yz - xz*yy);
-            weight = det_x * det_x;
-
-            weighted_dir = weighted_dir + axis_dir * weight;
-
-            // linear regression in y direction
-            axis_dir = Vec3(xz*yz - xy*zz, det_y, xy*xz - yz*xx);
-            weight = det_y * det_y;
-
-            if (weighted_dir.dot(axis_dir) < 0.0) weight = -weight;
-
-            weighted_dir = weighted_dir + axis_dir * weight;
-            
-            // linear regression in y direction
-            axis_dir = Vec3(xy*yz - xz*yy, xy*xz - yz*xx, det_z);
-            weight = det_z * det_z;
-
-            if (weighted_dir.dot(axis_dir) < 0.0) weight = -weight;
-
-            weighted_dir = weighted_dir + axis_dir * weight;
-
-            Vec3 normal = weighted_dir.normalise();
-            if (std::abs(normal.dot(up)) < CRITICAL_DOT) {
-                for (int i = 0; i < PIXELS_PER_PLANE; i++) {
-                    for (int j = 0; j < PIXELS_PER_PLANE; j++) {
-                        int x_index = plane_i * PIXELS_PER_PLANE + i;
-                        int y_index = plane_j * PIXELS_PER_PLANE + j;
-
-                        map2d[x_index][y_index] = 1.0;
-                    }
-                }
-            }
-        }
-    }
-
-    double end = std::clock();
-    double time = (end - start)  / double(CLOCKS_PER_SEC);
-    std::cout << "took " << time << "s" << std::endl;
-
-    return map2d;
+    return shifted;
 }
 
-void sin_wave_terrain(Map3D& map3d) {
-    for (float i = 0; i < XS; i += 2.5) {
-        for (float j = 0; j < YS; j += 2.5) {
-            float i2 = i * 2 * M_PI / 400;
-            float j2 = j * 2 * M_PI / 400; 
-            int k = (int) (24.5 * (sin(i2) + 1)); 
-            map3d[i][j][k] = 1.0;
-        }
-    }
+void save(cv::Mat& img) {
+    // use this function to easily display what c++ sees for debugging
+    cv::imwrite("../debug/cpp_map.png", img);
 }
 
-int main() {
-    Map3D* map3D = new Map3D;
-    sin_wave_terrain(*map3D);
-    Map2D passable = get_2d_map(*map3D);
+py::array_t<unsigned char> getObstacles(PointCloud& points){
+    py::buffer_info pc_info = points.request();
+    uint16_t* pc = static_cast<uint16_t *> (pc_info.ptr);
+    int num_pts = pc_info.size / 3;
+    
+    std::cout << "Received " << num_pts << " points." << std::endl;
+    std::cout << "Shape = (" << pc_info.shape[0] << ", " << pc_info.shape[1] << ")" << std::endl;
+    std::cout << "ndim = " << pc_info.ndim << std::endl;
+    std::cout << "strides = (" << pc_info.strides[0] << ", " << pc_info.strides[1] << ")" << std::endl;
+    std::cout << "itemsize = " << pc_info.itemsize << std::endl;
+    std::cout << "format = " << pc_info.format << std::endl;
 
-    free(map3D);
+    auto start = std::chrono::high_resolution_clock::now();
+    // These two height-maps should sandwhich every point in the point cloud
+    cv::Mat topHeightMap(cv::Size(XS, YS), CV_8UC1, cv::Scalar(c_neg_inf));
+    cv::Mat bottomHeightMap(cv::Size(XS, YS), CV_8UC1, cv::Scalar(c_inf));
+    // Finding max and min z for each x-y coordinate
+    for (int i = 0; i < pc_info.shape[0] * 3; i+=3) {
+        uint16_t x = pc[i];
+        uint16_t y = pc[i + 1];
+        uint16_t z = pc[i + 2] + MAP_BOTTOM; 
+        if (z > topHeightMap.at<unsigned char> (x, y)) topHeightMap.at<unsigned char> (x, y) = (unsigned char) z;
+        if (z < bottomHeightMap.at<unsigned char> (x, y)) bottomHeightMap.at<unsigned char> (x, y) = (unsigned char) z;
+    }
+                
+    //blurring the top height map so we can compare the heights of adjacent points
+    topHeightMap = cv::max(topHeightMap, shift(topHeightMap, -1, 0));
+    topHeightMap = cv::max(topHeightMap, shift(topHeightMap, 1, 0));
+    topHeightMap = cv::max(topHeightMap, shift(topHeightMap, 0, -1));
+    topHeightMap = cv::max(topHeightMap, shift(topHeightMap, 0, 1));
+    
+    cv::Mat diff;
+    cv::subtract(topHeightMap, bottomHeightMap, diff);
+    cv::threshold(diff, diff, c_inf, 0, cv::THRESH_TOZERO_INV);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    save(diff);
+    std::cout << "obstacle detection took " << time.count() << " microseconds" << std::endl;
+    // converting to numpy array to return
+    py::array_t<unsigned char> numpy_diff = py::array_t<unsigned char>({ diff.rows, diff.cols }, diff.data);
+    
+    auto converted = std::chrono::high_resolution_clock::now();
+    time = std::chrono::duration_cast<std::chrono::microseconds>(converted - end);
+    std::cout << "converting to numpy array took " << time.count() << " microseconds" << std::endl;
+    return numpy_diff;
+}
+
+PYBIND11_MODULE(height_mapper, module_handle) {
+    module_handle.doc() = "Nova Rover height-map obstacle detection algorithm binded to Python3";
+    module_handle.def("get_obstacles", &getObstacles); 
 }
---
+++ src/ros/rover/src/test_hmapping.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from height_mapper import get_obstacles
+import numpy as np
+import matplotlib.pyplot as plt
+from time import perf_counter as t
+
+if __name__=="__main__":
+
+    map3d = np.array([[[i, j, i // 40] for i in range(400)] for j in range(200)]).reshape(80000, 3)
+
+    begin = t()
+    map2d = get_obstacles(map3d)
+    end = t()
+
+    plt.imshow(map2d)
+    print("python: took " + str(end - begin) + " s")
+    plt.savefig("../debug/python_map.png")
