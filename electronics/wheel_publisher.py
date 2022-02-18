1ca932df043049a355117268e9ff3138f5d99ed7
novarover <novaroverteam@monash.edu>
2022-02-18 00:41:07 +0000

    Updated wheel publisher with correct veocities

--- electronics/wheel_publisher.py
+++ electronics/wheel_publisher.py
@@ -30,7 +30,19 @@ from core.msg import WheelData
 from coms_utils.can_interface import CANReceiver
 
 # The Wheel CAN arbitration IDs
-WHEEL_IDS = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6]
+WHEEL_IDS = [0x410, 0x420, 0x430, 0x440, 0x450, 0x460]
+
+# Maximum number of average
+STORED_DATA_LEN = 10
+
+# The value that 1.0 velocity maps to in RPM
+ENCODER_TO_RPM = 92.9
+
+# Store the wheel radius [m]
+WHEEL_RADIUS = 0.122
+
+# Mathematical PI
+PI = 3.141593
 
 
 # Main Wheel Publisher class
@@ -38,6 +50,10 @@ class WheelPublisher (Node):
 
     # Stores the current message values
     message: WheelData = WheelData()
+    
+    # Stores the velocities as an array of arrays
+    velocities = []
+    powers = []
 
     # Constructor sets up the publisher
     def __init__ (self):
@@ -51,22 +67,23 @@ class WheelPublisher (Node):
         # Set up the CAN interface for each wheel id
         self.cans = []
         for i in range(6):
-            self.cans.append(CANReceiver(channel="can0", filter_ids=[WHEEL_IDS[i]], receive_timeout=0.1))
+            self.cans.append(CANReceiver(channel="can0", filter_ids=[WHEEL_IDS[i]], receive_timeout=0.1, receive_fmt="<hh"))
+            self.velocities.append([0 for i in range(STORED_DATA_LEN)])
+            self.powers.append([0 for i in range(STORED_DATA_LEN)])
+        
 
         # Create the publisher
         super().__init__("wheel_publisher")
         self.publisher = self.create_publisher(WheelData, "/electronics/wheel_data", 10)
 
         # Create a time to constantly loop and check for data
-        self.read_timer = self.create_timer(0.1, self.read_callback)
+        self.read_timer = self.create_timer(0.01, self.read_callback)
 
         # Create a timer to publish the current data
         self.pub_timer = self.create_timer(0.1, self.publish_msg)
 
         # Create a timer to clear the current message data if nothing has happened
-        self.clear_timer = self.create_timer(1.0, self.clear_msg)
-
-        
+        self.clear_timer = self.create_timer(0.1, self.clear_msg)
 
     
     # Method that looks for any changes in the data from the CAN lines
@@ -77,25 +94,73 @@ class WheelPublisher (Node):
 
             # If a message exists
             if can_msg:
-                self.message.velocities[i] = int.from_bytes(can_msg, "big")
+            
+                # Read the velocity data
+                vel = can_msg.data[:2]
+                vel = int.from_bytes(vel, "little", signed=True)
+                # Get a negative for some wheels
+                if i <= 2: vel *= -1
+                self.velocities[i].append(self.convert_vel(vel))
+                del self.velocities[i][0]
                 
+                # Read the power data
+                power = can_msg.data[2:]
+                power = int.from_bytes(power, "little", signed=True)               
+                self.powers[i].append(self.convert_power(power))
+                del self.powers[i][0]
+                
+                # Update the timestamp
                 self.time = datetime.datetime.now()
+
 
     # Publishes the current message data that exists
     def publish_msg (self):
+    
+        # Get the average data in the message
+        for i in range(6):
+            self.message.velocities[i]  = sum(self.velocities[i]) / float(STORED_DATA_LEN)
+            self.message.powers[i]      = sum(self.powers[i]) / float(STORED_DATA_LEN)
+            self.message.rpms[i]        = self.convert_rpm(self.message.velocities[i])
+            self.message.velocities[i]  = self.convert_rpm_to_vel (self.message.rpms[i])
 
-        # Publish
+
+        # Publish the data
         self.publisher.publish(self.message)
 
     
     # Clears the current message if nothing has happened in a while
     def clear_msg (self):
+    
         # Check if the last message was a while ago
-        if (datetime.datetime.now() - self.time).total_seconds() > 1.0:
+        if (datetime.datetime.now() - self.time).total_seconds() > 0.5:
             # Clear the message
             self.message = WheelData()
+            
+            # Reset the averages
+            for i in range(6):
+                for j in range(STORED_DATA_LEN):
+                    self.velocities[i][j] = 0
+                    self.powers[i][j] = 0
+            
+     
+    # Converts the value of the velocity to something sensible     
+    # Converts a signed integer into a float
+    def convert_vel (self, value: int) -> float:
+        return value / 32768.0
+        
+        
+    # Converts the value of the power to something sensible
+    # Converts a signed integer into a float
+    def convert_power (self, value: int) -> float:
+        return value / 32768.0 if value > 0 else value / -32768.0
 
+    # Converts a raw velocity to an RPM
+    def convert_rpm (self, value: int) -> float:
+        return value * ENCODER_TO_RPM
 
+    def convert_rpm_to_vel (self, rpm: float) -> float:
+        return rpm / 60.0 * 2 * PI * WHEEL_RADIUS
+    
 
 # Main function sets up the ROS class
 def main(args=None):
