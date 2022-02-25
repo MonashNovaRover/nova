e2fea4654cc5f05f5451550638e7a99fa97fdaf2
MaxTory2001 <torymax01@gmail.com>
2022-02-25 12:22:12 +1100

    made combined height + plane mapper

--- src/ros/rover/autonomous/mapping/height_plane_mapper.py
+++ src/ros/rover/autonomous/mapping/height_plane_mapper.py
@@ -5,10 +5,10 @@ __package__ = "autonomous"
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Monash Nova Rover Team. Child class of the Mapper 
-class that maps the 2d surroundings by simply 
-fitting height maps over the maximum and minimum
-values of the most recent section of the 3d point 
-cloud sent by the Rover. 
+class that maps the 2d surroundings with both a
+height mapper and a plane fitter. Both maps are
+then fused to take advantage of the strengths of
+both algorithms.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 NODE: points_grid
 TOPICS:
@@ -21,25 +21,26 @@ ACTIONS: None
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PACKAGE: 	autonomous
 AUTHOR(S):	Max
-CREATION:	17/02/2022
-EDITED:		17/02/2022
+CREATION:	25/02/2022
+EDITED:		25/02/2022
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 TODO:
  - a lot 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-from mapping.mapper import Mapper
+from mapping.mapper import Mapper, HeightMapper, PlaneMapper
 from mapping.grid_2d import Grid2D
 import time
 import numpy as np
 import math_utils.transform as transform
 from config.runtime_params import max_fov_angle, max_point_depth, max_safe_obstacle, min_point_density
 from height_mapper import get_obstacles as get_height_obstacles
+from plane_fitter import get_obstacles as get_plane_obstacles
 from scipy.signal import convolve2d
 
 class HeightMapper(Mapper):
-    def __init__(self, length=20, width=20, height=5, resolution=0.1, detection_resolution=0.025, planner=None, _vis=True):
+    def __init__(self, length=20, width=20, height=5, resolution=0.1, detection_resolution=0.025, planner=None):
 
         # init node with node name points
         super().__init__(length, width, height, resolution, planner, _vis)
@@ -48,6 +49,8 @@ class HeightMapper(Mapper):
         self.resolution_ratio = int(self.planning_resolution / self.detection_resolution)
         self.detection_length = int(np.ceil((max_point_depth / self.detection_resolution) / self.resolution_ratio) * self.resolution_ratio) 
         self.detection_width = int(np.ceil(2 * self.detection_length * np.tan(max_fov_angle)))
+        self.height_mapper = HeightMapper(length, width, height, resolution, detection_resolution)
+        self.plane_mapper = PlaneMapper(length, width, height, resolution, detection_resolution)
         self.initialise_map()
 
     def initialise_map(self):
@@ -112,9 +115,12 @@ class HeightMapper(Mapper):
         """
         return self._map.map.astype(float) / 100
 
-    def get_obstacles(self, filtered_indices):
+    def get_height_obstacles(self, filtered_indices):
         return get_height_obstacles(filtered_indices, self.detection_length, self.detection_width)
         
+    def get_plane_obstacles(self, filtered_indices):
+        return get_plane_obstacles(filtered_indices, self.detection_length, self.detection_width, self.resolution_raito)
+
     def handle_pc(self, pts):
         """
         Uses height mapping to identify obstacles in 3d point cloud and generate a 2d
@@ -128,18 +134,27 @@ class HeightMapper(Mapper):
         no_yaw_pts = transform.transform_points_no_yaw(self.msg, pts)
 
         filtered_indices = self.filter_points(no_yaw_pts)
-        np.save("filtered_indices.npy", filtered_indices)
-        # cpp function finds steep areas in the high resolution map
-        obstacles, min_x = self.get_obstacles(filtered_indices)
+
+        # cpp functions finds steep areas in the high resolution map
+        height_obstacles, min_h_x = self.get_height_obstacles(filtered_indices)
+        plane_obstacles, min_p_x = self.get_plane_obstacles(filtered_indices)
 
         # downscaling from high_resolution planning map to low_resolution map
-        obs, min_x = self.downscale_obs(obstacles, min_x)
+        plane_obs, min_p_x = self.plane_mapper.downscale_obs(plane_obstacles, min_p_x)
+        height_obs, min_h_x = self.height_mapper.downscale_obs(height_obstacles, min_h_x)
 
-        rotated_obs = self.arrange_obstacles(obs, min_x)
+        # min_h_x and min_p_x should now be the same or very similar. Average them
+        min_x = 0.5 * (min_p_x + min_h_x)
+        
+        # any sharp drops located in the height mapper are added to the plane mapper
+        assert(plane_obs.shape == height_obs.shape)
+        plane_obs[height_obs >= 1.0] = 1.0;
+
+        rotated_obs = self.arrange_obstacles(plane_obs, min_x)
         self._map.add_obstacles(self.msg, rotated_obs)
 
     def publish(self, pts):
         """
-        Publish the 2d map over ros to be viewed in RVIZ
+        Publish the 2d maps over ros to be viewed in RVIZ
         """
         self._map.publish_grid()
