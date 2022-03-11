50b24f0325cd121f4676b040515436a59de80a99
Harrison Verrios <harrison.v1@live.com>
2022-03-11 16:14:01 +1100

    Updated default slider values

--- src/ros/rover/src/inputs/joystick_thrustmaster.cpp
+++ src/ros/rover/src/inputs/joystick_thrustmaster.cpp
@@ -13,15 +13,15 @@ AUTHOR(S):	Harrison Verrios
 
 
 // Constructor for the thrustmaster joysticks
-JoystickThrustmaster::JoystickThrustmaster(const bool left, const float offset)
+JoystickThrustmaster::JoystickThrustmaster(const bool left, const float offset, const float default_slider)
     : Joystick((left) ? INPUT_THRUST_LEFT : INPUT_THRUST_RIGHT, offset) {
-
+    default_ax_slider = default_slider;
 }
 
 // Checks for valid inputs
 void JoystickThrustmaster::check_valid_inputs() {
     // Don't perform the checks if valid already
-    if (valid_ax_stick_twist && valid_ax_thumb_y) return;
+    if (valid_ax_stick_twist && valid_ax_thumb_y && valid_ax_slider) return;
 
     // Check for stick twisting
     if (!valid_ax_stick_twist) {
@@ -31,6 +31,11 @@ void JoystickThrustmaster::check_valid_inputs() {
     // Check for thumb axis
     if (!valid_ax_thumb_y) {
         valid_ax_thumb_y = to_int(-convert_trg2ax(GamepadTriggerLength(controller, TRIGGER_RIGHT))) == 0.0;
+    }
+
+    // Check for slider axis
+    if (!valid_ax_slider) {
+        valid_ax_slider = convert_ax2trg(-stick_rx_f) != 0.5;
     }
 }
 
@@ -72,7 +77,10 @@ void JoystickThrustmaster::set_message_values() {
             msg.ax_thumb_y = 0.0;
 
         // Set the values of the slider
-        msg.ax_slider = convert_ax2trg(-stick_rx_f);
+        if (valid_ax_slider)
+            msg.ax_slider = convert_ax2trg(-stick_rx_f);
+        else
+            msg.ax_slider = default_ax_slider;
 
         // Set the state messages of each of the buttons
         msg.btn_thumb_l_state = get_button_state(BUTTON_X);
@@ -114,4 +122,5 @@ core::msg::InputJoystick JoystickThrustmaster::get_message() {
 void JoystickThrustmaster::reset_inputs () {
     valid_ax_stick_twist = false;
     valid_ax_thumb_y = false;
+    valid_ax_slider = false;
 }
