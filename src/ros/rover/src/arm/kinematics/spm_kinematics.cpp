fc026456923a8b404b946b833e0b23366698b724
Alexander Li <li420087@gmail.com>
2022-04-23 17:29:52 +1000

    altered all function return values into vectors.

--- src/ros/rover/src/arm/kinematics/spm_kinematics.cpp
+++ src/ros/rover/src/arm/kinematics/spm_kinematics.cpp
@@ -8,93 +8,32 @@ AUTHOR(S):	Alexander Li
 */
 
 #include "spm_kinematics.h"
-#include "arm_core.h"
-#include "print/print.h"
 #include <cmath>
 #define _USE_MATH_DEFINES
 
-SpmKinematics::SpmKinematics() : Node("spm_kinematics")
-{
-    //Initialise constants - 
-    joint_velocities_timer_period = 50ms; //this is the time_since_last_publish, used in integrator and differentiator
-    time_since_last_publish = 50; //in milliseconds
-    cmd_outputs_timer_period = 50ms;
-    num_base_joints = 3; // number of joints before the spm wrist joints
-    current_rpy_pos = {0, 0, 0}; //initialise current rpy joint positions
-    //SPM geometry
-    beta = 54.74 * M_PI / 180;
-    gamma = 54.74 * M_PI / 180;
-    alpha[0] = M_PI/2;
-    alpha[1] = M_PI/2;
-    alpha[2] = 2 * asin(sin(beta) * cos(M_PI/6));
-    eng[0] = 0;
-    eng[1] = 2 * M_PI / 3;
-    eng[2] = 4 * M_PI / 3;
-
-
-    // Create subscription to resolvers
-    resolver_sub = this->create_subscription<sensor_msgs::msg::JointState>(
-        "/electronics/resolvers", 10, std::bind(&SpmPositionKinematics::resolver_callback, this, _1)
-    );
-
-    // Create subscription to desired joint velocities
-    joint_velocities_sub = this->create_subscription<sensor_msgs::msg::JointState>(
-        "/control/joint_velocities", 10, std::bind(&SpmPositionKinematics::joint_velocities_callback, this, _1)
-    );
-
-    // Create timer and publisher for cmd_outputs
-    cmd_outputs_timer = this->create_wall_timer(
-        cmd_outputs_timer_period, std::bind(&SpmPositionKinematics::publish_cmd_outputs, this)
-    );
-    cmd_outputs_pub = this->create_publisher<sensor_msgs::msg::JointState>(
-        "/control/cmd_outputs", rclcpp::QoS(1).best_effort().deadline(200ms)
-    );
-
-    // Output set-up messages
-    Print::title("SPM KINEMATICS");
-    Print::print("Subscribed Topics:");
-    Print::print("/electronics/resolvers            [sensor_msgs/JointState]", 1);
-    Print::print("/control/joint_velocities         [sensor_msgs/JointState]", 1);
-    Print::print("Published Topics:");
-    Print::print("/control/cmd_outputs              [sensor_msgs/JointState]", 1);
-    Print::print("", true);
-}
-
-// Update current wrist joint positions, using resolvers
-void SpmKinematics::resolver_callback(const sensor_msgs::msg::JointState::SharedPtr msg)
-{
-    for (unsigned int i = 0; i < 3; i++) {
-        current_wrist_joint_pos[i] = msg.position[num_base_joints+i];
-    }
-}
-
-// Update desired rpy velocities, using control/joint_velocitites
-void SpmKinematics::joint_velocities_callback(const sensor_msgs::msg::JointState::SharedPtr msg)
-{
-    for (unsigned int i = 0; i < 3; i++) {
-        desired_rpy_vel[i] = msg.velocity[num_base_joints+i];
-    }
-}
+SpmKinematics::SpmKinematics() {}
 
 // Main solver
-void SpmKinematics::solve()
+std::vector<double> SpmKinematics::solve(std::vector<double> current_wrist_joint_pos, std::vector<double> desired_rpy_vel, int time_step)
 {
     // obtain current rpy
     previous_rpy_pos = current_rpy_pos;
     current_rpy_pos = spm_fk(current_wrist_joint_pos);
     // integrate
-    desired_rpy_pos = rpy_integrator(current_rpy_pos, desired_rpy_vel, time_since_last_publish);
+    std::vector<double> desired_rpy_pos = rpy_integrator(current_rpy_pos, desired_rpy_vel, time_step);
     // ik
-    desired_wrist_joint_pos = spm_ik(desired_rpy_pos);
+    std::vector<double> desired_wrist_joint_pos = spm_ik(desired_rpy_pos);
     // differentiate
-    desired_wrist_joint_vel = joint_differentiator(current_wrist_joint_pos, desired_wrist_joint_pos, time_since_last_publish);
+    std::vector<double> desired_wrist_joint_vel = joint_differentiator(current_wrist_joint_pos, desired_wrist_joint_pos, time_step);
+
+    return desired_wrist_joint_vel;
 }
 
 //perform spm position fk
-float *SpmKinematics::spm_fk(const float wrist_joint_pos[3])
+std::vector<double> SpmKinematics::spm_fk(std::vector<double> current_wrist_joint_pos)
 {
     //TODO: implement simultaneous equation solver
-    float v[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::vector<double> v = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     //solve fk, obtain v1, v2, v3, store into an array of v[9] = {v1x v1y v1z v2x v2y v2z v3x v3y v3z}
 
@@ -104,23 +43,23 @@ float *SpmKinematics::spm_fk(const float wrist_joint_pos[3])
 }
 
 // perform spm position ik
-float *SpmKinematics::spm_ik(const float desired_rpy_pos[3])
+std::vector<double> SpmKinematics::spm_ik(std::vector<double> desired_rpy_pos)
 {
-    static float theta[3];
+    std::vector<double> theta;
     // convert rpy to v
-    float *v = rpy_to_v(desired_rpy_pos);
+    std::vector<double> v = rpy_to_v(desired_rpy_pos);
 
     // solve for theta
     for (unsigned int i = 0; i < 3; i++) {
-        float A = (-sin(eng[i])*sin(gamma)*cos(alpha[i])+sin(eng[i])*cos(gamma)*sin(alpha[i])) * (-v[3*i+0])
-            + (cos(eng[i])*sin(gamma)*cos(alpha[i])-cos(eng[i])*cos(gamma)*sin(alpha[i])) * v[3*i+1]
-            + (cos(gamma)*cos(alpha[i])-sin(gamma)*sin(alha[i])) * v[3*i+2] - cos(alpha[1]);
-        float B = 2 * (cos(eng[i])*sin(alpha[i]) * (-v[3*i+0]) + sin(eng[i])*sin(alpha[i]) * v[3*i+1]);
-        float C = (-sin(eng[i])*sin(gamma)*cos(alpha[i])-sin(eng[i])*cos(gamma)*sin(alpha[i])) * (-v[3*i+0])
-            + (cos(eng[i])*sin(gamma)*cos(alpha[i])+cos(eng[i])*cos(gamma)*sin(alpha[i])) * v[3*i+1]
-            + (-cos(gamma)*cos(alpha[i])+sin(gamma)*sin(alha[i])) * v[3*i+2] - cos(alpha[1]);
+        double A = (-sin(eta[i])*sin(gamma)*cos(alpha[i])+sin(eta[i])*cos(gamma)*sin(alpha[i])) * (-v[3*i+0])
+            + (cos(eta[i])*sin(gamma)*cos(alpha[i])-cos(eta[i])*cos(gamma)*sin(alpha[i])) * v[3*i+1]
+            + (cos(gamma)*cos(alpha[i])-sin(gamma)*sin(alpha[i])) * v[3*i+2] - cos(alpha[1]);
+        double B = 2 * (cos(eta[i])*sin(alpha[i]) * (-v[3*i+0]) + sin(eta[i])*sin(alpha[i]) * v[3*i+1]);
+        double C = (-sin(eta[i])*sin(gamma)*cos(alpha[i])-sin(eta[i])*cos(gamma)*sin(alpha[i])) * (-v[3*i+0])
+            + (cos(eta[i])*sin(gamma)*cos(alpha[i])+cos(eta[i])*cos(gamma)*sin(alpha[i])) * v[3*i+1]
+            + (-cos(gamma)*cos(alpha[i])+sin(gamma)*sin(alpha[i])) * v[3*i+2] - cos(alpha[1]);
         // take positive root
-        float T = (-B + sqrt(B*B - 4*A*C)) / (2*A);
+        double T = (-B + sqrt(B*B - 4*A*C)) / (2*A);
         theta[i] = 2 * atan(T);
     }
 
@@ -129,9 +68,9 @@ float *SpmKinematics::spm_ik(const float desired_rpy_pos[3])
 
 
 // integrate rpy vel into rpy pos
-float *SpmKinematics::rpy_integrator(const float current_rpy[3], const float desired_rpy_vel[3], int time_step) 
+std::vector<double> SpmKinematics::rpy_integrator(std::vector<double> current_rpy, std::vector<double> desired_rpy_vel, int time_step)
 {
-    static float rpy_pos[3];
+    std::vector<double> rpy_pos;
     for (unsigned int i = 0; i < 3; i++) {
         rpy_pos[i] = current_rpy[i] + desired_rpy_vel[i] * time_step;
     }
@@ -140,42 +79,46 @@ float *SpmKinematics::rpy_integrator(const float current_rpy[3], const float des
 }
 
 // differentiate desired joint angles into desired joint velocities
-float *SpmKinematics::joint_differentiator(const float current_wrist_joint_pos[3], const float desired_wrist_joint_pos[3], int time_step)
+std::vector<double> SpmKinematics::joint_differentiator(
+        std::vector<double> desired_wrist_joint_pos, std::vector<double> current_wrist_joint_pos, int time_step
+    )
 {
-    static float joint_v[3];
+    std::vector<double> joint_v;
     for (unsigned int i = 0; i < 3; i++) {
-        joint_v[i] = (desired_wrist_joint_pos[i] - current_wrist_joint_post[i]) / time_step;
+        joint_v[i] = (desired_wrist_joint_pos[i] - current_wrist_joint_pos[i]) / time_step;
     }
     return joint_v;
 }
 
 // convert v vectors to rpy
-float *SpmKinematics::v_to_rpy(float v[9], float prev_rpy[3])
+std::vector<double> SpmKinematics::v_to_rpy(std::vector<double> v, std::vector<double> prev_rpy)
 {
     // extract v vectors
-    float v1[3] = {v[0], v[1], v[2]};
-    float v2[3] = {v[3], v[4], v[5]};
-    float v3[3] = {v[6], v[7], v[8]};
+    std::vector<double> v1 = {v[0], v[1], v[2]};
+    std::vector<double> v2 = {v[3], v[4], v[5]};
+    std::vector<double> v3 = {v[6], v[7], v[8]};
 
     // convert to axes of output frame
-    float *z = vector_scalar_product(vector_addition(v1, v2, v3), sqrt(vector_dot_product(vector_addition(v1, v2, v3))));
-    float *x = vector_scalar_product(vector_addition(v2, vector_scalar_product(z, -cos(beta))), 1/sin(beta));
-    float *y = vector_cross_product(z, x);
+    std::vector<double> z = vector_scalar_product(
+        vector_addition(v1, v2, v3), sqrt(vector_dot_product(vector_addition(v1, v2, v3), vector_addition(v1, v2, v3)))
+        );
+    std::vector<double> x = vector_scalar_product(vector_addition(v2, vector_scalar_product(z, -cos(beta))), 1/sin(beta));
+    std::vector<double> y = vector_cross_product(z, x);
 
     // set up solutions
-    float theta_a[3];
-    float theta_b[3];
+    std::vector<double> theta_a;
+    std::vector<double> theta_b;
     // finding 2 possible theta_y
-    float theta_a[1] = asin(z[0]);
-    float theta_b[1] = M_PI - theta_y_a;
+    theta_a[1] = asin(z[0]);
+    theta_b[1] = M_PI - theta_a[1];
     // finding corresponding angles of theta_x
-    float theta_a[0] = atan2(-z[1]/cos(theta_y_a), z[2]/cos(theta_y_a));
-    float theta_b[0] = atan2(-z[1]/cos(theta_y_b), z[2]/cos(theta_y_b));
+    theta_a[0] = atan2(-z[1]/cos(theta_a[1]), z[2]/cos(theta_a[1]));
+    theta_b[0] = atan2(-z[1]/cos(theta_b[1]), z[2]/cos(theta_b[1]));
     // finding corresponding angles of theta_z
-    float theta_a[2] = atan2(-y[0]/cos(theta_y_a), x[0]/cos(theta_y_a));
-    float theta_b[2] = atan2(-y[0]/cos(theta_y_b), x[0]/cos(theta_y_b));
+    theta_a[2] = atan2(-y[0]/cos(theta_a[1]), x[0]/cos(theta_a[1]));
+    theta_b[2] = atan2(-y[0]/cos(theta_b[1]), x[0]/cos(theta_b[1]));
     // choose between the solutions; select the one with smaller metric to previous Euler configuration
-    static float rpy[3] = {0, 0, 0};
+    std::vector<double> rpy;
     if (euler_metric(theta_a, prev_rpy) < euler_metric(theta_b, prev_rpy)) {
         rpy[0] = theta_a[0];
         rpy[1] = theta_a[1];
@@ -189,90 +132,90 @@ float *SpmKinematics::v_to_rpy(float v[9], float prev_rpy[3])
 }
 
 // convert rpy to v vectors
-float *SpmKinematics::rpy_to_v(float rpy[3])
+std::vector<double> SpmKinematics::rpy_to_v(std::vector<double> rpy)
 {
-    float theta_x = rpy[0];
-    float theta_y = rpy[1];
-    float theta_z = rpy[2];
+    double theta_x = rpy[0];
+    double theta_y = rpy[1];
+    double theta_z = rpy[2];
     
     // convert into the axes of output frame
-    float x[3] = {
+    std::vector<double> x = {
         cos(theta_z) * cos(theta_y),
         cos(theta_z) * sin(theta_y) * sin(theta_x) + sin(theta_z) * cos(theta_x),
         -cos(theta_z) * sin(theta_y) * cos(theta_x) + sin(theta_z) * sin(theta_x)
     };
-    float y[3] = {
+    std::vector<double> y = {
         -sin(theta_z) * cos(theta_y),
         -sin(theta_z) * sin(theta_y) * sin(theta_x) + cos(theta_z) * cos(theta_x), 
         sin(theta_z) * sin(theta_y) * cos(theta_x) + cos(theta_z) * sin(theta_x)
     };
-    float z[3] = {
+    std::vector<double> z = {
         sin(theta_y),
         -cos(theta_y) * sin(theta_x),
         cos(theta_y) * cos(theta_x)
     };
 
     // find v2
-    float *v2 = vector_addition(vector_scalar_product(x, sin(beta)), vector_scalar_product(z, cos(beta)));
+    std::vector<double> v2 = vector_addition(vector_scalar_product(x, sin(beta)), vector_scalar_product(z, cos(beta)));
 
     // find p1, p3
-    float *p1 = vector_addition(vector_scalar_product(x, cos(2 *M_PI/3)), vector_scalar_product(y, sin(2 *M_PI/3)));
-    float *p3 = vector_addition(vector_scalar_product(x, cos(-2 *M_PI/3)), vector_scalar_product(y, sin(-2 *M_PI/3)));
+    std::vector<double> p1 = vector_addition(vector_scalar_product(x, cos(2 *M_PI/3)), vector_scalar_product(y, sin(2 *M_PI/3)));
+    std::vector<double> p3 = vector_addition(vector_scalar_product(x, cos(-2 *M_PI/3)), vector_scalar_product(y, sin(-2 *M_PI/3)));
 
     //find v1, v3
-    float *v1 = vector_addition(vector_scalar_product(p1, sin(beta)), vector_scalar_product(z, cos(beta)));
-    float *v3 = vector_addition(vector_scalar_product(p3, sin(beta)), vector_scalar_product(z, cos(beta)));
+    std::vector<double> v1 = vector_addition(vector_scalar_product(p1, sin(beta)), vector_scalar_product(z, cos(beta)));
+    std::vector<double> v3 = vector_addition(vector_scalar_product(p3, sin(beta)), vector_scalar_product(z, cos(beta)));
 
     //return v vectors as a single array
-    static float v[9] = {v1[0], v1[1], v1[2], v2[0], v2[1], v2[2], v3[0], v3[1], v3[2]};
+    std::vector<double> v = {v1[0], v1[1], v1[2], v2[0], v2[1], v2[2], v3[0], v3[1], v3[2]};
     return v;
 }
 
 
 // vector addition for 2 vectors
-float *vector_addition(float x[3], float y[3])
+std::vector<double> SpmKinematics::vector_addition(std::vector<double> x, std::vector<double> y)
 {
-    static float output[3];
-    for (unsigned int i = 0; i < 3; i++) {
+    std::vector<double> output;
+    for (unsigned int i = 0; i < x.size(); i++) {
         output[i] = x[i] + y[i];
     }
     return output;
 }
 
 // vector addition for 3 vectors
-float *vector_addition(float x[3], float y[3], float z[3])
+std::vector<double> SpmKinematics::vector_addition(std::vector<double> x, std::vector<double> y, std::vector<double> z)
 {
-    static float output[3];
-    for (unsigned int i = 0; i < 3; i++) {
+    std::vector<double> output;
+    for (unsigned int i = 0; i < x.size(); i++) {
         output[i] = x[i] + y[i] + z[i];
     }
     return output;
 }
 
-// vector scalar product of length 3
-float *vector_scalar_product(float v[3], float s)
+// vector scalar product
+std::vector<double> SpmKinematics::vector_scalar_product(std::vector<double> v, double s)
 {
-    static float output[3];
-    for (unsigned int i = 0; i < 3; i++) {
+    std::vector<double> output;
+    for (unsigned int i = 0; i < v.size(); i++) {
         output[i] = v[i] * s;
     }
     return output;
 }
 
-//  dot product of vectors of length 3
-float vector_dot_product(float x[3], float y[3])
+//  dot product of vectors
+double SpmKinematics::vector_dot_product(std::vector<double> x, std::vector<double> y)
 {
-    float output = 0;;
-    for (unsigned int i = 0; i < 3; i++) {
+    double output = 0;
+    for (unsigned int i = 0; i < x.size(); i++) {
         output += x[i] * y[i];
     }
     return output;
 }
 
 // cross product of vectors of length 3
-float *vector_cross_product(float x[3], float y[3])
+std::vector<double> SpmKinematics::vector_cross_product(std::vector<double> x, std::vector<double> y)
 {
-    static float z[3];
+    std::vector<double> z;
     z[0] = x[1] * y[2] - x[2] * y[1];
     z[1] = x[2] * y[0] - x[0] * y[2];
     z[2] = x[0] * y[1] - x[1] * y[0];
@@ -280,30 +223,14 @@ float *vector_cross_product(float x[3], float y[3])
 }
 
 // custom metric for euler angles
-float euler_metric(float theta_a[3], float theta_b[3])
+double euler_metric(std::vector<double> theta_a, std::vector<double> theta_b)
 {
-    float dist_x = abs(theta_a[0] - theta_b[0]);
+    double dist_x = abs(theta_a[0] - theta_b[0]);
     if (dist_x > 180.0) {dist_x -= 180;}
-    float dist_y = abs(theta_a[1] - theta_b[1]);
+    double dist_y = abs(theta_a[1] - theta_b[1]);
     if (dist_y > 180.0) {dist_y -= 180;}
-    float dist_z = abs(theta_a[2] - theta_b[2]);
+    double dist_z = abs(theta_a[2] - theta_b[2]);
     if (dist_z > 180.0) {dist_z -= 180;}
     
     return sqrt(dist_x * dist_x + dist_y * dist_y + dist_z * dist_z);
-}
-
-//  Main function called when the script execution begins
-int main(int argc, char **argv)
-{
-    // Initialises the ROS C++ class
-    rclcpp::init(argc, argv);
-
-    // Runs the Publisher class
-    rclcpp::spin(std::make_shared<SpmKinematics>());
-
-    // Shutsdown ROS once complete
-    rclcpp::shutdown();
-
-    // Returns an empty value
-    return 0;
 }
--- src/ros/rover/src/arm/kinematics/spm_kinematics.h
+++ src/ros/rover/src/arm/kinematics/spm_kinematics.h
@@ -4,18 +4,10 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Monash Nova Rover Team
 
-This class performs the spm Roll-Pithc-Yaw (RPY) integration, the
+This class performs the spm Roll-Pithc-Yaw (RPY)(strictly speaking, theta_x, theta_y, theta_Z) integration, the
     spm IK, and the wrist joint angle differentiation.
 It reads the resolver data, and desired Roll-Pitch-Yaw velocities, and
     outputs the desired wrist joint velocities.
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-NODE: spm_kinematics
-TOPICS:
-  - /control/joint_velocities          [sensor_msgs/JointState]          [Subscribed]
-  - /electronics/resolvers             [sensor_msgs/JointState]          [Subscribed]
-  - /control/cmd_outputs               [sensor_msgs/JointState]          [Published]
-SERVICES: None
-ACTIONS: None
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PACKAGE: 	 control
 AUTHOR(S):   Alexander Li
@@ -24,135 +16,101 @@ EDITED:		 23/04/2022
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 TODO:
  - implement spm_fk
- - implement spm_ik
- - create publisher to cmd_outputs(?) (to where do we send the desired wrist joint velocities?)
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 
-// Include ROS client library
-#include "rclcpp/rclcpp.hpp"
-// Include message types
-#include "sensor_msgs/msg/joint_state.hpp"
 // Include vector 
 #include <vector>
+#include <cmath>
+#define _USE_MATH_DEFINES
 
 /* 
 Class which solves the spm kinematics
 */
-class SpmKinematics : public rclcpp::Node
+class SpmKinematics
 {
     //------------------------------------------------------------//
     private:
 
-    // Period that indicates time since last publish
-    std::chrono::milliseconds joint_velocities_timer_period;
-    float time_since_last_publish;
-
     // Wrist geometry
-    float beta;
-    float gamma;
-    float alpha[3];
-    float eng[3];
+    const double beta = 54.74 * M_PI / 180;
+    const double gamma = 54.74 * M_PI / 180;
+    const double alpha[3] = {M_PI/2, M_PI/2, 2*asin(sin(beta)*cos(M_PI/6))};
+    const double eta[3] = {0.0, 2*M_PI/3, 4*M_PI/3};
 
     // Track internal state
+    const int num_base_joints = 3;
+    std::vector<double> current_rpy_pos = {0, 0, 0};
+    std::vector<double> previous_rpy_pos;
     // Resolvers; current wrist joint positions
-    float current_wrist_joint_pos[3];
-    // SPM FK; current RPY positions (theta_X, theta_y, theta_z)
-    float current_rpy_pos[3];
-    float previous_rpy_pos[3];
+    double current_wrist_joint_pos[3];
     // desired RPY velocities from control/joint_velocities
-    float desired_rpy_vel[3];
-    // integrator; desired RPY positions
-    float desired_rpy_pos[3];
-    // SPM IK; desired wrist joint positions
-    float desired_wrist_joint_pos[3];
-    
-
-    // Subscription to resolvers
-    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr resolver_sub;
-    // Subscription to desired RPY velocities
-    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_velocities_sub;
-    // Publisher to /control/cmd_outputs
-    rclcpp::TimerBase::SharedPtr cmd_outputs_timer;
-    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr cmd_outputs_pub;
-
-
-    /// @brief  Callback for resolver subscription
-    ///         Updates the internal joint state, which is used for spm FK, and integrator and differentiator
-    void resolver_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
-
-    /// @brief  Callback for joint_velocities subscription
-    ///         
-    void joint_velocities_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
-
-    /// @brief  Callback for cmd_outputs publihser timer
-    ///         
-    void publish_cmd_outputs();
+    double desired_rpy_vel[3];
     
     //------------------------------------------------------------//
     public:
-
-    // desired wrist joint velocitites - output of the spm kinematics solver
-    float desired_wrist_joint_vel[3];
 
     /// Constructor. Initialisers the solvers and starts the node
     SpmKinematics();
 
     /// @brief  Ultimate solver function for spm kinematics
-    ///         Updates desired_wrist_joint_velocities -> this will be sent as a JointState message to cmd_outputs(?)
+    ///         Takes in current wrist joint positions (resolvers), and desired rpy velocities, and timestep
+    ///         Outputs desired wrist joint velocities
     ///         Or can be accessed in arm_kinematics.cpp
     ///         Calls numerous functions below in the process
-    void solve();
+    std::vector<double> solve(std::vector<double> current_wrist_joint_pos, std::vector<double> desired_rpy_vel, int time_step);
 
     /// @brief  Function for spm fk
-    ///         Takes in float array of wrist joint positions
-    ///         Outputs float array of RPY
+    ///         Takes in vector of wrist joint positions
+    ///         Outputs vector of RPY
     ///         Calls v_to_rpy() function in the process
-    float *spm_fk(const float wrist_joint_pos[3]);
+    std::vector<double> spm_fk(std::vector<double> current_wrist_joint_pos);
 
     /// @brief  Function for spm ik
-    ///         Takes in float array of desired RPY
-    ///         Outputs float array of desired wrist joint positions
+    ///         Takes in vector of desired RPY
+    ///         Outputs vector of desired wrist joint positions
     ///         Calls rpy_to_v() function in the process
-    float *spm_ik(const float desired_rpy_pos[3]);
+    std::vector<double> spm_ik(std::vector<double> desired_rpy_pos);
 
     /// @brief  Function to integrate RPY
-    ///         Takes in float array of current RPY, float array of desired RPY_VEL, and timestep
-    ///         Outputs float array of desired RPY
-    float *rpy_integrator(const float current_rpy[3], const float desired_rpy_vel[3], int time_step);
+    ///         Takes in vector of current RPY, vector of desired RPY_VELOCITIES, and timestep
+    ///         Outputs vector of desired RPY
+    std::vector<double> rpy_integrator(std::vector<double> current_rpy, std::vector<double> desired_rpy_vel, int time_step);
 
     /// @brief  Function to differentiate wrist joint position
-    ///         Takes in float array of desired wrist joint positions, current wrist joint positions, and timestep
-    ///         Outputs float array of desired wrist joint velocities
-    float *joint_differentiator(const float desired_wrist_joint_pos[3], const float current_wrist_joint_pos[3], int time_step);
+    ///         Takes in vector of desired wrist joint positions, current wrist joint positions, and timestep
+    ///         Outputs vector of desired wrist joint velocities
+    std::vector<double> joint_differentiator(
+        std::vector<double> desired_wrist_joint_pos, std::vector<double> current_wrist_joint_pos, int time_step
+    );
     
     /// @brief  Function for spm fk
-    ///         Takes in a float array representing v1x, v1y, v1z, v2x, v2y, v2z, v3x, v3y, v3z, also takes in previous rpy
-    ///         Outputs float array of RPY
+    ///         Takes in a vector representing v1x, v1y, v1z, v2x, v2y, v2z, v3x, v3y, v3z, also takes in previous rpy
+    ///         Outputs vector of RPY
     ///         Called by spm_fk()
-    float *v_to_rpy(float v[9], float prev_rpy[3]);
+    std::vector<double> v_to_rpy(std::vector<double> v, std::vector<double> prev_rpy);
 
     /// @brief  Function for spm ik
-    ///         Takes in a float array of RPY
-    ///         Outputs float array representing v1x, v1y, v1z, v2x, v2y, v2z, v3x, v3y, v3z
+    ///         Takes in a vector of RPY
+    ///         Outputs vector representing v1x, v1y, v1z, v2x, v2y, v2z, v3x, v3y, v3z
     ///         Called by spm_ik()
-    float *rpy_to_v(float rpy[3]);
+    std::vector<double> rpy_to_v(std::vector<double> rpy);
 
-    /// @brief  Mathematics function for vector addition of length 3
-    float *vector_addition(float x[3], float y[3])
+    /// @brief  Mathematics function for vector addition
+    std::vector<double> vector_addition(std::vector<double> x, std::vector<double> y);
 
-    /// @brief  Mathematics function for vector addition of length 3, for 3 vectors
-    float *vector_addition(float x[3], float y[3], float z[3])
+    /// @brief  Mathematics function for vector addition, for 3 vectors
+    std::vector<double> vector_addition(std::vector<double> x, std::vector<double> y, std::vector<double> z);
 
-    /// @brief  Mathematics function for vector scalar product of length 3
-    float *vector_scalar_product(float v[3], float s);
+    /// @brief  Mathematics function for vector scalar product 
+    std::vector<double> vector_scalar_product(std::vector<double> v, double s);
 
-    /// @brief  Mathematics function for dot product of vectors of length 3
-    float vector_dot_product(float x[3], float y[3]);
+    /// @brief  Mathematics function for dot product of vectors
+    double vector_dot_product(std::vector<double> x, std::vector<double> y);
 
     /// @brief  Mathematics function for cross product of vectors of length 3
-    float *vector_cross_product(float x[3], float y[3]);
+    std::vector<double> vector_cross_product(std::vector<double> x, std::vector<double> y);
 
     /// @brief  Mathematics function for finding a custom metric between euler configurations
-    float euler_metric(float theta_a[3], float theta_b[3]);
+    double euler_metric(std::vector<double> theta_a, std::vector<double> theta_b);
 };
