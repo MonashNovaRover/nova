f23867aeb90d7d0e344a07746dbdcf6bc1b81708
Jory Braun <joryb98@gmail.com>
2022-04-25 21:45:00 +1000

    Moved client outside of node context. Need to fix properly

---
+++ src/arm/kinematics/spm_kinematics.h
@@ -0,0 +1,13 @@
+// Placeholder so I can check build errors
+
+#include <vector>
+
+
+class SpmKinematics
+{
+    public:
+    
+    std::vector<double> spm_fk (std::vector<double>);
+    std::vector<double> spm_ik (std::vector<double>);
+    SpmKinematics(){};
+};
