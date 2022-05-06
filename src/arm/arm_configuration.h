e833bf70e0e3b94bab5c623a833f0faafbf47ad2
Jory Braun <joryb98@gmail.com>
2022-05-06 17:31:23 +1000

    Changed configuration for merge to testing

--- src/arm/arm_configuration.h
+++ src/arm/arm_configuration.h
@@ -38,6 +38,6 @@ namespace ArmConfig
     } EndEffectorType;
     
     // Set the variables here to one of the options above to build the arm for whatever hardware is attached
-    const WristType wrist_type = WRIST_SPM;
+    const WristType wrist_type = WRIST_CYCLOIDAL;
     const EndEffectorType end_effector_type = EE_EQUIPMENT_SERVICING;
 }
