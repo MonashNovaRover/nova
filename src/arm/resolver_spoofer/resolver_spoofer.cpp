eea8b0e478199cea4cf7d7c465273c4d2a7777ce
Jory Braun <joryb98@gmail.com>
2022-05-06 23:55:26 +1000

    Added proper calculation of discontinuity angles in resolver_spoofer

--- src/arm/resolver_spoofer/resolver_spoofer.cpp
+++ src/arm/resolver_spoofer/resolver_spoofer.cpp
@@ -27,16 +27,13 @@ void ResolverSpoofer::start_node()
     // Initial integration time
     last_integration_time = this->now();
 
-    // Set the discontinuity angles for each joint to be outside the typical range of motion
-    // For now just hardcode for the cycloidal wrist
-    joint_discontinuity_angles = std::vector<float> {
-        M_PI,
-        M_PI,
-        M_PI,
-        M_PI,
-        M_PI,
-        0
-    };
+    // Set the discontinuity angles for each joint
+    joint_discontinuity_angles = std::vector<float> (arm_config_info.joint_names.size());
+    for (unsigned int i = 0; i < joint_discontinuity_angles.size(); i++){
+        float limit_lower = arm_config_info.joint_limits_lower[i];
+        float limit_upper = arm_config_info.joint_limits_upper[i];
+        joint_discontinuity_angles[i] = wrap_to_2pi((limit_lower + limit_upper) / 2 + M_PI);
+    }
     
     // Create the subscription
     outputs_subscription = this->create_subscription<sensor_msgs::msg::JointState>(
@@ -65,7 +62,7 @@ void ResolverSpoofer::start_node()
 }
 
 // Convert a Real angle into the equivalent angle in [0, 2pi)
-double ResolverSpoofer::wrap_to_2pi(double angle){
+inline double ResolverSpoofer::wrap_to_2pi(double angle){
     angle = fmod(angle, 2*M_PI);
     if (angle < 0)
         angle += 2*M_PI;
@@ -73,7 +70,7 @@ double ResolverSpoofer::wrap_to_2pi(double angle){
 }
 
 // Move the discontinuity angle from 2pi to some specified angle
-double ResolverSpoofer::move_discontinuity(double angle, double discontinuity_angle){
+inline double ResolverSpoofer::move_discontinuity(double angle, double discontinuity_angle){
     return angle - 2*M_PI * (angle >= discontinuity_angle);
 }
 
