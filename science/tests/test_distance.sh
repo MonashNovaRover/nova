700365c2d6d92cddc8881f0ff25198786589c994
Harrison Verrios <harrison.v1@live.com>
2022-05-15 12:27:59 +0930

    Fixed tester script for distance sensor

--- science/tests/test_distance.sh
+++ science/tests/test_distance.sh
@@ -1,8 +1,8 @@
 # Sends data for the distance sensor (fake data)
 
 # Gets a random value for the integer
-randA=$((1 + $RANDOM % 10))
-randB=$((1 + $RANDOM % 10))
+randA=$((0 + $RANDOM % 10))
+randB=$((0 + $RANDOM % 10))
 
 # Send the CAn value
 cansend can1 004#00.$randA$randB
