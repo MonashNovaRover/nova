fbac397c9fd13b41afed577be1319218459bd0e9
Liam Whittle <lwhi0020@student.monash.edu>
2022-08-28 18:47:41 +1000

    Adding apt install screen to setup script

--- systemctl/setup_jetson_services.sh
+++ systemctl/setup_jetson_services.sh
@@ -2,6 +2,8 @@
 
 # WARNING: this should only be run on Jetson on the rover (not on a personal computer)
 # setting up the base service
+sudo apt update
+sudo apt install screen
 
 for name in base rover arm can; do
     echo "Setting up the ${name} service... "
@@ -11,3 +13,6 @@ for name in base rover arm can; do
     sudo systemctl enable ${name}.service
 done
 
+sudo systemctl daemon-reload
+
+echo "All services have been setup. Restart Jetson and follow terminal instructions for how to view running processes."
