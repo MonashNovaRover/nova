ed37d42282af9d9a02fce6e8d588aee527f0c5b1
novarover <novaroverteam@monash.edu>
2022-09-01 09:39:49 +0000

    auto startup works on Jetson

--- systemctl/rover.sh
+++ systemctl/rover.sh
@@ -7,7 +7,7 @@ source ./nova_ws/install/setup.bash
 # Check if screen session already exists (prevents duplicate roslaunch calls)
 if ! screen -list | grep -q "rover_launch"; then
 	# New command with logging, and ROBOT environment variable
-	screen -dmS "rover_launch" bash -c 'sleep 2 && ros2 launch core rover.launch.py'
+	screen -dmS "rover_launch" bash -c 'sleep 10 && ros2 launch core rover.launch.py'
 else
     echo "rover_launch already running!"
 fi
