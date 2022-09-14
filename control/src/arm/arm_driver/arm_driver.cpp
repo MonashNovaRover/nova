401c1faf926db3b34906e40d81145e2c7621df6c
Jory Braun <joryb98@gmail.com>
2022-09-14 16:58:45 +1000

    Fixed order of arm CMDs

--- control/src/arm/arm_driver/arm_driver.cpp
+++ control/src/arm/arm_driver/arm_driver.cpp
@@ -75,8 +75,8 @@ void ArmDriver::start_node()
     double clock_frequency = 30e6;
 
     std::vector<CMD*> joints = {
-        new CMD (1, 2, PID, STOP, 1, CMDOutputParameters(lower_joints_reduction, encoder_ppr, lower_joints_velocity_factor, clock_frequency)),  // J2
         new CMD (1, 1, PID, STOP, 1, CMDOutputParameters(lower_joints_reduction, encoder_ppr, lower_joints_velocity_factor, clock_frequency)),  // J1
+        new CMD (1, 2, PID, STOP, 1, CMDOutputParameters(lower_joints_reduction, encoder_ppr, lower_joints_velocity_factor, clock_frequency)),  // J2
         new CMD (1, 3, PID, STOP, 0, CMDOutputParameters(lower_joints_reduction, encoder_ppr, lower_joints_velocity_factor, clock_frequency)),  // J3
         new CMD (1, 4, PID, STOP, 0, CMDOutputParameters(wrist_reduction, encoder_ppr, wrist_velocity_factor, clock_frequency)),  // J4
         new CMD (1, 5, PID, STOP, 0, CMDOutputParameters(wrist_reduction, encoder_ppr, wrist_velocity_factor, clock_frequency)),  // J5
