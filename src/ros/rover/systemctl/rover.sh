8a5d4f1788ddb992c6bb8cd5f6d2d39a62592a5d
novarover <novaroverteam@monash.edu>
2022-09-16 01:37:07 +0000

    systemctl longer sleep so startup works

--- src/ros/rover/systemctl/rover.sh
+++ src/ros/rover/systemctl/rover.sh
@@ -6,7 +6,7 @@ source ./nova_ws/install/setup.bash
 
 # Check if screen session already exists (prevents duplicate roslaunch calls)
 if ! screen -list | grep -q "rover_launch"; then
-	screen -dmS "rover_launch" bash -c 'sleep 10 && ros2 launch core rover.launch.py'
+	screen -dmS "rover_launch" bash -c 'sleep 20 && ros2 launch core rover.launch.py'
 else
     echo "rover_launch already running!"
 fi
