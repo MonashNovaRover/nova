56b445a03b4a709f4975a3867964ebbb55804567
Jory Braun <joryb98@gmail.com>
2022-09-26 01:39:51 +1000

    Added arm_twistmapper to arm launch files

--- src/ros/rover/core/launch/arm_spoof.launch.py
+++ src/ros/rover/core/launch/arm_spoof.launch.py
@@ -26,6 +26,8 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='control', node_executable='arm_inputs', output='screen', emulate_tty=True),
         launch_ros.actions.Node(
+            package='control', node_executable='arm_twistmapper', output='screen', emulate_tty=True),
+        launch_ros.actions.Node(
             package='control', node_executable='arm_kinematics', output='screen', emulate_tty=True),
         launch_ros.actions.Node(
             package='control', node_executable='resolver_spoofer', output='screen', emulate_tty=True),
