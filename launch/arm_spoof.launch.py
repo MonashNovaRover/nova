e9817edb19e148340b8187bb5c38906e2d38ff28
Jory Braun <joryb98@gmail.com>
2022-09-28 13:57:59 +1000

    Added arm-control launch files

--- launch/arm_spoof.launch.py
+++ launch/arm_spoof.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='control', node_executable='arm_twistmapper', output='screen', emulate_tty=True),
         launch_ros.actions.Node(
-            package='control', node_executable='arm_kinematics', output='screen', emulate_tty=True),
+            package='control', node_executable='arm_control', output='screen', emulate_tty=True),
         launch_ros.actions.Node(
             package='control', node_executable='resolver_spoofer', output='screen', emulate_tty=True),
         launch_ros.actions.Node(
