b29148356e44b909c702bdb110fc0814531263ad
Jory Braun <joryb98@gmail.com>
2022-09-28 13:14:46 +1000

    Restructured arm_kinematics as a library for twistmapper and control

---
+++ src/ros/rover/src/arm/arm_control/arm_control.h
@@ -0,0 +1,149 @@
+#pragma once
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Monash Nova Rover Team
+
+This class performs the task-space and joint-space control
+  of the arm.
+The class reads resolver data published to ROS and updates
+  the arm model to match the real pose.
+It reads the desired task position, task velocity and joint
+  velocities and publishes the required output joint velocities
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODE: arm_control
+TOPICS:
+  - /control/arm_control_scheme        [core/ArmControlScheme]           [Subscribed]
+  - /electronics/resolvers             [sensor_msgs/JointState]          [Subscribed]
+  - /control/task_velocity             [geometry_msgs/TwistStamped]      [Subscribed]
+  - /control/input_joint_velocities    [sensor_msgs/JointState]          [Subscribed]
+  - /control/arm_coord_frames          [sensor_msgs/MultiDOFJointState]  [Published]
+  - /control/joint_velocities          [sensor_msgs/JointState]          [Published]
+SERVICES:
+  - /control/arm_config_info           [core/ArmConfigInfo]             [Server]
+ACTIONS: None
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE: 	 control
+AUTHOR(S):   Jory Braun
+CREATION:	 27/09/2022
+EDITED:		 27/09/2022
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+TODO:
+ - 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+// Include ROS client library
+#include "rclcpp/rclcpp.hpp"
+// Include message types
+#include "core/msg/arm_control_scheme.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "sensor_msgs/msg/multi_dof_joint_state.hpp"
+// Include service types
+#include "core/srv/arm_config_info.hpp"
+
+// Include libraries
+#include "arm_model.h"
+#include "arm_kinematics.h"
+
+// Use the standard namespaces
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+/* 
+Class which models the arm.
+Use real positions of joints and end effectors, but idealised links
+*/
+class ArmControl : public rclcpp::Node
+{
+    //------------------------------------------------------------//
+    private:
+
+    // Subscribers
+    rclcpp::Subscription<core::msg::ArmControlScheme>::SharedPtr control_scheme_sub;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr resolver_sub;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr input_joint_velocities_sub;
+    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr task_velocity_sub;
+    
+    // Periods at which to publish
+    std::chrono::milliseconds coord_frames_timer_period;
+    std::chrono::milliseconds joint_velocities_timer_period;
+
+    // Publisher timers
+    rclcpp::TimerBase::SharedPtr coord_frames_timer;
+    rclcpp::TimerBase::SharedPtr joint_velocities_timer;
+
+    // Publishers
+    rclcpp::Publisher<sensor_msgs::msg::MultiDOFJointState>::SharedPtr coord_frames_pub;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_velocities_pub;
+
+    // Services (servers)
+    rclcpp::Service<core::srv::ArmConfigInfo>::SharedPtr arm_config_info_service;
+
+    // Store state of last-received messages
+    core::msg::ArmControlScheme control_scheme;
+    sensor_msgs::msg::JointState joints;
+    sensor_msgs::msg::JointState joint_space_input;
+    geometry_msgs::msg::TwistStamped task_velocity;
+
+    // Store messages to be published (so only need to initialise once)
+    sensor_msgs::msg::MultiDOFJointState coord_frames;
+    // For joint_velocities, just use velocities section in joints
+
+    
+    // Arm model and solvers
+    ArmModel* arm_model;
+    ArmKinematics* arm_kinematics_solver;
+
+
+    /// @brief  Callback for control scheme subscription
+    ///         Updates the internal control scheme, which is used to determine how to solve IK
+    void control_scheme_callback(const core::msg::ArmControlScheme::SharedPtr msg);
+    
+    /// @brief  Callback for resolver subscription
+    ///         Updates the internal joint state, which is later used to update the model
+    void resolver_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
+
+    /// @brief  Callback for input joint velocities subscription
+    ///         Updates the internal joint-space joint velocities
+    void input_joint_velocities_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
+    /// @brief  Deadline callback for input joint velocities subscription
+    ///         Resets the internal joint-space joint velocities
+    void input_joint_velocities_deadline_callback();
+
+    /// @brief  Callback for task velocity subscription
+    ///         Updates the internal task velocity, which is later used to calculate the inverse kinematics
+    void task_velocity_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
+    /// @brief  Deadline callback for input task velocity subscription
+    ///         Resets the internal task velocity
+    void task_velocity_deadline_callback();
+    
+    /// @brief  Callback for arm_coord_frames publisher timer
+    ///         Updates the arm model using the latest resolver info, publishes to arm_cord_frames
+    void publish_coord_frames();
+
+    /// @brief  Get the joint-space velocities of all joints on the arm using inverse kinematics
+    ///         Uses the current joint positions and desired task velocity
+    ///         Adds joint velocities from IK and from joint-space inputs, and implements joint limits
+    void update_joint_velocities();
+
+    /// @brief  Callback for joint_velocities publihser timer
+    ///         Calculates the joint velocities from joint-space and task-space input, publishes to joint_velocities
+    void publish_joint_velocities();
+
+    /// @brief  Callback for arm model config service
+    ///         Returns details of the arm model
+    void arm_config_info_callback(
+        const core::srv::ArmConfigInfo::Request::SharedPtr request,
+        core::srv::ArmConfigInfo::Response::SharedPtr response
+    );
+    
+    //------------------------------------------------------------//
+    public:
+
+    /// Constructor. Initialisers the solvers and starts the node
+    ArmControl();
+    
+};
--- src/ros/rover/src/arm/arm_control/arm_twistmapper.cpp
+++ src/ros/rover/src/arm/arm_control/arm_twistmapper.cpp
@@ -9,9 +9,8 @@ AUTHOR(S):	Jory Braun
 
 #include "arm_twistmapper.h"
 
-#include <Eigen/Core>
-
 #include "arm_messages.h"
+#include "arm_type_translation.h"
 #include "../arm_configuration.h"
 #include "print/print.h"
 
@@ -21,6 +20,10 @@ AUTHOR(S):	Jory Braun
 
 ArmTwistMapper::ArmTwistMapper() : Node("arm_twist_mapper")
 {    
+    // Initialise publish timer periods
+    task_velocity_timer_period = 50ms;
+    
+    
     // Create subscription to arm control scheme
     control_scheme_sub = this->create_subscription<core::msg::ArmControlScheme>(
         "/control/arm_control_scheme", 10, std::bind(&ArmTwistMapper::control_scheme_callback, this, _1)
@@ -43,8 +46,8 @@ ArmTwistMapper::ArmTwistMapper() : Node("arm_twist_mapper")
         input_task_velocity_options
     );
 
+
     // Create timer and publisher for task_velocity
-    task_velocity_timer_period = 50ms;
     task_velocity_timer = this->create_wall_timer(
         task_velocity_timer_period, std::bind(&ArmTwistMapper::publish_task_velocity, this)
     );
@@ -52,10 +55,15 @@ ArmTwistMapper::ArmTwistMapper() : Node("arm_twist_mapper")
         "/control/task_velocity", rclcpp::QoS(1).best_effort().deadline(200ms)
     );
 
+
     // Initialise arm model and required solvers
     arm_model = new ArmModel(ArmConfig::wrist_type, ArmConfig::end_effector_type);
-    spm_solver = new SpmKinematics();
-    serial_fk_solver = new KDL::TreeFkSolverPos_recursive(*arm_model);
+    arm_kinematics_solver = new ArmKinematics(*arm_model, this->get_logger());
+
+    // Initialise arrays in internal data structures
+    // Use data from the arm model
+    joints = ArmMessages::get_empty_joint_state(arm_model->joint_names);
+
 
     // Output configuration messages
     // Convet module names to uppercase
@@ -110,57 +118,13 @@ void ArmTwistMapper::input_task_velocity_deadline_callback()
 }
 
 
-// Get the joint-space positions of the serial model of the arm
-inline KDL::JntArray ArmTwistMapper::get_serial_joint_positions()
-{
-    KDL::JntArray kdl_joints;
-    // Get the data directly from the resolvers
-    kdl_joints.data = Eigen::Matrix<double, 6, 1> (joints.position.data());
-    
-    // If using the SPM wrist, replace SPM input joint positions with equivalent serial pitch, yaw and roll
-    if (ArmConfig::wrist_type == ArmConfig::WRIST_SPM){
-        // Calculate SPM FK
-        std::vector<double> spm_joints (joints.position.begin() + 3, joints.position.begin() + 6); 
-        std::vector<double> serial_wrist_joints = spm_solver->spm_fk(spm_joints);
-        // Pitch
-        kdl_joints.data[3] = serial_wrist_joints[0];
-        // Yaw
-        kdl_joints.data[4] = serial_wrist_joints[1];
-        // Roll. Combine SPM roll and end-rotation since the KDL model requires 6 joints
-        kdl_joints.data[5] = serial_wrist_joints[2] + joints.position[6];
-    }
-
-    return kdl_joints;
-}
-
-
-// Calculate the FK for a given segment
-inline KDL::Frame ArmTwistMapper::calculate_serial_fk(KDL::JntArray kdl_joints, std::string segment_name)
-{
-    // Prepare the output data structure
-    KDL::Frame kdl_coord_frame = KDL::Frame::Identity();
-    
-    // Calculate the FK for the given segment. Store the result in kdl_coord_frame
-    int exit_value = serial_fk_solver->JntToCart(kdl_joints, kdl_coord_frame, segment_name);
-    if (exit_value == -1){
-        RCLCPP_WARN(this->get_logger(), "Number of positions provided does not match number of joints in tree");
-    }
-    else if (exit_value == -2){
-        RCLCPP_WARN(this->get_logger(), "Could not find segment %s in the tree", segment_name.c_str());
-    }
-
-    return kdl_coord_frame;
-}
-
-
 // Get the twist from the joysticks
-inline KDL::Twist ArmTwistMapper::get_control_twist()
+inline geometry_msgs::msg::Twist ArmTwistMapper::get_control_twist()
 {
     // Unpack the ROS2 task velocity into KDL::Vectors
-    const geometry_msgs::msg::Vector3& vec3_linear = task_velocity.twist.linear;
-    const geometry_msgs::msg::Vector3& vec3_angular = task_velocity.twist.angular;
-    KDL::Vector twist_linear (vec3_linear.x, vec3_linear.y, vec3_linear.z);
-    KDL::Vector twist_angular (vec3_angular.x, vec3_angular.y, vec3_angular.z);
+    KDL::Twist twist = ArmTypeTranslation::to_KDL_twist(task_velocity.twist);
+    KDL::Vector& twist_linear = twist.vel;
+    KDL::Vector& twist_angular = twist.rot;
     
     // Implement transformations on input linear and angular velocities
     // Endpoint frame control
@@ -169,7 +133,8 @@ inline KDL::Twist ArmTwistMapper::get_control_twist()
         // eg: forward on the left joystick is +ve x, but should be +ve z in end effector coordinates
         KDL::Rotation joystick_input_transform = KDL::Rotation::EulerZYX(M_PI / 2, -M_PI / 2, 0);
         // Transform from end effector coordinates to base frame coordinates
-        KDL::Rotation endpoint_frame_transform = calculate_serial_fk(get_serial_joint_positions(), arm_model->default_endpoint_name).M;
+        KDL::JntArray joint_positions = ArmTypeTranslation::to_KDL_jnt_array(joints.position);
+        KDL::Rotation endpoint_frame_transform = arm_kinematics_solver->fk_pos_end_effector(joint_positions).M;
         if (control_scheme.endpoint_frame_linear) {
             twist_linear = endpoint_frame_transform * joystick_input_transform * twist_linear;
         }
@@ -191,8 +156,8 @@ inline KDL::Twist ArmTwistMapper::get_control_twist()
         }
     }
 
-    // Compose into final twist
-    return KDL::Twist (twist_linear, twist_angular);
+    // Return the twist
+    return ArmTypeTranslation::to_ROS2_twist(twist);
 }
 
 
@@ -200,14 +165,7 @@ inline KDL::Twist ArmTwistMapper::get_control_twist()
 void ArmTwistMapper::publish_task_velocity()
 {
     // Get the twist in the rover frame
-    KDL::Twist kdl_twist = get_control_twist();
-    // Store the twist in the form ROS2 likes
-    task_velocity.twist.linear.x = kdl_twist.vel.x();
-    task_velocity.twist.linear.y = kdl_twist.vel.y();
-    task_velocity.twist.linear.z = kdl_twist.vel.z();
-    task_velocity.twist.angular.x = kdl_twist.rot.x();
-    task_velocity.twist.angular.y = kdl_twist.rot.y();
-    task_velocity.twist.angular.z = kdl_twist.rot.z();
+    task_velocity.twist = get_control_twist();
 
     // Update the header
     task_velocity.header.stamp = this->now();
