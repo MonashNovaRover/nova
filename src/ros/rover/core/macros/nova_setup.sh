e799c8d7f8d12928479ff84b584d8bcb0d8128fa
MaxTory2001 <mtor0008@student.monash.edu>
2022-10-03 08:42:35 +1100

    adding PAT https option as an alternative to ssh

--- src/ros/rover/core/macros/nova_setup.sh
+++ src/ros/rover/core/macros/nova_setup.sh
@@ -143,6 +143,7 @@ printf "Please enter your Git User Name: "
 read username
 printf "Please enter your Git Email: "
 read email
+git config --global credential.helper store
 git config --global user.name "$username"
 git config --global user.email "$email"
 
@@ -158,29 +159,50 @@ colcon build
 information "Cloning Repositories..."
 cd ~/nova_ws/src
 
-# Check if the SSH key exists
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
-    # Create the keygen
-    sudo ssh-keygen -t ed25519 -C "$email"
+echo "Would you like to clone with the https address and a PAT? (Y for PAT)"
+read PAT
 
-    echo ""
+# Check if requiring to install dependencies
+if [[ "$PAT" != "y" && "$PAT" != "Y" ]]; then
+    # Check if the SSH key exists
+    if [ ! -f ~/.ssh/id_ed25519.pub ]; then
+        # Create the keygen
+        sudo ssh-keygen -t ed25519 -C "$email"
 
-    # Display the SSH key
-    sudo cat ~/.ssh/id_ed25519.pub
+        echo ""
 
-    printf "Please copy your SSH key above to your GitHub account..."
+        # Display the SSH key
+        sudo cat ~/.ssh/id_ed25519.pub
 
+        printf "Please copy your SSH key above to your GitHub account..."
+
+        read empty
+    fi
+
+    # Clones all folders with ssh
+    git clone git@github.com:MonashNovaRover/autonomous.git
+    git clone git@github.com:MonashNovaRover/cameras.git
+    git clone git@github.com:MonashNovaRover/control.git
+    git clone git@github.com:MonashNovaRover/core.git
+    git clone git@github.com:MonashNovaRover/electronics.git
+    git clone git@github.com:MonashNovaRover/gui.git
+    git clone git@github.com:MonashNovaRover/science.git
+else
+    # Clone all folders with ssh
+    echo "Cloning with PAT"
+    echo "Create a Personal Access Token for your device on GitHub if you haven't already"
     read empty
+
+    git clone https://github.com:MonashNovaRover/autonomous.git
+    git clone https://github.com:MonashNovaRover/cameras.git
+    git clone https://github.com:MonashNovaRover/control.git
+    git clone https://github.com:MonashNovaRover/core.git
+    git clone https://github.com:MonashNovaRover/electronics.git
+    git clone https://github.com:MonashNovaRover/gui.git
+    git clone https://github.com:MonashNovaRover/science.git
 fi
 
-# Clones all folders
-git clone git@github.com:MonashNovaRover/autonomous.git
-git clone git@github.com:MonashNovaRover/cameras.git
-git clone git@github.com:MonashNovaRover/control.git
-git clone git@github.com:MonashNovaRover/core.git
-git clone git@github.com:MonashNovaRover/electronics.git
-git clone git@github.com:MonashNovaRover/gui.git
-git clone git@github.com:MonashNovaRover/science.git
+
 
 # Fix autonomous installs
 cd ~/nova_ws/src/rover/autonomous
@@ -193,10 +215,20 @@ git checkout --track remotes/origin/feature/ros2-foxy
 # Clone the other GitHub files
 mkdir -p ~/nova_ws/other
 cd ~/nova_ws/other
-git clone git@github.com:MonashNovaRover/arduinos.git
-git clone git@github.com:MonashNovaRover/pics.git
-git clone git@github.com:MonashNovaRover/ik_machine.git
-git clone git@github.com:MonashNovaRover/coms_utils.git
+
+if [[ "$PAT" != "y" && "$PAT" != "Y" ]]; then
+    git clone git@github.com:MonashNovaRover/arduinos.git
+    git clone git@github.com:MonashNovaRover/tutorials.git
+    git clone git@github.com:MonashNovaRover/pics.git
+    git clone git@github.com:MonashNovaRover/ik_machine.git
+    git clone git@github.com:MonashNovaRover/coms_utils.git
+else
+    git clone https://github.com:MonashNovaRover/arduinos.git
+    git clone https://github.com:MonashNovaRover/tutorials.git
+    git clone https://github.com:MonashNovaRover/pics.git
+    git clone https://github.com:MonashNovaRover/ik_machine.git
+    git clone https://github.com:MonashNovaRover/coms_utils.git
+fi
 
 # Add the nova.sh bash script to the bashrc
 information "Setting up Workspace..."
