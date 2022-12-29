9afdfe9b48dd0009464cf768b419ef13b2867852
MaxTory2001 <torymax01@gmail.com>
2022-12-30 09:30:03 +1100

    adding world-map transform

--- src/ros/rover/core/launch/rover.launch.py
+++ src/ros/rover/core/launch/rover.launch.py
@@ -23,55 +23,55 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='control', 
-            node_executable='drive_inputs', 
-            output='screen', 
+            package='control',
+            node_executable='drive_inputs',
+            output='screen',
             emulate_tty=True
         ),
         Node(
-            package='control', 
-            node_executable='driver', 
-            output='screen', 
+            package='control',
+            node_executable='driver',
+            output='screen',
             emulate_tty=True
         ),
         Node(
-            package='electronics', 
-            node_executable='wheel_publisher.py', 
-            output='screen', 
+            package='electronics',
+            node_executable='wheel_publisher.py',
+            output='screen',
             emulate_tty=True
         ),
         Node(
-            package='electronics', 
-            node_executable='gimbal_service.py', 
-            output='screen', 
+            package='electronics',
+            node_executable='gimbal_service.py',
+            output='screen',
             emulate_tty=True
         ),
         Node(
-            package='electronics', 
-            node_executable='LED_transmitter.py', 
-            output='screen', 
+            package='electronics',
+            node_executable='LED_transmitter.py',
+            output='screen',
             emulate_tty=True
         ),
         Node(
-            package='imu',  
-            node_executable='imu_node', 
-            output='screen', 
+            package='imu',
+            node_executable='imu_node',
+            output='screen',
             emulate_tty=True
         ),
         Node(
-            package='electronics', 
-            node_executable='CMD_service.py', 
-            output='screen', 
+            package='electronics',
+            node_executable='CMD_service.py',
+            output='screen',
             emulate_tty=True
-        ), 
-        
+        ),
+
         # tf2 static transformations
         # Add a new node defining a transform between any two frames necessary (ie GPS, imu, etc)
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             # TODO: Work out true extrinsics of camera relative to base of rover
-            arguments= ['0.5', '0', '0.4', '0', '0', '0', 'rover_base', 'd435_1']
+            arguments=['0', '0', '0', '0', '0', '0', 'world', 'map'],
             output='screen',
             emulate_tty=True
         ),
@@ -79,7 +79,15 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             # TODO: Work out true extrinsics of camera relative to base of rover
-            arguments= ['0.5', '0', '0.37', '0', '0', '0', 'rover_base', 'd435_1']
+            arguments=['0.48', '0', '0.48', '0', '0', '0', 'base_link', 'd435_1'],
+            output='screen',
+            emulate_tty=True
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            # TODO: Work out true extrinsics of camera relative to base of rover
+            arguments= ['0.48', '0', '0.46', '0', '0', '0', 'base_link', 't265'],
             output='screen',
             emulate_tty=True
         )
