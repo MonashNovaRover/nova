ecb3dd02c3c2cd2ec7833d6398c935436bf33e42
MaxTory2001 <torymax01@gmail.com>
2022-12-30 16:36:38 +1100

    extracting tf2 nodes to separate launch file

--- core/launch/rover.launch.py
+++ core/launch/rover.launch.py
@@ -63,32 +63,5 @@ def generate_launch_description():
             node_executable='CMD_service.py',
             output='screen',
             emulate_tty=True
-        ),
-
-        # tf2 static transformations
-        # Add a new node defining a transform between any two frames necessary (ie GPS, imu, etc)
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            # TODO: Work out true extrinsics of camera relative to base of rover
-            arguments=['0', '0', '0', '0', '0', '0', 'world', 'map'],
-            output='screen',
-            emulate_tty=True
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            # TODO: Work out true extrinsics of camera relative to base of rover
-            arguments=['0.48', '0', '0.48', '0', '0', '0', 'base_link', 'd435_1'],
-            output='screen',
-            emulate_tty=True
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            # TODO: Work out true extrinsics of camera relative to base of rover
-            arguments= ['0.48', '0', '0.46', '0', '0', '0', 'base_link', 't265'],
-            output='screen',
-            emulate_tty=True
         )
     ])
