f965b60739df4f0f236f9a31cc9360051749cd33
Jory Braun <joryb98@gmail.com>
2023-01-28 17:50:03 +1100

    Updated directories to use rover repo

--- core/nova.sh
+++ core/nova.sh
@@ -18,16 +18,12 @@ python3 ~/nova_ws/src/rover/core/macros/prompt.py
 
 
 # appending to pythonpath for autonomous folders
-<<<<<<< HEAD:core/nova.sh
+
 export PYTHONPATH=$PYTHONPATH:~/nova_ws/src/rover/autonomous/autonomous
-export PYTHONPATH=$PYTHONPATH:~/nova_ws/install/autonomousrlib/python3.8/site-packages
 export PYTHONPATH=$PYTHONPATH:~/nova_ws/rover/src/gui/gui/
-=======
-export PYTHONPATH=$PYTHONPATH:~/nova_ws/src/autonomous/autonomous
 export PYTHONPATH=$PYTHONPATH:~/nova_ws/install/autonomous/lib/python3.8/site-packages
 export PYTHONPATH=$PYTHONPATH:~/nova_ws/install/autonomous/lib/python3.6/site-packages
 export PYTHONPATH=$PYTHONPATH:~/nova_ws/src/gui/gui/
->>>>>>> replaced typo and added support for both python3.6 and 3.8 pybind files:nova.sh
 
 # Source the aliases (if ROS 2)
 if [[ $ROS_VERSION -eq 2 ]]; then
