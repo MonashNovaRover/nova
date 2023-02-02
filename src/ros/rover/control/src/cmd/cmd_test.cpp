81b031ec9597888cb9c652083a75470ebd4dc36a
tstr0006 <tstr0006@student.monash.edu>
2023-02-02 17:59:35 +1100

    updated test script

--- src/ros/rover/control/src/cmd/cmd_test.cpp
+++ src/ros/rover/control/src/cmd/cmd_test.cpp
@@ -1,14 +1,27 @@
-#include "print/print.h"
 #include "blcmd.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
-    BLCMD* blcmd = new BLCMD(2, 1, DRIVE_VELOCITY, true, STOP, 1);
+    BLCMD* blcmd = new BLCMD(2, 1, DRIVE_VELOCITY, false);
+
+
+    blcmd->drive(2.0);
+
+    string input_command;
 
     blcmd->stop();
-    blcmd->forward();
-    blcmd->reverse();
-    blcmd->drive(0.2);
-    // Returns an empty value
+
+    BLCMDConfig config = blcmd->get_configuration();
+
+    blcmd->set_drive_mode(DRIVE_POSITION);
+
+    blcmd->drive(3.142/2);
+
     return 0;
+
 }
