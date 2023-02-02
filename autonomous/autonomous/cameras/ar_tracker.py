b9a216c16c42f269e9a5e74e2333c20a9db5f3b2
novarova <novaroverteam@monash.edu>
2023-02-02 13:33:16 +1100

    ar tracking not dependent on opencv version to not crash

--- autonomous/autonomous/cameras/ar_tracker.py
+++ autonomous/autonomous/cameras/ar_tracker.py
@@ -87,7 +87,8 @@ class ArTracker(Node):
 
         if ids is not None:
             # projecting 2d camera coordinates into space
-            rot_mats, trans_mats, = ar.estimatePoseSingleMarkers(bboxs, self.marker_width_m, self.intrinsics, self.distortion)
+            pose = ar.estimatePoseSingleMarkers(bboxs, self.marker_width_m, self.intrinsics, self.distortion)
+            rot_mats, trans_mats = pose[0], pose[1]
             for _id, rot_mat, trans_mat in zip(ids, rot_mats, trans_mats):
                 transform = self.get_transform(rot_mat[0], trans_mat[0])
                 transform.child_frame_id = f"ar_tag_{_id}"
