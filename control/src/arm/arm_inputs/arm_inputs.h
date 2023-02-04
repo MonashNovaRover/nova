84ff57a33ada41c5ea46fb642afffa7b5a04dbfa
Jory Braun <joryb98@gmail.com>
2023-02-04 17:35:45 +1100

    Enabled position control

--- control/src/arm/arm_inputs/arm_inputs.h
+++ control/src/arm/arm_inputs/arm_inputs.h
@@ -43,7 +43,7 @@ TODO:
 #include "arm_config_info_client.h"
 
 // Position control enable override
-#define POSITION_CONTROL_ENABLE 0
+#define POSITION_CONTROL_ENABLE 1
 
 /* 
 Arm input class that handles input data from joysticks and publishes 
