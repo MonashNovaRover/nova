b0f669da6b61d39065e37f4ad0d9989cb35497c1
tstr0006 <tstr0006@student.monash.edu>
2023-02-13 14:23:34 +1100

    Added telemetry receive timeout

--- control/src/cmd/cmd_test.cpp
+++ control/src/cmd/cmd_test.cpp
@@ -7,24 +7,28 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    BLCMD* blcmd = new BLCMD(1, 1, DRIVE_VELOCITY, false);
+    BLCMD* blcmd = new BLCMD("vcan0", 1, DRIVE_VELOCITY, false);
 	
 
 
-	if (blcmd->set_config_variable(TELEMETRY_P1_SPEED,0x01)) cout << "PACKET 1 Speed Set" << endl;
-	if (blcmd->set_config_variable(TELEMETRY_P2_SPEED, 0x01)) cout << "PACKET 2 Speed Set" << endl;
-	if (blcmd->set_config_variable(TELEMETRY_P3_SPEED, 0x01)) cout << "PACKET 3 Speed Set" << endl;
-	if (blcmd->set_config_variable(TELEMETRY_P4_SPEED, 0x01)) cout << "PACKET 4 Speed Set" << endl;
+//	if (blcmd->set_config_variable(TELEMETRY_P1_SPEED,0x01)) cout << "PACKET 1 Speed Set" << endl;
+//	if (blcmd->set_config_variable(TELEMETRY_P2_SPEED, 0x01)) cout << "PACKET 2 Speed Set" << endl;
+//	if (blcmd->set_config_variable(TELEMETRY_P3_SPEED, 0x01)) cout << "PACKET 3 Speed Set" << endl;
+//	if (blcmd->set_config_variable(TELEMETRY_P4_SPEED, 0x01)) cout << "PACKET 4 Speed Set" << endl;
 
-	string in;
-	blcmd->drive(-0.075);
-
-	BLCMDTelemetry tel = blcmd->get_telemetry();
-
-	cin >> in;
+//	string in;
+//	blcmd->drive(-0.075);
+//
+//	BLCMDTelemetry tel = blcmd->get_telemetry();
+//
+//	cin >> in;
 	blcmd->drive(0);
+    blcmd->drive(0);
+    blcmd->drive(0.75);
+    blcmd->drive(0);
+    blcmd->stop();
 
-	cout << tel;
+//	cout << tel;
 
     return 0;
 
