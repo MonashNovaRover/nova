dadc8dabe0922e4118cd12c87de15384705710d7
MaxTory2001 <torymax01@gmail.com>
2023-02-18 22:01:22 +1100

    adding transform from base_link to chassis and launch forrover vis

--- core/launch/auto_base.launch.py
+++ core/launch/auto_base.launch.py
@@ -23,22 +23,16 @@ def generate_launch_description():
             arguments=['-d', str(rviz_path)],
             emulate_tty=True
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                FindPackageShare("control"), '/launch', '/rover_visualisation.launch.py'
-            ])
-        ),
+        #IncludeLaunchDescription(
+        #    PythonLaunchDescriptionSource([
+        #        FindPackageShare("control"), '/launch', '/rover_visualisation.launch.py'
+        #    ])
+        #),
         # autonomous nodes
         Node(
             package="autonomous",
             node_executable="path_vis.py",
             output="screen",
-            emulate_tty=True
-        ),
-        Node(
-            package='autonomous',
-            node_executable='rover_vis.py',
-            output='screen',
             emulate_tty=True
         ),
         Node(
