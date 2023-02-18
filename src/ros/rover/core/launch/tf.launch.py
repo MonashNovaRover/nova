4fa7ca20cd4f4fbb27cc189a1bfe23491f1f255e
novarova <novaroverteam@monash.edu>
2023-02-18 11:32:30 +1100

    launching autonomous with new vis

--- src/ros/rover/core/launch/tf.launch.py
+++ src/ros/rover/core/launch/tf.launch.py
@@ -16,6 +16,7 @@ CREATION:	30/12/22
 # Include the required launch parameters
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import numpy as np
 
 # Generate the launch file with all inputs
 def generate_launch_description():
@@ -43,6 +44,14 @@ def generate_launch_description():
             node_executable='static_transform_publisher',
             # TODO: Work out true extrinsics of camera relative to base of rover
             arguments= ['0.48', '0', '0.46', '0', '0', '0', 'base_link', 't265'],
+            output='screen',
+            emulate_tty=True
+        ),
+        Node(
+            package='tf2_ros',
+            node_executable='static_transform_publisher',
+            # TODO: Work out true extrinsics of camera relative to base of rover
+            arguments= ['0', '0', '0.38', str((-np.pi/2)), '0', '0', 'base_link', 'chassis'],
             output='screen',
             emulate_tty=True
         ),
