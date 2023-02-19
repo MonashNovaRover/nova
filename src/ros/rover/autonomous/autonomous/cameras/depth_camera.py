0d4eecb45a57ab43bd3492a1c6fd9e191cd706dd
MaxTory2001 <torymax01@gmail.com>
2023-02-19 21:11:45 +1100

    added depth rate parameters

--- src/ros/rover/autonomous/autonomous/cameras/depth_camera.py
+++ src/ros/rover/autonomous/autonomous/cameras/depth_camera.py
@@ -55,6 +55,10 @@ class DepthCamera(Node):
         # self.object_detector = ObjectDetector()
         self.cv_bridge : CvBridge = CvBridge()
 
+        self.param_pointcloud_frequency = self.declare_parameter("depth_cloud_rate_hz", 10).value
+        self.param_image_frequency = self.declare_parameter("image_process_rate_hz", 10).value
+        self.param_frame_frequency = self.declare_parameter("depth_cam_frame_rate_hz", 10).value
+
         self.cloud_publisher = self.create_publisher(PointCloud2, f"~{self.depth_frame_id}/cloud", 10)
         self.image_publisher = self.create_publisher(Image, f"~{self.depth_frame_id}/image", 10)
         # RVIZ CANNOT DISPLAY IMAGE MARKERS. USEFUL IN FOXGLOVE
