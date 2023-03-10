04a7b048c3b37f50851bfece0f8356172f6842a2
novarover <novaroverteam@monash.edu>
2023-03-11 01:04:51 +1100

    using jcan for sending excavation commands

--- src/ros/rover/control/src/excavation_construction/excavation_construction.cpp
+++ src/ros/rover/control/src/excavation_construction/excavation_construction.cpp
@@ -14,6 +14,29 @@ AUTHOR(S):	Manika Goyal
 
 using std::placeholders::_1;
 
+float ExcavationConstruction::get_can_commands () {
+        Frame scraper_arm;
+        Frame scraper_scoop;
+        Frame tile_placer;
+
+        scraper_arm.data.push_back(0);
+        scraper_arm.data.push_back(scraper_arm_direction);        
+        scraper_arm.data.push_back((uint8_t) (scraper_arm_velocity >> 8));
+        scraper_arm.data.push_back((uint8_t) (scraper_arm_velocity & 0xFF));
+
+        scraper_scoop.data.push_back(0);
+        scraper_scoop.data.push_back(scraper_scoop_direction);        
+        scraper_scoop.data.push_back((uint8_t) (scraper_scoop_velocity >> 8));
+        scraper_scoop.data.push_back((uint8_t) (scraper_scoop_velocity & 0xFF));
+
+        tile_placer.data.push_back(0);
+        tile_placer.data.push_back(tile_placer_direction);        
+        tile_placer.data.push_back((uint8_t) (tile_placer_velocity >> 8));
+        tile_placer.data.push_back((uint8_t) (tile_placer_velocity & 0xFF));
+
+        return (scraper_arm.data, scraper_scoop.data, tile_placer.data);
+
+}
 
 void ExcavationConstruction::joystick_l_callback (const core::msg::InputJoystick::SharedPtr msg) {
     
@@ -32,7 +55,8 @@ void ExcavationConstruction::joystick_l_callback (const core::msg::InputJoystick
     }
 
     // Update the inputs
-    scraper_arm_velocity = joystick_l.ax_stick_x;
+    scraper_arm_velocity = abs(joystick_l.ax_stick_x);
+    scraper_arm_direction = (joystick_l.ax_stick_x >= 0) ? 1:2
 }
 
 void ExcavationConstruction::joystick_r_callback (const core::msg::InputJoystick::SharedPtr msg) {
@@ -44,13 +68,16 @@ void ExcavationConstruction::joystick_r_callback (const core::msg::InputJoystick
         tile_placer_activated = true;
         // Update the inputs
         scraper_scoop_velocity = 0;
-        tile_placer_velocity = joystick_l.ax_stick_x;
+        tile_placer_velocity = abs( 255 * joystick_l.ax_stick_x );
+        scraper_arm_velocity = 0;
+        tile_placer_direction = (joystick_l.ax_stick_x >= 0) ? 5:6
     }
     else {
         tile_placer_activated = false;
         // Update the inputs
-        scraper_scoop_velocity = joystick_l.ax_stick_x;
+        scraper_scoop_velocity = abs( 255 * joystick_r.ax_stick_x);
         tile_placer_velocity = 0;
+        scraper_scoop_direction = (joystick_r.ax_stick_x >= 0) ? 3:4
     }  
 }
 
@@ -94,6 +121,9 @@ ExcavationConstruction::ExcavationConstruction() : Node("excavation_construction
     rclcpp::QoS qos = rclcpp::QoS(1).best_effort().deadline(ROSTimers::);
     rclcpp::SubscriptionOptions subscriber_options;
 
+    this->declare_parameter("canbus", "can0");
+    this->get_parameter("canbus").get_parameter_value().get<std::string>()
+
 
     // Create the publisher with a best effort QoS policy
     publisher = this->create_publisher<core::msg::ExcavationConstruction>("/control/excavation_construction", qos);
@@ -121,6 +151,22 @@ ExcavationConstruction::ExcavationConstruction() : Node("excavation_construction
 
     // Creates a timer function that runs a function on loop every 0.05 seconds
     timer = this->create_wall_timer(ROSTimers::drive_control, std::bind(&DriveInputs::publish_cmds, this));
+    
+    vector excavationConstructionCommands = get_can_commands();
+
+    if (excavationConstructionCommands)
+
+        Frame scraperScoopFrame = new_frame(0x0A0, {});
+        printf("Sending: %s...\n",scraperScoopFrame.to_string().c_str());
+        bus->send(scraperScoopFrame);
+
+        Frame scraperArmFrame = new_frame(0x0A0, {});
+        printf("Sending: %s...\n",scraperArmFrame.to_string().c_str());
+        bus->send(scraperArmFrame);
+
+        Frame tilePlacerFrame = new_frame(0x0A0, {});
+        printf("Sending: %s...\n",tilePlacerFrame.to_string().c_str());
+        bus->send(tilePlacerFrame);
 
     // Output set-up messages
     Print::title("EXCAVATION CONSTRUCTION");
