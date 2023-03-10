56bb0231899c091ed14c64133d38fbebeb20e6ef
novarover <novaroverteam@monash.edu>
2023-03-10 17:35:27 +1100

    fix typos

--- src/ros/rover/control/src/arm/arm_core/arm_messages.cpp
+++ src/ros/rover/control/src/arm/arm_core/arm_messages.cpp
@@ -18,12 +18,7 @@ sensor_msgs::msg::JointState ArmMessages::get_empty_joint_state(const std::vecto
     sensor_msgs::msg::JointState msg;
     msg.name = names;
     msg.position = std::vector<double> (num_joints);
-    msg.
-    
-    
-    
-    
-     = std::vector<double> (num_joints);
+    msg.velocity = std::vector<double> (num_joints);
     msg.effort = std::vector<double> (num_joints);
     return msg;
 }
