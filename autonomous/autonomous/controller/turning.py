dcfbc7b51556ceaa3d550570f3b5cfa81af8b871
MaxTory2001 <torymax01@gmail.com>
2023-03-10 18:28:58 +1100

    GRUC and initial pose from params file

--- autonomous/autonomous/controller/turning.py
+++ autonomous/autonomous/controller/turning.py
@@ -111,7 +111,7 @@ class YawStarTurner(Turner):
 
 class TankTurner(Turner):
     def turn(self, yaw_difference, position_vector=None, current_orientation=None):
-        return tank_turn_target_yaw_rate(yaw_difference), 0
+        return tank_turn_target_yaw_rate(yaw_difference), straight_drive_fraction
 
     def reset(self):
         # do nothing
