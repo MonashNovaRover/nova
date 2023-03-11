beef2b318c76184591cc9dc1c300605449640ed7
novarova <novaroverteam@monash.edu>
2023-03-11 15:16:14 +1100

    change direction mappings

--- control/control/excavation_construction.py
+++ control/control/excavation_construction.py
@@ -49,12 +49,12 @@ class ExcavationConstructionNode(Node):
         self.tile_placer_velocity = 0
         self.qos = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT, depth=1)
 
-        self.tile_placer_id_forwards = 0x1
-        self.tile_placer_id_backwards = 0x2
-        self.scraper_arm_id_forwards = 0x3
-        self.scraper_arm_id_backwards = 0x4
-        self.scraper_scoop_id_forwards = 0x5
-        self.scraper_scoop_id_backwards = 0x6
+        self.tile_placer_id_forwards = 0x2
+        self.tile_placer_id_backwards = 0x1
+        self.scraper_arm_id_forwards = 0x4
+        self.scraper_arm_id_backwards = 0x3
+        self.scraper_scoop_id_forwards = 0x6
+        self.scraper_scoop_id_backwards = 0x5
 
         # TODO: figure out why its not calling the subscriber callback functions
         # way-point publisher publishes a bunch of waypoints at once (hence using the 2D map datatype
@@ -129,7 +129,7 @@ class ExcavationConstructionNode(Node):
             self.tile_placer_activated = True
             # Update the inputs
 
-            self.tile_placer_velocity = abs( int( 127 * joystick_r.ax_stick_x ) )
+            self.tile_placer_velocity = abs( int( 200 * joystick_r.ax_stick_x ) )
             self.tile_placer_direction = self.tile_placer_id_forwards if joystick_r.ax_stick_x >= 0 else self.tile_placer_id_backwards
 
             # set scraper velocities to 0
