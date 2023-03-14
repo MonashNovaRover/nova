9b7f026c5d7b5bf796ff44c883c61aa948c4e5d3
novarover <novaroverteam@monash.edu>
2023-03-14 22:48:27 +1100

    removing wheel motors from CMD_publisher.py

--- src/ros/rover/electronics/electronics/CMD_publisher.py
+++ src/ros/rover/electronics/electronics/CMD_publisher.py
@@ -74,10 +74,8 @@ FCY = 30e6   # CMD instruction frequency
 CHASSIS_VEL_FAC = 150   # Set in control/drive/driver.cpp in arm-control branch
 ARM_LOW_VEL_FAC = 75   # For lower arm joints
 WRIST_VEL_FAC = 50   # For wrist joints
-
-# The value that 1.0 velocity maps to in radians per second
-# Calculated using a Tachometer
-ENCODER_TO_RAD_PER_SEC = 9.728465
+DUTY_CYCLE_FACTOR = 0x8400
+CURRENT_FACTOR = 1600
 
 # For converting angular to linear velocities (M)
 WHEEL_RADIUS = 0.122
@@ -92,10 +90,8 @@ PUBLISH_RATE = 20
 
 # Motor types
 class MotorType(IntEnum):
-    WHEEL = 0
-    WHEEL_PIVOT = 1
     ARM_JOINT = 2
-    ARM_EF = 3
+    ARM_EE = 3
     SCIENCE = 4
     
 
@@ -103,15 +99,10 @@ class MotorType(IntEnum):
 # be configured for the CMDs.
 
 # Motor definitions
-NUM_WHEELS = 6
-PIVOT_STEERING = False
 NUM_ARM_MOTORS = 7
-NUM_SCIENCE_MOTORS = 2
+NUM_SCIENCE_MOTORS = 0
 
 # The CMD CAN arbitration IDs
-WHEEL_IDS = [0x410, 0x420, 0x430, 0x440, 0x450, 0x460]
-WHEEL_PIVOT_IDS = [None] * NUM_WHEELS
-# TODO: Input true CMD IDs 
 ARM_MOTOR_IDS = [0x410, 0x420, 0x430, 0x440, 0x450, 0x460, 0x470]
 SCIENCE_MOTOR_IDS = [0x480, 0x490]
 
@@ -122,47 +113,37 @@ QUEUE_LENGTH = 10  # If not TUNING_PIDS
 # Each element in the queue is a list of length NUM_X_MOTORS, and each element is a CMDFeedback()
 # Object corresponding to the CMD defined in the Arrays above
 CMDQueues = {
-    TYPE_MOTOR: deque([[CMDFeedback() for _ in range(NUM_TYPE_MOTOR)]], maxlen=QUEUE_LENGTH)
-        for TYPE_MOTOR, NUM_TYPE_MOTOR in zip(['wheel', 'pivot', 'arm', 'sci'], 
-            [NUM_WHEELS, NUM_WHEELS if PIVOT_STEERING else 0, NUM_ARM_MOTORS, NUM_SCIENCE_MOTORS])
+    TYPE_MOTOR: deque(maxlen=QUEUE_LENGTH)
+        for TYPE_MOTOR in ['arm', 'sci'] 
 }
 
 # Main CMD Publisher class
 class CMDPublisher (Node):
     def __init__ (self):
         super().__init__("CMD_publisher")
-        self.get_logger().set_level(logging.INFO)
+        self.get_logger().set_level(logging.DEBUG)
 
         # Store the starting time
         self.last_read = self.get_clock().now().to_msg()
     
         # Create CAN receivers for each type of motor on the rover
-        self.wheel_cans = [CANReceiver(channel="can0", filter_ids=[WHEEL_IDS[i]], # can channel and ids of wheels
-                                 receive_timeout=0.003, # seconds to wait for message
-                                 receive_fmt=">HHhh", # 4 shorts in big-endian format, 2 signed, 2 unsigned 
-                                 bitrate=200000) # bitrate of can line
-                     for i in range(NUM_WHEELS)]
-
-        if PIVOT_STEERING:
-            self.wheel_pivot_cans = [CANReceiver(channel="can0", filter_ids=[WHEEL_PIVOT_IDS[i]], 
-                                    receive_timeout=0.003, 
-                                    receive_fmt="<HHhh", 
-                                    bitrate=200000) 
-                        for i in range(NUM_WHEELS)]
-
-        self.arm_cans = [CANReceiver(channel="can1", filter_ids=[ARM_MOTOR_IDS[i]],
-                                 receive_timeout=0.003, 
-                                 receive_fmt="<HHhh",
-                                 bitrate=200000) 
-                     for i in range(NUM_ARM_MOTORS)]
-
-        self.science_cans = [CANReceiver(channel="can1", filter_ids=[SCIENCE_MOTOR_IDS[i]], 
-                                 receive_timeout=0.003, 
-                                 receive_fmt="<HHhh", 
-                                 bitrate=200000) 
-                     for i in range(NUM_SCIENCE_MOTORS)]
+        #self.arm_cans = [CANReceiver(channel="can1", filter_ids=[ARM_MOTOR_IDS[i]],
+        #                         receive_timeout=0.003, 
+        #                         receive_fmt="<HHhh",
+        #                         bitrate=200000) 
+        #             for i in range(NUM_ARM_MOTORS)]
+#
+        #self.science_cans = [CANReceiver(channel="can1", filter_ids=[SCIENCE_MOTOR_IDS[i]], 
+        #                         receive_timeout=0.003, 
+        #                         receive_fmt="<HHhh", 
+        #                         bitrate=200000) 
+        #             for i in range(NUM_SCIENCE_MOTORS)]
 
         self.publisher = self.create_publisher(CMDsFeedback, "/electronics/cmd_feedback", 10)
+        self.get_logger().debug(f"Initialised the Wheel Publisher class with:"\
+                                f"- {NUM_ARM_MOTORS} arm CMDs\n"\
+                                f"- {NUM_SCIENCE_MOTORS} science CMDs"
+                                )
 
         # Create a subscriber to drive commands
         #self.subscription_man = self.create_subscription(DriveInput, "/control/drive_inputs", self.drive_callback, qos)
@@ -175,49 +156,30 @@ class CMDPublisher (Node):
         if not TUNING_PID:
             self.pub_timer = self.create_timer(1.0/float(PUBLISH_RATE), self.publish_feedback)
 
-        # Log initialisation information
-        self.get_logger().debug(f"Initialised the Wheel Publisher class with:"\
-                                f"- {NUM_WHEELS} wheels\n"\
-                                f"- {NUM_WHEELS} wheel pivots\n" if PIVOT_STEERING else "0 wheel pivots"\
-                                f"- {NUM_ARM_MOTORS} arm CMDs\n"\
-                                f"- {NUM_SCIENCE_MOTORS} science CMDs"
-                                )
-
     # Method that looks for any changes in the data from the CAN lines
     def read_callback (self):
         # Loop through each CAN line and receive data
         # Tell the read_cans method what type of motor we are passing it, so it knows what values to fill out
         t1 = perf_counter()
         self.get_logger().debug("READING CMDS")
-        wheel_feedback = self.read_cans(self.wheel_cans, MotorType.WHEEL)
-        t2 = perf_counter()
         sci_feedback = self.read_cans(self.science_cans, MotorType.SCIENCE)
-        t3 = perf_counter()
+        t2 = perf_counter()
         arm_feedback = self.read_cans(self.arm_cans[:(NUM_ARM_MOTORS - 1)], MotorType.ARM_JOINT)
+        t3 = perf_counter()
+        arm_ee_feedback = self.read_cans([self.arm_cans[-1]], MotorType.ARM_EE)
         t4 = perf_counter()
-        arm_ef_feedback = self.read_cans([self.arm_cans[-1]], MotorType.ARM_EF)
-        t5 = perf_counter()
-        if PIVOT_STEERING:
-            pivot_feedback = self.read_cans(self.wheel_pivot_cans, MotorType.WHEEL_PIVOT)
-        else:
-            pivot_feedback = None
-        if wheel_feedback is not None: 
-            CMDQueues["wheel"].append(wheel_feedback)
-        if pivot_feedback is not None:
-            CMDQueues["pivot"].append()
-        if arm_feedback is not None and arm_ef_feedback is not None:
-            CMDQueues["arm"].append(arm_feedback.extend(arm_ef_feedback))
+        if arm_feedback is not None and arm_ee_feedback is not None:
+            CMDQueues["arm"].append(arm_feedback.extend(arm_ee_feedback))
         if sci_feedback is not None:
             CMDQueues["sci"].append(sci_feedback)
         if TUNING_PID:
             self.publish_feedback()
-        t6 = perf_counter()
+        t5 = perf_counter()
         self.get_logger().debug(f"Performance report:\n"
-                f"Wheels took {t2 -t1}s\n"
-                f"SCI took {t3 - t2}s\n"
-                f"arm took {t4 - t3}s\n"
-                f"arm ef took {t5 - t4}s\n"
-                f"The rest took {t6 - t5}s")
+                f"SCI took {t2 - t1}s\n"
+                f"arm took {t3 - t2}s\n"
+                f"arm ee took {t4 - t3}s\n"
+                f"The rest took {t5 - t4}s")
             
     def read_cans(self, cans: List[CANReceiver], motor_type: MotorType):
         """Take a list of CANReceivers, read each of them into a CMDFeedback message, 
@@ -259,14 +221,10 @@ class CMDPublisher (Node):
                     except Exception as e:
                         self.get_logger().debug(f"failed to unpack CAN message: {can_msg}")
                         continue
-                    if motor_type == MotorType.ARM_JOINT or motor_type == MotorType.ARM_EF:
+                    if motor_type == MotorType.ARM_JOINT or motor_type == MotorType.ARM_EE:
                         # Arm conversion constants
                         ppr = ARM_PPR
                         vel_fac = ARM_LOW_VEL_FAC if ros_msg.id <= 3 else WRIST_VEL_FAC
-                    else:
-                        # Chassis and (presumed) sci constants
-                        ppr = CHASSIS_PPR
-                        vel_fac = CHASSIS_VEL_FAC
 
                     omega = self.convert_omega_to_SI(raw_omega, ppr, vel_fac)
 
@@ -274,14 +232,8 @@ class CMDPublisher (Node):
                     ros_msg.current = self.convert_current(current)
                     ros_msg.interval = int(interval)
 
-                    if motor_type == MotorType.WHEEL:
-                        # Get a negative for wheels on one side due to motor orientation 
-                        if i <= 2: omega *= -1
-                        # Wheels have a linear velocity based on their radius 
-                        ros_msg.vel = self.convert_angular_to_linear_wheel(omega)
-                        bus = 0
-                    elif motor_type == MotorType.ARM_EF:
-                        ros_msg.vel = self.convert_angular_to_linear_end_effector(omega)
+                    if motor_type == MotorType.ARM_EE:
+                        ros_msg.vel = 0.
                         bus = 1
                     elif motor_type == MotorType.ARM_JOINT or motor_type == MotorType.SCIENCE:
                         bus = 1
@@ -337,14 +289,10 @@ class CMDPublisher (Node):
 
         if TUNING_PID:
             # take only the latest value
-            message.wheels = CMDQueues["wheel"][-1]
-            message.wheel_pivots = CMDQueues["pivot"][-1]
             message.arm_motors = CMDQueues["arm"][-1]
             message.science_motors = CMDQueues["sci"][-1]
         else:
             # average the queue of values
-            message.wheels = self.average_messages(CMDQueues["wheel"])
-            message.wheel_pivots = self.average_messages(CMDQueues["pivot"])
             message.arm_motors = self.average_messages(CMDQueues["arm"])
             message.science_motors = self.average_messages(CMDQueues["sci"])
 
@@ -357,25 +305,21 @@ class CMDPublisher (Node):
     
     # Converts a raw velocity to an RPM
     def convert_omega_to_SI (self, value: int, ppr: int, vel_fac: int) -> float:
-        return (value / MAX_INT16) / (4 * ppr * vel_fac) / (PI * FCY) 
+        return (value / MAX_INT16) / (4 * ppr * vel_fac / (PI * FCY))
         
     # Converts the value of the duty_cycle to something sensible
     # Converts a signed integer into a float
     def convert_duty_cycle (self, value: int) -> float:
-        return 26.0 * value / 0x8400
+        return value / DUTY_CYCLE_FACTOR
 
     # Converts the value of the current to something sensible
     # Converts a signed integer into a float
     def convert_current (self, value: int) -> float:
-        return float(value) / 1600
+        return float(value) / CURRENT_FACTOR
 
     # Converts the angular velocity value to a speed in m/s based on the wheel radius
     def convert_angular_to_linear_wheel (self, omega: float) -> float:
         return omega * WHEEL_RADIUS
-    
-    # Converts the angular velocity value to a speed in m/s based on the end effector bolt pitch
-    def convert_angular_to_linear_end_effector (self, omega: float) -> float:
-        return omega * END_EFFECTOR_BOLT_PITCH / (2 * PI)
     
 
 # Main function sets up the ROS class
