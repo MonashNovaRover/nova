f4c8dbc7812c80e9a53779fe9ec74d983123e122
novarover <novaroverteam@monash.edu>
2023-03-19 00:26:38 +1100

    Merge branch 'arc_science_commands' of github.com:MonashNovaRover/rover into arc_science_commands

--- src/ros/rover/core/macros/alias.sh
+++ src/ros/rover/core/macros/alias.sh
@@ -54,6 +54,7 @@ alias arm_view='rviz2 -d ~/nova_ws/src/rover/control/rviz/arm_viz_2023.rviz'
 alias jetson='ssh -Y nvidia@192.168.1.204'
 alias jetson_wifi='ssh -Y nvidia@tegra-ubuntu'
 alias j2='ssh -Y nova@192.168.1.204'
+alias j2_wifi='ssh -Y nvidia@192.168.0.104'
 
 # GUI Aliases
 alias wombat='cd ~/nova_ws/src/gui/wombatx; npm run start'
@@ -68,6 +69,8 @@ alias sci='ros2 launch core science.launch.py'
 alias unity='ros2 launch core visualisation.launch.py'
 alias auto_base='ros2 launch core auto_base.launch.py'
 alias auto_drive='ros2 launch core auto_drive.launch.py'
+alias launch_viz='ros2 launch core viz.launch.py'
+alias launch_vis='ros2 launch core viz.launch.py'
 
 # Service Aliases
 alias arm_config_info='ros2 service call control/arm_config_info core/srv/ArmConfigInfo'
@@ -93,3 +96,7 @@ alias sci_copy="scp -r nvidia@192.168.1.204:nova_ws/src/rover/science/data/spect
 
 # Creating template program
 alias python_template="cp ~/nova_ws/src/rover/core/macros/python_ros_template.py ."
+
+# systemctl / screen aliases
+alias check='. ~/nova_ws/src/rover/core/macros/check.sh'     # Sets up the CAN lines with a virtual or real CAN
+alias rerun='. ~/nova_ws/src/rover/core/macros/rerun.sh'     # Sets up the CAN lines with a virtual or real CAN
