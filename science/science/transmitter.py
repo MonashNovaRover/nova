438c94c28b17c7b08606c31ec4a4df44f35ffea3
NikoVeee <nikogv14@gmail.com>
2023-03-21 19:27:32 +1100

    added repeated calls to transmitter

--- science/science/transmitter.py
+++ science/science/transmitter.py
@@ -24,11 +24,10 @@ import rclpy
 from rclpy.node import Node
 from core.srv import ScienceCommand
 
-# Include utilities for publishing CAN data
-from coms_utils.can_interface import CANTransceiver
-
 # Include other dependencies
 import json, os
+import jcan
+
 
 
 '''
@@ -51,7 +50,7 @@ class ServiceNode(Node):
 
         # Reads the command json data
         with open (directory, "r") as file:
-            self.archive = json.loads(file.read())
+            self.archive: dict  = json.loads(file.read())
         
         # Store the list of targets
         self.targets = list(self.archive["targets"].keys())
@@ -59,19 +58,88 @@ class ServiceNode(Node):
         # Store the list of argument decoding
         self.arg_encoding = self.archive["arg_encoding"]
 
-        # Start the service with the callback
-        self.service = self.create_service(ScienceCommand, '/science/transmitter', self.callback_func)
+        # Initialise all the data frames as found in commands.json
+        self.can_frame_data = self.can_frame_initialisation()
 
-        # Sets up the CAN transceiver interface with the correct ID and channels
+        #declare parameters
+        self.declare_parameter("canbus", "can1")
+
+        # Initialise the jcan bus
         try:
-            self.can = CANTransceiver(arbitration_id=0x0, channel="can1", bitrate=500000)
-        
-        # CAN does not exist - show error
+            self.bus = jcan.Bus()
+            # Open the can bus
+            self.bus.open(self.get_parameter("canbus").value)
         except:
             self.get_logger().error("\033[91;1m\nTransmitter ERROR: Unable to find can1 network.\033[0m")
             exit()
 
 
+        # Start the service with the callback
+        self.service = self.create_service(ScienceCommand, '/science/transmitter', self.callback_func)
+
+        self.can_timer = self.create_timer(0.5, self.can_send_callback)
+        
+
+
+    def execute_can_msg(self, arb_id, action_id, arg_id):
+        command = action_id + arg_id
+        try:
+            command_hex_list = list(bytearray.fromhex(command))
+            arb_hex = int("0x" + arb_id, 16)
+
+            self.bus.send(jcan.Frame(arb_hex, command_hex_list))
+        except Exception as e:
+            self.get_logger().error("\033[[1;91m\nTransmitter FAILED! Exception: %s; Command: %s#%s\033[0m" % (e, arb_id, command))
+            return False
+        else:
+            self.get_logger().warning("\033[1;92m\nTransmitter SUCCESS! Command: %s#%s\033[0m" % (arb_id, command))
+            return True
+        
+
+    def can_send_callback(self):
+        for arb_id in self.can_frame_data.keys():
+            for action_id in self.can_frame_data[arb_id].keys():
+                arg_id = self.can_frame_data[arb_id][action_id]
+                if arg_id is not None:
+                    success = self.execute_can_msg(arb_id, action_id, arg_id)
+                    if success and int(arg_id, 16) == 0:
+                        self.can_frame_data[arb_id][action_id] = None
+        
+
+    def can_frame_initialisation(self):
+        # Initialising the CAN frame data based on the commands.json file.
+        frame_data = dict()
+        for target in self.targets:
+            target_hex = self.archive['targets'][target]['hex']
+            frame_data[target_hex] = dict()
+            for action in self.archive['targets'][target]['actions']:
+                action_hex = self.archive['targets'][target]['actions'][action]['hex']
+                frame_data[target_hex][action_hex] = None
+
+        return frame_data
+    
+
+    def get_args(self, action, args):
+
+        arg_id = ""
+        # Go through each of the actions and ensure they exist
+        for arg in action["args"]:
+            # Ensure it exists
+            if arg not in args.keys():
+                raise Exception("Missing argument '%s'." % arg)
+
+            # If it does exist, check the decoder
+            value = args[arg]
+
+            # Check for non-number argument
+            if str(value).lower() not in self.arg_encoding.keys():
+                arg_id += format(int(value), "x").rjust(2, "0")
+
+            # Get the decoded value and add to the list
+            else:
+                arg_id += self.arg_encoding[str(value).lower()]
+        return arg_id
+        
     '''
     The callback function is execited when a command is received by the transmitter.
     It takes in a request and a response message and outputs the associated
@@ -94,7 +162,6 @@ class ServiceNode(Node):
 
             # Grab the arbitration ID
             arb_id = self.archive["targets"][target]["hex"]
-            self.can.arbitration_id = int(arb_id, 16)
 
             # Set the target data
             target = self.archive["targets"][target]
@@ -110,35 +177,9 @@ class ServiceNode(Node):
             action = target["actions"][action]
 
             # Store an argument id
-            arg_id = ""
+            arg_id = self.get_args(action, args)
 
-            # Go through each of the actions and ensure they exist
-            for arg in action["args"]:
-                # Ensure it exists
-                if arg not in args.keys():
-                    raise Exception("Missing argument '%s'." % arg)
-
-                # If it does exist, check the decoder
-                value = args[arg]
-
-                # Check for non-number argument
-                if str(value).lower() not in self.arg_encoding.keys():
-                    arg_id += format(int(value), "x").rjust(2, "0")
-
-                # Get the decoded value and add to the list
-                else:
-                    arg_id += self.arg_encoding[str(value).lower()]
-
-            # Create the command
-            command = action_id + arg_id
-
-            # Execute the CAN command
-            response.success = self.can.transmit(bytearray.fromhex(command))
-
-            # Print a success
-            self.get_logger().warning("\033[1;92m\nTransmitter SUCCESS! Command: %s#%s\033[0m" % (arb_id, command))
-
-        
+            self.can_frame_data[arb_id][action_id] = arg_id
         
         # If an error occurred
         except Exception as e:
