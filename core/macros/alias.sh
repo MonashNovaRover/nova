0f5208bff41a3ea6e13c9d27445af3c3f836cca4
novarover <novaroverteam@monash.edu>
2023-03-22 19:18:32 +1100

    fixed ssh command for j2

--- core/macros/alias.sh
+++ core/macros/alias.sh
@@ -53,7 +53,7 @@ alias arm_view='rviz2 -d ~/nova_ws/src/rover/control/rviz/arm_viz_2023.rviz'
 # Networking Aliases
 alias jetson='ssh -Y nvidia@192.168.1.204'
 alias jetson_wifi='ssh -Y nvidia@tegra-ubuntu'
-alias j2='ssh -Y nova@192.168.1.204'
+alias j2='ssh -Y nvidia@192.168.1.204'
 alias j2_wifi='ssh -Y nvidia@192.168.0.104'
 
 # GUI Aliases
