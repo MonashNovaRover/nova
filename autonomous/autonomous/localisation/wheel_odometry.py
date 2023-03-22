5c9c25046f78c3761fe43115598a1a4ad22ada7d
MaxTory2001 <torymax01@gmail.com>
2023-03-23 10:45:43 +1100

    corrected wheel velocities

--- autonomous/autonomous/localisation/wheel_odometry.py
+++ autonomous/autonomous/localisation/wheel_odometry.py
@@ -51,7 +51,8 @@ class TemplateNode(Node):
         self.param_wheel_radius = self.declare_parameter("wheel_val_to_m_s", 1.496).value
         # resolver angles [-27306, 27306] as a fraction of max int16 [-32768, 32767]
         self.param_resolver_factor = self.declare_parameter("pivot_scale_factor", 0.8333).value
-        self.param_wheel_offsets = self.declare_parameter("pivot_angle_offsets_rad", [ANGLE_OFFSET, -ANGLE_OFFSET, ANGLE_OFFSET, -ANGLE_OFFSET]).value
+        self.param_wheel_offsets = self.declare_parameter("pivot_angle_offsets_rad", [-ANGLE_OFFSET, -ANGLE_OFFSET, -ANGLE_OFFSET, -ANGLE_OFFSET]).value
+        self.param_wheels_reversed = self.declare_parameter("pivot_angle_reversed", [True, False, True, False]).value
 
         self.latest_telemetry = None
 
@@ -83,20 +84,22 @@ class TemplateNode(Node):
         # get pivot angles
         self.pivot_angles_rad = [
             # reverse direction for front left and back right wheels
-            (-1 if i % 2 == 0 else 1) *\
+            (-1 if self.param_wheels_reversed[i] else 1) *\
             # rescale to radians
-            (np.pi / 2) * pivot.resolver_position / self.param_resolver_factor +\
+            (pivot.resolver_position / self.param_resolver_factor +\
             # Add offset
-            self.param_wheel_offsets[i] 
+            self.param_wheel_offsets[i])
             for i, pivot in enumerate(self.latest_telemetry.pivots)
         ]
+
+        print(self.pivot_angles_rad)
         # Construct vectors from velocities and angles
         self.wheel_vectors = np.array([[vel * math.cos(angle), vel * math.sin(angle)] for vel, angle in zip(self.wheel_vels_m_s, self.pivot_angles_rad)])
         print(self.wheel_vectors)
 
         # average left and right wheel vectors to get imagined wheel velocity beside the centre of the wheel base
-        left_wheel_vector = np.sum(self.wheel_vectors[0:2], axis=0)
-        right_wheel_vector = np.sum(self.wheel_vectors[2:4], axis=0)
+        left_wheel_vector = np.mean(self.wheel_vectors[0:2], axis=0) 
+        right_wheel_vector = np.mean(self.wheel_vectors[2:4], axis=0)
 
         print(left_wheel_vector, right_wheel_vector)
 
