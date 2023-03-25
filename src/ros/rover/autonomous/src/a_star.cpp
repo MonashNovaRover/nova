53fed0e46d65d223bbdaf99d5132c66d1d2a9699
novarova <novaroverteam@monash.edu>
2023-03-25 19:18:51 +1100

    removed string pulling

--- src/ros/rover/autonomous/src/a_star.cpp
+++ src/ros/rover/autonomous/src/a_star.cpp
@@ -387,9 +387,8 @@ void string_pull_from_start(array<array<float, COL>, ROW>& grid, vector<Pair>& p
     }
 }
 
-template <size_t ROW, size_t COL>
-vector<Pair> construct_return_val(array<array<float, COL>, ROW>& grid, vector<Pair> path, Status status) {
-	string_pull_from_start(grid, path);
+vector<Pair> construct_return_val(vector<Pair> path, Status status) {
+	//string_pull_from_start(grid, path);
 	path.push_back(Pair((int) status, -1));
 
 	return path;
@@ -423,7 +422,7 @@ vector<Pair> aStarSearch(array<array<float, COL>, ROW>& grid,
 
 	// If the destination cell is the same as source cell, we have already found it
 	if (isDestination(src, dest)) {
-		return construct_return_val(grid, vector<Pair> {{src}}, status);
+		return construct_return_val(vector<Pair> {{src}}, status);
 	}
 
 	// If the source is in an obstacle
@@ -511,7 +510,7 @@ vector<Pair> aStarSearch(array<array<float, COL>, ROW>& grid,
 					cellDetails[neighbour.first][neighbour.second].parent = { i, j };
 					vector<Pair> path = tracePath(cellDetails, dest);
 
-					return construct_return_val(grid, path, status);
+					return construct_return_val(path, status);
 				}
 				
 				float gNew, hNew, fNew;
@@ -570,7 +569,7 @@ vector<Pair> aStarSearch(array<array<float, COL>, ROW>& grid,
 	vector<Pair> path = tracePath(cellDetails, nearest_point);
 
 	if (path.size() < CRITICAL_PATH_LEN) status |= Status::A_STAR_CRITICAL_NO_PATH;
-	return construct_return_val(grid, path, status);
+	return construct_return_val(path, status);
 }
 
 // Driver program to test above function
