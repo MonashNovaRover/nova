7100d9a2237ae8006b0c01e6363f806c0de2e2af
Jory Braun <joryb98@gmail.com>
2023-04-28 15:29:52 +1000

    Fixed initialisation when arm model not used

--- src/ros/rover/electronics/electronics/resolver_publisher.py
+++ src/ros/rover/electronics/electronics/resolver_publisher.py
@@ -283,15 +283,6 @@ class ResolverPublisher(Node):
             # Set up the callback timer
             self.client_check_timer = self.create_timer(0.1, self.client_check_callback)
         else:
-            # Set up the data structure that we would otherwise get from the arm nodes
-            self.arm_config_info = ArmConfigInfo.Response()
-            # Include all resolvers
-            for joint_name in self.joints_map.keys():
-                self.arm_config_info.joint_names.append(joint_name)
-            # Assume no joint limits
-            num_joints = len(self.arm_config_info.joint_names)
-            self.arm_config_info.joint_limits_lower = [-2*pi] * num_joints
-            self.arm_config_info.joint_limits_upper = [2*pi] * num_joints
             # Start the node
             self.start_node()
 
@@ -330,6 +321,19 @@ class ResolverPublisher(Node):
             arbitration_id = 0x0A1,
             transmit_fmt = '>B',  # Big-endian. uint8
             )
+
+        # Handle if the node is being run without the arm model
+        use_arm_data = self.get_parameter("use_arm_data").value
+        if not use_arm_data:
+            # Set up the data structure that we would otherwise get from the arm nodes
+            self.arm_config_info = ArmConfigInfo.Response()
+            # Include all resolvers
+            for joint_name in self.joints_map.keys():
+                self.arm_config_info.joint_names.append(joint_name)
+            # Assume no joint limits
+            num_joints = len(self.arm_config_info.joint_names)
+            self.arm_config_info.joint_limits_lower = [-2*pi] * num_joints
+            self.arm_config_info.joint_limits_upper = [2*pi] * num_joints
 
         # Create the output message type to track the resolver state
         self.resolver_state = JointState()
