6fcc8de1ef95335c94430da469f92fc48e3cb09f
MaxTory2001 <torymax01@gmail.com>
2023-04-30 22:19:37 +1000

    removed install step for pybind files to allow symlink install for autonomous

--- src/ros/rover/core/nova.sh
+++ src/ros/rover/core/nova.sh
@@ -19,7 +19,7 @@ python3 ~/nova_ws/src/rover/core/macros/prompt.py
 
 # appending to pythonpath for autonomous folders
 
-export PYTHONPATH=$PYTHONPATH:~/nova_ws/install/autonomous/lib/python3.8/site-packages
+export PYTHONPATH=$PYTHONPATH:~/nova_ws/build/autonomous
 export PYTHONPATH=$PYTHONPATH:~/nova_ws/src/gui/gui/
 export PYTHONPATH=$PYTHONPATH:/usr/lib/python3.8/site-packages/pyrealsense2
 
