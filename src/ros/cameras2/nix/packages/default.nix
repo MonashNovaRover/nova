3b2f178e16af407540a3b61c94c988315334810b
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-05-02 10:12:08 +1000

    chore(nix): Use latest nixos-unstable Nixpkgs + gst-plugins-rs

--- src/ros/cameras2/nix/packages/default.nix
+++ src/ros/cameras2/nix/packages/default.nix
@@ -2,6 +2,22 @@
 
 pkgs.callPackage ./ros.nix { } {
   inherit (stdenv.buildPlatform) system;
+  nixpkgs = (pkgs.applyPatches {
+    src = pkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "da45bf6ec7bbcc5d1e14d3795c025199f28e0de0";
+      hash = "sha256-sASwo8gBt7JDnOOstnps90K1wxmVfyhsTPPNTGBPjjg=";
+    };
+    patches = [
+      (pkgs.fetchpatch {
+        # gst-plugins-rs 0.10.6
+        # Can be removed once https://nixpk.gs/pr-tracker.html?pr=225143 hits nixos-unstable
+        url = "https://github.com/NixOS/nixpkgs/commit/7c79d2edc3f404eb836aaf7786db777f1e0cb144.patch";
+        hash = "sha256-yiHCUy1MzPDMd9nWn5e72JjtRpfT6jczfOg8+I/w8Gg=";
+      })
+    ];
+  });
   overlays = [
     (self: super: {
       ros = self.rosPackages.foxy;
