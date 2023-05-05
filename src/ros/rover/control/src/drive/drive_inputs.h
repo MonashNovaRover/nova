c31041ccedc149257b5322de3ac1fd48d8bda21e
tstr0006 <tstr0006@student.monash.edu>
2023-05-05 19:34:00 +1000

    Refactored drive_inputs

--- src/ros/rover/control/src/drive/drive_inputs.h
+++ src/ros/rover/control/src/drive/drive_inputs.h
@@ -43,13 +43,7 @@ const float INITIAL_MULT_SPEED = 0.3;
 // The minimum trigger speed multiplier to apply when the right trigger is held
 const float MIN_TRIGGER_MULTIPLIER = 0.4;
 
-
 // The enum denotes the current drive mode of the rover
-enum DriveMode {
-    PIVOT = 1,
-    STRAFE,
-    TANK,
-};
 
 /*
     HOW TO DRIVE THE ROVER:
@@ -78,7 +72,7 @@ private:
 
     // Stores the subscriber to the gamepad inputs
     rclcpp::Subscription<core::msg::InputGamepad>::SharedPtr gamepad_input_subscription;
-    rclcpp::Subscription<core::msg::DriveInput>::SharedPtr autonomous_input_subscription;
+    rclcpp::Subscription<core::msg::DriveInput>::SharedPtr autonomous_commands_subscription;
 
     // A flag for whether the controller is connected
     bool connected = false;
@@ -95,24 +89,20 @@ private:
     // Autonomous mode
     bool autonomous = false;
 
+    float radius = INFINITY;
+
+    int direction = 0;
+
+    float speed = 0.0;
+
     // Drive mode
-    DriveMode mode = TANK;
-
-
-
-    // Stores the current state of the input axis
-    float right_input_axis_y = 0.0;
-    float right_input_axis_x = 0.0;
-    float left_input_axis_y = 0.0;
-    float left_input_axis_x = 0.0;
+    unsigned char mode = core::msg::DriveInput::TANK;
 
     // Stores the current state of the trigger multiplier
     float trigger_speed = 1.0;
 
     // The current speed and steer multipliers
     float multiplier_speed = INITIAL_MULT_SPEED;
-
-
     //------------------------------------------------------------//
 private:
     /// @brief      Adjusts one of the multipliers between 0.1 and 1.0
@@ -121,19 +111,23 @@ private:
     /// @returns    The current value of the multiplier
     float adjust_multiplier (float& multiplier, bool increase);
 
-
     /// @brief      Publishes the drive commands after analysing
     ///                 the input data.
     void publish_cmds ();
-
 
     /// @brief      Callback function when input messages are received.
     /// @param      msg - A pointer to the input message
     void input_callback (const core::msg::InputGamepad::SharedPtr msg);
 
-    /// @brief      Callback function when deadline for subscriptions are exceeded
-    void deadline_exceeded();
+    /// @brief      Callback function when autonomous messages are received.
+    /// @param      msg - A pointer to the autonomous message
+    void autonomous_callback (const core::msg::DriveInput::SharedPtr msg);
 
+    /// @brief      Callback function when deadline for input subscription is exceeded
+    void input_deadline_exceeded();
+
+    /// @brief      Callback function when deadline for auto subscription is exceeded
+    void auto_deadline_exceeded();
     //------------------------------------------------------------//
 public:
 
