bcc5eed0a63b4c49f0fd500358603056e5544fc3
novarova <novaroverteam@monash.edu>
2023-05-21 21:08:33 +1000

    Fixed CAN ID to match interface guide in Notion

--- src/ros/rover/control/control/gimble_cam.py
+++ src/ros/rover/control/control/gimble_cam.py
@@ -43,7 +43,7 @@ class GimbleCam(Node):
         self.param_velocity_increment = self.declare_parameter("velocity_factor", 0.1).value
         self.max_velocity_value = self.declare_parameter("max_velocity_value", 128).value
         # can commands for gimble cam
-        self.velocity_cmd = 0x080
+        self.velocity_cmd = 0x081
         # Initially all motors spin backwards with 0 velocity
         self.velocity_factor = 0.5
         self.x_velocity = 0
