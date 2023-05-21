4b2ad9ca2c12a56c4087222a87849018a010f715
novarova <novaroverteam@monash.edu>
2023-05-21 21:08:54 +1000

    Fixed velocity bounds

--- src/ros/rover/control/control/gimble_cam.py
+++ src/ros/rover/control/control/gimble_cam.py
@@ -41,7 +41,7 @@ class GimbleCam(Node):
         self.get_logger().set_level(logging.INFO)
         self.param_can = self.declare_parameter("can_bus", "can1").value
         self.param_velocity_increment = self.declare_parameter("velocity_factor", 0.1).value
-        self.max_velocity_value = self.declare_parameter("max_velocity_value", 128).value
+        self.max_velocity_value = self.declare_parameter("max_velocity_value", 127).value
         # can commands for gimble cam
         self.velocity_cmd = 0x081
         # Initially all motors spin backwards with 0 velocity
@@ -95,7 +95,7 @@ class GimbleCam(Node):
             if msg.btn_bottom_r1_state == 1:
                 self.velocity_factor = min(self.param_velocity_increment + self.velocity_factor, 1)
             elif msg.btn_bottom_r4_state == 1:
-                self.velocity_factor = max(self.velocity_factor - self.param_velocity_increment, 0)
+                self.velocity_factor = max(self.velocity_factor - self.param_velocity_increment, self.param_velocity_increment)
 
             # set the x velocity
             if msg.btn_bottom_r2_state >= 1:
