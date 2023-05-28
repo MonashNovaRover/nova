7e14cf1970b0aff77acf74883b76a3c16535ab0f
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-05-27 18:20:38 -0600

    chore(nix): Upgrade Nixpkgs

--- src/ros/cameras2/nix/packages/default.nix
+++ src/ros/cameras2/nix/packages/default.nix
@@ -1,27 +1,26 @@
 { pkgs, stdenv }:
 
-pkgs.callPackage ./ros.nix { } {
+let
+  nixpkgs = pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "aeb75dba965e790de427b73315d5addf91a54955";
+    hash = "sha256-U3oOge4cHnav8OLGdRVhL45xoRj4Ppd+It6nPC9nNIU=";
+  };
+  rospkgs = pkgs.fetchFromGitHub {
+    owner = "lopsided98";
+    repo = "nix-ros-overlay";
+    rev = "650b5972c994c6d13fdac70869c19c52c056d8de";
+    hash = "sha256-czIXVpxwnsqAmHMj+XJdcqiwvmGoq9nPuWCkeZ0sLik=";
+  };
+in import rospkgs {
   inherit (stdenv.buildPlatform) system;
-  nixpkgs = (pkgs.applyPatches {
-    src = pkgs.fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs";
-      rev = "da45bf6ec7bbcc5d1e14d3795c025199f28e0de0";
-      hash = "sha256-sASwo8gBt7JDnOOstnps90K1wxmVfyhsTPPNTGBPjjg=";
-    };
-    patches = [
-      (pkgs.fetchpatch {
-        # gst-plugins-rs 0.10.6
-        # Can be removed once https://nixpk.gs/pr-tracker.html?pr=225143 hits nixos-unstable
-        url = "https://github.com/NixOS/nixpkgs/commit/7c79d2edc3f404eb836aaf7786db777f1e0cb144.patch";
-        hash = "sha256-yiHCUy1MzPDMd9nWn5e72JjtRpfT6jczfOg8+I/w8Gg=";
-      })
-    ];
-  });
+  inherit nixpkgs;
   overlays = [
     (self: super: {
       ros = self.rosPackages.foxy;
-      callRosPackage = path: overrides: rosOverrides: self.ros.callPackage (self.callPackage path overrides) rosOverrides;
+      callRosPackage = path: overrides: rosOverrides:
+        self.ros.callPackage (self.callPackage path overrides) rosOverrides;
     })
     (self: super: {
       # ROS packages
@@ -33,7 +32,8 @@ pkgs.callPackage ./ros.nix { } {
       # Python packages
       pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
         (pySelf: pySuper: {
-          pygobject-stubs = pySelf.callPackage ./python-modules/pygobject-stubs { };
+          pygobject-stubs =
+            pySelf.callPackage ./python-modules/pygobject-stubs { };
           pyqt5-stubs = pySelf.callPackage ./python-modules/pyqt5-stubs { };
         })
       ];
--- src/ros/cameras2/nix/packages/ros.nix
+++
@@ -1,8 +0,0 @@
-{ fetchFromGitHub }:
-
-import (fetchFromGitHub {
-  owner = "lopsided98";
-  repo = "nix-ros-overlay";
-  rev = "650b5972c994c6d13fdac70869c19c52c056d8de";
-  hash = "sha256-czIXVpxwnsqAmHMj+XJdcqiwvmGoq9nPuWCkeZ0sLik=";
-})
