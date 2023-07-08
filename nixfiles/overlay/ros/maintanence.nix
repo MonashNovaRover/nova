452115ddf70f306d44759693c492ae0b17fb4f39
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-08 20:48:25 +1000

    Allow overlaying all ROS distros in the ROS maintenance overlay

--- nixfiles/overlay/ros/maintanence.nix
+++ nixfiles/overlay/ros/maintanence.nix
@@ -1,31 +1,35 @@
 self: super:
 
 {
-  rosPackages = super.rosPackages // {
-    foxy = super.rosPackages.foxy.overrideScope (rosSelf: rosSuper: {
-      # Use ros2doctor from Humble: https://github.com/lopsided98/nix-ros-overlay/issues/75#issuecomment-1567281292
-      ros2doctor = rosSelf.callPackage self.rosPackages.humble.ros2doctor.override { };
+  rosPackages = (super.rosPackages.appendDistroOverlay
+    # Overlay for all ROS distros.
+    (rosSelf: rosSuper: { })
+    # Overlays for individual ROS distros.
+    (super.rosPackages // {
+      foxy = super.rosPackages.foxy.overrideScope (rosSelf: rosSuper: {
+        # Use ros2doctor from Humble: https://github.com/lopsided98/nix-ros-overlay/issues/75#issuecomment-1567281292
+        ros2doctor = rosSelf.callPackage self.rosPackages.humble.ros2doctor.override { };
 
-      # Add argcomplete as a propagated ros2cli dependency.
-      # https://github.com/ros2/ros2cli/pull/564
-      # https://github.com/ros2/ros2cli/blob/26715cbb0948258d6f04b94c909d035c5130456a/ros2cli/ros2cli/cli.py#L45
-      ros2cli = rosSuper.ros2cli.overrideAttrs ({ propagatedBuildInputs ? [ ], ... }: {
-        propagatedBuildInputs = propagatedBuildInputs ++ [
-          # TODO: Work out why the argcomplete overlay from maintanence is not being used here.
-          rosSelf.pythonPackages.argcomplete
-        ];
-      });
+        # Add argcomplete as a propagated ros2cli dependency.
+        # https://github.com/ros2/ros2cli/pull/564
+        # https://github.com/ros2/ros2cli/blob/26715cbb0948258d6f04b94c909d035c5130456a/ros2cli/ros2cli/cli.py#L45
+        ros2cli = rosSuper.ros2cli.overrideAttrs ({ propagatedBuildInputs ? [ ], ... }: {
+          propagatedBuildInputs = propagatedBuildInputs ++ [
+            # TODO: Work out why the argcomplete overlay from maintanence is not being used here.
+            rosSelf.pythonPackages.argcomplete
+          ];
+        });
 
-      # Backported compilation error fixes.
-      pendulum-control = rosSuper.pendulum-control.overrideAttrs ({ patches ? [ ], ... }: {
-        patches = patches ++ [
-          (self.fetchpatch {
-            url = "https://github.com/ros2/demos/commit/754612348e408675f526174c5f03786e08ad8a70.patch";
-            stripLen = 1;
-            hash = "sha256-B+UW1OL0SOs7mOEOtpu5CSo8zSk5ifJdwC/deY/7zTg=";
-          })
-        ];
+        # Backported compilation error fixes.
+        pendulum-control = rosSuper.pendulum-control.overrideAttrs ({ patches ? [ ], ... }: {
+          patches = patches ++ [
+            (self.fetchpatch {
+              url = "https://github.com/ros2/demos/commit/754612348e408675f526174c5f03786e08ad8a70.patch";
+              stripLen = 1;
+              hash = "sha256-B+UW1OL0SOs7mOEOtpu5CSo8zSk5ifJdwC/deY/7zTg=";
+            })
+          ];
+        });
       });
-    });
-  };
+    }));
 }
