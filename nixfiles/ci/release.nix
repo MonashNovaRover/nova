f22191b33d2ad5410b36d9d052429318c0dbe053
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-12 21:11:23 +1000

    Revert "ci: Reduce repeated evaluations"
    This reverts commit be2e5c03ed54f1348ebbf9485bd4931dc90c5ba.
    
    This seems to crash the evaluator.

--- nixfiles/ci/release.nix
+++ nixfiles/ci/release.nix
@@ -21,18 +21,8 @@ let
     ];
   };
 
-  novaFor = system:
-    # Based on https://github.com/NixOS/nixpkgs/blob/bec27fabee7ff51a4788840479b1730ed1b64427/pkgs/top-level/release-lib.nix#L24C21-L24C21.
-    # Caches instances to avoid repeated evaluations.
-    let
-      nova_x86_64_linux = mkNova (lib.pkgsFor "x86_64-linux");
-      nova_aarch64_linux = mkNova (lib.pkgsFor "aarch64-linux");
-    in
-    if system == "x86_64-linux" then nova_x86_64_linux
-    else if system == "aarch64-linux" then nova_aarch64_linux
-    else abort "unsupported system type: ${system}";
-
-  genNovaPlatformRecipes = f: lib.forAllSystems (system: f (novaFor system));
+  genNovaPlatformRecipes = f: lib.forAllSystems
+    (system: f (mkNova (lib.pkgsFor system)));
 in
 builtins.mapAttrs (name: genNovaPlatformRecipes) rec {
   workspace = nova: nova.pkgs.ros.nova-workspace;
