8707783d23e8f26b39f46c70b7ec755701a363de
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-12 20:57:41 +1000

    ci: Generate jobs for all platforms

--- nixfiles/ci/release.nix
+++ nixfiles/ci/release.nix
@@ -1,4 +1,5 @@
-{ nixpkgs
+{ supportedSystems
+, nixpkgs
 , src
 , rover
 , cameras2
@@ -7,7 +8,11 @@
 }:
 
 let
-  nova = import src {
+  lib = import (nixpkgs + /pkgs/top-level/release-lib.nix) { inherit supportedSystems; };
+  pkgs = lib.pkgs;
+
+  mkNova = pkgs: import src {
+    inherit pkgs;
     repos = [
       rover
       cameras2
@@ -15,8 +20,17 @@ let
       coms_utils
     ];
   };
+
+  systems = pkgs.lib.intersectLists supportedSystems [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
+
+  genNovaPlatformRecipes = f: pkgs.lib.genAttrs
+    systems
+    (system: f (mkNova (lib.pkgsFor system)));
 in
-rec {
-  workspace = nova.pkgs.ros.nova-workspace;
-  workspace-env-inputs = workspace.env.inputDerivation;
+builtins.mapAttrs (name: genNovaPlatformRecipes) rec {
+  workspace = nova: nova.pkgs.ros.nova-workspace;
+  workspace-env-inputs = nova: (workspace nova).env.inputDerivation;
 }
