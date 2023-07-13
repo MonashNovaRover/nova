ba4d2a6981565424233cff8c97308a2d109a31ac
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-14 00:56:30 +1000

    Allow using a custom Nova workspace in the home module

--- nixfiles/home/workspace/default.nix
+++ nixfiles/home/workspace/default.nix
@@ -6,12 +6,13 @@ in
 {
   options.nova.workspace = {
     enable = lib.mkEnableOption "the Nova Rover ROS workspace" // { default = true; };
+    package = lib.mkPackageOption pkgs [ "nova" "ros" "nova-workspace" ] { };
     gui.enable = lib.mkEnableOption "graphical applications such as RViz" // { default = false; };
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      (pkgs.nova.ros.nova-workspace.override {
+      (cfg.package.override {
         graphical = cfg.gui.enable;
       })
     ];
