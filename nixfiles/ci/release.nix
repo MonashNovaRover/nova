e1237c9b228bafbd42877a374bd5914c9f4305cf
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-13 19:47:01 +1000

    ci: Build packages more granularly

--- nixfiles/ci/release.nix
+++ nixfiles/ci/release.nix
@@ -8,23 +8,37 @@
 }:
 
 let
+  novaRepos = [
+    rover
+    cameras2
+    gui
+    coms_utils
+  ];
+
   lib = import (nixpkgs + /pkgs/top-level/release-lib.nix) { inherit supportedSystems; };
   pkgs = lib.pkgs;
 
-  mkNova = pkgs: import src {
-    inherit pkgs;
-    repos = [
-      rover
-      cameras2
-      gui
-      coms_utils
-    ];
-  };
+  mkNova = pkgs: import src { inherit pkgs; repos = novaRepos; };
+  novaFor = system: mkNova (lib.pkgsFor system);
+  novaForAllSystems = f: lib.forAllSystems (system: f (novaFor system));
 
-  genNovaPlatformRecipes = f: lib.forAllSystems
-    (system: f (mkNova (lib.pkgsFor system)));
+
+  packageLists = novaForAllSystems (nova:
+    # Build Nova Rover packages.
+    nova.pkgs.ros.nova-workspace.novaPackages
+
+    # Build other workspace packages.
+    ++ nova.pkgs.ros.nova-workspace.extraPackages
+
+    # Build development dependencies of Nova packages.
+    # This ensures that all the software needed to develop Nova software is
+    # available.
+    ++ map (pkg: pkg.inputDerivation) nova.pkgs.ros.nova-workspace.novaPackages
+
+    # Build software in the workspace development environment.
+    # While this will mostly overlap in scope with the line above, the workspace
+    # development environment contains additional tools that aren't needed to
+    # build individual packages, like Colcon.
+    ++ [ nova.pkgs.ros.nova-workspace.env.inputDerivation ]);
 in
-builtins.mapAttrs (name: genNovaPlatformRecipes) rec {
-  workspace = nova: nova.pkgs.ros.nova-workspace;
-  workspace-env-inputs = nova: (workspace nova).env.inputDerivation;
-}
+packageLists
