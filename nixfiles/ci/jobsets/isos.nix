bcb0e43bd1b9c9cb3712033af0c9708be99b3e93
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-14 12:12:44 +1000

    ci: Use native compression tools to build ISOs

--- nixfiles/ci/jobsets/isos.nix
+++ nixfiles/ci/jobsets/isos.nix
@@ -8,43 +8,65 @@ let
   lib = import ../lib.nix args;
 
   mkIso = nova:
+    { graphical ? false, includeWorkspace ? false }:
     let
       inherit (import ../workspaces.nix { inherit lib nova; })
         workspace
         hydraPatchedWorkspace;
-    in
-    { graphical ? false, includeWorkspace ? false }:
-    (import (nixpkgs + /nixos/lib/eval-config.nix) {
-      system = nova.pkgs.hostPlatform.system;
-      modules = [
-        nova.nixosModule
-        ../../nixos/installer/cd-dvd/nova-installation-cd-${if graphical then "graphical" else "base"}.nix
-        {
-          isoImage = {
-            isoBaseName = "nixos-nova${lib.releaseLib.pkgs.lib.optionalString graphical "-graphical"}${lib.releaseLib.pkgs.lib.optionalString includeWorkspace "-workspace"}";
-            compressImage = true;
-          };
-          nova = {
-            desktop.enable = graphical;
-            substituters.nova.password = "***REMOVED***";
-          };
-          home-manager.sharedModules = [{
-            # The Home Manager manual causes issues on Hydra.
-            manual = {
-              html.enable = false;
-              manpages.enable = false;
-              json.enable = false;
+
+      baseSystem = (import (nixpkgs + /nixos/lib/eval-config.nix) {
+        system = nova.pkgs.hostPlatform.system;
+        modules = [
+          nova.nixosModule
+          ../../nixos/installer/cd-dvd/nova-installation-cd-${if graphical then "graphical" else "base"}.nix
+          {
+            isoImage = {
+              isoBaseName = "nixos-nova${lib.releaseLib.pkgs.lib.optionalString graphical "-graphical"}${lib.releaseLib.pkgs.lib.optionalString includeWorkspace "-workspace"}";
+              compressImage = true;
             };
+
             nova = {
-              workspace = {
-                enable = includeWorkspace;
-                package = hydraPatchedWorkspace;
-              };
+              desktop.enable = graphical;
+              substituters.nova.password = "***REMOVED***";
             };
-          }];
-        }
-      ];
-    }).config.system.build.isoImage;
+            home-manager.sharedModules = [{
+              # The Home Manager manual causes issues on Hydra.
+              manual = {
+                html.enable = false;
+                manpages.enable = false;
+                json.enable = false;
+              };
+              nova = {
+                workspace = {
+                  enable = includeWorkspace;
+                  package = hydraPatchedWorkspace;
+                };
+              };
+            }];
+          }
+        ];
+      });
+
+      config = (baseSystem.extendModules {
+        modules = lib.releaseLib.pkgs.lib.optionals (nova.pkgs.hostPlatform.isx86_64 && (lib.releaseLib.pkgs.lib.systems.elaborate builtins.currentSystem).isAarch64) [
+          # Some build tools are prohibitively slow in QEMU.
+          # We can use the host tools to build the ISO.
+          {
+            system.build.squashfsStore = lib.releaseLib.pkgs.lib.mkForce (baseSystem.config.system.build.squashfsStore.override {
+              inherit (lib.releaseLib.pkgsFor builtins.currentSystem)
+                squashfsTools;
+            });
+
+            system.build.isoImage = lib.releaseLib.pkgs.lib.mkForce (baseSystem.config.system.build.isoImage.override {
+              inherit (lib.releaseLib.pkgsFor builtins.currentSystem)
+                xorriso
+                zstd;
+            });
+          }
+        ];
+      }).config;
+    in
+    config.system.build.isoImage;
 
   isoJobs = lib.novaForAllSystems (nova: {
     iso-base = mkIso nova { };
