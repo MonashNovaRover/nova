42e09cad17d6b96e32085609a9179f2970ce395d
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-15 19:20:33 +1000

    ci: Enable keep-outputs by default

--- nixfiles/nixos/common/ci/common/default.nix
+++ nixfiles/nixos/common/ci/common/default.nix
@@ -6,5 +6,9 @@ in
 {
   options.nova.ci.common.enable = lib.mkEnableOption "CI common services" // { internal = true; };
 
-  config = lib.mkIf cfg.enable { };
+  config = lib.mkIf cfg.enable {
+    nix.settings = lib.mkDefault {
+      keep-outputs = true;
+    };
+  };
 }
