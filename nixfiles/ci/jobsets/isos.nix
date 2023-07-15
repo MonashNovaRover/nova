4947625ac5cefb9e187b807c15fd34575f8ab22e
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-15 21:14:35 +1000

    Revert "ci: Work around evaluation bug in ISO jobset"
    This reverts commit 3ec5d3d0700fcf361856808b46559855c1204ad8.

--- nixfiles/ci/jobsets/isos.nix
+++ nixfiles/ci/jobsets/isos.nix
@@ -7,11 +7,9 @@
 let
   lib = import ../lib.nix args;
 
-  mkIso = system:
+  mkIso = nova:
     { graphical ? false, includeWorkspace ? false }:
     let
-      nova = lib.novaFor system;
-
       inherit (import ../workspaces.nix { inherit lib nova; })
         workspace
         hydraPatchedWorkspace;
@@ -48,40 +46,23 @@ let
         ];
       });
 
-      extensions =
-        # Note: Ideally, we would use lib.optionals, nova.pkgs.hostPlatform and
-        # lib.systems.elaborate here to avoid using hardcoded strings and
-        # verbose syntax.
-        #
-        # For some bizzare reason, however, the assertion added in
-        # https://github.com/NixOS/nixpkgs/pull/238154 fails *later on* whenever
-        # lib (from either nova.pkgs or lib.releaseLib.pkgs) is used in exactly
-        # this place - even if it's just in a trace statement with
-        # builtins.trace. (nova.pkgs.hostPlatforms must use lib at some stage.)
-        #
-        # This is likely due to a bug in Nix or Nixpkgs (expression evaluations
-        # are not supposed to have side effects!), and may have something to do
-        # with the "poor man's memoization" in releaseLib.pkgsFor.
-        #
-        # TODO: Make a minimal reproduction and submit a bug report in the Nix
-        # issue tracker.
-        if system == "x86_64-linux" && builtins.currentSystem == "aarch64-linux" then [
-          # Some build tools are prohibitively slow in QEMU.
-          # We can use the host tools to build the ISO.
-          (prev: [{
-            system.build.squashfsStore = lib.releaseLib.pkgs.lib.mkForce (prev.system.build.squashfsStore.override {
-              inherit (lib.releaseLib.pkgsFor builtins.currentSystem)
-                squashfsTools;
-            });
-          }])
-          (prev: [{
-            system.build.isoImage = lib.releaseLib.pkgs.lib.mkForce (prev.system.build.isoImage.override {
-              inherit (lib.releaseLib.pkgsFor builtins.currentSystem)
-                xorriso
-                zstd;
-            });
-          }])
-        ] else [ ];
+      extensions = lib.releaseLib.pkgs.lib.optionals (nova.pkgs.hostPlatform.isx86_64 && (lib.releaseLib.pkgs.lib.systems.elaborate builtins.currentSystem).isAarch64) [
+        # Some build tools are prohibitively slow in QEMU.
+        # We can use the host tools to build the ISO.
+        (prev: [{
+          system.build.squashfsStore = lib.releaseLib.pkgs.lib.mkForce (prev.system.build.squashfsStore.override {
+            inherit (lib.releaseLib.pkgsFor builtins.currentSystem)
+              squashfsTools;
+          });
+        }])
+        (prev: [{
+          system.build.isoImage = lib.releaseLib.pkgs.lib.mkForce (prev.system.build.isoImage.override {
+            inherit (lib.releaseLib.pkgsFor builtins.currentSystem)
+              xorriso
+              zstd;
+          });
+        }])
+      ];
 
       config = (builtins.foldl'
         (system: mkModules: system.extendModules { modules = mkModules system.config; })
@@ -91,11 +72,11 @@ let
     in
     config.system.build.isoImage;
 
-  isoJobs = lib.releaseLib.forAllSystems (system: {
-    iso-base = mkIso system { };
-    iso-base-workspace = mkIso system { includeWorkspace = true; };
-    iso-graphical = mkIso system { graphical = true; };
-    iso-graphical-workspace = mkIso system { graphical = true; includeWorkspace = true; };
+  isoJobs = lib.novaForAllSystems (nova: {
+    iso-base = mkIso nova { };
+    iso-base-workspace = mkIso nova { includeWorkspace = true; };
+    iso-graphical = mkIso nova { graphical = true; };
+    iso-graphical-workspace = mkIso nova { graphical = true; includeWorkspace = true; };
   });
 in
 isoJobs
