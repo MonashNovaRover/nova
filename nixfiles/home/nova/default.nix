d038cb8eb01935901e0a1a3575b0fe29e82c586c
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-30 18:20:36 +1000

    home: Create XDG user directories for nova

--- nixfiles/home/nova/default.nix
+++ nixfiles/home/nova/default.nix
@@ -3,5 +3,9 @@
 {
   config = lib.mkIf (config.home.username == "nova") {
     programs.bash.enable = true;
+    xdg.userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
   };
 }
