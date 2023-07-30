d43672d8f668bdcb2b6ee93b492e349498fa95c2
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-31 01:12:00 +1000

    home: Only enable GNOME extensions when desktop is enabled

--- nixfiles/home/desktop/extensions/basic.nix
+++ nixfiles/home/desktop/extensions/basic.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
+  cfg = config.nova.desktop;
   extensions = with pkgs.gnomeExtensions; [
     start-overlay-in-application-view
   ];
 in
 {
-  home.packages = extensions;
-  dconf.settings."org/gnome/shell".enabled-extensions = map (extension: extension.extensionUuid) extensions;
+  config = lib.mkIf cfg.enable {
+    home.packages = extensions;
+    dconf.settings."org/gnome/shell".enabled-extensions = map (extension: extension.extensionUuid) extensions;
+  };
 }
