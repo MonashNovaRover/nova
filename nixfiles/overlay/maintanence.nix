7b0d2c892f7bdcbe95d44a6aa62d745c12afb268
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-07-31 09:22:22 +1000

    argcomplete: Remove upgrade override
    Nixpkgs now has the latest version.

--- nixfiles/overlay/maintanence.nix
+++ nixfiles/overlay/maintanence.nix
@@ -1,15 +1,3 @@
 self: super:
 
-{
-  pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
-    (pyself: pysuper: {
-      argcomplete = pysuper.argcomplete.overridePythonAttrs ({ pname, ... }: rec {
-        version = "3.1.1";
-        src = pyself.fetchPypi {
-          inherit pname version;
-          hash = "sha256-bExWPxTwFECq/6Pq4TRBxdsjV7Xuxjmr58CxUzRiff8=";
-        };
-      });
-    })
-  ];
-}
+{ }
