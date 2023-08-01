0063d57f28541672b6327472005f0be338f55f9c
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-08-01 23:37:32 +1000

    ci: Add missing update operator in patched workspace expression

--- nixfiles/ci/workspaces.nix
+++ nixfiles/ci/workspaces.nix
@@ -8,7 +8,7 @@ rec {
     # There is no easy way at this stage to determine if Hydra has access to
     # any real x86_64 machines, so these changes will apply indiscriminately.
     (lib.releaseLib.pkgs.lib.optionalAttrs (nova.pkgs.hostPlatform.isx86_64 && (lib.releaseLib.pkgs.lib.systems.elaborate builtins.currentSystem).isAarch64) rec {
-      novaPackages = workspace.novaPackages {
+      novaPackages = workspace.novaPackages // {
         # The GUI frontend fails to build, but the output contains only
         # static Web assets, and is architecture-independent. Use the
         # Aarch64 version instead.
