bbab88527b7a75d98cb4fc1c651a17469e47f4d2
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-08-03 13:49:56 +1000

    nixos: Persist store operations by default

--- nixfiles/nixos/common/virtualisation/vm.nix
+++ nixfiles/nixos/common/virtualisation/vm.nix
@@ -1,5 +1,13 @@
 {
   virtualisation.vmVariant = { lib, ... }: {
+    # Persist store operations by default to reduce confusion.
+    #
+    # When the Nix store is cleaned on a reboot, its database is not updated
+    # accordingly to remove the lost paths. This causes problems when trying to
+    # use those paths after rebooting - Nix thinks they still exist, but they
+    # do not.
+    virtualisation.writableStoreUseTmpfs = lib.mkDefault false;
+
     services.xserver.enable = lib.mkDefault true;
 
     nova = {
