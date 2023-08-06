1cbf178d4d3294cc648344b0da252468a57b1104
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-08-06 20:15:22 +1000

    Unify rclnodejs derivations

--- nixfiles/packages/ros/default.nix
+++ nixfiles/packages/ros/default.nix
@@ -2,6 +2,5 @@
 
 {
   nova-workspace = callPackage ./nova-workspace { };
-  rclnodejs-message-generator = callPackage ./rclnodejs-message-generator { };
-  rclnodejs-messages = callPackage ./rclnodejs-messages { };
+  rclnodejs = callPackage ./rclnodejs { };
 }
