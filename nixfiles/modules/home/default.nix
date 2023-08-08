4dcc2ddd75e11b48c076a75df1424a711f452377
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-08-08 13:28:54 +1000

    Move modules to modules directory

--- nixfiles/modules/home/default.nix
+++ nixfiles/modules/home/default.nix
@@ -10,6 +10,6 @@
 
   nixpkgs = {
     config.allowUnfree = true;
-    overlays = lib.mkBefore [ (self: super: { nova = (import ../. { pkgs = self; }).pkgs; }) ];
+    overlays = lib.mkBefore [ (self: super: { nova = (import ../../. { pkgs = self; }).pkgs; }) ];
   };
 }
--- nixfiles/modules/nixos/default.nix
+++ nixfiles/modules/nixos/default.nix
@@ -9,7 +9,7 @@
 
   nixpkgs = {
     config.allowUnfree = true;
-    overlays = lib.mkBefore [ (self: super: { nova = (import ../. { pkgs = self; }).pkgs; }) ];
+    overlays = lib.mkBefore [ (self: super: { nova = (import ../../. { pkgs = self; }).pkgs; }) ];
   };
 
   home-manager.nova.sharedModules = [ ../home ];
