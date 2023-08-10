5447cee8951698e16c534683016dd478d7be1d3d
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-08-10 06:31:15 +0000

    nixos: metabox-v158pnh: Disable CUDA support
    This causes issues like https://github.com/NixOS/nixpkgs/issues/144054 due to https://github.com/NixOS/nixpkgs/pull/221370.

--- nixfiles/modules/nixos/devices/metabox-v158pnh/graphics/compute.nix
+++ nixfiles/modules/nixos/devices/metabox-v158pnh/graphics/compute.nix
@@ -3,8 +3,8 @@
 {
   config = lib.mkIf config.devices.metabox-v158pnh.enable {
     hardware.opengl.extraPackages = with pkgs; [ intel-compute-runtime ];
-    nixpkgs.config.cudaSupport = true;
-    nix.settings = {
+    nixpkgs.config.cudaSupport = false;
+    nix.settings = lib.mkIf config.nixpkgs.config.cudaSupport {
       substituters = [ "https://cuda-maintainers.cachix.org" ];
       trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
     };
