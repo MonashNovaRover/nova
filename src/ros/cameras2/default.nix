f3f718200765d937b53ac765fefc013143219fb7
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-08-12 22:58:29 +1000

    feat(nix): Add aliases

--- src/ros/cameras2/default.nix
+++ src/ros/cameras2/default.nix
@@ -3,4 +3,9 @@
     nova-camera-msgs = callPackage ./nix/packages/camera-msgs { };
     nova-cameras2 = callPackage ./nix/packages/cameras2 { };
   };
+
+  shellAliases = rec {
+    cameras = "ros2 launch cameras2 camera_server_launch.py platform:=rover";
+    cameras_all = "${cameras} autostart:=true";
+  };
 }
