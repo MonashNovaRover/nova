1410f99d44c189a4fa88be98f9140d1d03ff2626
ecthellion99 <tstr0006@student.monash.edu>
2023-09-16 21:37:04 +1000

    removed visibility_control (only required for windows)

--- src/ros/rover/rover_hardware/include/rover_hardware/blcmd_hardware.hpp
+++ src/ros/rover/rover_hardware/include/rover_hardware/blcmd_hardware.hpp
@@ -18,7 +18,6 @@
 #include <string>
 #include <vector>
 
-#include "rover_hardware/visibility_control.h"
 #include "hardware_interface/actuator_interface.hpp"
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
@@ -31,39 +30,32 @@ namespace rover_hardware
 class BLCMDHardware : public hardware_interface::ActuatorInterface
 {
 public:
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_init(
     const hardware_interface::HardwareInfo & info) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_configure(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_deactivate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::return_type read(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::return_type write(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
   std::vector<double> hw_commands_;
   std::vector<double> hw_states_;
+
 };
 
 }  // namespace rover_hardware
