ea8c0a1dde640797529ed24d299b9a39cd4c3b0e
ecthellion99 <tstr0006@student.monash.edu>
2023-09-16 21:33:18 +1000

    Added nix package definitions for rover_hardware

---
+++ src/ros/rover/nix/packages/rover_hardware/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, buildRosPackage
+, ament-cmake
+, rclcpp
+, rclcpp-lifecycle
+, hardware-interface
+}:
+
+buildRosPackage {
+  name = "rover_hardware";
+  buildType = "ament_cmake";
+
+  src = builtins.path rec {
+    name = "rover_hardware-source";
+    path = ../../../control;
+    filter = lib.novaSourceFilter [ ]
+      path;
+  };
+
+  nativeBuildInputs = [ ament-cmake ];
+
+  buildInputs = [
+    rclcpp
+    rclcpp-lifecycle
+    hardware-interface
+  ];
+}
