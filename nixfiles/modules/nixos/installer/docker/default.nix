400238d6b688b41291d20b963ab932cc249ff23b
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-09-18 15:44:43 +1000

    nixos: Add a Docker image module

---
+++ nixfiles/modules/nixos/installer/docker/default.nix
@@ -0,0 +1,23 @@
+{ config, lib, modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/virtualisation/docker-image.nix")
+  ];
+
+  # Undo some changes made by the minimal profile
+  environment.noXlibs = false;
+  programs.command-not-found.enable = true;
+
+  nova = {
+    profile = "shared";
+    desktop.enable = false;
+  };
+
+  home-manager.nova.sharedModules = [{
+    home.stateVersion = lib.mkDefault config.system.nixos.release;
+    nova = {
+      workspace.enable = false;
+    };
+  }];
+}
