576bfe6f184282f71b0402745d40db4469b9ae95
Monash Nova Rover <novaroverteam@monash.edu>
2023-09-18 04:59:47 +0000

    nixos: metabox-v158pnh: Enable CUDA binary cache unconditionally

--- nixfiles/modules/nixos/devices/metabox-v158pnh/graphics/compute.nix
+++ nixfiles/modules/nixos/devices/metabox-v158pnh/graphics/compute.nix
@@ -4,7 +4,7 @@
   config = lib.mkIf config.devices.metabox-v158pnh.enable {
     hardware.opengl.extraPackages = with pkgs; [ intel-compute-runtime ];
     nixpkgs.config.cudaSupport = false;
-    nix.settings = lib.mkIf config.nixpkgs.config.cudaSupport {
+    nix.settings = {
       substituters = [ "https://cuda-maintainers.cachix.org" ];
       trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
     };
