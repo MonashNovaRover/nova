32275a28313225cba8479f292a7f1754ac2374c6
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-09-21 01:33:44 +1000

    ci: Enable the cuda-maintainers binary cache

--- nixfiles/modules/nixos/common/ci/common/default.nix
+++ nixfiles/modules/nixos/common/ci/common/default.nix
@@ -12,6 +12,9 @@ in
       min-free = lib.mkDefault (01 * 1024 * 1024 * 1024);
       max-free = lib.mkDefault (32 * 1024 * 1024 * 1024);
       keep-outputs = lib.mkDefault true;
+
+      substituters = [ "cuda-maintainers.cachix.org" ];
+      trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
     };
 
     nova.substituters.ros.enable = true;
