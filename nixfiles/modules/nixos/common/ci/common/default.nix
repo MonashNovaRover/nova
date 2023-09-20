b9d0103388d9bb9f137a9de0a1c334faa27148a4
Joshua Leivenzon <jlei0014@student.monash.edu>
2023-09-21 01:37:49 +1000

    ci: Use HTTPS for the cuda-maintainers binary cache

--- nixfiles/modules/nixos/common/ci/common/default.nix
+++ nixfiles/modules/nixos/common/ci/common/default.nix
@@ -13,7 +13,7 @@ in
       max-free = lib.mkDefault (32 * 1024 * 1024 * 1024);
       keep-outputs = lib.mkDefault true;
 
-      substituters = [ "cuda-maintainers.cachix.org" ];
+      substituters = [ "https://cuda-maintainers.cachix.org" ];
       trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
     };
 
