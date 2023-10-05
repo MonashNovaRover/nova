74340b1fc112fcc87282fac1a87988b6c97c26ce
ecthelion99 <tstr0006@student.monash.edu>
2023-10-05 21:51:22 +1100

    ros2 control launch file

---
+++ src/ros/rover/core/launch/ros2_control.launch.py
@@ -0,0 +1,78 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Monash Nova Rover Team
+
+Execute this code on the rover to publish the urdf
+    static transforms and associated joint states
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODES:
+  - robot_state_publisher
+  - rover_state_publisher
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE: 	core
+CREATION:	27/04/2023
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import (
+    Command,
+    LaunchConfiguration,
+    FindExecutable,
+    PathJoinSubstitution,
+)
+from launch.conditions import IfCondition, UnlessCondition
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+import xacro
+
+# Generate the launch file with all inputs
+def generate_launch_description():
+    gazebo = LaunchConfiguration('gazebo', default=False)
+    core_path = get_package_share_path('core')
+    default_model_path = core_path / 'urdf/rover.urdf.xacro'
+    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+            description='Absolute path to robot urdf file')
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            " ",
+            PathJoinSubstitution([
+                FindPackageShare("core"),
+                "urdf",
+                "rover.urdf.xacro"
+            ]),
+            " ",
+            "gazebo:=",
+            gazebo
+        ]
+    )
+
+    robot_description = {'robot_description': ParameterValue(robot_description_content, value_type=str)}
+
+    controllers = PathJoinSubstitution(
+        [FindPackageShare("core"), "params", "controllers.yaml"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, controllers]
+    )
+
+
+# robot_state_publisher_node = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     parameters=[{'robot_description': ParameterValue(robot_description, value_type=str)}]
+    # )
+
+    return LaunchDescription([
+        control_node,
+    ])
