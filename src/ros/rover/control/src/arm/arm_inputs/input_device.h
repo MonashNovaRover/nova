3c8fcce639a01d78620ebcf7296b0ced815ac2e5
Jiayi Gu <Matthewgjy@gmail.com>
2023-10-18 16:52:39 +1100

    Added documentation for virtual functions

--- src/ros/rover/control/src/arm/arm_inputs/input_device.h
+++ src/ros/rover/control/src/arm/arm_inputs/input_device.h
@@ -29,14 +29,29 @@ class InputDevice {
     //------------------------------------------------------------//
     public:
 
+    /// @brief returns the inputs for locking devices and joint limits
     virtual CommonInputCollections::ControlSchemeInputs get_arm_lock_inputs() = 0;
+
+    /// @brief returns the inputs for the control scheme
     virtual CommonInputCollections::ControlSchemeInputs get_control_scheme_inputs() = 0;
+    
+    /// @brief returns the inputs for the end effector
     virtual CommonInputCollections::EndEffectorInputs get_end_effector_inputs() = 0;
+    
+    /// @brief returns the inputs for the joint velocities
     virtual CommonInputCollections::JointVelocityInputs get_joint_velocity_inputs() = 0;
+    
+    /// @brief returns the inputs for the twist
     virtual CommonInputCollections::TwistInputs get_twist_inputs() = 0;
+
+    /// @brief returns whether the device is connected
     virtual bool is_connected() = 0;
+
     // Should take argument type of any message share ptr, but I cannot find the base class for that 
     template<typename Message>
+    /// @brief      Sets the message to the input device
     virtual void set_message(Message msg, int idx) = 0;
+    
+    /// @brief     Resets the message of the input device
     virtual void reset_message() = 0;
 };
