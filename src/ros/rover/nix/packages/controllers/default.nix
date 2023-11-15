c176781a0d3a991a3c824181ef4269ea41c3aa56
BAMS <modulepackdata@energyrenaissance.com>
2023-11-15 21:56:32 +1100

    Revert "controllers: Add Nix files"
    This reverts commit f71c5499cdf8d590ca7bd3c045104ed6f97611db.

--- src/ros/rover/nix/packages/controllers/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-
-with pkgs;
-
-{
-  nova-four-wheel-steering-controller = callPackage ./four-wheel-steering-controller { };
-  nova-pivot-drive-controller = callPackage ./pivot-drive-controller { };
-}
