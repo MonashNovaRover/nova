8c5928edb6998c7ce2ac2e63e76f7d3fe2ca7e08
MaxTory2001 <torymax01@gmail.com>
2023-12-23 23:20:56 +1100

    Restored old launch files for now

---
+++ src/ros/rover/core/launch/old_launch/drive.launch.py
@@ -0,0 +1,37 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Monash Nova Rover Team
+
+Execute this code on the rover to start all
+   rover control scripts.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODES:
+  - control/drive/drive_inputs      [drive_cmd]
+  - control/drive/driver            [driver]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE: 	core
+CREATION:	15/12/2021
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+# Include the required launch parameters
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import UnlessCondition
+from launch_ros.actions import Node
+
+
+# Generate the launch file with all inputs
+def generate_launch_description():
+    gazebo = LaunchConfiguration('gazebo', default=False)
+    return LaunchDescription([
+        Node(
+            package='control', executable='drive_inputs', output='screen', emulate_tty=True,
+            parameters=[{'use_sim_time': gazebo}]),
+        Node(
+            package='control', executable='driver', output='screen', emulate_tty=True,
+            parameters=[{'use_sim_time': gazebo, 'gazebo': gazebo}]),
+        Node(
+            package='electronics', executable='LED_transmitter.py', output='screen', emulate_tty=True,
+            condition=UnlessCondition(gazebo)),
+    ])
