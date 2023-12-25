7453722ef40a2396c9d53bf15c4dfe138116f97d
Kabilan VS <51330370+KABILAN235@users.noreply.github.com>
2023-12-25 21:42:58 +0530

    wip: Camera Component

---
+++ src/ros/nova-gui/nova-gui/src/views/shared/CamerasPage.tsx
@@ -0,0 +1,12 @@
+import { CameraComponent } from "../../components/CameraComponent/CameraComponent";
+
+export const CameraPage = () => {
+  return (
+    <div className="">
+      <CameraComponent
+        cameraName="My Phone Camera"
+        src="https://cdn.britannica.com/17/83817-050-67C814CD/Mount-Everest.jpg"
+      />
+    </div>
+  );
+};
