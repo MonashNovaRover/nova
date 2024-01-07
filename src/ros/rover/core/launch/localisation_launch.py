421c9bc6ec11b7e2d95be27f4345c633f015196c
MaxTory2001 <torymax01@gmail.com>
2024-01-07 18:40:14 +1100

    rtabmap working

--- src/ros/rover/core/launch/localisation_launch.py
+++ src/ros/rover/core/launch/localisation_launch.py
@@ -23,7 +23,6 @@ from launch.conditions import IfCondition, UnlessCondition
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 from launch_ros.actions import Node
 
@@ -32,11 +31,18 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # Declare a launch configuration argument of the name "t265"
     use_sim_time = LaunchConfiguration('use_sim_time')
+    slam = LaunchConfiguration('slam')
 
     use_sim_time_arg = DeclareLaunchArgument(
         'use_sim_time',
         default_value='true',
         description='Use simulation (Gazebo) clock if true')
+
+    use_slam_arg = DeclareLaunchArgument(
+        'slam',
+        default_value='true',
+        description='True to use RTABMAP, False to use robot_localization'
+    )
 
     ukf_localisation = Node(
         package='robot_localization',
@@ -48,13 +54,23 @@ def generate_launch_description():
 
     # TODO: Implement real SLAM rather than publishing a static transform. This is currently necessary for things to be visible in the RVIZ 'map' frame
     slam_cmd = Node(
+        condition=UnlessCondition(slam),
         package='tf2_ros',
         executable='static_transform_publisher',
         arguments = ['--x', '0', '--y', '0', '--z', '0', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'map', '--child-frame-id', 'odom']
     )
 
+    slam_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource((get_package_share_path("core") / 'launch' / 'rtabmap_launch.py').as_posix()),
+        condition=IfCondition(slam),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+        }.items()
+    )
+    
     return LaunchDescription([
         use_sim_time_arg,
+        use_slam_arg,
         ukf_localisation,
         slam_cmd,
     ])
