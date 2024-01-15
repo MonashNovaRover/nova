9b28b1f74e67c8bcbbe34b87fa48245211bc7cce
ecthelion99 <tstr0006@student.monash.edu>
2024-01-15 18:36:19 +1100

    seperated input msgs into input_msgs package

---
+++ src/ros/rover/nix/packages/input-msgs/default.nix
@@ -0,0 +1,20 @@
+{ lib
+, buildRosPackage
+, ament-cmake
+, rosidl-default-generators
+, std-msgs
+}:
+
+buildRosPackage {
+  name = "input-msgs";
+  buildType = "ament_cmake";
+
+  src = builtins.path rec {
+    name = "input-msgs-source";
+    path = ../../../input_msgs;
+    filter = lib.novaSourceFilter [ ] path;
+  };
+
+  nativeBuildInputs = [ ament-cmake rosidl-default-generators ];
+  propagatedBuildInputs = [ std-msgs ];
+}
