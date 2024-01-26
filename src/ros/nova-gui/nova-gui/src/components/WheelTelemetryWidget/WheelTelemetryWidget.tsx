486fd561b55f6adee3efcf5625196df537f7b48c
Bailey Chessum <bche0080@student.monash.edu>
2024-01-27 02:14:30 +1100

    fix: removed .map from useSelectors in WheelTelemetryWidget

--- src/ros/nova-gui/nova-gui/src/components/WheelTelemetryWidget/WheelTelemetryWidget.tsx
+++ src/ros/nova-gui/nova-gui/src/components/WheelTelemetryWidget/WheelTelemetryWidget.tsx
@@ -5,88 +5,33 @@ import {
   CardProps,
   Image
 } from "@nextui-org/react";
-import React, {ReactNode, useEffect} from "react";
+import React, {useEffect} from "react";
 import '../DriveModeWidget/DriveWidget.css';
 import './WheelTelemetryWidget.css';
-import { DriveProgress } from "../DriveSpeedWidget/DriveProgress.tsx";
 import {useBifrost} from "../../redux/actions/useBifrostAction";
 import {RosTopics} from "../../ros/rosTopics";
 import {RootState} from "../../redux/RootState";
 import {useSelector} from "react-redux";
 import RoverTopDownImage from "../../assets/rover-top-down.png";
 import {ChevronUp} from "react-feather";
+import WheelTelemetryWidgetCell, {IWheelTelemetryWidgetCellProps} from "./WheelTelemetryWidgetCell.tsx";
 
 // Properties for the DriveModeWidget component.
 export interface IDriveWheelWidgetProps extends CardProps {
 
 }
 
-// Properties for the DriveWidgetWheelData component.
-export interface IWheelTelemetryWidgetCellProps extends CardProps {
-  wheelValue: number,
-  pivotValue: number,
-  label: ReactNode
-}
-
 /**
- * A component for displaying the wheel data for the DriveModeWidget
- */
-const WheelTelemetryWidgetCell: React.FC<IWheelTelemetryWidgetCellProps> = (props: IWheelTelemetryWidgetCellProps) => {
-
-  const wheelProgress = (
-    <DriveProgress size="lg"
-                   value={props.wheelValue}
-                   maxValue={1}
-                   aria-label="Wheel Amount"
-                   autoColor={true}
-                   disableAnimation={true}>
-      <div className="grid grid-flow-col gap-3 auto-cols-fr text-small">
-        <span>WHEEL</span>
-        <span>{`${(props.wheelValue * 100).toFixed(0)}%`}</span>
-      </div>
-    </DriveProgress>
-  );
-
-  const pivotProgress = (
-    <DriveProgress size="lg"
-                   value={props.pivotValue}
-                   maxValue={1}
-                   aria-label="Pivot Amount"
-                   autoColor={true}
-                   disableAnimation={true}>
-      <div className="grid grid-flow-col gap-3 auto-cols-fr text-small">
-        <span>PIVOT</span>
-        <span>{`${(props.pivotValue * 100).toFixed(0)}%`}</span>
-      </div>
-    </DriveProgress>
-  );
-
-  const label = (
-    <span className="text-sm uppercase tracking-widest text-center text-default-300 text-opacity-80">
-      {props.label}
-    </span>
-  )
-
-  return <Card shadow="sm" className="bg-content2" {...props}>
-    <CardBody className="pt-1 flex gap-1 font-semibold flex-col content-center">
-      {label}
-      <div className="flex flex-col gap-2 content-center">
-        {wheelProgress}
-        {pivotProgress}
-      </div>
-    </CardBody>
-  </Card>
-}
-
-/**
- * A component that displays data for driving the rover.
+ * A component that displays wheel telemetry.
  */
 const WheelTelemetryWidget: React.FC<IDriveWheelWidgetProps> = (props: IDriveWheelWidgetProps) => {
   const bifrost = useBifrost(RosTopics.TELEMETRTY);
-  const pivotCurrents = useSelector((state: RootState) => state.telemetryStore.pivots
-      .map(p => p.q_current));
-  const wheelCurrents = useSelector((state: RootState) => state.telemetryStore.wheels
-      .map(w => w.q_current));
+
+  const pivots = useSelector((state: RootState) => state.telemetryStore.pivots)
+  const pivotCurrents = pivots.map(p => p.q_current);
+
+  const wheels = useSelector((state: RootState) => state.telemetryStore.wheels)
+  const wheelCurrents = wheels.map(w => w.q_current);
 
   useEffect(() => {
     bifrost.syncWithRover();
