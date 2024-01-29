f090bb7e37aa48b5f46ebd6d6d834f5834b3c851
Monash Nova Rover <novaroverteam@monash.edu>
2024-01-29 14:52:13 +1100

    skeleton for layout, and added documentation

---
+++ src/ros/nova-gui/nova-gui/src/components/RamanSpec/RamanCCDInputs.tsx
@@ -0,0 +1,13 @@
+/**
+ * Author: Connor Macdougall
+ * This component is for the CCD inputs.
+ * It sends request to the 'raman_spectra' ROS service.
+ */
+
+const RamanCCDInputs: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default RamanCCDInputs;
