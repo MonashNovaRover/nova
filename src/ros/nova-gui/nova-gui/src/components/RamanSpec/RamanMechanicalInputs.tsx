11a57da525e934626298256dec14207169acbd84
Monash Nova Rover <novaroverteam@monash.edu>
2024-01-29 14:52:13 +1100

    skeleton for layout, and added documentation

---
+++ src/ros/nova-gui/nova-gui/src/components/RamanSpec/RamanMechanicalInputs.tsx
@@ -0,0 +1,13 @@
+/**
+ * Author: Connor Macdougall
+ * This component is for the mechanical inputs that would be necessary to collect raman spectra.
+ * This is currently under going changes mechanically
+ */
+
+const RamanMechanicalInputs: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default RamanMechanicalInputs;
---
+++ src/ros/nova-gui/nova-gui/src/components/RamanSpec/RamanOutputComparison.tsx
@@ -0,0 +1,13 @@
+/**
+ * Author: Connor Macdougall
+ * This component outputs responses from the CCD, and also has graphs to compare output data against.
+ * It accepts responses from the 'raman_spectra' ROS service.
+ */
+
+const RamanOutputComparison: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default RamanOutputComparison;
