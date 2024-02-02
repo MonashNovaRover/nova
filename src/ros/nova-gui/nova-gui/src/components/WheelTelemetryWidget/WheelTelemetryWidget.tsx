5fa8dc32e6408be5f8dab9bae9a3e185c1b26067
Bailey Chessum <bche0080@student.monash.edu>
2024-02-02 17:57:14 +1100

    fix: made rover-top-down.png dark

--- src/ros/nova-gui/nova-gui/src/components/WheelTelemetryWidget/WheelTelemetryWidget.tsx
+++ src/ros/nova-gui/nova-gui/src/components/WheelTelemetryWidget/WheelTelemetryWidget.tsx
@@ -67,7 +67,7 @@ const WheelTelemetryWidget: React.FC<IDriveWheelWidgetProps> = (props: IDriveWhe
           <span className="text-default-300 text-opacity-80">
             <ChevronUp size={20}></ChevronUp>
           </span>
-          <Image className="" radius="none" src={RoverTopDownImage}></Image>
+          <Image className="mx-2" radius="none" src={RoverTopDownImage}></Image>
         </div>
         {cellProps.map((cellProp, index) => (
             <WheelTelemetryWidgetCell
