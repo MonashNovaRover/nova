7e5b0cb7add11b3f259077651fa8c27ec3955ec1
Bailey Chessum <bche0080@student.monash.edu>
2024-02-05 21:14:15 +1100

    feat: draft of SiteSelectWidget.tsx

---
+++ src/ros/nova-gui/nova-gui/src/components/SiteSelectWidget/SiteSelectWidget.tsx
@@ -0,0 +1,23 @@
+import {Card, CardBody, CardHeader, CardProps} from "@nextui-org/react";
+import SegmentedPicker from "../SegmentedPicker/SegmentedPicker.tsx";
+
+const SiteSelectWidget: React.FC<CardProps> = (props) => {
+
+  return (
+    <Card {...props}>
+      <CardHeader className="pb-0">
+        Site Select
+      </CardHeader>
+      <CardBody>
+        <SegmentedPicker fullWidth>
+          <>Site 1</>
+          <>Site 2</>
+          <>Site 3</>
+          <>Site 4</>
+        </SegmentedPicker>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default SiteSelectWidget;
