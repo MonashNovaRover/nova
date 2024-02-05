16a01ed2d1ae12ded490ed09195c6479100af45f
Bailey Chessum <bche0080@student.monash.edu>
2024-02-05 19:00:47 +1100

    fix: SegmentedPicker.tsx type errors

--- src/ros/nova-gui/nova-gui/src/components/SegmentedPicker/SegmentedPicker.tsx
+++ src/ros/nova-gui/nova-gui/src/components/SegmentedPicker/SegmentedPicker.tsx
@@ -1,11 +1,10 @@
 import {Tab, Tabs, TabsProps} from "@nextui-org/react";
-import React, {Children, Key} from "react";
-
+import React, {Children, Key, ReactNode} from "react";
 
 interface SegmentedPickerProps extends TabsProps {
   selectedIndex?: number,
-  onIndexChange?: number,
-  children?: ReactChildren
+  onIndexChange?: ((index: number) => void),
+  children: ReactNode
 }
 
 /**
@@ -15,11 +14,25 @@ interface SegmentedPickerProps extends TabsProps {
  * onSelectionChange. Children should be the labels for each option, not Tab elements.
  * @constructor
  */
-const SegmentedPicker: React.FC<SegmentedPickerProps> = ({ children: children, ...props}) => {
-  const onSelectionChange = props.onIndexChange ? (key: Key) => props.onIndexChange(+key) : undefined;
-  const selectedKey = props.selectedIndex?.toString();
+const SegmentedPicker: React.FC<SegmentedPickerProps> = ({
+  children: children,
+  onIndexChange: onIndexChange,
+  selectedIndex: selectedIndex,
+  ...props
+}) => {
 
-  const titles = Children.toArray(children);
+  // Converts Key to number. We eliminate bigint, as no SegmentedPicker should have enough items to justify it.
+  const keyToNumber: (key: Key) => number = (key) => {
+    const value = key.valueOf();
+
+    if (typeof value === "bigint")
+      return Number(value);
+    return +value;
+  }
+
+  const onSelectionChange = !onIndexChange ? undefined
+    : (key: Key) => onIndexChange!(keyToNumber(key))
+  const selectedKey = selectedIndex?.toString();
 
   return (
     <Tabs {...props} onSelectionChange={onSelectionChange} selectedKey={selectedKey}>
