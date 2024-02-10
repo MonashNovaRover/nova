cd3d4ca6705ffcbbdff90663d03edb9788b1f08f
Joshua Leivenzon <jlei0014@student.monash.edu>
2024-02-10 23:43:56 +1100

    groot: Init

--- nixfiles/packages/other/default.nix
+++ nixfiles/packages/other/default.nix
@@ -3,6 +3,7 @@
 {
   github-gitignore = callPackage ./github-gitignore { };
   libgphoto2-theta = callPackage ./libgphoto2-theta { };
+  groot = callPackage ({ libsForQt5 }: libsForQt5.callPackage ./groot { }) { };
   gstthetauvc = callPackage ./gstthetauvc { };
   jcan = callPackage ./jcan { };
   libuvc-theta = callPackage ./libuvc-theta { };
