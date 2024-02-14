3234151e39da133ee58df13cf9395af39ed84122
Bailey Chessum <bche0080@student.monash.edu>
2024-02-14 19:59:42 +1100

    feat: added webgl float uniforms through component properties

--- src/ros/nova-gui/nova-gui/src/components/WebGL360CamWidget/vert.ts
+++ src/ros/nova-gui/nova-gui/src/components/WebGL360CamWidget/vert.ts
@@ -1,14 +1,22 @@
 const Vert = `#version 300 es
 
-precision highp float;
+precision mediump float;
 in vec4 a_position;
-in vec4 a_color;
+uniform float time;
+//in vec4 a_color;
+//out vec4 v_color;
 
-out vec4 v_color;
+out vec2 uv;
 
 void main() {
-  v_color = a_color;
-  gl_Position = a_position;
+  //v_color = a_color;
+  
+  // transform the positions to make the frame spin in a circle
+  gl_Position = vec4(a_position.x * 0.5, a_position.y * 0.5, a_position.zw)+ vec4(cos(time) * 0.5,sin(time) * 0.5,0,0);
+  
+  
+  
+  uv = (a_position.xy + vec2(1.0, 1.0)) / 2.0;
 }
 `
 export default Vert;
--- src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/WebGLCanvas.tsx
+++ src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/WebGLCanvas.tsx
@@ -2,11 +2,11 @@ import React, {memo, useEffect, useRef} from "react";
 import useGL from "./useGL.tsx";
 import {useProgram} from "./useProgram.tsx";
 import useVertexAttributes, {IVertexAttribute} from "./useVertexAttributes.tsx";
+import useUniforms, {vec, vec4} from "./useUniforms.tsx";
 
 
 
-export type vec2 = [number, number];
-export type vec4 = [number, number, number, number];
+
 
 
 
@@ -21,38 +21,47 @@ export interface IWebGLCanvasProps {
   height?: number,
   vertexCount?: number,
 
+  // When true, changes the width and height of the canvas automatically to be the width and height on the screen.
+  autoSize?: boolean
+
   vertexAttributes?: {[key: string] : IVertexAttribute}
 
   clearColor?: vec4
+
+  uniforms?: {[key: string] : vec}
 }
 
 
 
 const UnmemoedWebGLCanvas: React.FC<IWebGLCanvasProps> = ({
-  vert, frag, vertexCount, vertexAttributes, clearColor, ...canvasProps
+  vert, frag, vertexCount, vertexAttributes, clearColor, uniforms, ...canvasProps
 }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const gl = useGL(canvasRef);
   const program = useProgram(gl, vert, frag);
 
+  // Effect to update the clear color
   useEffect(() => {
     gl?.clearColor(...(clearColor ?? [0, 0, 0, 0]));
   }, [gl, clearColor]);
 
   // Apply vertex attributes. (might include a position buffer of some kind?)
-  useVertexAttributes(gl, program, vertexAttributes, vertexCount ?? 4);
+  useVertexAttributes(gl, program, vertexAttributes);
 
+  // Apply uniforms
+  useUniforms(gl, program, uniforms);
+
+  // Effect to redraw the canvas
   useEffect(() => {
-    if (!gl)
+    if (!gl || !program)
       return;
 
-    console.log("new GL created")
+    // Redraw
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexCount ?? 4);
+  }, [gl, program, uniforms, vertexAttributes, vertexCount]);
 
-  }, [gl]);
-
-
-
-  return <canvas {...canvasProps} ref={canvasRef}></canvas>;
+  return <canvas {...canvasProps}  ref={canvasRef}></canvas>;
 }
 
 const WebGLCanvas = memo(UnmemoedWebGLCanvas);
