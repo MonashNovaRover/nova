ae24bff09fef3dc0111390ecbbf9da32a1194538
Bailey Chessum <bche0080@student.monash.edu>
2024-02-15 23:48:50 +1100

    feat: Improved documentation and began sampler code

--- src/ros/nova-gui/nova-gui/src/components/WebGL360CamWidget/vert.ts
+++ src/ros/nova-gui/nova-gui/src/components/WebGL360CamWidget/vert.ts
@@ -6,6 +6,9 @@ uniform float time;
 //in vec4 a_color;
 //out vec4 v_color;
 
+in vec2 a_texCoord;
+out vec2 v_texCoord;
+
 out vec2 uv;
 
 void main() {
@@ -14,7 +17,7 @@ void main() {
   // transform the positions to make the frame spin in a circle
   gl_Position = vec4(a_position.x * 0.5, a_position.y * 0.5, a_position.zw)+ vec4(cos(time) * 0.5,sin(time) * 0.5,0,0);
   
-  
+  v_texCoord = a_texCoord;
   
   uv = (a_position.xy + vec2(1.0, 1.0)) / 2.0;
 }
