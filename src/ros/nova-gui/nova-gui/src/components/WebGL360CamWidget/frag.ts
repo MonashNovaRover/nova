54b0a29d35e4b49d3b078f2833486984aa8ba2d0
Bailey Chessum <bche0080@student.monash.edu>
2024-02-17 22:49:55 +1100

    feat: made myself a party with webgl

--- src/ros/nova-gui/nova-gui/src/components/WebGL360CamWidget/frag.ts
+++ src/ros/nova-gui/nova-gui/src/components/WebGL360CamWidget/frag.ts
@@ -7,28 +7,36 @@ in vec2 v_texCoord;
 out vec4 fragColor;
 
 uniform float time;
+uniform vec2 mousePos;
+
 uniform sampler2D webcam;
 //uniform sampler2D rover; 
 
 
 
 void main() {
-  // Time varying pixel color
-  vec3 col = 0.5 + 0.5 * cos(time + vec3(v_texCoord.xy, 0.0) + vec3(0,2,4));
+  
   
   //vec4 roverCol = texture(webcam, v_texCoord);
   //roverCol = vec4(roverCol.xyz * roverCol.w, roverCol.w); 
   
-  vec4 webcamCol = texture(webcam, v_texCoord);
+  float scale = 4.0;
+  float speed = 1.0;
+  vec2 velocityOffset = vec2(time) * speed;
+  
+  vec4 webcamCol = texture(webcam, scale * v_texCoord - 0.001 * mousePos + velocityOffset);
   webcamCol = vec4(webcamCol.xyz * webcamCol.w, webcamCol.w); 
-  webcamCol = (webcamCol * vec4(col, 1.0));
+  //webcamCol = (webcamCol * vec4(col, 1.0));
+  
+  // Time varying pixel color
+  vec3 col = 0.6 + 0.4 * cos(vec3(4.0*time) + vec3(velocityOffset, time*speed) + vec3(v_texCoord.xy, 2.0*v_texCoord.x + v_texCoord.y)* 8.0 + vec3(0,2,4));
    
   
   
   //vec4 mixImageCol = webcamCol + roverCol; // mix(webcamCol, roverCol, roverCol.w);
 
   // Output to screen
-  fragColor = webcamCol;
+  fragColor = webcamCol * vec4(col, 1.0);
   // texture ((vec4(v_texCoord, 0.0, 1.0) + vec4(col, 1.0)) * 0.5);
 }
 `
--- src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/webgl-utils/loadVideoTexture.ts
+++ src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/webgl-utils/loadVideoTexture.ts
@@ -53,8 +53,8 @@ export default function loadVideoTexture(gl: WebGL2RenderingContext, video: HTML
     } else {
       // No, it's not a power of 2. Turn off mips and set
       // wrapping to clamp to edge
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     }
 
@@ -73,8 +73,8 @@ export default function loadVideoTexture(gl: WebGL2RenderingContext, video: HTML
 
   // Turn off mips and set wrapping to clamp to edge so it
   // will work regardless of the dimensions of the video.
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 
   return texture ?? undefined;
