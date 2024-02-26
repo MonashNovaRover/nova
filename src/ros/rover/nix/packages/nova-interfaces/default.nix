1d16887e2959c0a02eda2acb30a12e767a2d5295
Dylan Gonzalez <dylcg10@gmail.com>
2024-02-26 14:52:53 +1100

    nix files for new refactored packages from core

---
+++ src/ros/rover/nix/packages/nova-interfaces/default.nix
@@ -0,0 +1,24 @@
+{ lib
+, buildRosPackage
+, ament-cmake
+, rosidl-default-generators
+, std-msgs
+, geometry-msgs
+, nav-msgs
+, launch
+, launch-ros
+}:
+
+buildRosPackage {
+  name = "nova-interfaces";
+  buildType = "ament_cmake";
+
+  src = builtins.path rec {
+    name = "nova-interfaces-source";
+    path = ../../../nova_interfaces;
+    filter = lib.novaSourceFilter [ ] path;
+  };
+
+  nativeBuildInputs = [ ament-cmake rosidl-default-generators ];
+  propagatedBuildInputs = [std-msgs nav-msgs geometry-msgs ];
+}
---
+++ src/ros/rover/nix/packages/rover-description/default.nix
@@ -0,0 +1,24 @@
+{ lib
+, buildRosPackage
+, ament-cmake
+, rosidl-default-generators
+, std-msgs
+, geometry-msgs
+, nav-msgs
+, launch
+, launch-ros
+}:
+
+buildRosPackage {
+  name = "rover-description";
+  buildType = "ament_cmake";
+
+  src = builtins.path rec {
+    name = "rover-description-source";
+    path = ../../../rover_description;
+    filter = lib.novaSourceFilter [ ] path;
+  };
+
+  nativeBuildInputs = [ ament-cmake rosidl-default-generators ];
+  #propagatedBuildInputs = [std-msgs nav-msgs geometry-msgs launch launch-ros ];
+}
