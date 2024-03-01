b591169fe9992e57144eddab4f5508b5948f5726
Kabilan VS <kabiniki4849@gmail.com>
2024-03-01 20:24:55 +1100

    fix: Bro I Forgot to Push

--- src/ros/nova-gui/nova-gui/src/views/shared/CamerasPage/CameraPageConstants.ts
+++ src/ros/nova-gui/nova-gui/src/views/shared/CamerasPage/CameraPageConstants.ts
@@ -12,53 +12,6 @@ export enum ARCCompModes {
   AUTONOMOUS = "autonomous",
 }
 
-export const XTREME_DELIVERY_CAMS: string[] = [
-  "mast_arm_stow",
-  "mast_down",
-  "mast_forward",
-  "mast_backward",
-  "arm_end_side",
-  "arm_end_top",
-  "arm_end_finger",
-  "arm_end_forward",
-];
-
-export const AUTO_CAMS: string[] = [
-  "mast_arm_stow",
-  "mast_down",
-  "mast_forward",
-  "mast_backward",
-];
-
-export const SCI_CAMS: string[] = ["camera5", "camera6"];
-
-export const EQUIPMENT_SERVICE: string[] = ["camera7", "camera8"];
-
-export const cameraSections: CameraView[] = [
-  {
-    viewTitle: "Extreme Delivery",
-    cameraSerials: XTREME_DELIVERY_CAMS,
-  },
-  {
-    viewTitle: "Autonomous",
-    cameraSerials: AUTO_CAMS,
-  },
-  {
-    viewTitle: "Science",
-    cameraSerials: SCI_CAMS,
-  },
-  {
-    viewTitle: "Equipment Servicing",
-    cameraSerials: EQUIPMENT_SERVICE,
-  },
-];
-
-export const allCams = [
-  ...XTREME_DELIVERY_CAMS,
-  ...AUTO_CAMS,
-  ...SCI_CAMS,
-  ...EQUIPMENT_SERVICE,
-];
 
 export const initialFilters: CameraFilters = {
   flipCamera: false,
@@ -71,8 +24,6 @@ export const initialFilters: CameraFilters = {
 export const post_landing_views: CameraView[] = [
   {
     cameraSerials: [
-      "camera1",
-      "Microsoft_Microsoft®_LifeCam_HD-3000",
       "mast_arm_stow",
       "mast_down",
       "mast_forward",
@@ -86,8 +37,6 @@ export const post_landing_views: CameraView[] = [
   },
   {
     cameraSerials: [
-      "Sonix_Technology_Co.__Ltd._BisonCam_NB_Pro",
-      "camera2",
       "mast_arm_stow",
       "mast_down",
       "mast_forward",
