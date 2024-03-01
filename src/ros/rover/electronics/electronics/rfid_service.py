984a4591491bc37b0785ff7babc635a50ba61b48
Bailey Chessum <bche0080@student.monash.edu>
2024-03-02 00:50:58 +1100

    feature: made rfid_service.py more robust through encoding output data

--- src/ros/rover/electronics/electronics/rfid_service.py
+++ src/ros/rover/electronics/electronics/rfid_service.py
@@ -21,30 +21,52 @@ TODO:
 """
 from core.srv import RFIDCommand
 
+import argparse
+
 import rclpy, time
 from rclpy.node import Node
 
+from std_msgs.msg import String
+from std_srvs.srv import Empty
+
+from urllib.parse import quote
+
 from serial import Serial
+
 
 class RFIDService(Node):
 
-    def __init__(self):
+    def __init__(self, port: str):
         super().__init__('rfid_service')
+
+        if port is None:
+            port = "/dev/ttyELBAUMRFID69420"
 
         self.get_logger().info("Waiting for RFID scanner to be plugged in.")
 
         # Loop until the RFID is plugged in
         while True:
             try:
-                self.ser = Serial(baudrate = 115200, port = '/dev/ttyELBAUMRFID69420') # TODO: Check port
+                self.ser = Serial(baudrate = 115200, port = port) # TODO: Check port
                 break
             except:
-                time.sleep(1.0)
+                time.sleep(0.1)
             
-        self.srv = self.create_service(RFIDCommand, '/electronics/rfid_service', self.handle_rfid_request)   
-        self.EOM = b'\r' # carriage ret for EOM
+        self.srv = self.create_service(RFIDCommand, '/electronics/rfid_service', self.handle_rfid_request)
+
+        self.read_service = self.create_service(RFIDCommand, '/electronics/rfid/read', self.handle_read)
+
+        self.unprocessed = ""
+
+        self.service_arduino_timer = self.create_timer(0.05, self.service_arduino)
+
+        self.data_publisher = self.create_publisher(String, "/electronics/rfid/data", 10)
+
+        self.EOM = b'\n' # carriage ret for EOM
         self.get_logger().set_level(10) # FOR DEBUGGING
         self.get_logger().info('Started RFID service successfully!')
+
+
 
     def handle_rfid_request(self, request: RFIDCommand, response):
         self.get_logger().info(f'Processing {request.command} RFID request')
@@ -53,7 +75,7 @@ class RFIDService(Node):
 
         if cmd in ['read', 'clear', 'restart', 'dump']:
             self.write_msg(cmd)
-            response.response = self.read_data()
+            response.response = "output has been moved" #self.read_data()
         elif cmd in ['write', 'poll']:
             self.write_msg(cmd)
             self.write_msg(request.data)
@@ -71,6 +93,10 @@ class RFIDService(Node):
             # sometimes crashes here which we don't want to do. If we can't log its not a massive issue
             pass
         return response
+
+    def handle_read(self, request: Empty, response):
+        self.write_msg("read")
+        return response
     
     def read_data(self) -> str:
         '''
@@ -78,20 +104,103 @@ class RFIDService(Node):
         '''
         # read until EOM
         self.get_logger().debug('Reading data')
-        input_buffer_length = self.ser.in_waiting
-        data = self.ser.read(size=input_buffer_length)
-        data = data.strip(self.EOM) # remove EOM from response
-        data = data.strip(b'\0') # strip any null chars from data
-        self.get_logger().debug('data')
-        print(data) #print raw bytes
+
+        # input_buffer_length = self.ser.in_waiting
+        # data = self.ser.read(size=input_buffer_length)
+        #data = data.strip(self.EOM) # remove EOM from response
+        # data = data.strip(b'\0') # strip any null chars from data
+        # self.get_logger().debug('data')
+
+        # print(data) #print raw bytes
+
         # return as string
         try:
-            return str(data)
+            return "e"# str(data)
         except Exception:
             self.get_logger().error('Failed to decode RFID arduino response')
             # dump raw hex to logger
-            self.get_logger().error(f'Raw data: {data}')
+            #self.get_logger().error(f'Raw data: {data}')
             return 'Service error: Failed to decode message'
+
+    def service_arduino(self):
+        """
+        Handles any data from the arduino
+        """
+        if self.ser.in_waiting > 0:
+            rawdata = self.ser.read(self.ser.in_waiting)
+            try:
+                new_text = rawdata.decode("ascii")
+                self.unprocessed += new_text
+            except:
+                self.get_logger().error(f"Failed to decode raw data into ascii: \"{quote(rawdata)}\"")
+
+
+        split_unprocessed_data = self.unprocessed.split('\n', 1)
+        while len(split_unprocessed_data) == 2:
+            line = split_unprocessed_data[0]
+            self.unprocessed = split_unprocessed_data[1]
+
+            self.process_serial_input(line)
+
+            split_unprocessed_data = self.unprocessed.split('\n', 1)
+
+
+    def process_serial_input(self, data: str):
+        """
+        Using data sent over serial by the arduino, call an appropriate function to handle the message.
+        :param data: The line sent by the arduino
+        :return: None
+        """
+        if len(data.strip()) == 0:
+            return
+
+        command = ""
+        args = ""
+
+        split_data = data.split(' ', 1)
+        if len(split_data) == 2:
+            [command, args] = split_data
+        elif len(split_data) == 1:
+            command = data
+
+        command = command.lower().strip(':').strip()
+
+        if command == "data":
+            self.on_received_data(args)
+        elif command == "error":
+            self.get_logger().error("Arduino: " + args)
+        elif command == "info":
+            self.get_logger().info("Arduino: " + args)
+        elif command == "debug":
+            self.get_logger().debug("Arduino: " + args)
+        elif command == "warn" or command == "warning":
+            self.get_logger().warn("Arduino: " + args)
+        else:
+            self.get_logger().warn(f"Arduino sent unknown command \"{quote(command)}\" with args \"{quote(args)}\"")
+
+    def on_received_data(self, data):
+        data_values = data.strip().split(' ')
+
+        # Try convert
+        def process_hex(value: str) -> str:
+            try:
+                return chr(int(value, 16))
+            except:
+                self.get_logger().warn(f"Tried to parse \"{quote(value)}\" as hex value when reading data.")
+                return '\0'
+
+        chars = [process_hex(x) for x in data_values]
+        txt = ''.join(chars)
+
+        txt = txt.strip()
+        txt = txt.strip('\0').rstrip()
+
+        self.get_logger().info(f"data: \"{txt}\".")
+
+        msg = String()
+        msg.data = txt
+
+        self.data_publisher.publish(msg)
 
     def write_msg(self, msg: str):
         '''
@@ -104,7 +213,6 @@ class RFIDService(Node):
         msg.rstrip('\0') 
         data = bytearray(msg, encoding='ascii')
         data.append(ord(self.EOM)) #add EOM
-
         CHUNK_SIZE = 60
 
         for i in range(0, len(data), CHUNK_SIZE):
@@ -117,10 +225,19 @@ class RFIDService(Node):
         self.ser.close()
         return super().destroy_node()
 
+def cli_parser():
+    parser = argparse.ArgumentParser(description="The RFID node", usage="rfid [-p PORT]")
+    parser.add_argument("-p", "--port", type=str, default="/dev/ttyELBAUMRFID69420", help="the port to use for usb serial")
+    args = parser.parse_args()
+
+    return args
+
 def main(args=None):
     rclpy.init(args=args)
 
-    rfid_service = RFIDService()
+    args2 = cli_parser()
+
+    rfid_service = RFIDService(args2.port)
 
     rclpy.spin(rfid_service)
 
