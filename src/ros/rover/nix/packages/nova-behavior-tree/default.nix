21d5000269000129966e59fa16d0676159e1238b
Monash Nova Rover <novaroverteam@monash.edu>
2024-03-04 21:44:18 +1100

    Working parameters

--- src/ros/rover/nix/packages/nova-behavior-tree/default.nix
+++ src/ros/rover/nix/packages/nova-behavior-tree/default.nix
@@ -6,6 +6,9 @@
 , pluginlib
 , rosidl-default-generators
 , behaviortree-cpp-v3
+, geometry-msgs
+, tf2-ros
+, nav2-util
 , std-srvs
 , launch
 , launch-ros
@@ -22,6 +25,6 @@ buildRosPackage {
   };
 
   nativeBuildInputs = [ ament-cmake rosidl-default-generators ];
-  buildInputs = [ pluginlib std-srvs rclcpp nav2-behavior-tree behaviortree-cpp-v3 ];
+  buildInputs = [ pluginlib std-srvs rclcpp nav2-behavior-tree behaviortree-cpp-v3 nav2-util tf2-ros geometry-msgs];
   propagatedBuildInputs = [ launch launch-ros];
 }
