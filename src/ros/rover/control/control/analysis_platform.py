2c528dfd0eca0a4df608657cddb897295b4fd18e
Tristan Clark <tcla0012@student.monash.edu>
2024-03-08 22:57:28 +1100

    add documentation to analysis platform

--- src/ros/rover/control/control/analysis_platform.py
+++ src/ros/rover/control/control/analysis_platform.py
@@ -2,19 +2,18 @@
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Purpose: Control Auger Height and Drill Spin using Joysticks
+Purpose: Control Analysis Arm Platform using Joysticks
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-NODE: auger
+NODE: analysis_platform
 TOPICS:
   - subscriber: /control/input_joystick_l [InputJoystick]
-  - subscriber: /control/input_joystick_r [InputJoystick]
 SERVICES: None
 ACTIONS: None
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PACKAGE:
 AUTHOR(S):	Tristan Clark
-CREATION:	24/02/2024
-EDITED:		24/02/2024
+CREATION:	08/03/2024
+EDITED:		08/03/2024
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
@@ -44,7 +43,7 @@ class AnalysisPlatformNode(Node):
     PLATFORM_UP = Direction.POSITIVE
     PLATFORM_DOWN = Direction.NEGATIVE
     # max_velocity
-    MAX_VELOCITY_PERCENT = 0.5
+    MAX_VELOCITY_PERCENT = 0.75
     # time of flight
     TIME_OF_FLIGHT_OFFSET = 10
     TIME_OF_FLIGHT_BOTTOM = 20
@@ -61,40 +60,54 @@ class AnalysisPlatformNode(Node):
         super().__init__("analysis_platform")
 
         self.get_logger().set_level(logging.DEBUG)
+
+        # ROS parameters: used so that they can be changed without recompiling or rerunning the node
         self.declare_parameter(self.CAN_BUS_PARAM, self.CAN_BUS)
         self.declare_parameter(self.PLATFORM_MAX_VEL_PERCENT_PARAM, self.MAX_VELOCITY_PERCENT)
         self.declare_parameter(self.TIME_OF_FLIGHT_BOTTOM_PARAM, self.TIME_OF_FLIGHT_BOTTOM)
 
-        self.platform = OneAxisControlLimits(max_percent=self.get_parameter(self.PLATFORM_MAX_VEL_PERCENT_PARAM).value)
-        self.platform_controller = JonoCardController(card_id=self.JONO_ID_PLATFORM, 
-                                                      pos_command=self.PLATFORM_UP_COMMAND, 
-                                                      neg_command=self.PLATFORM_DOWN_COMMAND, 
-                                                      control=self.platform)
-        
-        self.top_limit = False
-        self.bottom_limit = False
+        # Create the custom control classes
+        # Platform control
+        self.platform = OneAxisControlLimits(
+            max_percent=self.get_parameter(self.PLATFORM_MAX_VEL_PERCENT_PARAM).value
+        )
+        # Platform card controller for Jono Card
+        self.platform_controller = JonoCardController(
+            card_id=self.JONO_ID_PLATFORM, 
+            pos_command=self.PLATFORM_UP_COMMAND, 
+            neg_command=self.PLATFORM_DOWN_COMMAND, 
+            control=self.platform
+        )
+
         self.joystick_lock = True
 
-
+        # Set up the QoS profile and deadline callback
         deadline = Duration(nanoseconds=2e8)        
         events = SubscriptionEventCallbacks(deadline=self.deadline_callback)
         self.qos = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT, depth=1, deadline=deadline)
 
+        # Create the joystick subscribers
         self.joystick_l_sub = self.create_subscription(InputJoystick, "/control/input_joystick_l", self.joystick_l_callback, self.qos, event_callbacks=events)
 
         self.bus = jcan.Bus()
         self.bus.set_id_filter([self.JONO_ID_TIME_OF_FLIGHT, self.JONO_ID_LIMIT_SWITCH])
 
+        # Add callbacks for receiving can feedback
+        # Time of flight sensor and limit switch
         self.bus.add_callback(self.JONO_ID_TIME_OF_FLIGHT, self.callback_receive_can_time_of_flight)
         self.bus.add_callback(self.JONO_ID_LIMIT_SWITCH, self.callback_receive_can_limit_switch)
 
+        # Open the can bus
         self.bus.open(self.get_parameter(self.CAN_BUS_PARAM).value)
+
+        # Create timers for sending can commands and spinning the bus
         self.timer_jcan_commands = self.create_timer(0.05, self.callback_send_can_commands)
         self.timer_jcan_spin = self.create_timer(0.01, self.bus.spin)
 
 
     def callback_send_can_commands(self):
-        """Take current internal state and publish over CAN
+        """
+        Take current internal state and publish over CAN
         Sends can commands for auger and drill together
         """
         # The list of values will be cast to uint8's by JCAN library - so be careful to double check the values!
@@ -109,6 +122,11 @@ class AnalysisPlatformNode(Node):
             print(e)
 
     def convert_time_of_flight(self, raw_height: int) -> float:
+        """
+        Convert raw time of flight sensor reading (in cm)
+        to remove offset height and a base bottom reading greater than 0
+        Final value should be close to real height (in cm)
+        """
         height = raw_height - self.TIME_OF_FLIGHT_OFFSET - self.get_parameter(self.TIME_OF_FLIGHT_BOTTOM_PARAM).value
         if height < 0:
             height = 0
@@ -149,11 +167,17 @@ class AnalysisPlatformNode(Node):
             self.get_logger().info(f"Received unknown frame {frame}")
         
     def deadline_callback(self, _info):
+        """
+        Callback for when the deadline is missed
+        """
         # Set all speeds to 0
         self.get_logger().warning("200ms Callback deadline missed")
         self.platform.stop()
 
     def check_joystick_lock(self):
+        """
+        Checks if the joysticks are locked
+        """
         if self.joystick_lock:
             self.get_logger().info("Joysticks locked!")
             self.platform.stop()
@@ -172,11 +196,13 @@ class AnalysisPlatformNode(Node):
             self.joystick_lock = False
 
     def update_platform_height(self, joystick_l: InputJoystick):
-        # Auger height direction is determined by the right joystick's x-axis direction
+        # analysis platform height direction is determined by the right joystick's x-axis direction
         self.platform.update_direction(self.PLATFORM_DOWN if joystick_l.ax_stick_x >= 0 else self.PLATFORM_UP)
         self.platform.update_velocity(abs(int(joystick_l.ax_stick_x)))
 
         # button override time of flight
+        # allows operators to lower the platform even 
+        # if the time of flight sensor is reading the 0 / reached bottom
         if joystick_l.btn_thumb_d_state >= 1:
             self.platform.update_velocity(velocity=0.1, ignore_limits=True)
             self.platform.update_direction(self.PLATFORM_DOWN)
