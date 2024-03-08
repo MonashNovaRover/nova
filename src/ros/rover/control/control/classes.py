1706fea1fcf0a1daabfbf95013903a3b8bc72605
Tristan Clark <tcla0012@student.monash.edu>
2024-03-08 18:40:45 +1100

    create python classes for basic Jono and CMD controls

---
+++ src/ros/rover/control/control/classes.py
@@ -0,0 +1,118 @@
+from enum import Enum
+from struct import pack
+import jcan
+
+
+class Card(Enum):
+    CMD = "CMD"
+    JONO = "JONO"
+
+class Direction(Enum):
+    POSITIVE = 1
+    NEGATIVE = -1
+
+class OneAxisControl:
+    def __init__(self, direction: Direction = Direction.POSITIVE, velocity: float = 0.0, max_percent: float = 1.0):
+        self.direction = direction
+        self.velocity = velocity
+        self.max_percent = max_percent
+
+    def update_direction(self, direction: Direction):
+        if (direction == Direction.POSITIVE or direction == Direction.NEGATIVE):
+            self.direction = direction
+        else:
+            raise ValueError("Invalid direction")
+        
+    def update_velocity(self, velocity: float):
+        if (0.0 <= velocity <= 1.0):
+            self.velocity = velocity
+        else:
+            raise ValueError("Invalid velocity")
+        
+    def update_max_percent(self, max_percent: float):
+        if (0.0 <= max_percent <= 1.0):
+            self.max_percent = max_percent
+        else:
+            raise ValueError("Invalid max_percent")
+
+
+class CardInterface():
+    def __init__(self, card: Card, max_value: int, card_id: hex, control: OneAxisControl):
+        self.card = card
+        self.max_value = max_value
+        self.card_id = card_id
+        self.control = control
+
+    def update_direction(self, direction: Direction):
+        self.control.update_direction(direction)
+
+    def update_velocity(self, velocity: float):
+        self.control.update_velocity(velocity)
+
+    def update_max_percent(self, max_percent: float):
+        self.control.update_max_percent(max_percent)
+
+    def update_velocity(self, velocity: float):
+        if (velocity > 1.0):
+            velocity = 1.0
+        elif (velocity < 0.0):
+            velocity = 0.0
+        self.control.velocity = velocity
+
+    def get_card(self):
+        return self.card
+    
+    def get_max_value(self):
+        return self.max_value
+    
+    def get_frame(self) -> jcan.Frame:
+        pass
+
+
+    
+class JonoCardController(CardInterface):
+    def __init__(self, card_id: hex, pos_command: hex, neg_command: hex, control: OneAxisControl):
+        super().__init__(card=Card.JONO, max_value=255, card_id=card_id, control=control)
+        self.pos_command = pos_command
+        self.neg_command = neg_command
+        self.control = control
+
+    def get_frame(self) -> jcan.Frame:
+
+        command: hex
+        if self.control.direction == Direction.POSITIVE:
+            command = self.pos_command
+        else:
+            command = self.neg_command
+        
+        data = int(self.control.velocity * self.max_value * self.control.max_percent)
+
+        if data > self.max_value:
+            data = self.max_value
+
+        packed_data = [command, data]
+
+        frame = jcan.Frame(id=self.card_id, data=packed_data)
+
+        return frame
+    
+
+class CMDCardController(CardInterface):
+    def __init__(self, card_id: hex, control: OneAxisControl):
+        super().__init__(card=Card.CMD, max_value=32767, card_id=card_id)
+        self.control = control
+
+    def get_frame(self) -> jcan.Frame:
+        
+        data = int(self.control.direction.value * self.control.velocity * self.max_value * self.control.max_percent)
+
+        packed_data = list(pack('>h', int(data)))
+
+        frame = jcan.Frame(id=self.card_id, data=packed_data)
+
+        return frame
+            
+            
+        
+
+
