010e299b7e0c047addcdd7bde0788c454d155f15
Dylan Gonzalez <dylcg10@gmail.com>
2024-03-09 21:08:26 +1100

    update Marker publisher w/ colors + text

--- src/ros/rover/autonomous/autonomous/vis/nn_3d_to_marker.py
+++ src/ros/rover/autonomous/autonomous/vis/nn_3d_to_marker.py
@@ -30,10 +30,12 @@ from vision_msgs.msg import Detection3DArray, Detection3D
 from geometry_msgs.msg import PoseStamped
 
 COLORS = {
-    "Class_0": (0.9, 0.9, 0.9),
-    "Class_1": (0.9, 0.45, 0.0),
-    "Class_2": (0.0, 0.9, 0.0),
-    "Class_3": (0.0, 0.0, 0.9),
+    "0": (0.0, 0.0, 0.9),
+    "1": (0.0, 0.0, 0.0),
+    "2": (0.0, 0.9, 0.0),
+    "3": (0.96, 0.66, 0.05),
+    "4": (1.0, 1.0, 1.0),
+    "5": (0.9, 0.88, 0.12)
 }
 
 class MarkerPublisher(Node):
@@ -42,6 +44,7 @@ class MarkerPublisher(Node):
         self.sub_goals = self.create_subscription(Detection3DArray, "/oak/nn/spatial_detections", self.cb, 10)
 
         self.publisher = self.create_publisher(MarkerArray, "/oak/nn/spatial_detections_markers", 10)
+        self.publisher_text = self.create_publisher(MarkerArray, "/oak/nn/spatial_detections_text", 10)
 
         self.last_detections : Detection3DArray = None
 
@@ -51,7 +54,7 @@ class MarkerPublisher(Node):
     def cb(self, msg):
         self.last_detections = msg
 
-    def get_marker(self, point, c: tuple, index: int, namespace: str) -> None:
+    def get_marker(self, point, c: tuple, index: int, type_) -> None:
         """
         :params: c is color tuple (r,g,b) between 0 and 1
         """
@@ -60,14 +63,18 @@ class MarkerPublisher(Node):
         msg.pose.position.y = point[1]
         msg.pose.position.z = point[2]
         msg.pose.orientation.w = 1.0
-        msg.type = Marker.CUBE
+        msg.type = type_
         msg.scale.x = .15
         msg.scale.y = .15
-        msg.scale.z = 1.0
+        msg.scale.z = .15
         msg.color.r = c[0]
         msg.color.g = c[1]
         msg.color.b = c[2]
         msg.color.a = 1.
+
+        if type_ == Marker.TEXT_VIEW_FACING:
+            msg.text = f'{point[0]:.2f}' + "," + f'{point[1]:.2f}'
+
         msg.lifetime = Duration(seconds=0.2).to_msg()
         # Namespace - raw messages can be separated from confirmed cubes
         #msg.ns = namespace
@@ -80,24 +87,31 @@ class MarkerPublisher(Node):
         Publishes the markers
         """
         msg = MarkerArray()
+        msg_text = MarkerArray()
         det : Detection3D
-        for i, det in enumerate(self.last_detections.detections):
-            color = COLORS["final"]
-            
-            print("det [pub]: ", det)
-            det_pose = det.results[0].pose.pose
-            marker : Marker = self.get_marker((det_pose.position.x, det_pose.position.y, 0.), color, i, "")
-            marker.header.frame_id = self.last_detections.header.frame_id
-            msg.markers.append(marker)
+        if self.last_detections != None:
+            for i, det in enumerate(self.last_detections.detections):
+                #print("det [pub]: ", det)
+                det_pose = det.results[0].pose.pose
+                marker : Marker = self.get_marker((det_pose.position.x, det_pose.position.y, 0.), COLORS[det.results[0].hypothesis.class_id], i, Marker.CUBE)
+                marker_text : Marker = self.get_marker((det_pose.position.x, det_pose.position.y, 0.4), COLORS[det.results[0].hypothesis.class_id], i, Marker.TEXT_VIEW_FACING)
+
+                marker.header.frame_id = self.last_detections.header.frame_id
+                marker_text.header.frame_id = self.last_detections.header.frame_id
+                msg.markers.append(marker)
+                msg_text.markers.append(marker_text)
+
         self.publisher.publish(msg)
+        self.publisher_text.publish(msg_text)
 
     def timer_callback(self):
         """
         Called every timer_period. Publishes to self.publisher
         :return:
         """
-        if self.last_detections is not None:
-            self.pub()
+        #if self.last_detections is not None:
+        #   self.pub()
+        self.pub()
 
 def main():
     rclpy.init()
