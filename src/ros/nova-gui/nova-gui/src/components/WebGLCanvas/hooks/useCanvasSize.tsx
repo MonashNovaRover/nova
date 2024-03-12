f496c27f14cdbe6babb3a9cc5cc9829f0c064dab
Bailey Chessum <bche0080@student.monash.edu>
2024-03-12 19:46:50 +1100

    feat: created dedicated microscope thresholding component

--- src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/hooks/useCanvasSize.tsx
+++ src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/hooks/useCanvasSize.tsx
@@ -1,26 +1,32 @@
 import {useEffect, useLayoutEffect, useState} from "react";
 import {CanvasWithGL} from "./useGL.tsx";
+import {size} from "lodash";
 
-export default function useCanvasSize({gl, canvasRef} : CanvasWithGL): [number, number] {
+export default function useCanvasSize({gl, canvasRef} : CanvasWithGL, sizeTarget?: Element): [number, number] {
   const [width, setWidth] = useState(4);
   const [height, setHeight] = useState(3);
 
-  useLayoutEffect(() => {
-    const pixelRatio = window.devicePixelRatio
 
+
+  useLayoutEffect(() => {
     const canvas = canvasRef.current;
-    if (canvas) {
-      const resize = () => {
-        const rect = canvas.parentElement?.getBoundingClientRect() ?? canvas.getBoundingClientRect();
-        setWidth(pixelRatio * rect.width);
-        setHeight(pixelRatio * rect.height);
-      };
-      const resizeObserver = new ResizeObserver(resize);
-      resizeObserver.observe(canvas);
-      const observer = new MutationObserver(resize);
-      observer.observe(canvas, {attributes: true, attributeFilter: ["style"]});
-    }
-  }, []);
+    if (!canvas)
+      return;
+
+    const pixelRatio = window.devicePixelRatio
+    const absoluteSizeTarget = sizeTarget ?? canvas;
+
+    const resize = () => {
+      const rect = canvas.parentElement?.getBoundingClientRect() ?? canvas.getBoundingClientRect();
+      setWidth(pixelRatio * rect.width);
+      setHeight(pixelRatio * rect.height);
+    };
+
+    const resizeObserver = new ResizeObserver(resize);
+    resizeObserver.observe(absoluteSizeTarget);
+    const observer = new MutationObserver(resize);
+    observer.observe(absoluteSizeTarget, {attributes: true, attributeFilter: ["style"]});
+  }, [sizeTarget]);
 
   useEffect(() => {
     if (!gl)
@@ -28,9 +34,7 @@ export default function useCanvasSize({gl, canvasRef} : CanvasWithGL): [number, 
 
     console.log("Resized canvas")
 
-
-    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-
+    gl.viewport(0, 0, width, height);
   }, [gl, width, height]);
 
   return [
