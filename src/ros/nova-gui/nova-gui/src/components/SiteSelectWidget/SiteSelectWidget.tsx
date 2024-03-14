c89bfeaab8043aaf1133d4901830995d891f1a6a
Bailey Chessum <bche0080@student.monash.edu>
2024-03-14 19:30:02 +1100

    feat: added icons for the space resources file type selection

--- src/ros/nova-gui/nova-gui/src/components/SiteSelectWidget/SiteSelectWidget.tsx
+++ src/ros/nova-gui/nova-gui/src/components/SiteSelectWidget/SiteSelectWidget.tsx
@@ -1,7 +1,7 @@
 import {Card, CardBody, CardHeader, CardProps, Select, SelectItem} from "@nextui-org/react";
 import SegmentedPicker from "../SegmentedPicker/SegmentedPicker.tsx";
 import React from "react";
-import {Circle, Droplet} from "react-feather";
+import {Box, Droplet} from "react-feather";
 import SpaceResourceSiteType from "../nir-probe/SpaceResourcesSiteType.tsx";
 import SpaceResourcesSiteType from "../nir-probe/SpaceResourcesSiteType.tsx";
 
@@ -27,7 +27,7 @@ const siteTypeSelectOptions = [
   {
     type: SpaceResourceSiteType.ILMENITE,
     name: "Ilmenite",
-    icon: (<Circle/>)
+    icon: (<Box/>)
   }
 ]
 
@@ -37,8 +37,23 @@ const SiteSelectWidget: React.FC<SiteSelectWidgetProps> = ({
 
   return (
     <Card {...cardProps}>
-      <CardHeader className="pb-0">
-        Site Select
+      <CardHeader className="pb-0 flex flex-row gap-3">
+        <div className="grow">Site Select</div>
+        <Select
+          selectedKeys={[`${currentSiteType}`]}
+          className="min-w-unit-32 w-48 shrink"
+          size="md"
+          labelPlacement="outside-left"
+          onChange={e => onSiteTypeChanged?.(+e.target.value as SpaceResourcesSiteType)}
+          aria-label="Site Type"
+          startContent={siteTypeSelectOptions[currentSiteType].icon}
+        >
+          {siteTypeSelectOptions.map(({type, name, icon}) => (
+            <SelectItem key={`${type}`} value={type} startContent={icon}>
+              {name}
+            </SelectItem>
+          ))}
+        </Select>
       </CardHeader>
       <CardBody className="flex flex-row gap-3">
 
@@ -50,21 +65,6 @@ const SiteSelectWidget: React.FC<SiteSelectWidgetProps> = ({
           <>Site 3</>
           <>Site 4</>
         </SegmentedPicker>
-
-        <Select
-          selectedKeys={[`${currentSiteType}`]}
-          className="w-32"
-          size="md"
-          labelPlacement="outside-left"
-          onChange={e => onSiteTypeChanged?.(+e.target.value as SpaceResourcesSiteType)}
-          aria-label="Site Type"
-        >
-          {siteTypeSelectOptions.map(({type, name, icon}) => (
-            <SelectItem key={`${type}`} value={type} startContent={icon}>
-              {name}
-            </SelectItem>
-          ))}
-        </Select>
 
       </CardBody>
     </Card>
