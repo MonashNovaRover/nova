77f94b9fa50cf777086e49476842cead1ace49df
Mariah McCleery <mariahmccleery@outlook.com>
2024-03-14 20:22:12 +1100

    Update arc base view to display nova logo

--- src/ros/nova-gui/nova-gui/src/views/arc/ARCBaseView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/arc/ARCBaseView.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import novaLogo from "../../assets/nova-logo.png";
 
 const ARCBaseView: React.FC = () => {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className="flex justify-center items-center h-full w-full">
+      <img src={novaLogo} className="w-5/12 opacity-60" alt="Nova Logo" />
     </div>
   );
 };
