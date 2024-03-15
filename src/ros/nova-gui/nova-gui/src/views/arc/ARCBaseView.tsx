e5ae749833550f909512abb2b8e33138111ea7a0
Tristan Clark <tcla0012@student.monash.edu>
2024-03-15 14:58:41 +1100

    cleanup arc base view

--- src/ros/nova-gui/nova-gui/src/views/arc/ARCBaseView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/arc/ARCBaseView.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import KilnWidget from "../../components/KilnWidget/KilnWidget";
-import WheelTelemetryWidget from "../../components/WheelTelemetryWidget/WheelTelemetryWidget";
-import DriveModeWidget from "../../components/DriveModeWidget/DriveModeWidget";
 
 const ARCBaseView: React.FC = () => {
   return (
     <div>
       <KilnWidget/>
-      <WheelTelemetryWidget/>
-      <DriveModeWidget/>
     </div>
   );
 };
