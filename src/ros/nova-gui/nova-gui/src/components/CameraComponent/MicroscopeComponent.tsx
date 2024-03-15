25b27885caae74001372e6837fb210b95cb68a68
Kabilan VS <51330370+KABILAN235@users.noreply.github.com>
2024-03-15 14:53:07 +1100

    update: Update Camera Component to the latest changes

--- src/ros/nova-gui/nova-gui/src/components/CameraComponent/MicroscopeComponent.tsx
+++ src/ros/nova-gui/nova-gui/src/components/CameraComponent/MicroscopeComponent.tsx
@@ -1,34 +1,25 @@
-import {
-  Card,
-  Slider,
-} from "@nextui-org/react";
-import { CameraComponent, CameraComponentProps } from "./CameraComponent";
+import { Card, Slider } from "@nextui-org/react";
+import { CameraComponent } from "./CameraComponent";
 import { useState } from "react";
+import { useCameraStreamer } from "./hooks/useCameraStreamer";
 
-interface MicroscopeComponentProps extends CameraComponentProps {
+export const MicroscopeComponent = () => {
+  const [zoomFocus, setZoomFocus] = useState<number>(0);
 
-}
-
-
-
-export const MicroscopeComponent = (props: MicroscopeComponentProps) => {
-  const { cameraName, cameraSerial } = props;
-  const [zoomFocus, setZoomFocus] = useState<number>(0)
+  useCameraStreamer();
 
   return (
     <Card className={`m-4`}>
-      <CameraComponent
-        cameraName={cameraName}
-        cameraSerial={cameraSerial} />
-      <Slider 
+      <CameraComponent cameraSerial={"microscope_camera"} />
+      <Slider
         className="max-w-full pt-2 pb-6 pl-8 pr-8"
-        size="lg" label="Zoom/Focus" 
-        startContent="1x" 
+        size="lg"
+        label="Zoom/Focus"
+        startContent="1x"
         endContent="100x"
         value={zoomFocus}
-        onChange={(value) => setZoomFocus(value as number)}/>
-
+        onChange={(value) => setZoomFocus(value as number)}
+      />
     </Card>
   );
 };
-
