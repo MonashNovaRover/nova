f59d4e2ff19488316c869bac63e8f152e65a3686
Tristan Clark <tcla0012@student.monash.edu>
2024-03-22 00:22:19 +1100

    add scraper card type ros params to ec launch file

--- src/ros/rover/core/launch/ec_rover.launch.py
+++ src/ros/rover/core/launch/ec_rover.launch.py
@@ -2,17 +2,20 @@
 import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 
 # Generate the launch file with all inputs
 def generate_launch_description():
     core_dir = get_package_share_directory('core')
     
-    return LaunchDescription([      
+    return LaunchDescription([ 
+        DeclareLaunchArgument('scraper_card_type', default_value='CMD', description='Card type for the scraper node'),     
         Node(
-            package='control', executable='scraper.py', output='screen', emulate_tty=True),
+            package='control', executable='scraper.py', output='screen', emulate_tty=True, parameters=[{'card_type': LaunchConfiguration('scraper_card_type')}]),
         Node(
             package='control', executable='tile_placer.py', output='screen', emulate_tty=True),
         IncludeLaunchDescription(
