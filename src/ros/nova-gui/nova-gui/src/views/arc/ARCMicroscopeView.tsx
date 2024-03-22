c248600ce3a2779afd824ca064908db0faee96ef
Bailey Chessum <bche0080@student.monash.edu>
2024-03-22 04:35:38 +0000

    swapped left and right cols for microscope view

--- src/ros/nova-gui/nova-gui/src/views/arc/ARCMicroscopeView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/arc/ARCMicroscopeView.tsx
@@ -7,17 +7,17 @@ import KilnWidget from "../../components/KilnWidget/KilnWidget.tsx";
 
 export const ARCMicroscopeView = () => {
   return (
-    <div className="p-3 w-screen min-h-screen max-h-full">
+    <div className="p-3 max-h-full">
       <div className="grid grid-flow-col auto-cols-fr justify-between items-stretch gap-3 ">
+        <div className="flex flex-col flex-grow col-span-3">
+          <MicroscopeThresholdWidget cameraSerial="science_microscope" />
+        </div>
         <div className="flex flex-col gap-3 col-span-2">
           <AnalysisPlatformHeight/>
           <DriveModeWidget />
           <WheelTelemetryWidget />
           <DriveSpeedWidget />
           <KilnWidget />
-        </div>
-        <div className="flex flex-col flex-grow col-span-3">
-          <MicroscopeThresholdWidget cameraSerial="science_microscope" />
         </div>
       </div>
     </div>
