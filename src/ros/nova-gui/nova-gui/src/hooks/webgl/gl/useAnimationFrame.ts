be24094b2102628ee14542d763ec969fbf84b86c
Bailey Chessum <bche0080@student.monash.edu>
2024-04-10 03:44:09 +1000

    fix: made react linter happy in useAnimationFrame.ts

--- src/ros/nova-gui/nova-gui/src/hooks/webgl/gl/useAnimationFrame.ts
+++ src/ros/nova-gui/nova-gui/src/hooks/webgl/gl/useAnimationFrame.ts
@@ -1,4 +1,4 @@
-import {useEffect, useRef} from "react";
+import {useCallback, useEffect, useRef} from "react";
 
 export default function useAnimationFrame(callback: (time: number, deltaTime: number) => void): void {
   // Use useRef for mutable variables that we want to persist
@@ -6,17 +6,17 @@ export default function useAnimationFrame(callback: (time: number, deltaTime: nu
   const requestRef = useRef(-1);
   const previousTimeRef = useRef(0);
 
-  const animationCallback = (time: number) => {
+  const animationCallback = useCallback((time: number) => {
     const deltaTime = time - (previousTimeRef.current ?? time);
     callback(time * 0.001, deltaTime * 0.001);
 
     previousTimeRef.current = time;
     requestRef.current = requestAnimationFrame(animationCallback);
-  }
+  }, [callback])
 
   useEffect(() => {
     requestRef.current = requestAnimationFrame(animationCallback);
     return () => cancelAnimationFrame(requestRef.current);
-  }, []); // Make sure the effect runs only once
+  }, [animationCallback]); // Make sure the effect runs only once
 }
 
