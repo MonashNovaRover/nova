6f4436b30f63ad2b8422237a53db807a673923f1
Bailey Chessum <bche0080@student.monash.edu>
2024-04-09 20:55:06 +1000

    doc: added some more documentation to the new OOP style webgl program state class

--- src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/hooks/program/GLProgramState.ts
+++ src/ros/nova-gui/nova-gui/src/components/WebGLCanvas/hooks/program/GLProgramState.ts
@@ -4,7 +4,11 @@ import initShaderProgram from "../../webgl-utils/initShaderProgram.ts";
 import {CanvasWithGL} from "../gl/useGL.ts";
 
 export default class GLProgramState implements RenderQueueItem {
+  // The actual WebGL program. This should not be exposed to the user of the hooks, so they must go through the effect
+  // queues when they want to modify webgl stuff.
   private program?: WebGLProgram;
+
+  // The effect queue allowing the user to run code that depends on the WebGLProgram in sync with the render loop
   public readonly queue: EffectQueue<[WebGL2RenderingContext, WebGLProgram]>;
 
   // The vertex shader source code
@@ -27,6 +31,12 @@ export default class GLProgramState implements RenderQueueItem {
     this.renderQueueID = gl.renderQueue.push(this);
   }
 
+  /**
+   * Supplies new shaders for the program to use, likely causing a recompilation.
+   * @param gl The graphics context
+   * @param vert The new vertex shader source code
+   * @param frag The new fragment shader source code
+   */
   public setShaders(gl: CanvasWithGL, vert: string, frag: string): void {
     // This condition is to prevent the program from being immediately recompiled by the use effect in useProgram.
     if (this.vert === vert && this.frag === frag)
@@ -38,6 +48,10 @@ export default class GLProgramState implements RenderQueueItem {
     gl.renderQueue.update(this.renderQueueID);
   }
 
+  /**
+   * The code ran when rendering a new frame.
+   * @param context The rendering context to make calls to
+   */
   public render(context: WebGL2RenderingContext): void {
     if (this.program === undefined)
       throw Error("RenderQueue tried to render GLProgramState without a program. Did you call RenderQueueItem.setup?");
@@ -48,8 +62,11 @@ export default class GLProgramState implements RenderQueueItem {
     context.drawArrays(context.TRIANGLE_STRIP, 0, this.numberOfVertices);
   }
 
+  /**
+   * Initializes the program when the context becomes ready to use
+   * @param context The rendering context used by the program
+   */
   public setup(context: WebGL2RenderingContext): void {
     this.program = initShaderProgram(context, this.vert, this.frag);
   }
-
 }
