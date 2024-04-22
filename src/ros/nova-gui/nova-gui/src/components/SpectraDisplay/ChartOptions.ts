ff896bfdc465d1ee5b0b552c31fdc17576ec9277
Connor Macdougall <cmac0046@student.monash.edu>
2024-04-22 21:45:19 +1000

    Copied relevant files from feat/raman-spec for PR
    Made distinct branch so that the appropriate changes can be put in PR.
    Raman spec is approaching GUI readiness but should not block this feature.

---
+++ src/ros/nova-gui/nova-gui/src/components/SpectraDisplay/ChartOptions.ts
@@ -0,0 +1,76 @@
+/*
+ * Presets for chart styling
+ * Author: Connor Macdougall
+*/
+
+import { ApexOptions } from "apexcharts";
+
+export enum ChartStyle {
+    Default,
+}
+
+export const ChartOptions = (style: ChartStyle): ApexOptions => {
+    if (style == ChartStyle.Default) {
+        return {
+            title: {
+                text: '',
+                align: "center",
+                floating: true,
+                style: {
+                    fontSize: '18px',
+                    color: '#fff'
+                },
+            },
+            stroke: {
+                curve: "smooth"
+            },
+            chart: {
+                animations: {enabled: false},
+              type: 'line',
+              background: "000",
+              toolbar: {
+                show: false
+              }
+            },
+            tooltip: {
+                theme: "dark",
+                fixed: {
+                    offsetX: 10,
+                    offsetY: 10
+                }
+            },
+            xaxis: {
+                title: {
+                    text: 'Raman Shift (1/cm)',
+                    style: {
+                        fontSize: '14px',
+                        color: '#fff'
+                    }
+                },
+                labels: {
+                    show: true,
+                }
+            },
+            yaxis: {
+                min: 0,
+                max: 100,
+                title: {
+                    text: 'Normalised intensity',
+                    style: {
+                        fontSize: '14px',
+                        color: '#fff'
+                    }
+                },
+                labels: {
+                    show: false,
+                }
+            },
+            grid: {
+                show: false
+            },
+            colors: ["#992F7B", "#C4841D"],
+          };
+    } else {
+        return ChartOptions(ChartStyle.Default);
+    }
+}
