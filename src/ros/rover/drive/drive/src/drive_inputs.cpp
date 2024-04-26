30188102b8de66c67f694df86033d2f8b870dc72
novarover <novaroverteam-projectmanager@monash.edu>
2024-04-26 19:25:14 +1000

    rename auto input commands topic

--- src/ros/rover/drive/drive/src/drive_inputs.cpp
+++ src/ros/rover/drive/drive/src/drive_inputs.cpp
@@ -220,9 +220,9 @@ DriveInputs::DriveInputs() : Node("drive_inputs")
 
     // Creates the input subscription
     gamepad_input_subscription = this->create_subscription<input_interfaces::msg::InputGamepad>(
-        "/input/input_gamepad", qos, std::bind(&DriveInputs::input_callback, this, _1), input_subscriber_options);
+        "/inputs/input_gamepad", qos, std::bind(&DriveInputs::input_callback, this, _1), input_subscriber_options);
     autonomous_commands_subscription = this->create_subscription<drive_interfaces::msg::DriveInput>(
-        "/input/autonomous_commands", qos, std::bind(&DriveInputs::autonomous_callback, this, _1), auto_subscriber_options);
+        "/inputs/autonomous_commands", qos, std::bind(&DriveInputs::autonomous_callback, this, _1), auto_subscriber_options);
     // Creates a timer function that runs a function on loop every 0.05 seconds
     drive_timer = this->create_wall_timer(DriveTimers::drive_control, std::bind(&DriveInputs::publish_cmds, this));
     info_timer = this->create_wall_timer(DriveTimers::drive_info, std::bind(&DriveInputs::publish_info, this));
