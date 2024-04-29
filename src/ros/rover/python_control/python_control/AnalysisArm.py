2cd458e05b20f886b4d2e2dc1377b6691de1cb79
Tristan Clark <tcla0012@student.monash.edu>
2024-04-29 17:50:42 +1000

    cleanup controller node

--- src/ros/rover/python_control/python_control/AnalysisArm.py
+++ src/ros/rover/python_control/python_control/AnalysisArm.py
@@ -1,29 +1,31 @@
 import jcan
-from control.ControllerNode import ControllerNode
-from control.classes.cards.CMDCardController import CMDCardController
-from control.classes.controls.OneAxisControl import OneAxisControl
-from control.control.classes.sensors.IntegerSensor import IntegerSensor
-from control.control.classes.limits.IntegerLimit import IntegerLimit
-from control.classes.limits.LimitSwitchLimit import LimitSwitchLimit
-from control.classes.sensors.LimitSwitchSensor import LimitSwitchSensor
+from python_control.ControllerNode import ControllerNode
+from python_control.classes.cards.CMDCardController import CMDCardController
+from python_control.classes.controls.OneAxisControl import OneAxisControl
+from python_control.classes.sensors.IntegerSensor import IntegerSensor
+from python_control.classes.limits.IntegerLimit import IntegerLimit
+from python_control.classes.limits.LimitSwitchLimit import LimitSwitchLimit
+from python_control.classes.sensors.LimitSwitchSensor import LimitSwitchSensor
 
 
 class AnalysisArm(ControllerNode):
 
-
     CAN_BUS = "can1"
-    CMD_ID = 0x00
+    CMD_ID = 0x10
     PLATFORM_MAX_PERCENT = 0.5
 
-    TOF_FRAME_ID = 0x00
-    LIMIT_SWITCH_FRAME_ID = 0x00
-    LIMIT_SWITCH_COMMAND_ID = 0x00
+    TOF_FRAME_ID = 0x01
+    LIMIT_SWITCH_FRAME_ID = 0x02
+    LIMIT_SWITCH_COMMAND_ID = 0x03
 
 
     def __init__(self):
         super.__init__("AnalysisArm", self.CAN_BUS)
-        self.bus = jcan.Bus()
 
+        ## Add CAN ID Filters
+        self.bus.set_id_filter([self.TOF_FRAME_ID, self.LIMIT_SWITCH_FRAME_ID])
+
+        ## Create sensors
         tof_sensor = IntegerSensor(
             bus=self.bus,
             frame_id=self.TOF_FRAME_ID,
@@ -35,6 +37,7 @@ class AnalysisArm(ControllerNode):
             command_id=self.LIMIT_SWITCH_COMMAND_ID,
         )
 
+        # Create limits
         platform_bottom_limit = IntegerLimit(
             bus=self.bus,
             maximum=False,
@@ -47,17 +50,25 @@ class AnalysisArm(ControllerNode):
             limit_switch=limit_switch_top
         )
 
+        ## Create controls
         platform = OneAxisControl(
             max_percent=self.PLATFORM_MAX_PERCENT,
             pos_limit=platform_bottom_limit,
             neg_limit=platform_top_limit,
         )
 
+        ## Create controllers
         self.platform_controller = CMDCardController(
             bus=self.bus,
             card_id=self.CMD_ID,
             control=platform,
         )
+
+        ## Add the controllers to the node's of controllers
+        self.add_controller("platform", self.platform_controller)
+
+        ## Start the CAN bus
+        self.start_can()
 
         
 
--- src/ros/rover/python_control/python_control/ControllerNode.py
+++ src/ros/rover/python_control/python_control/ControllerNode.py
@@ -18,6 +18,7 @@ EDITED:		24/02/2024
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import abc
 from python_control.classes.cards.CardController import CardController
 from python_control.classes.limits.Limit import Limit
 from python_control.classes.sensors.Sensor import Sensor
@@ -32,10 +33,9 @@ from rclpy.duration import Duration
 from core.msg import InputJoystick
 
 
-class ControllerNode(Node):
+class ControllerNode(abc.ABC, Node):
     # ROS parameter names
     CAN_BUS_PARAM = "can_bus"
-
 
     def __init__(self,  name: str, can_bus: str, logging_level: int = logging.INFO):
         super().__init__(name)
@@ -45,9 +45,12 @@ class ControllerNode(Node):
 
         self.declare_parameter(self.CAN_BUS_PARAM, can_bus)
 
+        self.bus = jcan.Bus()
+
         self.joystick_lock = True
 
         self.controllers : dict[str, CardController] = {}
+        self.limits : dict[str, Limit] = {}
         self.sensors : dict[str, Sensor] = {}
 
         deadline = Duration(nanoseconds=2e8)
@@ -57,27 +60,24 @@ class ControllerNode(Node):
         self.joystick_l_sub = self.create_subscription(InputJoystick, "/control/input_joystick_l", self.joystick_l_callback, self.qos, event_callbacks=events)
         self.joystick_r_sub = self.create_subscription(InputJoystick, "/control/input_joystick_r", self.joystick_r_callback, self.qos, event_callbacks=events)
 
+        self.timer_send_commands = self.create_timer(0.05, self.callback_send_commands)
+        self.timer_jcan_spin = self.create_timer(0.01, self.bus.spin)
+
         self.get_logger().info(f"{self.get_name()} started using {self.get_parameter(self.CARD_TYPE_PARAM).value} card type")
         self.get_logger().info("Joysticks Locked")
+
+    def start_can(self):
+        """Setup the CAN bus"""
+        self.bus.open(self.get_can_bus())
 
     def get_can_bus(self):
         return self.get_parameter(self.CAN_BUS_PARAM).value
 
     def add_controller(self, controller_name: str, controller: CardController):
         self.controllers[controller_name] = controller
-        if controller.control.pos_limit is not None:
-            pos_limit : Limit = controller.control.pos_limit
-            self.add_receive_can_callback(pos_limit.get_frame_id(), pos_limit.frame_callback)
-        if controller.control.neg_limit is not None:
-            neg_limit : Limit = controller.control.neg_limit
-            self.add_receive_can_callback(neg_limit.get_frame_id(), neg_limit.frame_callback)
 
     def add_sensor(self, sensor_name: str, sensor: Sensor):
         self.sensors[sensor_name] = sensor
-        self.add_receive_can_callback(sensor.get_frame_id(), sensor.frame_callback)
-
-
-
 
     def callback_send_commands(self):
         """Take current internal state and publish over CAN
@@ -101,81 +101,12 @@ class ControllerNode(Node):
         """
         Stop all motors
         """
-        for controller in self.controllers:
+        for controller in self.controllers.values():
             controller.control.stop()
-
-    # def callback_receive_can_time_of_flight(self, frame: jcan.Frame):
-    #     """Receive can feedback for auger limit switches
-    #     """
-    #     self.get_logger().debug(f"Received {hex(frame.id)} {frame.data}")
-    #     for controller in self.controllers:
-    #         if frame.id == frame_id:
-    #             controller.update_time_of_flight(frame)
-    #             return
-
-    #     if len(frame.data) != 2:
-    #         self.get_logger().error(f"Time of flight error")
-    #         return
-
-    #     if frame.id == self.JONO_ID_TIME_OF_FLIGHT:
-       
-    #         raw_height = int(frame.data[1] + (frame.data[0] << 8))
-    #         height = self.convert_time_of_flight(raw_height)
-    #         self.get_logger().debug(f"Raw height: {raw_height}, Converted height: {height}")
-    #         if height == 0:
-    #             self.get_logger().debug("Time of flight hit bottom")
-    #             self.platform.update_limit_neg(True)
-    #         else:
-    #             self.platform.update_limit_neg(False)
-
-    #         self.publish_time_of_flight(height)
-    #     else:
-    #         self.get_logger().warn(f"Received unknown frame {frame}")
-
-
-    def callback_receive_can_feedback(self, frame: jcan.Frame):
-        """Receive can feedback for auger limit switches
-        """
-        self.get_logger().debug(f"Received {hex(frame.id)} {frame.data}")
- 
-        if frame.id == self.CARD_ID_RECEIVE:
-            if frame.data[0] == self.AUGER_LIMIT_SWITCH_TOP:
-                if frame.data[1] == self.AUGER_LIMIT_SWITCH_CLEAR:
-                    self.top_limit = False
-                else:
-                    self.get_logger().debug("Top limit switch hit")
-                    self.top_limit = True
-               
-            elif frame.data[0] == self.AUGER_LIMIT_SWITCH_BOTTOM:
-                if frame.data[1] == self.AUGER_LIMIT_SWITCH_CLEAR:
-                    self.bottom_limit = False
-                else:
-                    self.get_logger().debug("Bottom limit switch hit")
-                    self.bottom_limit = True
-        else:
-            self.get_logger().warn(f"Received unknown frame {frame}")
-        
-
-
-    def auger_stop_state(self):
-        self.auger_velocity = 0
-        self.auger_direction = self.AUGER_UP
-
-    def drill_stop_state(self):
-        self.drill_velocity = 0
-        self.drill_direction = self.DRILL_CLOCKWISE
-
-    
-    def deadline_callback(self, _info):
-        # Set all speeds to 0
-        self.get_logger().warning("200ms Callback deadline missed")
-        self.auger_stop_state()
-        self.drill_stop_state()
 
     def check_joystick_lock(self):
         if self.joystick_lock:
-            self.auger_stop_state()
-            self.drill_stop_state()
+            self.stop_state()
             return True
         return False
 
@@ -188,48 +119,22 @@ class ControllerNode(Node):
         # Joysticks unlocked when bottom l5 button is pressed on the left joystick
         if joystick_l.btn_bottom_l5_state >= 1 and self.joystick_lock:
             self.get_logger().info("Joysticks Unlocked")
-            self.joystick_lock = False
-
-    def update_auger_height(self, joystick_r: InputJoystick):
-        # Auger height direction is determined by the right joystick's x-axis direction
-        self.auger_direction = self.AUGER_DOWN if joystick_r.ax_stick_x >= 0 else self.AUGER_UP 
-
-        # Auger velocity is determined by the right joystick's x-axis magnitude
-        # If the auger is at the top or bottom limit, the velocity is set to 0
-        if ((self.auger_direction == self.AUGER_UP and self.top_limit) or 
-            (self.auger_direction == self.AUGER_DOWN and self.bottom_limit)):
-            self.auger_velocity = 0
-        else:
-            self.auger_velocity = abs(int(self.get_parameter(self.AUGER_MAX_VELOCITY_PARAM).value * joystick_r.ax_stick_x))
-
-    def update_drill_spin(self, joystick_r: InputJoystick):
-        # Drill spin direction is determined by the right joystick thumb buttons
-        # Thumb right = clockwise, Thumb left = counterclockwise
-        if joystick_r.btn_thumb_r_state >= 1:
-            self.drill_direction = self.DRILL_CLOCKWISE
-        elif joystick_r.btn_thumb_l_state >= 1:
-            self.drill_direction = self.DRILL_COUNTERCLOCKWISE
-        
-        # Drill spin velocity is determined by the right joystick trigger
-        if joystick_r.btn_thumb_u_state >= 1:
-            self.drill_velocity = self.get_parameter(self.DRILL_MAX_VELOCITY_PARAM).value
-        else:
-            self.drill_velocity = 0      
+            self.joystick_lock = False    
 
     def joystick_l_callback(self, msg: InputJoystick):
         """
         Updates the classes internal msg state
         :return: None
         """
-        self.get_logger().debug("called l")
+        self.get_logger().debug("Left Joystick")
 
-        joystick_l = msg
+        self.update_joystick_lock(msg)
 
-        self.update_joystick_lock(joystick_l)
+        if self.check_joystick_lock():
+            return
 
-    
-
-
+        self.joystick_l(msg)
+        
 
     def joystick_r_callback(self, msg: InputJoystick):
         """
@@ -237,33 +142,26 @@ class ControllerNode(Node):
         :param msg: core.msg.RoverPose message from the subscriber callback
         :return: None
         """
-        self.get_logger().debug("called r")
-
-        joystick_r = msg
+        self.get_logger().debug("Right Joystick")
 
         if self.check_joystick_lock():
             return
+        
+        self.joystick_r(msg)
+        
+    @abc.abstractmethod
+    def joystick_r(self, joystick_r: InputJoystick):
+        pass
 
-        # Update the inputs
+    @abc.abstractmethod
+    def joystick_l(self, joystick_l: InputJoystick):
+        pass
+  
+        
 
-        self.update_auger_height(joystick_r)
-        self.update_drill_spin(joystick_r)
-
-          
 
 
-    # construct the can commands for the auger and drill
-    def get_can_commands(self):
-        auger_data_value = self.auger_direction * self.auger_velocity
-        drill_data_value = self.drill_direction * self.drill_velocity
-        self.get_logger().debug(f"Auger: {auger_data_value}, Drill: {drill_data_value}")
 
-        # pack the values into a list of bytes
-        # packing int into 2 bytes big endian
-        auger_data = list(pack('>h', int(auger_data_value)))
-        drill_data = list(pack('>h', int(drill_data_value)))
-
-        return auger_data, drill_data
 
 
 
