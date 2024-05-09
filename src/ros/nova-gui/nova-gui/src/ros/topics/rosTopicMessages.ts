f6c37636ee6c7044a0ebd8b294b55f974698bb03
Monash Nova Rover <novaroverteam@monash.edu>
2024-05-09 18:45:57 +1000

    updated source location for spec topic message

--- src/ros/nova-gui/nova-gui/src/ros/topics/rosTopicMessages.ts
+++ src/ros/nova-gui/nova-gui/src/ros/topics/rosTopicMessages.ts
@@ -31,5 +31,5 @@ export const rosTopicMessages = {
   [RosTopic.KILN_DATA]: "nova_interfaces/msg/KilnData",
   [RosTopic.NIR_DATA]: "nova_interfaces/msg/NIRProbeData",
   [RosTopic.MICROSCOPE_SERVO]: "nova_interfaces/msg/MicroscopeServoInfo",
-  [RosTopic.RAMAN_SPEC_MSG]: "core/msg/RamanSpectrum"
+  [RosTopic.RAMAN_SPEC_MSG]: "nova_interfaces/msg/RamanSpectrum"
 };
