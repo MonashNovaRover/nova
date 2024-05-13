b6cdc17b2be149ad5f11a9d2c4c40041b4a862a8
Tristan Clark <tcla0012@student.monash.edu>
2024-05-13 18:01:34 +1000

    Add apply button to Sample tray

--- src/ros/nova-gui/nova-gui/src/components/SampleTray/SampleTray.tsx
+++ src/ros/nova-gui/nova-gui/src/components/SampleTray/SampleTray.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SegmentedPicker from "../SegmentedPicker/SegmentedPicker";
 import { useRosAction } from "../../hooks/ros/useRosAction";
 import { RosAction } from "../../ros/actions/RosAction";
+import { Button } from "@nextui-org/react";
 
 const SAMPLE_TRAY_POSITIONS = [
     {
@@ -33,16 +34,10 @@ const SampleTray: React.FC<SampleTrayProps> = () => {
     
     const [sampleTrayPosition, setSampleTrayPosition] = useState<number>(0);
     const {sendGoal, feedback} = useRosAction(RosAction.SCIENCE_SAMPLE_TRAY);
-    const [initialPosition, setInitialPosition] = useState<boolean>(true);
 
-    const onChangePosition = (position: number) => {
-        if (initialPosition) {
-            setInitialPosition(false);
-            return;
-        }
-        setSampleTrayPosition(position);
+    const onApply = () => {
         const action = {
-            goal: SAMPLE_TRAY_POSITIONS[position].value,
+            goal: SAMPLE_TRAY_POSITIONS[sampleTrayPosition].value,
         }
         sendGoal(action);
     }
@@ -52,13 +47,14 @@ const SampleTray: React.FC<SampleTrayProps> = () => {
             <SegmentedPicker 
                 fullWidth
                 className={"grow"}
-                onIndexChange={(i) => onChangePosition(i)}
+                onIndexChange={(i) => setSampleTrayPosition(i)}
                 >
             {SAMPLE_TRAY_POSITIONS.map((position, i) => (
                 <div key={i} className={i === sampleTrayPosition ? "text-primary-500" : ""}>{position.display}</div>
                 ))}
             </SegmentedPicker>
             <div className="text-center">{`Current Position: ${feedback?.current_position}, Goal Position: ${feedback?.goal_position}`}</div>
+            <Button onClick={() => onApply()}>Apply</Button>
         </>
       );
   };
