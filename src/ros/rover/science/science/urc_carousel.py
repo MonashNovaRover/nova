1a7e622ad73a7322c100201a637931c8b12010e7
Tristan Clark <tcla0012@student.monash.edu>
2024-05-15 20:58:52 +1000

    Add Carousel Node - copied from Sample Tray

---
+++ src/ros/rover/science/science/urc_carousel.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import functools
+from python_control.controls.OneAxisPositionControl import OneAxisPositionControl
+from python_control.controllers.StepperPCBController import StepperPCBPositionController
+from python_control.sensors.IntegerSensor import IntegerSensor
+from python_control.sensors.CommandSensor import CommandSensor
+from python_control.ControllerNode import ControllerNode
+import rclpy
+from input_interfaces.msg import InputJoystick
+from rclpy.action import ActionServer
+from nova_interfaces.action import Stepper
+
+
+def meters_to_steps(distance_m, ticks_per_rev, lead_screw_thread_pitch_m):
+    """
+    Convert a distance to steps
+    :param distance: The distance to convert in meters
+    :param ticks_per_rev: The number of ticks per revolution of the stepper motor
+    :param lead_screw_thread_pitch: The pitch of the lead screw in meters
+    :return: The number of steps required to move the distance
+    """
+    return int(distance_m * ticks_per_rev / lead_screw_thread_pitch_m)
+
+THREAD_PITCH_M = 0.008
+TICKS_PER_REV = 200
+GAP = 0.084
+
+GAP_STEPS = meters_to_steps(GAP, TICKS_PER_REV, THREAD_PITCH_M)
+
+class URCCarousel(ControllerNode):
+
+    # CAN BUS NAME
+    # The name of the CAN bus to use
+    CAN_BUS = "can1"
+
+    # ROS ACTION
+    # The name of the ROS action to use
+    CAROUSEL_ACTION = "/science/carousel_action"
+
+    # SENDING CARD IDS
+    # Add any CONTROL FRAME / CARD IDS here
+    STEPPER_PCB_SEND = 0x006
+
+    # RECEIVING CARD IDS
+    # Add any SENSOR FRAME / CARD IDS here
+    STEPPER_PCB_RECV_POS = 0x456
+    STEPPER_PCB_RECV_ZERO = 0x406
+
+
+    # NAMES
+    # Add any CONTROL names here
+    CAROUSEL_CONTROL = "carousel"
+    CAROUSEL_POS = "carousel_pos"
+    CAROUSEL_ZERO = "carousel_zero"
+
+    # CONTROL PARAMETERS
+    # Positions
+    NUM_CUVETTES = 14
+
+    # Position Names
+    ZERO = OneAxisPositionControl.ZERO
+
+    # SENDING COMMAND IDS
+    # Add any CONTROL command ids here
+    STEPPER_SEND_POS_COMMAND_ID = 0x00 # TODO: REPLACE COMMAND
+    STEPPER_SEND_SET_COMMAND_ID = 0x00 # TODO: REPLACE COMMAND 
+
+    # RECEIVING COMMAND IDS
+    # Add any SENSOR command ids here
+    STEPPER_RECV_POS_COMMAND_ID = 0x01 # TODO: REPLACE COMMAND
+
+    def __init__(self):
+        super().__init__(name="URCCarousel", can_bus=self.CAN_BUS)
+        logger = self.get_logger()
+
+        ## Add CAN ID Filters
+        self.bus.set_id_filter([self.STEPPER_PCB_RECV_POS, self.STEPPER_PCB_RECV_ZERO])
+
+        ## Create sensors
+        self.carousel_pos_sensor = IntegerSensor(
+            bus=self.bus,
+            logger=logger,
+            frame_id=self.STEPPER_PCB_RECV_POS,
+            command_id=self.STEPPER_RECV_POS_COMMAND_ID,
+            initial_value=0,
+        )
+
+        ## Add sensors to the node
+        self.add_sensor(self.CAROUSEL_POS, self.carousel_pos_sensor)
+
+
+        ## Create controls
+        POSITIONS = {
+            self.ZERO: 0
+        }
+
+        for pos in range(1, self.NUM_CUVETTES):
+            POSITIONS[str(pos)] = (pos - 1) * GAP_STEPS
+
+
+        self.carousel_control = OneAxisPositionControl(
+            logger=logger,
+            positions=POSITIONS,
+            position_sensor=self.carousel_pos_sensor,
+        )
+
+        logger.info('Positions: {0}'.format(self.carousel_control.get_positions()))
+
+        ## Create controllers
+        self.carousel_controller = StepperPCBPositionController(
+            logger=logger,
+            bus=self.bus,
+            frame_id=self.STEPPER_PCB_SEND,
+            pos_command_id=self.STEPPER_SEND_POS_COMMAND_ID,
+            set_command_id=self.STEPPER_SEND_SET_COMMAND_ID,
+            control=self.carousel_control,
+        )
+
+        # Add Actions
+        self.go_to_action = ActionServer(self, Stepper, self.CAROUSEL_ACTION, self.carousel_controller.stepper_action_callback)
+
+        ## Start the CAN bus
+        self.start_can()
+
+
+    def joystick_l(self, joystick_l: InputJoystick):
+        pass
+
+    def joystick_r(self, joystick_r: InputJoystick):
+        pass
+
+def main():
+    rclpy.init()
+    node = URCCarousel()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
