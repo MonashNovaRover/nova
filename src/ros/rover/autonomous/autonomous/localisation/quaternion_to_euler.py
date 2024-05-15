080b6b7a1a1cad6439d6ccced7ab8f7f9d872317
Dylan Gonzalez <dylcg10@gmail.com>
2024-05-15 18:19:48 +1000

    fix quaternion_to_euler node

--- src/ros/rover/autonomous/autonomous/localisation/quaternion_to_euler.py
+++ src/ros/rover/autonomous/autonomous/localisation/quaternion_to_euler.py
@@ -1,48 +1,53 @@
 #!/usr/bin/python3
 import rclpy
+from rclpy.node import Node
 import math
-from geometry_msgs.msg import Quaternion
+#from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import Imu
 
-def quaternion_callback(msg):
-    # Convert quaternion to Euler angles
-    roll, pitch, yaw = quaternion_to_euler(msg)
+class QuaternionToEuler(Node):
+    def __init__(self):
+        super().__init__('quaternion_to_euler')
 
-    # Print the Euler angles
-    node.get_logger().info(f'Roll: {roll}, Pitch: {pitch}, Yaw: {yaw}')
+        self.subscription = self.create_subscription(
+            Imu,
+            '/oak/imu/data',
+            self.quaternion_callback,
+            10
+        )
 
-def quaternion_to_euler(q):
-    q = q.orientation
-    roll = 0.0
-    pitch = 0.0
-    yaw = 0.0
+    def quaternion_callback(self, msg):
+        q = msg.orientation
+        # Convert quaternion to Euler angles
+        roll, pitch, yaw = self.quaternion_to_euler(q)
 
-    # Convert quaternion to Euler angles
-    t0 = +2.0 * (q.w * q.x + q.y * q.z)
-    t1 = +1.0 - 2.0 * (q.x**2 + q.y**2)
-    roll = math.atan2(t0, t1)
+        # Print the Euler angles
+        self.get_logger().info(f'Roll: {roll}, Pitch: {pitch}, Yaw: {yaw}')
 
-    t2 = +2.0 * (q.w * q.y - q.z * q.x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch = math.asin(t2)
+    def quaternion_to_euler(self, q):
+        roll = 0.0
+        pitch = 0.0
+        yaw = 0.0
 
-    t3 = +2.0 * (q.w * q.z + q.x * q.y)
-    t4 = +1.0 - 2.0 * (q.y**2 + q.z**2)
-    yaw = math.atan2(t3, t4)
+        # Convert quaternion to Euler angles
+        t0 = +2.0 * (q.w * q.x + q.y * q.z)
+        t1 = +1.0 - 2.0 * (q.x**2 + q.y**2)
+        roll = math.atan2(t0, t1)
 
-    return roll, pitch, yaw
+        t2 = +2.0 * (q.w * q.y - q.z * q.x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch = math.asin(t2)
+
+        t3 = +2.0 * (q.w * q.z + q.x * q.y)
+        t4 = +1.0 - 2.0 * (q.y**2 + q.z**2)
+        yaw = math.atan2(t3, t4)
+
+        return roll, pitch, yaw
 
 def main(args=None):
     rclpy.init(args=args)
-    node = rclpy.create_node('quaternion_to_euler_node')
-
-    # Subscribe to the Quaternion topic
-    subscription = node.create_subscription(
-        Quaternion,
-        '/oak/imu/data',
-        quaternion_callback,
-        10
-    )
+    node = QuaternionToEuler()
 
     rclpy.spin(node)
 
