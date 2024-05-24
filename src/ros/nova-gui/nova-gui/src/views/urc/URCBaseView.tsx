acc5b3954e5502977d24f0feab25be6090dfe898
Tristan Clark <tcla0012@student.monash.edu>
2024-05-25 09:03:30 +1000

    fix layout

--- src/ros/nova-gui/nova-gui/src/views/urc/URCBaseView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/urc/URCBaseView.tsx
@@ -1,7 +1,7 @@
 import UVVisSpec from "../../components/UVVisSpec/UVVisSpec.tsx";
 
 const URCBaseView: React.FC = () => {
-  return <div className="m-3 grid grid-cols-1 gap-3">
+  return <div className="m-3 grid grid-cols-2 gap-3">
     <UVVisSpec />
   </div>;
 };
