99c8ea33d82e580c9b517adce818a48c13fe16a6
Kabilan VS <kabiniki4849@gmail.com>
2024-05-25 15:22:25 +1000

    WIP on feat/offline-cartographer

--- src/ros/nova-gui/nova-gui/src/components/Maps/maptiler/MapTilerMap.tsx
+++ src/ros/nova-gui/nova-gui/src/components/Maps/maptiler/MapTilerMap.tsx
@@ -2,10 +2,16 @@ import React, { useEffect, useRef, useState } from "react";
 import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import { useCartographerMarkers } from "../hooks/useCartographerMarkers";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/RootState";
 
 export const MapTilerMap = (props: { overlay: React.ReactNode }) => {
   const mapContainer = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<maptilersdk.Map>();
+
+  const baseStationIp = useSelector(
+    (state: RootState) => state.uiState.baseStationIP
+  );
 
   useCartographerMarkers(map);
   useEffect(() => {
@@ -23,7 +29,9 @@ export const MapTilerMap = (props: { overlay: React.ReactNode }) => {
         name: "Cartographer",
         sources: {
           tiles: {
-            tiles: ["http://localhost:8080/data/MDRS_Hi_Res/{z}/{x}/{y}.png"], // Todo: Replace with Rover IP
+            tiles: [
+              `http://${baseStationIp}:8080/data/MDRS_Hi_Res/{z}/{x}/{y}.png`,
+            ],
             type: "raster",
             bounds: [-110.809, 38.3917, -110.765, 38.4177],
           },
