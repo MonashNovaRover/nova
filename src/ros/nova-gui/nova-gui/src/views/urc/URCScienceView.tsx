406d9820a37a4173f2441781f5dc9c545588dbfa
Tristan Clark <tcla0012@student.monash.edu>
2024-05-25 14:56:22 +1000

    Clean up URC Science view

--- src/ros/nova-gui/nova-gui/src/views/urc/URCScienceView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/urc/URCScienceView.tsx
@@ -5,9 +5,9 @@ import HydroprobeWidget from "../../components/HydroprobeWidget/HydroprobeWidget
 
 const URCScienceView: React.FC = () => {
   return (
-    <div className="grid w-full gap-3 p-3 auto-cols-fr s:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6">
+    <div className="grid w-full gap-3 p-3 grid-cols-6">
       <HydroprobeWidget className="row-start-1 w-full col-span-3" />
-      <PlatformWidget />
+      <PlatformWidget className="row-start-2 w-full col-span-2" />
     </div>
   );
 };
