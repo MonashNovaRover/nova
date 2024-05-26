7981ca1d53007bd50a24c9967937b24e544f9f23
Tristan Clark <tcla0012@student.monash.edu>
2024-05-26 13:08:39 +1000

    Remove unused chart styles

--- src/ros/nova-gui/nova-gui/src/components/SpectraDisplay/ChartOptions.ts
+++ src/ros/nova-gui/nova-gui/src/components/SpectraDisplay/ChartOptions.ts
@@ -7,8 +7,6 @@ import { ApexOptions } from "apexcharts";
 
 export enum ChartStyle {
     Default,
-    RamanMain,
-    RamanData,
 }
 
 export const ChartOptions = (style: ChartStyle): ApexOptions => {
@@ -56,127 +54,6 @@ export const ChartOptions = (style: ChartStyle): ApexOptions => {
             yaxis: {
                 min: 0,
                 max: 1,
-                title: {
-                    text: 'Normalised intensity',
-                    style: {
-                        fontSize: '14px',
-                        color: '#fff'
-                    }
-                },
-                labels: {
-                    show: false,
-                }
-            },
-            grid: {
-                show: false
-            },
-            colors: ["#992F7B", "#C4841D"],
-          };
-    }
-    else if (style == ChartStyle.RamanMain) {
-        return {
-            title: {
-                text: 'CCD Output',
-                align: "center",
-                floating: true,
-                style: {
-                    fontSize: '18px',
-                    color: '#fff'
-                },
-            },
-            stroke: {
-                curve: "smooth"
-            },
-            chart: {
-                animations: {enabled: false},
-                type: 'line',
-                background: "000",
-                toolbar: {
-                    show: false
-                }
-            },
-            tooltip: {
-                theme: "dark",
-                fixed: {
-                    offsetX: 10,
-                    offsetY: 10
-                }
-            },
-            xaxis: {
-                title: {
-                    text: 'Raman Shift (1/cm)',
-                    style: {
-                        fontSize: '14px',
-                        color: '#fff'
-                    }
-                },
-                labels: {
-                    show: true,
-                }
-            },
-            yaxis: {
-                min: 0,
-                max: 100,
-                title: {
-                    text: 'Normalised intensity',
-                    style: {
-                        fontSize: '14px',
-                        color: '#fff'
-                    }
-                },
-                labels: {
-                    show: false,
-                }
-            },
-            grid: {
-                show: false
-            },
-            colors: ["#992F7B", "#C4841D", "#66AAF9", "#F31260"]
-        };
-    } else if (style == ChartStyle.RamanData) {
-        return {
-            title: {
-                text: '',
-                align: "center",
-                floating: true,
-                style: {
-                    fontSize: '18px',
-                    color: '#fff'
-                },
-            },
-            stroke: {
-                curve: "smooth"
-            },
-            chart: {
-                animations: {enabled: false},
-              type: 'line',
-              background: "000",
-              toolbar: {
-                show: false
-              }
-            },
-            tooltip: {
-                theme: "dark",
-                fixed: {
-                    offsetX: 10,
-                    offsetY: 10
-                }
-            },
-            xaxis: {
-                title: {
-                    text: 'Raman Shift (1/cm)',
-                    style: {
-                        fontSize: '14px',
-                        color: '#fff'
-                    }
-                },
-                labels: {
-                    show: true,
-                }
-            },
-            yaxis: {
-                min: 0,
-                max: 100,
                 title: {
                     text: 'Normalised intensity',
                     style: {
