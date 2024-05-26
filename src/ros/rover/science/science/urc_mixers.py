6e83cba161245e11e77449886ccea1826b74a954
Tristan Clark <tcla0012@student.monash.edu>
2024-05-27 03:22:05 +1000

    Add urc mixers

---
+++ src/ros/rover/science/science/urc_mixers.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+from python_control.sensors.LimitSwitchSensor import LimitSwitchSensor
+from python_control.limits.LimitSwitchLimit import LimitSwitchLimit
+from python_control.controls.Direction import Direction
+from python_control.controls.OneAxisVelocityControl import OneAxisVelocityControl
+from python_control.controllers.CMDVelocityController import CMDVelocityController
+from python_control.sensors.CommandSensor import CommandSensor
+import rclpy
+from python_control.ControllerNode import ControllerNode
+from input_interfaces.msg import InputJoystick
+
+
+class URCAuger(ControllerNode):
+
+    # CAN BUS NAME
+    # The name of the CAN bus to use
+    CAN_BUS = "can1"
+
+    # SENDING CARD IDS
+    # Add any CONTROL FRAME / CARD IDS here
+    MIXER_1_SEND_FRAME_ID = 0x041
+    MIXER_2_SEND_FRAME_ID = 0x042
+
+    # CONTROL NAMES
+    # Add any CONTROL names here
+    MIXER_1_NAME = "mixer_1"
+    MIXER_2_NAME = "mixer_2"
+
+    # CONTROL PARAMETERS
+    # Max Speed as a Percentage (0.0 to 1.0)
+    MIXER_1_MAX_PERCENT = 0.75
+    MIXER_2_MAX_PERCENT = 0.75
+    
+    # CONTROL DIRECTIONS
+    # Add any CONTROL DIRECTIONS here
+    MIXER_1_CLOCKWISE = Direction.POSITIVE
+    MIXER_1_DOWN = Direction.NEGATIVE
+    MIXER_2_CLOCKWISE = Direction.POSITIVE
+    MIXER_2_COUNTERCLOCKWISE = Direction.NEGATIVE    
+
+    def __init__(self):
+        super(URCAuger, self).__init__(name="URCMixers", can_bus=self.CAN_BUS)
+        logger = self.get_logger()
+
+ 
+
+        ## Create controls
+        self.mixer_1 = OneAxisVelocityControl(
+            logger=logger,
+            max_percent=self.MIXER_1_MAX_PERCENT,
+            direction=self.MIXER_1_CLOCKWISE,
+        )
+        self.mixer_2 = OneAxisVelocityControl(
+            logger=logger,
+            max_percent=self.MIXER_2_MAX_PERCENT,
+            direction=self.MIXER_2_CLOCKWISE,
+        )
+
+
+        ## Create controllers
+        self.mixer_1_controller = CMDVelocityController(
+            logger=logger,
+            bus=self.bus,
+            frame_id=self.MIXER_1_SEND_FRAME_ID,
+            control=self.mixer_1
+        )
+        self.mixer_2_controller = CMDVelocityController(
+            logger=logger,
+            bus=self.bus,
+            frame_id=self.MIXER_2_SEND_FRAME_ID,
+            control=self.mixer_2
+        )
+
+        ## Add the controllers to the node's of controllers
+        self.add_controller(self.MIXER_1_NAME, self.mixer_1_controller)
+        self.add_controller(self.MIXER_2_NAME, self.mixer_2_controller)
+
+        ## Start the CAN bus
+        self.start_can()
+
+    def update_auger_actuation(self, joystick_r: InputJoystick):
+        # Auger height direction is determined by the right joystick's x-axis direction
+        self.mixer_1.update_direction(self.MIXER_1_CLOCKWISE if joystick_r.ax_stick_x >= 0 else self.MIXER_1_DOWN)
+
+        # Auger velocity is determined by the right joystick's x-axis magnitude
+        self.mixer_1.update_velocity(velocity=abs(joystick_r.ax_stick_x), ignore_limits=(joystick_r.btn_thumb_d_state >= 1))
+
+    def update_auger_drill(self, joystick_r: InputJoystick):
+        # Drill spin direction is determined by the right joystick thumb buttons
+        # Thumb right = clockwise, Thumb left = counterclockwise
+        if joystick_r.btn_thumb_r_state >= 1:
+            self.mixer_2.update_direction(self.MIXER_2_CLOCKWISE)
+        elif joystick_r.btn_thumb_l_state >= 1:
+            self.mixer_2.update_direction(self.MIXER_2_COUNTERCLOCKWISE)
+        
+        # Drill spin velocity is determined by the right joystick trigger
+        if joystick_r.btn_thumb_u_state >= 1:
+            self.mixer_2.update_velocity(1.0)
+        else:
+            self.mixer_2.update_velocity(0)
+
+    def joystick_l(self, joystick_l: InputJoystick):
+        pass
+
+    def joystick_r(self, joystick_r: InputJoystick):
+        self.update_auger_actuation(joystick_r)
+        self.update_auger_drill(joystick_r)
+
+def main():
+    rclpy.init()
+    node = URCAuger()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
