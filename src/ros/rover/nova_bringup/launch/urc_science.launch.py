b89de4cc10d7ddd1310e1b451ab42f0703d8ad37
Tristan Clark <tcla0012@student.monash.edu>
2024-05-27 03:27:19 +1000

    URC Science Launch file

---
+++ src/ros/rover/nova_bringup/launch/urc_science.launch.py
@@ -0,0 +1,44 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Monash Nova Rover Team
+
+Execute this code on the rover to start all
+   science scripts.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODES:
+  - science/nir_probe_publisher.py      [nir_probe_publisher]
+  - science/microscope_servo.py         [microscope_servo]
+  - science/kiln_server.py'             [kiln_server]
+  - control/auger.py                    [auger]
+  - control/analysis_platform.py        [analysis_platform]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE: 	core
+CREATION:	17/03/2024
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+# Include the required launch parameters
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='science', executable='urc_analysis_arm.py', output='screen', emulate_tty=True),
+
+        Node(
+            package='science', executable='urc_auger.py', output='screen', emulate_tty=True),
+
+        Node(
+            package='science', executable='urc_pumps.py', output='screen', emulate_tty=True),
+
+        Node(
+            package='science', executable='urc_sample_tray.py', output='screen', emulate_tty=True),
+
+        # Node(
+        #     package='science', executable='uv_vis_spec.py', output='screen', emulate_tty=True),
+
+        # Node(
+        #     package='science', executable='theta360cam.py', output='screen', emulate_tty=True),
+    ])
