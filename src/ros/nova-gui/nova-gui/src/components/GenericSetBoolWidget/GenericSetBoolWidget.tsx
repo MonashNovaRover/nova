d39927d008ddf915973b80b9f8b6e335715d1afd
Joshua Leivenzon <jlei0014@student.monash.edu>
2024-05-29 00:16:16 -0600

    Add GenericSetBoolWidget

---
+++ src/ros/nova-gui/nova-gui/src/components/GenericSetBoolWidget/GenericSetBoolWidget.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+import {useBifrost} from "../../redux/actions/bifrost/useBifrostAction.ts";
+import {RosService} from "../../ros/services/rosService.ts";
+import {IRosStdSrvsSetBoolResponse} from "../../ros/rosTypes.ts";
+import {Card, CardBody, CardHeader, Switch} from "@nextui-org/react";
+
+export interface GenericSetBoolWidgetProps {
+  label?: string
+  service: RosService
+}
+
+const GenericSetBoolWidget: React.FC<GenericSetBoolWidgetProps> = (props) => {
+  const [isOn, setIsOn] = useState<boolean>(false);
+  const bifrost = useBifrost({service: props.service});
+
+  useEffect(() => {
+    bifrost.syncWithTopic();
+  }, [bifrost]);
+
+  const changeValue = (value: boolean) => {
+    bifrost.callService({data: value}, {
+      handleResponse: (response) => {
+        const boolResponse = response as IRosStdSrvsSetBoolResponse;
+        if (boolResponse?.success) {
+          setIsOn(value);
+        }
+      }
+    });
+  }
+
+  return <Card>
+    <CardHeader>{props.label ?? props.service}</CardHeader>
+    <CardBody>
+      <Switch isSelected={isOn} onChange={() => changeValue(!isOn)}/>
+    </CardBody>
+  </Card>
+}
+
+export default GenericSetBoolWidget
