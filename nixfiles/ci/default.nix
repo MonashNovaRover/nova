0190ec0b6fa274272e6ecae996dc4a8a9d7669a9
Bailey Chessum <bailey.chessum1@gmail.com>
2024-09-28 15:01:30 +1000

    Changed ci/default.nix to use fix/hydra-evaluation branch as source

--- nixfiles/ci/default.nix
+++ nixfiles/ci/default.nix
@@ -38,7 +38,7 @@ let
   } // args // {
     inputs = {
       nixpkgs = mkGitHubInput { owner = "NixOS"; repo = "nixpkgs"; branch = "nixos-unstable"; };
-      nova-monorepo = mkNovaInput { repo = "nova"; };
+      nova-monorepo = mkNovaInput { repo = "nova"; branch = "fix/hydra-evaluation"; };
       supportedSystems = {
         type = "nix";
         value = "[ \"${builtins.concatStringsSep "\" \"" supportedSystems}\" ]";
