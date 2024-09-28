b719b0bcbb92bbe5c1b06001d78b61e26a17d448
Bailey Chessum <bailey.chessum1@gmail.com>
2024-09-28 17:15:07 +1000

    make source filter always return true

--- nixfiles/ci/jobsets/test.nix
+++ nixfiles/ci/jobsets/test.nix
@@ -13,15 +13,12 @@ let
     (self: super: import (nixfiles + "/packages/other") { inherit (self) callPackage; })
   ]; };
 
-
-
   evaledSource = builtins.path rec {
     name = "arm-interfaces-source";
     path = ../../external/src/ros/rover/arm/arm_interfaces;
-    filter = pkgs.lib.novaSourceFilter [ ] path;
+    filter = (path: type: true);
   };
 in
-  #throw lib.repos
   throw evaledSource
 
 # [ /nix/store/w4qxnwr7vj3f45wf7bx62zsashg9pl0i-source/nixfiles/external/src/other/coms_utils
@@ -34,3 +31,5 @@ in
 
 # [ /nix/store/6pzhkxnh5yc4zj89z05j5c5wjwh769m5-source/nixfiles/external/src/ros/rover/test.nix
 #   /nix/store/6pzhkxnh5yc4zj89z05j5c5wjwh769m5-source/nixfiles/external/src/ros/rover/nix/packages/arm-interfaces «thunk» ]
+
+# import ~/nova/nixfiles/ci/jobsets/test.nix { nixpkgs = <nixpkgs>; nova-monorepo = ./nova; supportedSystems = null; }
