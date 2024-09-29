ce9eaea7d41f5a437d9774adb8396473fcefa9da
Bailey Chessum <bailey.chessum1@gmail.com>
2024-09-29 17:55:49 +1000

    fix: add back changes from og nixfiles that were missing for some reason

--- nixfiles/modules/nixos/common/peripherals/default.nix
+++ nixfiles/modules/nixos/common/peripherals/default.nix
@@ -1,6 +1,27 @@
 { config, ... }:
 
 {
+  # Some drivers need to be patched for certain kernels.
+  nixpkgs.overlays = [
+    (self: super: {
+      linuxKernel = super.linuxKernel // {
+        packages = super.linuxKernel.packages // {
+          linux_5_10 = super.linuxKernel.packages.linux_5_10.extend (linuxSelf: linuxSuper: {
+            xone = linuxSuper.xone.overrideAttrs rec {
+              version = "0.3";
+              src = self.fetchFromGitHub {
+                owner = "medusalix";
+                repo = "xone";
+                rev = "v${version}";
+                hash = "sha256-h+j4xCV9R6hp9trsv1NByh9m0UBafOz42ZuYUjclILE=";
+              };
+            };
+          });
+        };
+      };
+    })
+  ];
+
   # Modern XBOX controllers
   hardware.xone.enable = true;
 }
