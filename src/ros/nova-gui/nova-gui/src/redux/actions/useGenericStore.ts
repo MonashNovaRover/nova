5ebd38953499546e8f3cd266521d19866f2945b7
Felicity Matthews <felicitymatthews30@gmail.com>
2024-09-30 09:10:41 +1000

    improve error checking/throwing

--- src/ros/nova-gui/nova-gui/src/redux/actions/useGenericStore.ts
+++ src/ros/nova-gui/nova-gui/src/redux/actions/useGenericStore.ts
@@ -15,13 +15,17 @@ export function useGenericStore<T>(storeName: string): [T, (a: T) => void] {
   const value = useSelector((store: RootState) => {
 
     // type checks
-    if (!Object.prototype.hasOwnProperty.call(store, storeName))
-      throw console.error(`${storeName} is not a store in redux`)
+    if (!Object.prototype.hasOwnProperty.call(store, storeName)) {
+      console.error(`${storeName} is not a store in redux`);
+      throw new Error(`${storeName} is not a store in redux`);
+    }
 
     const valueStore = store[storeName as keyof typeof store] as GenericStoreState<T>;
 
-    if (valueStore.value === undefined)
-      throw console.error(`${storeName} is not a generic store`);
+    if (valueStore.value === undefined) {
+      console.error(`${storeName} is not a generic store`);
+      throw new Error(`${storeName} is not a generic store`);
+    }
 
     return valueStore.value as T
   })
