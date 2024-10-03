b9b6e4dd763307beb19ad0fdd653a9445c1c17f2
Bailey Chessum <bailey.chessum1@gmail.com>
2024-10-03 21:22:29 +1000

    Added todos for migrating code that depended on gazebo classic

--- src/ros/rover/nix/packages/core/default.nix
+++ src/ros/rover/nix/packages/core/default.nix
@@ -13,6 +13,10 @@
 , joint-state-publisher
 , controller-manager
 , ros2-control
+# TODO: Replace with gazebo harmonic, after migrating the launch files
+# https://gazebosim.org/docs/harmonic/migrating_gazebo_classic_ros2_packages/
+#, gazebo-ros
+#, gazebo-ros2-control
 , ros2-controllers
 , pluginlib
 , robot-localization
