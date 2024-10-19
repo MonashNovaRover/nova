99e9d1f5b239f248e89860b540bf6b6dcda77d4f
Felicity Matthews <felicitymatthews30@gmail.com>
2024-10-19 14:39:17 +1100

    rename rootReducer to reduxStores

--- src/ros/nova-gui/nova-gui/src/redux/store/rootReducerFilters.ts
+++ src/ros/nova-gui/nova-gui/src/redux/store/rootReducerFilters.ts
@@ -1,46 +1,48 @@
-import {rootReducer, RootReducerKey} from "../RootReducer.ts";
 import {combineReducers} from "@reduxjs/toolkit";
 
 /**
- * converts rootReducer which is a mix of StoreContexts and Reducers
+ * converts stores which is a mix of StoreContexts and Reducers
  * into a single reducer
+ *
+ * @param stores a struct containing stores that are either StoreContexts or Reducers
  */
-export const getReducers = () => {
-  const reducers = Object.keys(rootReducer).reduce((acc, val) => {
+export const getReducers = <T extends object>(stores: T) => {
+  const reducers = Object.keys(stores).reduce((acc, val) => {
     // nothing changes when the store is already a reducer
-    const key = val as RootReducerKey
-    if (rootReducer[val as RootReducerKey] instanceof Function)
+    const key = val as keyof typeof stores
+    if (stores[key] instanceof Function)
       return acc
 
     // set the store to the reducer when store is a StoreContext
-    const reducerKey = "reducer" as keyof typeof rootReducer[typeof key]
-    if (rootReducer[key][reducerKey] !== undefined)
-      return {...acc, [key]: rootReducer[key][reducerKey]}
+    const reducerKey = "reducer" as keyof typeof stores[typeof key]
+    if (stores[key][reducerKey] !== undefined)
+      return {...acc, [key]: stores[key][reducerKey]}
 
-    console.log(`root reducer item is not a Reducer or a StoreContext: ${rootReducer[key]}`)
-    throw Error(`root reducer item is not a Reducer or a StoreContext: ${rootReducer[key]}`)
-  }, rootReducer)
+    console.log(`root stores item is not a Reducer or a StoreContext: ${stores[key]}`)
+    throw Error(`root stores item is not a Reducer or a StoreContext: ${stores[key]}`)
+  }, stores)
 
   return combineReducers(reducers)
 }
 
 /**
- * Filters the rootReducer and creates a blacklist which includes all
+ * Filters the stores and creates a blacklist which includes all
  * store names that have the designated field marked as false if they
  * are a StoreContext
  *
+ * @param stores a struct containing stores that are either StoreContexts or Reducers
  * @param field the field of a StoreContext to create a blacklist from
  */
-export const filterRootReducer = (field: string) => {
-  return Object.keys(rootReducer)
+export const filterRootStores = <T extends object>(stores: T, field: string) => {
+  return Object.keys(stores)
     .filter((val) => {
       // ignore non StoreContext types
-      const key = val as RootReducerKey
-      if (rootReducer[val as RootReducerKey] instanceof Function)
+      const key = val as keyof typeof stores
+      if (stores[key] instanceof Function)
         return false
 
       // check if a StoreContext has the field set to false
-      const reducerKey = field as keyof typeof rootReducer[typeof key]
-      return rootReducer[key][reducerKey] !== undefined && !rootReducer[key][reducerKey]
+      const reducerKey = field as keyof typeof stores[typeof key]
+      return stores[key][reducerKey] !== undefined && !stores[key][reducerKey]
     })
 }
