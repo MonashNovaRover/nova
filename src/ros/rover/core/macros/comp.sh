161bb509e3d4b3e0baac625465481f2be89f37fb
Victor Bartlinski <vicbart08@gmail.com>
2024-11-13 16:05:22 +1100

    removed autonomous and core, no longer used

--- src/ros/rover/core/macros/comp.sh
+++
@@ -1,7 +0,0 @@
-read -s password
-
-gnome-terminal --tab -- sshpass -p $password ssh nvidia@192.168.1.204 &
-gnome-terminal --tab -- sshpass -p $password ssh nvidia@192.168.1.204 &
-gnome-terminal -- sshpass -p $password ssh nvidia@192.168.1.204 &
-gnome-terminal -- sshpass -p $password ssh nvidia@192.168.1.204 &
-sshpass -p $password ssh nvidia@192.168.1.204
