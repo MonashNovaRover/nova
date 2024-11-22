c20285e0c5347283bd8a825c993a22e110626986
Anthony Lew <anthony@v01dbreaker.cc>
2024-11-22 16:56:33 +1100

    Removed copied derivation of imu-transformer

--- nixfiles/packages/ros/nova-workspace/default.nix
+++ nixfiles/packages/ros/nova-workspace/default.nix
@@ -8,6 +8,7 @@
 , rqt-common-plugins
 , gdb
 , gps-umd
+, imu-transformer
 
 , nova-electronics ? throw "electronics is needed, but not available!"
 , nova-science ? throw "science is needed, but not available!"
@@ -104,7 +105,8 @@ in
       ros-gz
       gz-ros2-control
       gps-umd
-      rqt rqt-common-plugins;
+      rqt rqt-common-plugins
+      imu-transformer;
   }) // extraPackages;
   prebuiltShellPackages = {
     inherit
