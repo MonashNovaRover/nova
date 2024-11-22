06ce9fe7b88f4db891aa09770375f3c1fbcce387
Anthony Lew <anthony@v01dbreaker.cc>
2024-11-22 16:56:33 +1100

    Removed copied derivation of imu-transformer

--- nixfiles/packages/ros/default.nix
+++ nixfiles/packages/ros/default.nix
@@ -3,7 +3,7 @@
 {
   depthai = callPackage ./depthai { };
   librealsense2-gui = callPackage ./librealsense2-gui { };
-  imu-transformer = callPackage ./imu-transformer { };
+  #imu-transformer = callPackage ./imu-transformer { };
   nova-workspace = callPackage ./nova-workspace { };
   rclnodejs = callPackage ./rclnodejs { };
   realsense-patches = callPackage ./realsense-patches { };
