de0a802ef02178939a0109dee39a4745aefacd8e
Victor Bartlinski <vicbart08@gmail.com>
2024-11-25 13:09:16 +1100

    cleaned up urdf.launch.py

--- src/ros/rover/auto_bringup/launch/urdf.launch.py
+++ src/ros/rover/auto_bringup/launch/urdf.launch.py
@@ -17,7 +17,7 @@ CREATION:	27/04/2023
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch.conditions import IfCondition, UnlessCondition
 
@@ -25,41 +25,37 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
-# Generate the launch file with all inputs
+def launch_setup(context, *args, **kwargs):
+    gazebo = LaunchConfiguration('gazebo').perform(context)
+    model = LaunchConfiguration('model').perform(context)
+    
+    return [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': 
+                ParameterValue(Command(['xacro ', model, " ", "gazebo:=", gazebo]), value_type=str)
+            }]
+        )
+    ]
+
+
 def generate_launch_description():
     rover_description_dir = FindPackageShare('rover_description')
 
-    gazebo = LaunchConfiguration('gazebo')
-
-    model_arg = DeclareLaunchArgument(name='model', default_value=PathJoinSubstitution([rover_description_dir, 'urdf', 'rover.urdf.xacro']),
-            description='Absolute path to robot urdf file')
-
-    gazebo_arg = DeclareLaunchArgument(
-        'gazebo', 
-        default_value='true',
-        description='Launch with gazebo or not')
-
-    robot_description = ParameterValue(
-        Command(
-            [
-                'xacro ', 
-                LaunchConfiguration('model'),
-                " ",
-                "gazebo:=",
-                gazebo
-            ]
+    declared_arguments = [
+        DeclareLaunchArgument(
+            name='gazebo', 
+            default_value='true',
+            description='Launch with gazebo or not',
         ),
-        value_type=str
+        DeclareLaunchArgument(
+            name='model', 
+            default_value=PathJoinSubstitution([rover_description_dir, 'urdf', 'rover.urdf.xacro']),
+            description='Absolute path to robot urdf file',
+        ),
+    ]
+    
+    return LaunchDescription(  
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
     )
-
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
-    )
-
-    return LaunchDescription([
-        model_arg,
-        gazebo_arg,
-        robot_state_publisher_node,
-    ])
