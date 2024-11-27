5ddcf3957d0d581a4f6c4252ce24323e8e3b1d51
rotaryViper <93423444+rotaryViper@users.noreply.github.com>
2024-11-27 23:47:31 +1100

    Installing and configuring vitest

---
+++ src/ros/nova-gui/nova-gui/vitest.workspace.ts
@@ -0,0 +1,29 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  // If you want to keep running your existing tests in Node.js, uncomment the next line.
+  // 'vite.config.ts',
+  {
+    extends: 'vite.config.ts',
+    test: {
+      browser: {
+        enabled: true,
+        name: 'chrome',
+        provider: 'webdriverio',
+        // https://webdriver.io
+        providerOptions: {
+          capabilities: {
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+              binary: '/etc/profiles/per-user/nova/bin/chromium',
+               args: ["--ozone-platform=wayland"]
+            },
+            'wdio:chromedriverOptions': {
+              binary: '/run/current-system/sw/bin/chromedriver'
+            },
+          },
+        },
+      },
+    },
+  },
+])
