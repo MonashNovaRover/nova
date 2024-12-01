57041a346b4cea4875c39cd4713ef3dac072bc28
Kabilan VS <kabiniki4849@gmail.com>
2024-12-01 20:24:29 +1100

    feat: moved teleop_drive_joy to nova_interfaces

--- src/ros/rover/teleop_drive_joy/src/teleop_drive_joy.cpp
+++ src/ros/rover/teleop_drive_joy/src/teleop_drive_joy.cpp
@@ -1,27 +1,9 @@
 /**
-Software License Agreement (BSD)
-
-\authors   Mike Purvis <mpurvis@clearpathrobotics.com>
-\copyright Copyright (c) 2014, Clearpath Robotics, Inc., All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that
-the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, node_ list of conditions and the
-   following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, node_ list of conditions and the
-   following disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name of Clearpath Robotics nor the names of its contributors may be used to endorse or promote
-   products derived from node_ software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WAR-
-RANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, IN-
-DIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
+ * @file teleop_drive_joy.cpp
+ * @brief Teleop Drive Joy node to Translate Joy Messages from /joy to Drive Commands
+ * @author Dylan Gonzalez
+ * Last Edited by Kabi
+ */
 #include <cinttypes>
 #include <functional>
 #include <map>
@@ -59,9 +41,9 @@ namespace teleop_drive_joy
 
   TeleopDriveJoy::TeleopDriveJoy(const rclcpp::NodeOptions &options) : node_{std::make_shared<rclcpp::Node>("teleop_drive_joy_node_", options)}
   {
-    drive_input_pub = node_->create_publisher<drive_interfaces::msg::DriveInputStamped>(DEFAULT_OUTPUT_TOPIC, 50);
+    drive_input_pub = node_->create_publisher<nova_interfaces::msg::DriveInputStamped>(DEFAULT_OUTPUT_TOPIC, 50);
     cmd_vel_pub = node_->create_publisher<geometry_msgs::msg::TwistStamped>(DEFAULT_OUTPUT_TOPIC_TWIST, 50);
-    drive_info_pub = node_->create_publisher<drive_interfaces::msg::DriveInfo>(DEFAULT_OUTPUT_TOPIC_INFO, 50);
+    drive_info_pub = node_->create_publisher<nova_interfaces::msg::DriveInfo>(DEFAULT_OUTPUT_TOPIC_INFO, 50);
 
     joy_sub = node_->create_subscription<sensor_msgs::msg::Joy>(DEFAULT_INPUT_TOPIC, rclcpp::QoS(10), std::bind(&TeleopDriveJoy::joyCallback, this, _1));
 
@@ -74,22 +56,22 @@ namespace teleop_drive_joy
     {
       params_ = param_listener_->get_params();
     }
-
-    parameters_client = node_->create_client<rcl_interfaces::srv::SetParameters>("/pivot_drive_controller/set_parameters");
   }
 
-  double TeleopDriveJoy::getVal(const sensor_msgs::msg::Joy::SharedPtr joy_msg, const std::map<std::string, int64_t> &axis_map,
-                                const std::map<std::string, double> &scale_map, const std::string &fieldname)
+  void TeleopDriveJoy::joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
   {
-    if (axis_map.find(fieldname) == axis_map.end() ||
-        axis_map.at(fieldname) == -1L ||
-        scale_map.find(fieldname) == scale_map.end() ||
-        static_cast<int>(joy_msg->axes.size()) <= axis_map.at(fieldname))
-    {
-      return 0.0;
-    }
 
-    return joy_msg->axes[axis_map.at(fieldname)] * scale_map.at(fieldname);
+    handleButtonCallbacks(joy_msg);
+
+    if (!current_state.locked)
+    {
+      sendDriveCommand(joy_msg);
+    }
+    else
+    {
+      // When lock button is pressed, immediately send a single halt command to Stop the Rover
+      sendHaltCommand();
+    }
   }
 
   void TeleopDriveJoy::sendDriveCommand(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
@@ -97,8 +79,8 @@ namespace teleop_drive_joy
 
     auto controller_params = params_.controllers_map.at(modeToController(control_mode));
 
-    double angular = joy_msg->axes[controller_params.axis_angular.z] * controller_params.scale_angular.z;
-    double linear = joy_msg->axes[controller_params.axis_linear.x] * controller_params.scale_linear.x;
+    double angular = joy_msg->axes[controller_params.axis_angular_z] * controller_params.scale_angular_z;
+    double linear = joy_msg->axes[controller_params.axis_linear_x] * controller_params.scale_linear_x;
 
     if (current_state.autonomous_mode)
     {
@@ -111,13 +93,12 @@ namespace teleop_drive_joy
     }
     else
     {
-      auto drive_input_msg = std::make_unique<drive_interfaces::msg::DriveInputStamped>();
+      auto drive_input_msg = std::make_unique<nova_interfaces::msg::DriveInputStamped>();
 
       drive_input_msg->drive_input.radius = angular == 0 ? INFINITY : (1.0 / pow(abs(angular), 2)) - 1;
       drive_input_msg->drive_input.direction = angular > 0 ? -1 : angular < 0 ? 1
                                                                               : 0;
       drive_input_msg->drive_input.speed = linear;
-      drive_input_msg->drive_input.mode = previous_state.drive_mode;
       drive_input_msg->header.stamp = node_->now();
 
       drive_input_pub->publish(std::move(drive_input_msg));
@@ -125,7 +106,31 @@ namespace teleop_drive_joy
 
     sent_lock_msg = false;
     previous_state = current_state;
+    current_state.drive_mode = controlModeToDriveMode(control_mode);
     drive_info_pub->publish(std::move(current_state));
+  }
+
+  void TeleopDriveJoy::sendHaltCommand()
+  {
+    if (!current_state.autonomous_mode)
+    {
+      if (!sent_lock_msg)
+      {
+        // Initializes with zeros by default.
+        auto drive_input_msg = std::make_unique<nova_interfaces::msg::DriveInputStamped>();
+        drive_input_pub->publish(std::move(drive_input_msg));
+
+        sent_lock_msg = true;
+      }
+    }
+    else
+    {
+      // We want to continously publish a zero commmand to override any continuous autonomous messages
+
+      // Initializes with zeros by default.
+      auto cmd_vel_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
+      cmd_vel_pub->publish(std::move(cmd_vel_msg));
+    }
   }
 
   void TeleopDriveJoy::handleButtonCallbacks(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
@@ -155,17 +160,17 @@ namespace teleop_drive_joy
     }
 
     // Controller Switches
-    if (joy_msg->buttons[params_.button_pivot_drive_controller] && current_state.drive_mode != drive_interfaces::msg::DriveInput::PIVOT)
+    if (joy_msg->buttons[params_.button_pivot_drive_controller] && control_mode != ControlMode::PIVOT_DRIVE)
     {
       RCLCPP_INFO(node_->get_logger(), "BUTTON: pivot_drive_mode");
       switchController(ControlMode::PIVOT_DRIVE);
     }
-    else if (joy_msg->buttons[params_.button_strafe_controller] && current_state.drive_mode != drive_interfaces::msg::DriveInput::STRAFE)
+    else if (joy_msg->buttons[params_.button_strafe_controller] && control_mode != ControlMode::STRAFE_DRIVE)
     {
       RCLCPP_INFO(node_->get_logger(), "BUTTON: strafe_mode");
       switchController(ControlMode::STRAFE_DRIVE);
     }
-    else if (joy_msg->buttons[params_.button_nova_diff_drive_controller] && current_state.drive_mode != drive_interfaces::msg::DriveInput::TANK)
+    else if (joy_msg->buttons[params_.button_nova_diff_drive_controller] && control_mode != ControlMode::STRAFE_DRIVE)
     {
       RCLCPP_INFO(node_->get_logger(), "BUTTON: diff_drive_mode");
       switchController(ControlMode::DIFF_DRIVE);
@@ -194,42 +199,6 @@ namespace teleop_drive_joy
     } else {
       RCLCPP_ERROR(node_->get_logger(), "Failed to switch to %s. Is drive.launch.py running?", prettyPrintMode(requested_control_mode).c_str());
     } });
-  }
-
-  void TeleopDriveJoy::joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
-  {
-
-    handleButtonCallbacks(joy_msg);
-
-    if (!current_state.locked)
-    {
-      sendDriveCommand(joy_msg);
-    }
-    else
-    {
-      // When lock button is pressed, immediately send a single no-motion command
-      // in order to stop the rover.
-
-      if (!current_state.autonomous_mode)
-      {
-        if (!sent_lock_msg)
-        {
-          // Initializes with zeros by default.
-          auto drive_input_msg = std::make_unique<drive_interfaces::msg::DriveInputStamped>();
-          drive_input_pub->publish(std::move(drive_input_msg));
-
-          sent_lock_msg = true;
-        }
-      }
-      else
-      {
-        // We want to continously publish a zero commmand to override any continuous autonomous messages
-
-        // Initializes with zeros by default.
-        auto cmd_vel_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
-        cmd_vel_pub->publish(std::move(cmd_vel_msg));
-      }
-    }
   }
 
 } // namespace teleop_drive_joy
