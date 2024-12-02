24eae10e63932ea56c242b856bc3bbdb9c57c7a9
Anthony Lew <anthony@v01dbreaker.cc>
2024-12-02 14:45:10 +1100

    Added new rover package, meshes and sdf file

---
+++ src/ros/rover/nix/packages/new-rover-description/default.nix
@@ -0,0 +1,23 @@
+{ lib
+, buildRosPackage
+, ament-cmake
+, std-msgs
+, geometry-msgs
+, nav-msgs
+, launch
+, launch-ros
+}:
+
+buildRosPackage {
+  name = "new-rover-description";
+  buildType = "ament_cmake";
+
+  src = builtins.path rec {
+    name = "rover-description-source";
+    path = ../../../new_rover_description;
+    filter = lib.novaSourceFilter [ ] path;
+  };
+
+  nativeBuildInputs = [ ament-cmake ];
+  #propagatedBuildInputs = [std-msgs nav-msgs geometry-msgs launch launch-ros ];
+}
