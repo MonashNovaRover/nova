80aa105cfa80546f124cb1349019a47fb0812924
RohitPilakkat <rohit.pilakkat@gmail.com>
2024-12-04 13:17:54 +1100

    Saving progress on packaging onshape-to-robot for nixos

--- nixfiles/packages/python/default.nix
+++ nixfiles/packages/python/default.nix
@@ -3,4 +3,5 @@
 {
   jcan = callPackage ./jcan { };
   mkdocs-safe-text-plugin = callPackage ./mkdocs-safe-text-plugin { };
+  onshape-to-robot = callPackage ./onshape-to-robot { inherit callPackage; };
 }
