61ddfd9e6be9875bbdfd37ec2cb3515765168c3b
RohitPilakkat <rohit.pilakkat@gmail.com>
2024-12-05 02:29:50 +1100

    feat: onshape-to-robot and related tools derivation

--- nixfiles/packages/python/default.nix
+++ nixfiles/packages/python/default.nix
@@ -3,5 +3,5 @@
 {
   jcan = callPackage ./jcan { };
   mkdocs-safe-text-plugin = callPackage ./mkdocs-safe-text-plugin { };
-  onshape-to-robot = callPackage ./onshape-to-robot { inherit callPackage; };
+  onshape-to-robot = callPackage ./onshape-to-robot { };
 }
