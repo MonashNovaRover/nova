e0bca54de854026ac4258fb526c809d849a6ecc7
Kabilan VS <kabiniki4849@gmail.com>
2024-12-08 15:53:29 +1100

    fix: teleop_drive_joy is now a normal node

--- src/ros/rover/teleop_drive_joy/launch/teleop-launch.py
+++ src/ros/rover/teleop_drive_joy/launch/teleop-launch.py
@@ -1,60 +1,74 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 import launch
 import launch_ros.actions
 
 
 def generate_launch_description():
-    joystick = launch.substitutions.LaunchConfiguration("joystick")
-    joy_dev = launch.substitutions.LaunchConfiguration("joy_dev")
+    # Launch configurations
+    joystick = launch.substitutions.LaunchConfiguration("joystick", default="xbox")
+    joy_device = launch.substitutions.LaunchConfiguration(
+        "joy_dev", default="/dev/input/js0"
+    )
+    joy_vel = launch.substitutions.LaunchConfiguration("joy_vel", default="cmd_vel")
     config_filepath = launch.substitutions.LaunchConfiguration("config_filepath")
 
+    # Path substitution for config file
+    default_config_path = launch.substitutions.LaunchConfiguration(
+        "config_filepath",
+        default=[
+            launch.substitutions.TextSubstitution(
+                text=os.path.join(
+                    get_package_share_directory("teleop_drive_joy"),
+                    "config",
+                    "",
+                )
+            ),
+            joystick,
+            launch.substitutions.TextSubstitution(text=".config.yaml"),
+        ],
+    )
+
+    # Nodes
+    joy_node = launch_ros.actions.Node(
+        package="joy",
+        executable="joy_node",
+        name="joy_node",
+        parameters=[
+            {
+                "dev": joy_device,
+                "deadzone": 0.1,
+                "autorepeat_rate": 20.0,
+            }
+        ],
+    )
+
+    teleop_node = launch_ros.actions.Node(
+        package="teleop_drive_joy",
+        executable="teleop_drive_joy_node",
+        name="teleop_drive_joy_node",
+        parameters=[config_filepath],
+        remappings=[
+            ("/cmd_vel", joy_vel),
+        ],
+    )
+
+    # Launch Description
     return launch.LaunchDescription(
         [
+            # Log Information
             launch.actions.LogInfo(msg=["Joystick Loaded: ", joystick]),
-            launch.actions.DeclareLaunchArgument("joy_vel", default_value="cmd_vel"),
-            launch.actions.DeclareLaunchArgument(
-                "joystick", default_value="xbox"
-            ),  # xbox, ps5, nintendo
+            # Declare Launch Arguments
+            launch.actions.DeclareLaunchArgument("joystick", default_value="xbox"),
             launch.actions.DeclareLaunchArgument(
                 "joy_dev", default_value="/dev/input/js0"
             ),
+            launch.actions.DeclareLaunchArgument("joy_vel", default_value="cmd_vel"),
             launch.actions.DeclareLaunchArgument(
-                "config_filepath",
-                default_value=[
-                    launch.substitutions.TextSubstitution(
-                        text=os.path.join(
-                            get_package_share_directory("teleop_drive_joy"),
-                            "config",
-                            "",
-                        )
-                    ),
-                    joystick,
-                    launch.substitutions.TextSubstitution(text=".config.yaml"),
-                ],
+                "config_filepath", default_value=default_config_path
             ),
-            launch_ros.actions.Node(
-                package="joy",
-                executable="joy_node",
-                name="joy_node",
-                parameters=[
-                    {
-                        "dev": joy_dev,
-                        "deadzone": 0.1,
-                        "autorepeat_rate": 20.0,
-                    }
-                ],
-            ),
-            launch_ros.actions.Node(
-                package="teleop_drive_joy",
-                executable="teleop_drive_joy_node",
-                name="teleop_drive_joy_node",
-                parameters=[config_filepath],
-                remappings={
-                    ("/cmd_vel", launch.substitutions.LaunchConfiguration("joy_vel"))
-                },
-            ),
+            # Add Nodes
+            joy_node,
+            teleop_node,
         ]
     )
--- src/ros/rover/teleop_drive_joy/src/teleop_drive_joy.cpp
+++ src/ros/rover/teleop_drive_joy/src/teleop_drive_joy.cpp
@@ -1,24 +1,10 @@
 /**
  * @file teleop_drive_joy.cpp
- * @brief Teleop Drive Joy node to Translate Joy Messages from /joy to Drive Commands
- * @author Dylan Gonzalez
+ * @brief Teleop Drive Joy node to translate Joy messages from /joy to Drive commands
  * Last Edited by Kabi
  */
-#include <cinttypes>
-#include <functional>
-#include <map>
-#include <memory>
-#include <set>
-#include <string>
-
-#include <rclcpp/rclcpp.hpp>
-#include <rcutils/logging_macros.h>
-#include <builtin_interfaces/msg/duration.hpp>
 
 #include "teleop_drive_joy/teleop_drive_joy.hpp"
-
-#define ROS_INFO_NAMED RCUTILS_LOG_INFO_NAMED
-#define ROS_INFO_COND_NAMED RCUTILS_LOG_INFO_EXPRESSION_NAMED
 
 using namespace std::chrono_literals;
 
@@ -32,25 +18,28 @@ namespace
 }
 
 using std::placeholders::_1;
+
 namespace teleop_drive_joy
 {
 
-  rclcpp::node_interfaces::NodeBaseInterface::SharedPtr TeleopDriveJoy::get_node_base_interface()
+  TeleopDriveJoy::TeleopDriveJoy(const rclcpp::NodeOptions &options)
+      : Node("teleop_drive_joy_node", options)
   {
-    return node_->get_node_base_interface();
+    drive_input_pub = this->create_publisher<nova_interfaces::msg::DriveInputStamped>(DEFAULT_OUTPUT_TOPIC, 50);
+    cmd_vel_pub = this->create_publisher<geometry_msgs::msg::TwistStamped>(DEFAULT_OUTPUT_TOPIC_TWIST, 50);
+    drive_info_pub = this->create_publisher<nova_interfaces::msg::DriveInfo>(DEFAULT_OUTPUT_TOPIC_INFO, 50);
+
+    joy_sub = this->create_subscription<sensor_msgs::msg::Joy>(
+        DEFAULT_INPUT_TOPIC, rclcpp::QoS(10), std::bind(&TeleopDriveJoy::joyCallback, this, _1));
+
+    switch_controller_client = this->create_client<controller_manager_msgs::srv::SwitchController>("/controller_manager/switch_controller");
+
+    control_mode = ControlMode::PIVOT_DRIVE;
   }
 
-  TeleopDriveJoy::TeleopDriveJoy(const rclcpp::NodeOptions &options) : node_{std::make_shared<rclcpp::Node>("teleop_drive_joy_node_", options)}
+  void TeleopDriveJoy::initializeParams()
   {
-    drive_input_pub = node_->create_publisher<nova_interfaces::msg::DriveInputStamped>(DEFAULT_OUTPUT_TOPIC, 50);
-    cmd_vel_pub = node_->create_publisher<geometry_msgs::msg::TwistStamped>(DEFAULT_OUTPUT_TOPIC_TWIST, 50);
-    drive_info_pub = node_->create_publisher<nova_interfaces::msg::DriveInfo>(DEFAULT_OUTPUT_TOPIC_INFO, 50);
-
-    joy_sub = node_->create_subscription<sensor_msgs::msg::Joy>(DEFAULT_INPUT_TOPIC, rclcpp::QoS(10), std::bind(&TeleopDriveJoy::joyCallback, this, _1));
-
-    switch_controller_client = node_->create_client<controller_manager_msgs::srv::SwitchController>("/controller_manager/switch_controller");
-
-    param_listener_ = std::make_shared<ParamListener>(node_);
+    param_listener_ = std::make_shared<ParamListener>(this->shared_from_this());
     params_ = param_listener_->get_params();
 
     if (param_listener_->is_old(params_))
@@ -61,7 +50,6 @@ namespace teleop_drive_joy
 
   void TeleopDriveJoy::joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
   {
-
     handleButtonCallbacks(joy_msg);
 
     if (!current_state.locked)
@@ -70,14 +58,12 @@ namespace teleop_drive_joy
     }
     else
     {
-      // When lock button is pressed, immediately send a single halt command to Stop the Rover
       sendHaltCommand();
     }
   }
 
   void TeleopDriveJoy::sendDriveCommand(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
   {
-
     auto controller_params = params_.controllers_map.at(modeToController(control_mode));
 
     double angular = joy_msg->axes[controller_params.axis_angular_z] * controller_params.scale_angular_z;
@@ -88,7 +74,7 @@ namespace teleop_drive_joy
       auto cmd_vel_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
       cmd_vel_msg->twist.angular.z = angular;
       cmd_vel_msg->twist.linear.x = linear;
-      cmd_vel_msg->header.stamp = node_->now();
+      cmd_vel_msg->header.stamp = this->now();
 
       cmd_vel_pub->publish(std::move(cmd_vel_msg));
     }
@@ -96,11 +82,11 @@ namespace teleop_drive_joy
     {
       auto drive_input_msg = std::make_unique<nova_interfaces::msg::DriveInputStamped>();
 
-      drive_input_msg->drive_input.radius = angular == 0 ? INFINITY : (1.0 / pow(abs(angular), 2)) - 1;
+      drive_input_msg->drive_input.radius = angular == 0 ? INFINITY : (1.0 / std::pow(std::abs(angular), 2)) - 1;
       drive_input_msg->drive_input.direction = angular > 0 ? -1 : angular < 0 ? 1
                                                                               : 0;
       drive_input_msg->drive_input.speed = linear;
-      drive_input_msg->header.stamp = node_->now();
+      drive_input_msg->header.stamp = this->now();
 
       drive_input_pub->publish(std::move(drive_input_msg));
     }
@@ -108,7 +94,13 @@ namespace teleop_drive_joy
     sent_lock_msg = false;
     previous_state = current_state;
     current_state.drive_mode = controlModeToDriveMode(control_mode);
-    drive_info_pub->publish(std::move(current_state));
+
+    auto drive_info_msg = std::make_unique<nova_interfaces::msg::DriveInfo>();
+    drive_info_msg->locked = current_state.locked;
+    drive_info_msg->autonomous_mode = current_state.autonomous_mode;
+    drive_info_msg->drive_mode = current_state.drive_mode;
+
+    drive_info_pub->publish(std::move(drive_info_msg));
   }
 
   void TeleopDriveJoy::sendHaltCommand()
@@ -117,32 +109,41 @@ namespace teleop_drive_joy
     {
       if (!sent_lock_msg)
       {
-        // Initializes with zeros by default.
         auto drive_input_msg = std::make_unique<nova_interfaces::msg::DriveInputStamped>();
         drive_input_pub->publish(std::move(drive_input_msg));
-        drive_info_pub->publish(std::move(current_state));
+
+        auto drive_info_msg = std::make_unique<nova_interfaces::msg::DriveInfo>();
+        drive_info_msg->locked = current_state.locked;
+        drive_info_msg->autonomous_mode = current_state.autonomous_mode;
+        drive_info_msg->drive_mode = current_state.drive_mode;
+
+        drive_info_pub->publish(std::move(drive_info_msg));
         sent_lock_msg = true;
       }
     }
     else
     {
-      // We want to continously publish a zero commmand to override any continuous autonomous messages
-
-      // Initializes with zeros by default.
       auto cmd_vel_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
       cmd_vel_pub->publish(std::move(cmd_vel_msg));
-      drive_info_pub->publish(std::move(current_state));
+
+      auto drive_info_msg = std::make_unique<nova_interfaces::msg::DriveInfo>();
+      drive_info_msg->locked = current_state.locked;
+      drive_info_msg->autonomous_mode = current_state.autonomous_mode;
+      drive_info_msg->drive_mode = current_state.drive_mode;
+
+      drive_info_pub->publish(std::move(drive_info_msg));
     }
   }
 
   void TeleopDriveJoy::handleButtonCallbacks(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
   {
-    auto now = node_->now();
+    auto now = this->now();
 
-    auto isDebounced = [&](int button_index) -> bool {
+    auto isDebounced = [&](int button_index) -> bool
+    {
       if (joy_msg->buttons[button_index] &&
           (last_button_press_time_.find(button_index) == last_button_press_time_.end() ||
-          (now - last_button_press_time_[button_index]) > rclcpp::Duration(BUTTON_DEBOUNCE_INTERVAL)))
+           (now - last_button_press_time_[button_index]) > rclcpp::Duration(BUTTON_DEBOUNCE_INTERVAL)))
       {
         last_button_press_time_[button_index] = now;
         return true;
@@ -154,40 +155,40 @@ namespace teleop_drive_joy
     if (isDebounced(params_.button_unlock) && current_state.locked)
     {
       current_state.locked = false;
-      RCLCPP_INFO(node_->get_logger(), "BUTTON: unlock");
+      RCLCPP_INFO(this->get_logger(), "BUTTON: unlock");
     }
     else if (isDebounced(params_.button_lock) && !current_state.locked)
     {
       current_state.locked = true;
-      RCLCPP_INFO(node_->get_logger(), "BUTTON: lock");
+      RCLCPP_INFO(this->get_logger(), "BUTTON: lock");
     }
 
     // Autonomous and Manual
     if (isDebounced(params_.button_autonomous_control) && !current_state.autonomous_mode)
     {
       current_state.autonomous_mode = true;
-      RCLCPP_INFO(node_->get_logger(), "BUTTON: autonomous_control");
+      RCLCPP_INFO(this->get_logger(), "BUTTON: autonomous_control");
     }
     else if (isDebounced(params_.button_manual_control) && current_state.autonomous_mode)
     {
       current_state.autonomous_mode = false;
-      RCLCPP_INFO(node_->get_logger(), "BUTTON: manual_control");
+      RCLCPP_INFO(this->get_logger(), "BUTTON: manual_control");
     }
 
     // Controller Switches
     if (isDebounced(params_.button_pivot_drive_controller) && control_mode != ControlMode::PIVOT_DRIVE)
     {
-      RCLCPP_INFO(node_->get_logger(), "BUTTON: pivot_drive_mode");
+      RCLCPP_INFO(this->get_logger(), "BUTTON: pivot_drive_mode");
       switchController(ControlMode::PIVOT_DRIVE);
     }
     else if (isDebounced(params_.button_strafe_controller) && control_mode != ControlMode::STRAFE_DRIVE)
     {
-      RCLCPP_INFO(node_->get_logger(), "BUTTON: strafe_mode");
+      RCLCPP_INFO(this->get_logger(), "BUTTON: strafe_mode");
       switchController(ControlMode::STRAFE_DRIVE);
     }
     else if (isDebounced(params_.button_nova_diff_drive_controller) && control_mode != ControlMode::DIFF_DRIVE)
     {
-      RCLCPP_INFO(node_->get_logger(), "BUTTON: diff_drive_mode");
+      RCLCPP_INFO(this->get_logger(), "BUTTON: diff_drive_mode");
       switchController(ControlMode::DIFF_DRIVE);
     }
   }
@@ -196,7 +197,11 @@ namespace teleop_drive_joy
   {
     if (requested_control_mode == control_mode)
       return;
-    RCLCPP_INFO(node_->get_logger(), "Changing from %s to %s", prettyPrintMode(control_mode).c_str(), prettyPrintMode(requested_control_mode).c_str());
+
+    RCLCPP_INFO(this->get_logger(), "Changing from %s to %s",
+                prettyPrintMode(control_mode).c_str(),
+                prettyPrintMode(requested_control_mode).c_str());
+
     std::string activate_controller = modeToController(requested_control_mode);
     std::string deactivate_controller = modeToController(control_mode);
 
@@ -206,17 +211,35 @@ namespace teleop_drive_joy
     request->strictness = 2;
     request->activate_asap = true;
 
-    switch_controller_client->async_send_request(request, [this, requested_control_mode](rclcpp::Client<controller_manager_msgs::srv::SwitchController>::SharedFuture result)
-                                                 {
-    if (result.get()->ok) {
-      RCLCPP_INFO(node_->get_logger(), "Successfully switched to %s.", prettyPrintMode(requested_control_mode).c_str());
-      control_mode = requested_control_mode;
-    } else {
-      RCLCPP_ERROR(node_->get_logger(), "Failed to switch to %s. Is drive.launch.py running?", prettyPrintMode(requested_control_mode).c_str());
-    } });
+    auto future = switch_controller_client->async_send_request(request);
+
+    try
+    {
+      auto result = future.get();
+      if (result->ok)
+      {
+        RCLCPP_INFO(this->get_logger(), "Successfully switched to %s.", prettyPrintMode(requested_control_mode).c_str());
+        control_mode = requested_control_mode;
+      }
+      else
+      {
+        RCLCPP_ERROR(this->get_logger(), "Failed to switch to %s. Is drive.launch.py running?", prettyPrintMode(requested_control_mode).c_str());
+      }
+    }
+    catch (const std::exception &e)
+    {
+      RCLCPP_ERROR(this->get_logger(), "Service call failed: %s", e.what());
+    }
   }
 
 } // namespace teleop_drive_joy
 
-#include "rclcpp_components/register_node_macro.hpp"
-RCLCPP_COMPONENTS_REGISTER_NODE(teleop_drive_joy::TeleopDriveJoy)
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<teleop_drive_joy::TeleopDriveJoy>();
+  node->initializeParams();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
