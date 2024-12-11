f6fc162bed9ae5a80de8dcbed6dc09ca71df459f
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2024-12-12 09:10:26 +1100

    Corrected rover-help filepath

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -99,7 +99,7 @@ in
             hydra-vomit = "${pkgs.bash}/bin/bash ${../../../scripts/hydra-vomit.sh}";
 
             # Rover aliases
-            rover-help = "cat ~/Documents/roverhelp.txt"
+            rover-help = "cat ~/nova/nixfiles/doc/roverhelp.txt"
             launch-base = "~/BUILDS/master/bin/ros2 launch nova_bringup base.launch.py"
             launch-drive = "~/BUILDS/master/bin/ros2 launch nova_bringup drive.launch.py"
           }
