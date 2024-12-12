5e0304f54530ee1a9194037eac6bdad3eda2e228
Bailey Chessum <bailey.chessum1@gmail.com>
2024-12-12 15:11:21 +1100

    fix: added sauce-code-pro using home manager

--- nixfiles/modules/home/desktop/fonts.nix
+++ nixfiles/modules/home/desktop/fonts.nix
@@ -6,9 +6,14 @@ in
 {
   config = lib.mkIf cfg.enable {
     fonts.fontconfig.enable = true;
+    fonts.fontconfig.defaultFonts.monospace = [ "SauceCodePro" "SourceCodePro" ];
 
     home.packages = with pkgs; [
+<<<<<<< HEAD
       nerd-fonts._0xproto
+=======
+      # (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+>>>>>>> 4116677 (fix: added sauce-code-pro using home manager)
       nerd-fonts.sauce-code-pro
     ];
   };
