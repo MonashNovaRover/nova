8eac7d77626094f18d70f4837923fdc127c36423
Bailey Chessum <bailey.chessum1@gmail.com>
2024-12-12 15:34:46 +1100

    fix: made the base monospace font work again

--- nixfiles/modules/home/desktop/fonts.nix
+++ nixfiles/modules/home/desktop/fonts.nix
@@ -5,12 +5,21 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    fonts.fontconfig.enable = true;
-    fonts.fontconfig.defaultFonts.monospace = [ "SauceCodePro" "SourceCodePro" ];
+    fonts = {
+      fontconfig = {
+        enable = true;
+
+        defaultFonts = {
+          # nerdfonts don't display without this definition
+          monospace = [ "Monospace 12" ];
+        };
+      };
+    };
 
     home.packages = with pkgs; [
       nerd-fonts._0xproto
       nerd-fonts.sauce-code-pro
+      source-code-pro
     ];
   };
 }
