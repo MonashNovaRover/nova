38f22ebfc522e91acea8d7d6699f3bc9433bc165
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2024-12-14 17:49:17 +1100

    Added launch-arm and launch-ec aliases

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -102,6 +102,8 @@ in
             rover-help = "more ~/nova/nixfiles/doc/rover-help.md";
             launch-base = "~/Builds/master/bin/ros2 launch nova_bringup base.launch.py";
             launch-drive = "~/Builds/master/bin/ros2 launch nova_bringup drive.launch.py";
+            launch-arm = "~/Builds/master/bin/ros2 launch nova_bringup arm.launch.py";
+            launch-ec = "~/Builds/master/bin/ros2 launch nova_bringup ec_rover.launch.py";
           }
         ];
 
