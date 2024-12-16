02682255b6c581c22a501add0100bd5e29722e9f
Anthony Lew <anthony@v01dbreaker.cc>
2024-12-16 21:44:16 +1100

    Changed file paths across nova for restructured rover description

--- src/ros/rover/controllers/pivot_drive_controller/test/test_load_pivot_drive_controller.cpp
+++ src/ros/rover/controllers/pivot_drive_controller/test/test_load_pivot_drive_controller.cpp
@@ -28,7 +28,7 @@ TEST(TestLoadPivotDriveController, load_controller)
     std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
 
   std::string rover_description_share_path = ament_index_cpp::get_package_share_directory("rover_description");
-  std::string urdf_path = rover_description_share_path + "/urdf/rover.urdf.xacro";
+  std::string urdf_path = rover_description_share_path + "/base/urdf/rover.urdf.xacro";
 
 
   controller_manager::ControllerManager cm(
