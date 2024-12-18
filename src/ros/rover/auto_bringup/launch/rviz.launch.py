9ce5ecbad1eb79f0f9f99c23aabd14ca9a198172
Monash Nova Rover <novaroverteam@monash.edu>
2024-12-18 13:19:44 +1100

    added params file as launch argument to rviz.launch.py

--- src/ros/rover/auto_bringup/launch/rviz.launch.py
+++ src/ros/rover/auto_bringup/launch/rviz.launch.py
@@ -1,20 +1,34 @@
 from ament_index_python.packages import get_package_share_path
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
-def generate_launch_description():
+def launch_setup(context, *args, **kwargs):
+    params_file = LaunchConfiguration('params_file')
 
-    return LaunchDescription([
+    return [
         Node(
             package='rviz2',
             namespace='',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', [PathJoinSubstitution([FindPackageShare('auto_bringup'), 'rviz', 'navigation.rviz'])]]
+            arguments=['-d', [params_file]]
         ),
-    ])
+    ]
+
+def generate_launch_description():
+    auto_bringup_dir = FindPackageShare('auto_bringup')
+
+    declared_arguments = [
+        DeclareLaunchArgument(
+            name='params_file',
+            default_value=PathJoinSubstitution([auto_bringup_dir, 'rviz', 'navigation.rviz']),
+            description='Absolute path to rviz navigation params file',
+        ),
+    ]
+
+    return LaunchDescription(  
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
