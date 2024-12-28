8f3a7c00e2dbc01ef68f01ef816c367685061b71
Victor Bartlinski <vicbart08@gmail.com>
2024-12-28 11:39:34 +1100

    cleaned up teleop.launch.py

--- src/ros/rover/teleop_drive_joy/launch/teleop.launch.py
+++ src/ros/rover/teleop_drive_joy/launch/teleop.launch.py
@@ -1,74 +1,67 @@
-import os
-from ament_index_python.packages import get_package_share_directory
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch.actions import OpaqueFunction, DeclareLaunchArgument, LogInfo
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
+
+def launch_setup(context, *args, **kwargs):
+    teleop_drive_joy_dir = FindPackageShare('teleop_drive_joy')
+
+    # Launch configurations
+    joystick = LaunchConfiguration('joystick').perform(context)
+    joy_device = LaunchConfiguration('joy_dev')
+    joy_vel = LaunchConfiguration('joy_vel')
+    params_file = LaunchConfiguration(
+        'params_file',
+        default=PathJoinSubstitution([teleop_drive_joy_dir, 'config', f'{joystick}.config.yaml']),
+    )
+
+    return [
+        # Add Nodes
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node',
+            parameters=[
+                {'dev': joy_device,
+                 'deadzone': 0.1,
+                 'autorepeat_rate': 20.0}
+            ],
+        ),
+        Node(
+            package='teleop_drive_joy',
+            executable='teleop_drive_joy_node',
+            name='teleop_drive_joy_node',
+            parameters=[PathJoinSubstitution([teleop_drive_joy_dir, 'config', f'{joystick}.config.yaml'])],
+            remappings=[
+                ('/cmd_vel', joy_vel),
+            ],
+        ),
+        # Log Information
+        LogInfo(msg=['Joystick Loaded: ', joystick]),
+    ]
 
 def generate_launch_description():
-    # Launch configurations
-    joystick = launch.substitutions.LaunchConfiguration("joystick", default="xbox")
-    joy_device = launch.substitutions.LaunchConfiguration(
-        "joy_dev", default="/dev/input/js0"
-    )
-    joy_vel = launch.substitutions.LaunchConfiguration("joy_vel", default="cmd_vel")
-    config_filepath = launch.substitutions.LaunchConfiguration("config_filepath")
+    declared_arguments = [
+        DeclareLaunchArgument(
+            name='joystick', 
+            default_value='xbox',
+        ),
+        DeclareLaunchArgument(
+            name='joy_dev', 
+            default_value='/dev/input/js0',
+        ),
+        DeclareLaunchArgument(
+            name='joy_vel', 
+            default_value='cmd_vel'
+        ),
+        DeclareLaunchArgument(
+            name='params_file',
+            default_value='', #Defined in launch_setup due to requiring another launch argument
+        ),
+    ]
 
-    # Path substitution for config file
-    default_config_path = launch.substitutions.LaunchConfiguration(
-        "config_filepath",
-        default=[
-            launch.substitutions.TextSubstitution(
-                text=os.path.join(
-                    get_package_share_directory("teleop_drive_joy"),
-                    "config",
-                    "",
-                )
-            ),
-            joystick,
-            launch.substitutions.TextSubstitution(text=".config.yaml"),
-        ],
-    )
-
-    # Nodes
-    joy_node = launch_ros.actions.Node(
-        package="joy",
-        executable="joy_node",
-        name="joy_node",
-        parameters=[
-            {
-                "dev": joy_device,
-                "deadzone": 0.1,
-                "autorepeat_rate": 20.0,
-            }
-        ],
-    )
-
-    teleop_node = launch_ros.actions.Node(
-        package="teleop_drive_joy",
-        executable="teleop_drive_joy_node",
-        name="teleop_drive_joy_node",
-        parameters=[config_filepath],
-        remappings=[
-            ("/cmd_vel", joy_vel),
-        ],
-    )
-
-    # Launch Description
-    return launch.LaunchDescription(
-        [
-            # Log Information
-            launch.actions.LogInfo(msg=["Joystick Loaded: ", joystick]),
-            # Declare Launch Arguments
-            launch.actions.DeclareLaunchArgument("joystick", default_value="xbox"),
-            launch.actions.DeclareLaunchArgument(
-                "joy_dev", default_value="/dev/input/js0"
-            ),
-            launch.actions.DeclareLaunchArgument("joy_vel", default_value="cmd_vel"),
-            launch.actions.DeclareLaunchArgument(
-                "config_filepath", default_value=default_config_path
-            ),
-            # Add Nodes
-            joy_node,
-            teleop_node,
-        ]
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
     )
