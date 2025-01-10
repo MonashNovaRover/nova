1d30a2cc8850417db7cbce60e4fa939de4936c07
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-01-10 16:00:02 +1100

    Updated zero-pivots.sh
    Formatting

--- nixfiles/scripts/zero-pivots.sh
+++ nixfiles/scripts/zero-pivots.sh
@@ -2,13 +2,13 @@
 
 # This script zeroes out the pivot for the chosen BLCMD.
 
-echo "Ensure that drive is not running before continuing."
+echo "Ensure that drive is not running before you continue."
+echo
 
 while true; do
   read -p "BLCMD ID: " blcmd_id
 
   if [[ "$blcmd_id" =~ ^[1-8]$ ]]; then
-    echo "cansend in progress..."
     break
   else
     echo "Invalid input. Use the 'list-blcmds' command to view all BLCMD IDs."
@@ -17,6 +17,10 @@ done
 
 echo "Please manually swivel the pivot to ensure the wheel is straight."
 read -p "Press any key to continue." -n1 -s
+echo
+
+echo "cansend in progress..."
+echo
 
 cansend can0 0${blcmd_id}8#
 sleep 1
@@ -24,4 +28,5 @@ cansend can0 0${blcmd_id}4#c764
 sleep 1
 cansend can0 0${blcmd_id}8#
 
+echo
 echo "Done."
