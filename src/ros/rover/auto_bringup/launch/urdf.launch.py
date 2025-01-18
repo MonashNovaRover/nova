e10725eaff4a10591ebcb83d18e6e19e656980ca
RohitPilakkat <rohit.pilakkat@gmail.com>
2025-01-18 23:27:19 +1100

    Merged master and relevant arm-urdf files

--- src/ros/rover/auto_bringup/launch/urdf.launch.py
+++ src/ros/rover/auto_bringup/launch/urdf.launch.py
@@ -26,6 +26,7 @@ def launch_setup(context, *args, **kwargs):
     gazebo = LaunchConfiguration('gazebo').perform(context)
     model = LaunchConfiguration('model').perform(context)
     robot_name = LaunchConfiguration('robot_name').perform(context)
+    arm = LaunchConfiguration('arm').perform(context)
     use_local_mesh = LaunchConfiguration('use_local_mesh').perform(context)
 
     return [
@@ -33,7 +34,18 @@ def launch_setup(context, *args, **kwargs):
             package='robot_state_publisher',
             executable='robot_state_publisher',
             parameters=[{'robot_description': 
-                ParameterValue(Command(['xacro ', model, ' ', 'gazebo:=', gazebo, ' ', 'robot_name:=', robot_name, ' ', 'angle:=', angle, ' ', 'use_local_mesh:=', use_local_mesh]), value_type=str)
+                ParameterValue(Command(['xacro ', model, ' ', 'gazebo:=', gazebo, ' ', 'robot_name:=', robot_name, ' ', 'angle:=', angle, ' ', 'arm:=', arm, ' ', 'use_local_mesh:=', use_local_mesh]), value_type=str)
+            }]
+        ),
+        # Launch joint states for arm
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            namespace='',
+            output='screen',
+            emulate_tty=True,
+            parameters=[{
+                'source_list': ['/arm/joint_states', '/joint_states']
             }]
         )
     ]
@@ -44,7 +56,7 @@ def generate_launch_description():
 
     declared_arguments = [
         DeclareLaunchArgument(
-            name='angle', 
+            name='angle',
             default_value='15',
             description='Angle (in degrees) at which the camera is mounted',
         ),
@@ -62,6 +74,11 @@ def generate_launch_description():
             name='robot_name',
             default_value='Banksia',
             description='name of the robot',
+        ),
+        DeclareLaunchArgument(
+            name='arm',
+            default_value='false',
+            description='whether to launch arm',
         ),
         DeclareLaunchArgument(
             name='use_local_mesh',
