edc1fa17d7222785bf1e41f674864d6ef991d8ab
Victor Bartlinski <vicbart08@gmail.com>
2025-01-19 13:57:06 +1100

    backup

---
+++ src/ros/rover/nav2_autonomous/nova_behavior_tree/plugins/ar_tag_detected_condition.cpp
@@ -0,0 +1,109 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <functional>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+
+#include <aruco_opencv_msgs/msg/aruco_detection.hpp>
+#include <aruco_opencv_msgs/msg/marker_pose.hpp>
+#include <rclcpp/callback_group.hpp>
+#include <rclcpp/qos.hpp>
+#include <rclcpp/subscription_options.hpp>
+
+#include "nova_behavior_tree/ar_tag_detected_condition.hpp"
+
+namespace nova_behavior_tree
+{
+
+    ARTagDetectedCondition::ARTagDetectedCondition(
+        const std::string &condition_name,
+        const BT::NodeConfiguration &conf)
+        : BT::ConditionNode(condition_name, conf),
+          initialized_(false)
+    {
+    }
+
+    void ARTagDetectedCondition::initialize()
+    {
+        node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+        callback_group_ = node_->create_callback_group(
+            rclcpp::CallbackGroupType::MutuallyExclusive, 
+            false);
+        callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
+
+        rclcpp::SubscriptionOptions sub_option;
+        sub_option.callback_group = callback_group_;
+
+        sub_ar_tag_ = node_->create_subscription<aruco_opencv_msgs::msg::ArucoDetection>(
+            "/aruco_detections", 
+            rclcpp::SystemDefaultsQoS(), 
+            std::bind(&ARTagDetectedCondition::callback_ar_tag, this, std::placeholders::_1), 
+            sub_option
+        );
+
+        initialized_ = true;
+    }
+
+    void ARTagDetectedCondition::callback_ar_tag(const aruco_opencv_msgs::msg::ArucoDetection::SharedPtr msg)
+    {
+        // Save first ar tag id
+        tag_found_ = false;
+        for (aruco_opencv_msgs::msg::MarkerPose marker : msg->markers) {
+            tag_id_ = marker.marker_id;
+            tag_found_ = true;
+            return;
+        }
+    }
+
+    BT::NodeStatus ARTagDetectedCondition::tick()
+    {
+        if (!initialized_)
+        {
+            initialize();
+        }
+
+        callback_group_executor_.spin_some();
+
+        if (detected())
+        {
+            return BT::NodeStatus::SUCCESS;
+        }
+        return BT::NodeStatus::FAILURE;
+    }
+
+    bool ARTagDetectedCondition::detected()
+    {
+        IDs visited_goals;
+        getInput("visited goals", visited_goals);
+        if (tag_found_)
+        {
+            for (int id : visited_goals)
+            {
+                if (id == tag_id_)
+                {
+                    return false;
+                }
+            }
+            setOutput("ar_id", tag_id_);
+        }
+        return tag_found_;
+    }
+
+} // namespace nova_behavior_tree
+
+#include "behaviortree_cpp/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+    factory.registerNodeType<nova_behavior_tree::ARTagDetectedCondition>("ARTagDetected");
+}
