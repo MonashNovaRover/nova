5995fe14a47c5a35a8a6f398b7afbf0ccc9261ba
Orlando Chamberlain <ocha0025@student.monash.edu>
2025-01-26 23:36:31 +1100

    ros2control node for arm: initial work
    Based off of the diff drive controller. Not sure how to test this.
    
    Still need to figure out what messages to get from teleop, how to send
    commands to blcmd, and how to set up speed limits for the arm if
    applicable.

--- src/ros/rover/nix/packages/controllers/default.nix
+++ src/ros/rover/nix/packages/controllers/default.nix
@@ -6,4 +6,5 @@ with pkgs;
   nova-pivot-drive-controller = callPackage ./pivot-drive-controller { };
   nova-strafe-controller = callPackage ./strafe-controller { };
   nova-diff-drive-controller = callPackage ./nova-diff-drive-controller { };
+  nova-arm-controller = callPackage ./nova-arm-controller { };
 }
