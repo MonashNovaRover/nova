fca7523d62c846690f785fc4b4feec8b68aefc5a
Terry Tian <97772315+tytia@users.noreply.github.com>
2025-01-28 14:42:31 +1100

    Created nav2_utils file and added method to convert PoseStamped messages to strings

---
+++ src/ros/rover/nav2_autonomous/nova_behavior_tree/include/nova_behavior_tree/nav2_utils.hpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2021 Samsung Research America
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NOVA_BEHAVIOR_TREE__NAV2_UTILS_HPP
+#define NOVA_BEHAVIOR_TREE__NAV2_UTILS_HPP
+
+#include <string>
+#include <sstream>
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "rclcpp/logging.hpp"
+
+namespace nova_behavior_tree
+{
+
+    std::string poseStampedToString(const geometry_msgs::msg::PoseStamped &pose)
+    {
+        std::ostringstream oss;
+        oss << "PoseStamped :"
+            << "  Header:"
+            << "    frame_id: " << pose.header.frame_id.c_str()
+            << "    stamp: " << pose.header.stamp.sec << "." << pose.header.stamp.nanosec
+            << "  Pose:"
+            << "    position: " << "[x: " << pose.pose.position.x 
+                                << ", y: " << pose.pose.position.y 
+                                << ", z: " << pose.pose.position.z << "]"
+            << "    orientation: " << "[x: " << pose.pose.orientation.x 
+                                   << ", y: " << pose.pose.orientation.y 
+                                   << ", z: " << pose.pose.orientation.z 
+                                   << ", w: " << pose.pose.orientation.w << "]";
+        
+        return oss.str();
+    }
+
+}
+
+#endif // NOVA_BEHAVIOR_TREE__NAV2_UTILS_HPP_
