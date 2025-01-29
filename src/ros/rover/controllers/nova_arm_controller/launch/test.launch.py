aa364fe6ab90b893cda9ba26886c58ae36b3ff22
Orlando Chamberlain <ocha0025@student.monash.edu>
2025-01-30 00:45:26 +1100

    arm controller: still broken
    I don't know how to launch this :(
    
    The 'type' param was not defined for 'pivot_drive_controller'.
    [spawner-1] [FATAL] [1738158201.952814041] [spawner_nova_arm_controller]: Failed loading controller nova_arm_controller
    
    a lot of these for all the ros2 control controllers. Not sure why, might
    figure out tomorrow hopefully. This is when running
    ros2 launch auto_bringup gazebo.launch.py arm:=true
    or
    ros2 launch auto_bringup control.launch.py arm:=true

--- src/ros/rover/controllers/nova_arm_controller/launch/test.launch.py
+++ src/ros/rover/controllers/nova_arm_controller/launch/test.launch.py
@@ -3,14 +3,36 @@
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, GroupAction
 from launch_ros.actions import Node
 
 
 # Generate the launch file with all inputs
 def generate_launch_description():
+    gazebo = False
+    model = 'rover7'
     return LaunchDescription([
         Node(
-            package='nova_arm_controller', executable='nova_arm_controller', output='screen',
+            package='controller_manager',
+            executable='spawner',
+            arguments=['velocity_controller'], #, '-t', 'velocity_controller/nova_arm_controller'],
+        ),
+        GroupAction(
+            actions=[
+                Node(
+                    package='controller_manager',
+                    executable='spawner',
+                    arguments=['joint_state_broadcaster']
+                ),
+                Node(
+                    package='controller_manager',
+                    executable='ros2_control_node',
+                    parameters=['nova_arm_controller'],
+                    remappings=[('/controller_manager/robot_description', '/robot_description')],
+                ),
+                ],
         ),
     ]
                              )
