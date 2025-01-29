2f1bfe1bdb9c0d5854c93e92839e591bba4def6a
Victor Bartlinski <vicbart08@gmail.com>
2025-01-30 02:58:50 +1100

    cleaned up camera.launch.py

--- src/ros/rover/auto_bringup/launch/camera.launch.py
+++ src/ros/rover/auto_bringup/launch/camera.launch.py
@@ -12,48 +12,38 @@ PACKAGE: 	auto_bringup
 CREATION:	13/11/2024
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import  Node, ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import EqualsSubstitution
 
 def launch_setup(context, *args, **kwargs):
-    auto_bringup_dir = get_package_share_directory('auto_bringup')
-    depthai_dir = get_package_share_directory('depthai_ros_driver')
+    depthai_dir = FindPackageShare('depthai_ros_driver')
 
-    camera_model = LaunchConfiguration('camera_model').perform(context)
+    ar = LaunchConfiguration('ar')
+    ar_params = LaunchConfiguration('ar')
+    camera_model = LaunchConfiguration('camera_model')
+    gazebo = LaunchConfiguration('gazebo')
     name = LaunchConfiguration('name').perform(context)
-    params_file = LaunchConfiguration('params_file')
-    parent_frame = LaunchConfiguration('parent_frame').perform(context)
-    use_filter = LaunchConfiguration('use_filter').perform(context)
+    oak_params = LaunchConfiguration('oak_params')
+    parent_frame = LaunchConfiguration('parent_frame')
+    pointclouds = LaunchConfiguration('pointclouds')
+    rectify_image = LaunchConfiguration('rectify_image')
 
     return [
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(PathJoinSubstitution([depthai_dir, 'launch', 'camera.launch.py'])),
-            condition=IfCondition(
-                EqualsSubstitution(LaunchConfiguration('gazebo'), "False")
-            ),
+            condition=UnlessCondition(gazebo),
+            launch_description_source=PythonLaunchDescriptionSource(PathJoinSubstitution([depthai_dir, 'launch', 'camera.launch.py'])),
             launch_arguments={'name': name,
                               'parent_frame': parent_frame,
-                              'params_file': params_file,
+                              'params_file': oak_params,
                               'camera_model': camera_model,
                               'rectify_rgb': 'false',
                               }.items()
-        ),
-        Node(
-            condition=IfCondition(use_filter),
-            package='nova_pointcloud_filter',
-            executable='depth_filter',
-            name='depth_filter',
-            remappings=[('/depth/image', f'{name}/stereo/image_raw'),
-                        ('/depth/image_filtered', f'{name}/stereo/image_filtered')],
-            parameters=[{'t_filter': 0, 'r_filter': 0, 'b_filter': 75, 'l_filter': 0}]
         ),
         ComposableNodeContainer(
             name=f"{name}_image_proc_container",
@@ -62,31 +52,31 @@ def launch_setup(context, *args, **kwargs):
             executable="component_container",
             composable_node_descriptions=[
                 ComposableNode(
-                    condition=IfCondition(LaunchConfiguration('rectify_image')),
+                    condition=IfCondition(rectify_image),
                     package='image_proc',
                     plugin='image_proc::RectifyNode',
                     name='rectify_color_node',
                     remappings=[
                         ('image', f'{name}/rgb/image_raw'),
                         ('camera_info', f'{name}/rgb/camera_info'),
-                        ('image_rect', f'{name}/rgb/image_rect')
-                    ]),
+                        ('image_rect', f'{name}/rgb/image_rect')],
+                ),
                 ComposableNode(
-                    condition=IfCondition(LaunchConfiguration('pointclouds')),
+                    condition=IfCondition(pointclouds),
                     package='depth_image_proc',
                     plugin='depth_image_proc::PointCloudXyzNode',
                     name='point_cloud_xyz',
                     remappings=[('image_rect', f'{name}/stereo/image_raw'),
                                 ('camera_info', f'{name}/stereo/camera_info'),
-                                ('points', f'{name}/depth/points')]
+                                ('points', f'{name}/depth/points')],
                 ),
-            ]
+            ],
         ),
         Node(
-            condition=IfCondition(LaunchConfiguration('ar_tag')),
+            condition=IfCondition(ar),
             package='aruco_opencv',
             executable='aruco_tracker_autostart',
-            arguments=['--ros-args', '--params-file', PathJoinSubstitution([auto_bringup_dir, 'params', 'aruco_tracker.yaml'])],
+            arguments=['--ros-args', '--params-file', ar_params],
         ),
         Node(
             package='imu_transformer',
@@ -101,16 +91,53 @@ def launch_setup(context, *args, **kwargs):
 
 def generate_launch_description():
     auto_bringup_dir = FindPackageShare('auto_bringup')
+
     declared_arguments = [
-        DeclareLaunchArgument('name', default_value='oak'),
-        DeclareLaunchArgument('parent_frame', default_value='camera_link'),
-        DeclareLaunchArgument('camera_model', default_value='OAK-D-LR'),
-        DeclareLaunchArgument('params_file', default_value=PathJoinSubstitution([auto_bringup_dir, 'params', 'depthai_oakd_rgbd.yaml'])),
-        DeclareLaunchArgument('ar_tag', default_value='True'),
-        DeclareLaunchArgument('use_filter', default_value='False'), # To Be Exoerimented so False for now
-        DeclareLaunchArgument('gazebo', default_value='False'),
-        DeclareLaunchArgument('pointclouds', default_value='True'),
-        DeclareLaunchArgument('rectify_image', default_value='True')
+        DeclareLaunchArgument(
+            name='ar',
+            default_value='True',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='ar_params',
+            default_value=PathJoinSubstitution([auto_bringup_dir, 'params', 'aruco_tracker.yaml']),
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='camera_model',
+            default_value='OAK-D-LR',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='gazebo',
+            default_value='False',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='name',
+            default_value='oak',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='oak_params',
+            default_value=PathJoinSubstitution([auto_bringup_dir, 'params', 'oak.yaml']),
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='parent_frame',
+            default_value='camera_link',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='pointclouds',
+            default_value='True',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='rectify_image',
+            default_value='True',
+            description='',
+        ),
     ]
 
     return LaunchDescription(
