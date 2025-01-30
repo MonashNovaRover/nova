f7eb1ea5a6444b58cb6b4438011972487b446476
Matthewheekh <66135287+Matthewheekh@users.noreply.github.com>
2025-01-30 19:01:22 +1100

    Add battery component in the top bar (#83)

--- src/ros/nova-gui/nova-gui/src/redux/RootReducer.ts
+++ src/ros/nova-gui/nova-gui/src/redux/RootReducer.ts
@@ -7,7 +7,8 @@ import {
   IRosCmdInterfacesCmDsFeedback,
   IRosNovaInterfacesNirProbeDataConst,
   IRosSensorMsgsRange,
-  IRosNovaInterfacesHydraprobeData
+  IRosNovaInterfacesHydraprobeData,
+  IRosStdMsgsHeader
 } from "../ros/rosTypes";
 import { cartographerSlice } from "./slices/CartographerSlice";
 import { initialNavSatMessage } from "./models/CartographerState";
@@ -130,7 +131,7 @@ export const reduxStores = {
   tofStore: createBifrostStore({ topic: RosTopic.TOF }, {
     header: {
       frame_id: "",
-    } as IRosSensorMsgsRange["header"],
+    } as IRosStdMsgsHeader,
     min_range: 0.0,
     max_range: 150.0,
     range: 0.0,
@@ -231,6 +232,30 @@ export const reduxStores = {
   ),
 
   cartographerState: cartographerSlice.reducer,
+
+  batteryStore: createBifrostStore(
+    { topic: RosTopic.BATTERY_STATE },
+    {
+      header: {
+        frame_id: "",
+      } as IRosStdMsgsHeader,
+      voltage: 0,
+      temperature: 0,
+      current: 0,
+      charge: 0,
+      capacity: 0,
+      design_capacity: 0,
+      percentage: 0,
+      power_supply_status: 0,
+      power_supply_health: 0,
+      power_supply_technology: 0,
+      present: false,
+      cell_voltage: [],
+      cell_temperature: [],
+      location: "",
+      serial_number: ""
+    }
+  ),
 
   // Generic stores
   currentSite: createGenericStore("currentSite", Site.SITE_1),
