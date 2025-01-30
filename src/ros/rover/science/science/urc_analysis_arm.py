db8897e07e99c69df2a0f76962524381f2482ca4
Felicity Matthews <felicitymatthews30@gmail.com>
2025-01-30 16:07:31 +1100

    updating can ids

--- src/ros/rover/science/science/urc_analysis_arm.py
+++ src/ros/rover/science/science/urc_analysis_arm.py
@@ -36,7 +36,7 @@ class AnalysisArm(JoystickControllerNode):
 
     # SENDING CARD IDS
     # Add any CONTROL FRAME / CARD IDS here
-    CMD_ID = 0x032
+    CMD_ID = 0x012
 
     # RECEIVING CARD IDS
     # Add any SENSOR FRAME / CARD IDS here
