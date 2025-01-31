7c089fa960a6dcf0d8da67f51f46d6bdcccb7e2a
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-01-31 19:57:35 +1100

    Update nova-gui.nix

--- nixfiles/modules/nixos/common/workspace/nova-gui.nix
+++ nixfiles/modules/nixos/common/workspace/nova-gui.nix
@@ -1,13 +1,63 @@
 { config, pkgs, lib, ... }:
 
+let
+  cfg = config.nova.workspace.services;
+in
 {
+  options.nova.workspace.services = {
+    enable = lib.mkEnableOption "workspace services";
+    gui = {
+      enable = lib.mkEnableOption "GUI services";
+      frontendPackage = lib.mkPackageOption pkgs "frontend resource" {
+        default = [ "nova" "nova-gui-frontend" ];
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    # General configuration
+    {
+      users = {
+        groups.nova-workspace = { };
+        users.nova-workspace = {
+          isSystemUser = true;
+          group = config.users.groups.nova-workspace.name;
+        };
+      };
+
+      lib.nova.mkWorkspaceService = { path ? [], script, ... }@args: args // {
+        serviceConfig.User = config.users.users.nova-workspace.name;
+        serviceConfig.Group = config.users.users.nova-workspace.group;
+        serviceConfig.StateDirectory = "nova-workspace";
+        serviceConfig.StateDirectoryMode = 0750;
+        serviceConfig.LogsDirectory = "nova-workspace";
+        path = [
+          (config.nova.workspace.package.override {
+            interactive = false;
+            graphical = false;
+            extraPackages = {
+              inherit (pkgs.nova.rosPackages.${config.nova.workspace.package.rosDistro})
+                ros2cli
+                ros2run
+                ros2launch;
+            };
+          })
+        ] ++ path;
+        script = ''
+          export ROS_HOME="$STATE_DIRECTORY"
+          export ROS_LOG_DIR="$LOGS_DIRECTORY"
+          ${script}
+        '';
+      };
+    }
+  ]);
+
   systemd.services.nova-gui = {
     description = "Launches nova-gui frontend and backend";
     after = [ "network.target" "roscore.service" ];
 
     serviceConfig = {
       User = "nova-workspace";
-      Group = "nova-workspace";  # Ensuring the user is in the correct group
       WorkingDirectory = "/home/nova/nova/src/ros/nova-gui/nova-gui";
       Environment = "HOME=/home/nova";
 
@@ -28,24 +78,4 @@
 
     wantedBy = [ "multi-user.target" ];
   };
-
-  # Ensure correct user and group with appropriate permissions
-  users = {
-    groups.nova-workspace = {};
-
-    users.nova-workspace = {
-      isSystemUser = true;
-      group = "nova-workspace";
-      home = "/home/nova";
-      shell = pkgs.zsh;  # Default shell (can be adjusted as needed)
-    };
-  };
-
-  # Ensure the correct directory permissions for the nova-workspace user
-  environment.etc."passwd".text = ''
-    nova-workspace:x:1001:1001:nova-workspace:/home/nova:/run/current-system/sw/bin/bash
-  '';
-  systemd.tmpfiles.rules = [
-    "d /home/nova/nova/src/ros/nova-gui/nova-gui 0770 nova-workspace nova-workspace -"
-  ];
 }
