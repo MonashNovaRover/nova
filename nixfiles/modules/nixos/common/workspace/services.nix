f05bf6e6d4126015fdbf1e19d82a3496e003c124
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-01-31 16:49:00 +1100

    Updated services.nix

--- nixfiles/modules/nixos/common/workspace/services.nix
+++ nixfiles/modules/nixos/common/workspace/services.nix
@@ -8,59 +8,80 @@ in
     enable = lib.mkEnableOption "workspace services" // { default = config.nova.workspace.enable; };
     gui = {
       enable = lib.mkEnableOption "GUI services" // { default = true; };
+      frontendPackage = lib.mkPackageOption pkgs "frontend resource" {
+        default = [ "nova" "nova-gui-frontend" ];
+      };
     };
   };
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
+    # General configuration
     {
-      # Ensure the group exists before defining the user
-      users.groups.nova-workspace = { };
-
-      users.users.nova-workspace = {
-        isSystemUser = true;
-        group = "nova-workspace";
-        home = "/var/lib/nova-workspace";
-        createHome = true;
+      users = {
+        groups.nova-workspace = { };
+        users.nova-workspace = {
+          isSystemUser = true;
+          group = config.users.groups.nova-workspace.name;
+        };
       };
 
+      lib.nova.mkWorkspaceService = { path ? [ ], script, ... }@args: args // {
+        serviceConfig.User = config.users.users.nova-workspace.name;
+        serviceConfig.Group = config.users.users.nova-workspace.group;
+        serviceConfig.StateDirectory = "nova-workspace";
+        serviceConfig.StateDirectoryMode = 0750;
+        serviceConfig.LogsDirectory = "nova-workspace";
+        path = [
+          (config.nova.workspace.package.override {
+            interactive = false;
+            graphical = false;
+            extraPackages = {
+              inherit (pkgs.nova.rosPackages.${config.nova.workspace.package.rosDistro})
+                ros2cli
+                ros2run
+                ros2launch;
+            };
+          })
+        ] ++ path;
+        script = ''
+          export ROS_HOME="$STATE_DIRECTORY"
+          export ROS_LOG_DIR="$LOGS_DIRECTORY"
+          ${script}
+        '';
+      };
+    }
+
+        # GUI services
+    (lib.mkIf cfg.gui.enable {
       systemd.services = {
-        nova-gui = {
-          description = "Nova GUI Frontend";
-          after = [ "network.target" ];
-          wantedBy = [ "multi-user.target" ];
-          serviceConfig = {
-            Type = "simple";
-            User = "nova-workspace";
-            Group = "nova-workspace";
-            WorkingDirectory = "/var/lib/nova-workspace";
-            ExecStart = "${pkgs.bash}/bin/bash -c '
-              export ROS_TS_DEFINITIONS=/var/lib/nova-workspace/rosTypes.ts
-              ${pkgs.nova-shell}/bin/nova-shell -A pkgs.ros.nova-gui
-              cd nova/src/ros/nova-gui/nova-gui
-              yarn install
-              ln -sf \"$ROS_TS_DEFINITIONS\" src/ros/rosTypes.ts
-              yarn dev
-            '";
-            Restart = "always";
-          };
+        gui-backend = config.lib.nova.mkWorkspaceService {
+          description = "Nova ROSBridge WebSocket Server";
+          script = "ros2 launch rosbridge_server rosbridge_websocket_launch.xml";
         };
 
-        nova-rosbridge = {
-          description = "Nova ROSBridge WebSocket Server";
-          after = [ "network.target" ];
+        gui-frontend = {
           wantedBy = [ "multi-user.target" ];
-          serviceConfig = {
-            Type = "simple";
-            User = "nova-workspace";
-            Group = "nova-workspace";
-            WorkingDirectory = "/var/lib/nova-workspace";
-            ExecStart = "${pkgs.bash}/bin/bash -c 'ros2 launch rosbridge_server rosbridge_websocket_launch.xml'";
-            Restart = "always";
-          };
+          requires = [ "gui-backend.service" ];
+          after = [ "gui-backend.service" ];
+          path = with pkgs; [
+            (nova.nova-gui-frontend-server.override {
+              nova-gui-frontend = cfg.gui.frontendPackage;
+            })
+            nodejs
+            yarn
+          ];
+          script = ''
+            cd nova/src/ros/nova-gui/nova-gui
+            yarn install
+            ln -sf "$ROS_TS_DEFINITIONS" src/ros/rosTypes.ts
+            yarn dev
+          '';
+          serviceConfig.DynamicUser = true;
+          serviceConfig.AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
         };
       };
 
       networking.firewall.allowedTCPPorts = [ 80 ];
-    }
+    })
   ]);
 }
