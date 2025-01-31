85d48b8fdde8ad72c6fdd8269d11bee592189b62
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-01-31 19:29:33 +1100

    Update nova-gui.nix

--- nixfiles/modules/nixos/common/workspace/nova-gui.nix
+++ nixfiles/modules/nixos/common/workspace/nova-gui.nix
@@ -10,18 +10,16 @@
       WorkingDirectory = "/home/nova/nova/src/ros/nova-gui/nova-gui";
       Environment = "HOME=/home/nova";
 
-      # Use the system-wide nix-shell to run yarn install
-      ExecStartPre = ''
-        nix-shell /home/nova/nova/nixfiles -A pkgs.ros.nova-gui --run "${pkgs.nodejs}/bin/yarn install"
-      '';
+      # Use the full path to nix-shell
+      ExecStartPre = "/run/current-system/sw/bin/nix-shell /home/nova/nova/nixfiles -A pkgs.ros.nova-gui --run yarn install";
 
       # Launch the backend (rosbridge)
       ExecStartPost = ''
         ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
       '';
 
-      # Start the frontend (yarn dev) using system-wide yarn
-      ExecStart = "${pkgs.nodejs}/bin/yarn dev";
+      # Start the frontend (yarn dev)
+      ExecStart = "${pkgs.yarn}/bin/yarn dev";
 
       Restart = "always";
       RestartSec = "3s";  # Restart in case of failure
