1b2cd58ce91ade488c6fef1336ca3699fd9c4d22
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-01-31 19:17:03 +1100

    Updated nova-gui.nix

--- nixfiles/modules/nixos/common/workspace/nova-gui.nix
+++ nixfiles/modules/nixos/common/workspace/nova-gui.nix
@@ -1,8 +1,13 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  # Extract the correct directory paths based on the configuration options
+  cfg = config.nova.macros;
+  nixfileDir = lib.strings.concatStringsSep "/" [ cfg.sourceDir ".." "nixfiles" ];
+in
 {
   systemd.services.nova-gui = {
-    description = "nova-gui service";
+    description = "Launches nova-gui frontend and backend";
     after = [ "network.target" "roscore.service" ];
 
     serviceConfig = {
@@ -10,15 +15,18 @@
       WorkingDirectory = "/home/nova/nova/src/ros/nova-gui/nova-gui";
       Environment = "HOME=/home/nova";
 
+      # Ensure the correct environment is set up with nix-shell
       ExecStartPre = ''
-        nova-shell -A pkgs.ros.nova-gui
-        ${pkgs.nodejs}/bin/yarn install
+        # Use nix-shell to set up the environment and run yarn install
+        nix-shell ${nixfileDir} -A pkgs.ros.nova-gui --run "${pkgs.nodejs}/bin/yarn install"
       '';
 
+      # Launch the backend (rosbridge)
       ExecStartPost = ''
         ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
       '';
 
+      # Start the frontend (yarn dev)
       ExecStart = "${pkgs.nodejs}/bin/yarn dev";
 
       Restart = "always";
