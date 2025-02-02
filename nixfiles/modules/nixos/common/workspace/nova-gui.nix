958035c69c3cf898c7d2b2fa757c4dc57a7c4152
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-02-02 19:42:55 +1100

    Fixed permissions issue with nova-workspace

--- nixfiles/modules/nixos/common/workspace/nova-gui.nix
+++ nixfiles/modules/nixos/common/workspace/nova-gui.nix
@@ -1,81 +1,38 @@
 { config, pkgs, lib, ... }:
 
 let
-  cfg = config.nova.workspace.services;
+  cfg = config.nova.workspace.services.gui;
 in
 {
-  options.nova.workspace.services = {
-    enable = lib.mkEnableOption "workspace services";
-    gui = {
-      enable = lib.mkEnableOption "GUI services";
-      frontendPackage = lib.mkPackageOption pkgs "frontend resource" {
-        default = [ "nova" "nova-gui-frontend" ];
-      };
-    };
-  };
+  config = lib.mkIf cfg.enable {
+    systemd.services.nova-gui = {
+      description = "nova-gui";
+      after = [ "network.target" "roscore.service" ];
 
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    # General configuration
-    {
-      users = {
-        groups.nova-workspace = { };
-        users.nova-workspace = {
-          isSystemUser = true;
-          group = config.users.groups.nova-workspace.name;
-        };
-      };
+      serviceConfig = {
+        User = config.users.users.nova-workspace.name;
+        Group = config.users.users.nova-workspace.group;
 
-      lib.nova.mkWorkspaceService = { path ? [], script, ... }@args: args // {
-        serviceConfig.User = config.users.users.nova-workspace.name;
-        serviceConfig.Group = config.users.users.nova-workspace.group;
-        serviceConfig.StateDirectory = "nova-workspace";
-        serviceConfig.StateDirectoryMode = 0750;
-        serviceConfig.LogsDirectory = "nova-workspace";
-        path = [
-          (config.nova.workspace.package.override {
-            interactive = false;
-            graphical = false;
-            extraPackages = {
-              inherit (pkgs.nova.rosPackages.${config.nova.workspace.package.rosDistro})
-                ros2cli
-                ros2run
-                ros2launch;
-            };
-          })
-        ] ++ path;
-        script = ''
-          export ROS_HOME="$STATE_DIRECTORY"
-          export ROS_LOG_DIR="$LOGS_DIRECTORY"
-          ${script}
+        StateDirectory = "nova-workspace";
+        StateDirectoryMode = 0750;
+        LogsDirectory = "nova-workspace";
+
+        Environment = "HOME=/var/lib/nova-workspace";
+        WorkingDirectory = "/var/lib/nova-workspace";
+
+        ExecStartPre = "/run/current-system/sw/bin/nix-shell /var/lib/nova-workspace/nixfiles -A pkgs.ros.nova-gui --run yarn install";
+
+        ExecStart = "${pkgs.yarn}/bin/yarn dev";
+
+        ExecStartPost = ''
+          ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
         '';
+
+        Restart = "always";
+        RestartSec = "30s";
       };
-    }
-  ]);
 
-  systemd.services.nova-gui = {
-    description = "Launches nova-gui frontend and backend";
-    after = [ "network.target" "roscore.service" ];
-
-    serviceConfig = {
-      User = "nova-workspace";
-      WorkingDirectory = "/home/nova/nova/src/ros/nova-gui/nova-gui";
-      Environment = "HOME=/home/nova";
-
-      # Use the full path to nix-shell
-      ExecStartPre = "/run/current-system/sw/bin/nix-shell /home/nova/nova/nixfiles -A pkgs.ros.nova-gui --run yarn install";
-
-      # Launch the backend (rosbridge)
-      ExecStartPost = ''
-        ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
-      '';
-
-      # Start the frontend (yarn dev)
-      ExecStart = "${pkgs.yarn}/bin/yarn dev";
-
-      Restart = "always";
-      RestartSec = "3s";  # Restart in case of failure
+      wantedBy = [ "multi-user.target" ];
     };
-
-    wantedBy = [ "multi-user.target" ];
   };
 }
