92d0ba210565bf2c80ed743d5694dcce58936106
Victor Bartlinski <118893280+vic-bart@users.noreply.github.com>
2025-02-04 09:39:40 +1100

    Reformatting nova_bringup (#151)

--- src/ros/rover/nova_bringup/launch/pl_base.launch.py
+++ src/ros/rover/nova_bringup/launch/pl_base.launch.py
@@ -1,4 +1,4 @@
-"""
+'''
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Monash Nova Rover Team
 
@@ -9,41 +9,35 @@ NODES:
   - control/inputs/inputs_publisher     [inputs]
   - electronics/electronics/radio_monitor.py
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-PACKAGE: 	core
-CREATION:	15/12/2021
+CREATION:   15/12/2021
+EDITED:     04/02/2025
+EDITED BY: Taaj Street, Victor Bartlinski
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
-
-# Include the required launch parameters
+'''
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
-
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
+def launch_setup(context, *args, **kwargs):
+    nova_bringup_dir = FindPackageShare('nova_bringup')
 
-# Generate the launch file with all inputs
-def generate_launch_description():
-
-    return LaunchDescription([
-       IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare('nova_bringup'),
-                'launch',
-                'base.launch.py'
-            ]
-            )
-        )),
+    return [
+        IncludeLaunchDescription(
+            launch_description_source=PythonLaunchDescriptionSource(PathJoinSubstitution([nova_bringup_dir, 'launch', 'base.launch.py'])),
+        ),
         Node(
             package='rviz2',
             namespace='',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', [PathJoinSubstitution([FindPackageShare('nova_bringup'), 'rviz', 'arm.rviz'])]]
-        ) 
-        
-    ])
+            arguments=['-d', [PathJoinSubstitution([nova_bringup_dir, 'rviz', 'arm.rviz'])]],
+        ),
+    ]
+
+def generate_launch_description():
+    return LaunchDescription(
+        [OpaqueFunction(function=launch_setup)]
+    )
