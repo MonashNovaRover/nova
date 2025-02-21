d78321a4c77208de928cd10e8e2620377dfab490
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-02-21 17:55:14 +1100

    Add generic store reset setting (#190)

--- src/ros/nova-gui/nova-gui/src/components/settings/SettingsModal.tsx
+++ src/ros/nova-gui/nova-gui/src/components/settings/SettingsModal.tsx
@@ -1,57 +1,22 @@
 import {
-  Button,
-  Input,
   Modal,
   ModalBody,
   ModalContent,
-  ModalFooter,
-  ModalHeader,
+  ModalHeader, Tab, Tabs,
 } from "@nextui-org/react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/RootState";
 import { useUIActions } from "../../redux/actions/useUIActions";
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
-import { isIPAddress } from "../../utils/regexUtils";
+import StoreSettings from "./StoreSettings.tsx";
+import {IPSettings} from "./IPSettings.tsx";
 
+/**
+ * Settings Model containing GUI settings
+ * @constructor
+ */
 export function SettingsModal() {
   const uiActions = useUIActions();
   const uiState = useSelector((state: RootState) => state.uiState);
-
-  const [baseStationIP, setBaseStationIP] = useState<string>(
-    uiState.baseStationIP
-  );
-
-  const [roverIP, setRoverIP] = useState<string>(uiState.roverIP);
-
-  useEffect(() => {
-    if (uiState.baseStationIP != baseStationIP)
-      setBaseStationIP(uiState.baseStationIP);
-
-    if (uiState.roverIP != roverIP)
-      setRoverIP(uiState.roverIP);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [uiState.baseStationIP, uiState.roverIP]);
-
-  const submit = () => {
-    if (!isIPAddress(baseStationIP) || !isIPAddress(roverIP)) {
-      toast.error("Invalid IP address");
-      return;
-    }
-
-    uiActions.updateIP(baseStationIP, roverIP);
-
-    closeModal();
-  };
-
-  const onBaseIPChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setBaseStationIP(event.target.value);
-  };
-
-  const onRoverIPChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRoverIP(event.target.value);
-  };
 
   const closeModal = () => uiActions.setSettingsModal(false);
   return (
@@ -63,28 +28,23 @@ export function SettingsModal() {
       <ModalContent>
         <ModalHeader className="flex flex-col gap-1">Settings</ModalHeader>
         <ModalBody>
-          <Input
-            fullWidth
-            label="Base IP"
-            value={baseStationIP}
-            type="text"
-            onChange={onBaseIPChange}
-            isInvalid={!isIPAddress(baseStationIP)}
-          />
-          <Input
-            fullWidth
-            label="Rover IP"
-            value={roverIP}
-            type="text"
-            onChange={onRoverIPChange}
-            isInvalid={!isIPAddress(roverIP)}
-          />
+
+          <Tabs
+            variant="underlined"
+            classNames={{
+              tabList: "gap-6 w-full relative rounded-none p-0 border-b border-divider",
+            }}
+          >
+            <Tab title="IP">
+              <IPSettings/>
+            </Tab>
+
+            <Tab title="Store">
+              <StoreSettings/>
+            </Tab>
+          </Tabs>
+
         </ModalBody>
-        <ModalFooter>
-          <Button size="sm" color="success" variant="flat" onPress={submit}>
-            Submit
-          </Button>
-        </ModalFooter>
       </ModalContent>
     </Modal>
   );
---
+++ src/ros/nova-gui/nova-gui/src/components/settings/StoreSettings.tsx
@@ -0,0 +1,59 @@
+import {Button, Input} from "@nextui-org/react";
+import {useResetGenericStore} from "../../hooks/useResetGenericStore.ts";
+import {useState} from "react";
+
+/**
+ * Store Settings component containing UI for resetting generic stores.
+ * @constructor
+ */
+function StoreSettings() {
+  const resetStore = useResetGenericStore()
+  const [storeName, setStoreName] = useState("")
+  const [result, setResult] = useState<boolean | undefined>(undefined)
+
+  const onSubmit = () => {
+    setResult(resetStore(storeName))
+  }
+
+  const onInputChange = (s: string) => {
+    setStoreName(s)
+    setResult(undefined)
+  }
+
+  const success = (
+    <div className="text-green-500">
+      Success!
+    </div>
+  )
+
+  const warning = (
+    <div className="text-danger">
+      Caution: you will lose any associated data saved within local storage.
+    </div>
+  )
+
+  return (
+    <div className="flex flex-col gap-3 mt-4 mb-5">
+      <Input
+        fullWidth
+        label="Reset Generic Store"
+        labelPlacement="outside"
+        value={storeName}
+        onValueChange={onInputChange}
+        errorMessage={result === false ? "Invalid generic store name" : ""}
+        description={result === true ? success : warning}
+        isInvalid={result === false}
+        placeholder="Generic Store Name"
+        type="text"
+      />
+      <div className="flex flex-row">
+        <div className="grow"></div>
+        <Button size="sm" color="success" variant="flat" onPress={onSubmit}>
+          Reset
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default StoreSettings;
