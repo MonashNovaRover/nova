88ae8332c79eb2dd23e5e37d7a23c42a60cf56dc
Bailey Chessum <bailey.chessum1@gmail.com>
2025-02-25 17:20:45 +1100

    cleaned up some code

--- src/ros/rover/controllers/nova_ik_controller/src/nova_ik_controller.cpp
+++ src/ros/rover/controllers/nova_ik_controller/src/nova_ik_controller.cpp
@@ -15,9 +15,11 @@
 namespace
 {
   constexpr auto DEFAULT_INPUT_TOPIC_END_EFFECTOR_TWIST = "/arm_ik_twist_stamped"; // TODO: changeme
-  const auto ENDEFFECTOR_BASIS = tf2::Matrix3x3(tf2::Quaternion(-0.5, 0.5, -0.5, -0.5));
+  /// The inverse of the global orientation the kinematics origin frame wants to rotate to for a roll,pitch,yaw of 0,0,0
+  const auto ENDEFFECTOR_BASIS_INVERSE = tf2::Matrix3x3(tf2::Quaternion(-0.5, 0.5, -0.5, -0.5));
   constexpr auto ENDEFFECTOR_KINEMATICS_FRAME = "endeffector_kinematics";
   constexpr auto KINEMATICS_ORIGIN_FRAME = "arm_kinematics_origin";
+  constexpr auto TWISTMAPPER_TARGET_FRAME = "arm_twistmapper_target";
 } // namespace
 
 using std::placeholders::_1;
@@ -63,8 +65,6 @@ namespace nova_ik_controller
   {
     std::vector<std::string> conf_names;
 
-    // TODO: Add state interfaces for setting the initial twistmapper position
-
     return {interface_configuration_type::INDIVIDUAL, conf_names};
   }
 
@@ -78,7 +78,6 @@ namespace nova_ik_controller
     }
 
     const auto& twist = twist_stamped->twist;
-
 
     tf2::Vector3 tf2_twist_angular;
     tf2::fromMsg(twist.angular, tf2_twist_angular);
@@ -102,16 +101,8 @@ namespace nova_ik_controller
     // TODO: Test this actually works as expected.
     // TODO: Add a tf buffer and make use of the header.frame_id from the twist_stamped to allow IK to be done relative to anything. Then Teleop would just need to use the end effector frame by default
     // TODO: If using tf like this, also have the twistmapper broadcast the target frame, so it can reference itself.
-    const auto displacement = tf2::Transform(rotation_matrix, linear);
-
-    // _twistmapper_pose.mult(tf2::Transform(_twistmapper_pose), displacement);
     twistmapper_pose_.setOrigin(linear + twistmapper_pose_.getOrigin());
-    //twistmapper_pose_.setBasis(twistmapper_pose_.getBasis() * rotation_matrix);
     twistmapper_pose_.setBasis(rotation_matrix);
-
-    auto origin = twistmapper_pose_.getOrigin();
-    auto quat = twistmapper_pose_.getRotation();
-    RCLCPP_INFO_ONCE(get_node()->get_logger(), "Pose: [%f, %f, %f], [%f, %f, %f, %f]", origin.x(), origin.y(), origin.z(), quat.x(), quat.y(), quat.z(), quat.w());
 
     // Publish twistmapper pose to tf2
     geometry_msgs::msg::TransformStamped transform_stamped;
@@ -119,7 +110,7 @@ namespace nova_ik_controller
     transform_stamped.header.stamp = time;
 
     // TODO: Parameterize
-    transform_stamped.child_frame_id = "arm_twistmapper_target";
+    transform_stamped.child_frame_id = TWISTMAPPER_TARGET_FRAME;
     transform_stamped.header.frame_id = KINEMATICS_ORIGIN_FRAME;
 
     RCLCPP_INFO_ONCE(get_node()->get_logger(), "Broadcasting twistmapper pose as '%s', child of '%s'.",
@@ -191,10 +182,9 @@ namespace nova_ik_controller
     twist_stamped_sub = get_node()->create_subscription<geometry_msgs::msg::TwistStamped>(
       DEFAULT_INPUT_TOPIC_END_EFFECTOR_TWIST,
       rclcpp::SystemDefaultsQoS(),
-      //std::bind(&NovaIKController::teleop_callback, this, _1));
       [this, logger](const std::shared_ptr<geometry_msgs::msg::TwistStamped> msg) -> void
       {
-        RCLCPP_INFO_ONCE(logger, "Twist callback!");
+        RCLCPP_INFO_ONCE(logger, "Twist message received.");
 
         if (!subscriber_is_active_)
         {
@@ -217,15 +207,6 @@ namespace nova_ik_controller
 
     RCLCPP_INFO(logger, "Created twist stamped subscription");
 
-    // Initialize the twist stamped
-//    std::shared_ptr<geometry_msgs::msg::TwistStamped> twist_stamped;
-//    received_twist_stamped_ptr.get(twist_stamped);
-//    auto* init_twist_stamped = new geometry_msgs::msg::TwistStamped();
-//    init_twist_stamped->twist = geometry_msgs::msg::Twist();
-//    init_twist_stamped->twist.angular = geometry_msgs::msg::Vector3();
-//    init_twist_stamped->twist.angular = geometry_msgs::msg::Vector3();
-//    twist_stamped.reset(init_twist_stamped);
-
     previous_update_timestamp_ = get_node()->get_clock()->now();
     return controller_interface::CallbackReturn::SUCCESS;
   }
@@ -247,25 +228,16 @@ namespace nova_ik_controller
     subscriber_is_active_ = true;
 
     try {
-      auto tf_transform = tf_buffer_->lookupTransform(KINEMATICS_ORIGIN_FRAME, ENDEFFECTOR_KINEMATICS_FRAME, tf2::TimePointZero);
+      auto tf_transform = tf_buffer_->lookupTransform(
+        KINEMATICS_ORIGIN_FRAME, ENDEFFECTOR_KINEMATICS_FRAME, tf2::TimePointZero);
       tf2::fromMsg(tf_transform.transform, twistmapper_pose_);
 
       double roll, pitch, yaw;
       twistmapper_pose_.getBasis().getRPY(roll, pitch, yaw);
-      RCLCPP_INFO(get_node()->get_logger(), "Initial pose RPY -- R: %f, P: %f, Y: %f", roll, pitch, yaw);
-
-      // The orientation of the endeffector at the zero position is not the identity matrix. So, rotate the pose so these orientations line up
-      // twistmapper_pose_.setBasis(ENDEFFECTOR_BASIS.inverse() * twistmapper_pose_.getBasis());
-      twistmapper_pose_.getBasis().getRPY(roll, pitch, yaw);
-
-      RCLCPP_INFO(get_node()->get_logger(), "Initial pose RPY after ro -- R: %f, P: %f, Y: %f", roll, pitch, yaw);
 
       twistmapper_pose_rpy_.setX(roll);
       twistmapper_pose_rpy_.setY(pitch);
       twistmapper_pose_rpy_.setZ(yaw);
-
-      RCLCPP_INFO(get_node()->get_logger(), "R: %f, P: %f, Y: %f *", roll, pitch, yaw);
-
     } catch (const tf2::TransformException & ex) {
       RCLCPP_INFO(
         get_node()->get_logger(), "Could not transform %s to %s: %s",
@@ -404,37 +376,18 @@ namespace nova_ik_controller
     auto x = origin.getX();
     auto y = origin.getY();
     auto z = origin.getZ();
-    double roll, pitch, yaw;
 
-
-//  pose.getBasis().getRPY(roll, pitch, yaw);
-
-    roll = twistmapper_pose_rpy_.x();
-    pitch = twistmapper_pose_rpy_.y();
-    yaw = twistmapper_pose_rpy_.z();
-
-    tf2::Matrix3x3 rotated_basis = pose.getBasis() * ENDEFFECTOR_BASIS.inverse();
-
-    constexpr double rad_to_deg = 57.2957795131;
-    roll *= rad_to_deg;
-    pitch *= rad_to_deg;
-    yaw *= rad_to_deg;
-
-    RCLCPP_INFO(get_node()->get_logger(), "R: %f, P: %f, Y: %f", roll, pitch, yaw);
-    // pitch += 90;
+    tf2::Matrix3x3 rotated_basis = pose.getBasis() * ENDEFFECTOR_BASIS_INVERSE;
 
     double l1r = lengths[0];
     double l2r = lengths[1];
     double l3 = lengths[2];
 
-//    Eigen::Matrix3d rz_alp { {cosd(yaw), -sind(yaw), 0}, {sind(yaw), cosd(yaw), 0}, {0, 0, 1} }; // Yaw
-//    Eigen::Matrix3d ry_beta { {cosd(pitch), 0, sind(pitch)}, {0, 1, 0}, { -sind(pitch), 0, cosd(pitch)} }; // Pitch
-//    Eigen::Matrix3d rx_gam { {1, 0, 0}, {0, cosd(roll), -sind(roll)}, {0, sind(roll), cosd(roll)} }; // Roll
     Eigen::Matrix3d rxyz {
       {rotated_basis[0][0], rotated_basis[0][1], rotated_basis[0][2]},
       {rotated_basis[1][0], rotated_basis[1][1], rotated_basis[1][2]},
       {rotated_basis[2][0], rotated_basis[2][1], rotated_basis[2][2]}
-    };  /// = rz_alp * ry_beta * rx_gam; // rxyz orientation matrix
+    };  // rxyz orientation matrix
 
     Eigen::Matrix4d t07r = Eigen::Matrix4d::Zero();
     t07r.topLeftCorner<3,3>() = rxyz;
@@ -482,35 +435,7 @@ namespace nova_ik_controller
     double j6 = atan2(-r37r(2, 1) / cos(j5), r37r(2, 0) / cos(j5));
 
     std::array<double, 6> new_joints = { -j1, -j2bo, -j3bo, -j4, -j5, -j6 };
-
-    RCLCPP_INFO(get_node()->get_logger(), "{ %f, %f, %f, %f, %f, %f }", -j1, -j2bo, -j3bo, -j4, -j5, -j6);
-
-    // Removed this conversion, as im 99% sure ros2 control uses radians rather than degrees: - Bailey
-    //   converts all elements in new_joints from radians to degrees, then puts them into joints to be returned
-    //for (int i = 0; i < 6; i++)
-    //  new_joints[i] = new_joints[i] / (M_PI / 180);
-
     return new_joints;
-  }
-
-  void NovaIKController::teleop_callback(const std::shared_ptr<geometry_msgs::msg::TwistStamped> msg)
-  {
-    RCLCPP_INFO_ONCE(get_node()->get_logger(), "Teleop callback!");
-
-    if (!subscriber_is_active_)
-    {
-      RCLCPP_WARN_ONCE(get_node()->get_logger(), "Can't accept new commands. subscriber is inactive");
-      return;
-    }
-    if ((msg->header.stamp.sec == 0) && (msg->header.stamp.nanosec == 0))
-    {
-      RCLCPP_WARN_ONCE(get_node()->get_logger(),
-          "Received message with zero timestamp, setting it to current "
-          "time, this message will only be shown once");
-      msg->header.stamp = get_node()->get_clock()->now();
-    }
-
-    received_twist_stamped_ptr.set(msg);
   }
 } // namespace nova_ik_controller
 
