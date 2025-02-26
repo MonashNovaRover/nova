584ad68e3c97ac2b76d1486beca05123e080d738
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-02-26 20:01:15 +1100

    cop-mode toggle (and off) functionality

--- nixfiles/scripts/cop-mode.sh
+++ nixfiles/scripts/cop-mode.sh
@@ -1,8 +1,17 @@
 #! /usr/bin/env bash
 
-while true; do
-    cansend can0 095#0100
-    sleep 0.3
-    cansend can0 095#0300
-    sleep 0.3
-done
+if [ "$1" == "on" ]; then
+    echo "Cop Mode Enabled"
+    while true; do
+        cansend can0 095#0100
+        sleep 0.3
+        cansend can0 095#0300
+        sleep 0.3
+    done
+elif [ "$1" == "off" ]; then
+    echo "Cop Mode Disabled"
+    cansend can0 091#0000
+else
+    echo "Usage: $0 {on|off}"
+    exit 1
+fi
