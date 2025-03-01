f5bed6a8b82a4ae6f56552494594caf94a7f0321
Kabilan VS <kabiniki4849@gmail.com>
2025-03-01 19:20:29 +1100

    Feat/kiln temp control #203
    # Conflicts:
    #	src/ros/nova-gui/nova-gui/src/components/CameraComponent/components/CameraVideo.tsx

--- src/ros/nova-gui/nova-gui/src/components/CameraComponent/components/CameraVideo.tsx
+++ src/ros/nova-gui/nova-gui/src/components/CameraComponent/components/CameraVideo.tsx
@@ -1,11 +1,9 @@
 import React, { LegacyRef } from "react";
-// import { CameraFilters} from "../CameraComponent";
-import {CameraSerials} from "../../../views/shared/CamerasPage/CameraPageConstants";
-import { CameraFilters,CameraComponent } from "../CameraComponent";
+import { CameraFilters } from "../CameraComponent";
 
 interface CameraVideoProps {
-    videoRef: LegacyRef<HTMLVideoElement> | undefined;
-    filters: CameraFilters;
+  videoRef: LegacyRef<HTMLVideoElement> | undefined;
+  filters: CameraFilters;
 }
 
 const CameraVideo: React.FC<CameraVideoProps> = ({ videoRef, filters }) => {
@@ -14,26 +12,20 @@ const CameraVideo: React.FC<CameraVideoProps> = ({ videoRef, filters }) => {
   const contrast = `contrast(${filters.contrast}%)`;
   const brightness = `brightness(${filters.brightness}%)`;
   const inversion = `invert(${filters.invertCamera ? 1 : 0})`;
-
-    console.log("CameraSerials.SCIENCE_GIMBAL:", CameraSerials.SCIENCE_GIMBAL);
-
-
-        console.log("Check working")
-        return (
-            <video
-                style={{
-                    transform: `${scaling} ${rotation}`,
-                    filter: `${contrast} ${brightness} ${inversion}`,
-                }}
-                controls={false}
-                autoPlay
-                loop
-                muted
-                playsInline
-                ref={videoRef}
-                className="z-0 w-full h-full object-cover"
-            />
-        );
-    };
-
+  return (
+    <video
+      style={{
+        transform: `${scaling} ${rotation}`,
+        filter: `${contrast} ${brightness} ${inversion}`,
+      }}
+      controls={false}
+      autoPlay
+      loop
+      muted
+      playsInline
+      ref={videoRef}
+      className="z-0 w-full h-full object-cover"
+    />
+  );
+};
 export default CameraVideo;
