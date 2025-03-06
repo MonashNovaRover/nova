275ee8b6153e1806036ffded7013cb86d54ab562
Tash Lee <tlee0160@student.monash.edu>
2025-03-06 16:22:22 +1100

    WASD MVP WIP & comments for click&drag

--- src/ros/nova-gui/nova-gui/src/components/shared/Overlay/GimbalCamOverlay.tsx
+++ src/ros/nova-gui/nova-gui/src/components/shared/Overlay/GimbalCamOverlay.tsx
@@ -2,7 +2,7 @@
 
  */
 
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {Simulate} from "react-dom/test-utils";
 import mouseMove = Simulate.mouseMove;
 import {useSelector} from "react-redux";
@@ -17,28 +17,65 @@ export const GimbalCamOverlay : React.FC<GimbalCamOverlayProps> = (props) => {
   const [step, setStep] = useState(1);
 
   // const gimbalService = useSelector(
-  //   (state: RootState) => state.MoveScimbalCam
+  //   (state: RootState) => state.scimbalCamResponse
   // );
 
-  const serviceBifrost = useBifrost({ service: RosService.MoveScimbalCam});
-  const incrementTilt = (step) => serviceBifrost.callServiceToRedux({angles[0]: MoveScimbalCam.angles[0]+step});
-  const incrementPan = (step) => serviceBifrost.callServiceToRedux({angles[1]: MoveScimbalCam.angles[1]}+step);
+  const serviceBifrost = useBifrost({ service: RosService.SCIMBAL_COMMAND});
+  const incrementTilt = (step: number) => serviceBifrost.callServiceToRedux({
+    angles: [step, 0]
+  });
+  const incrementPan = (step: number) => serviceBifrost.callServiceToRedux({
+    angles: [0, step]
+  });
 
-  const [position, setPosition] = useState({ x: 100, y: 100 });
+  //const [position, setPosition] = useState({ x: 100, y: 100 });
 
   //WASD Controls
+  useEffect(() => {
   window.addEventListener('keydown', (e) =>{
     switch(e.key){
       case 'a':
         incrementPan(-step)
+        break
       case 'd':
         incrementPan(step)
+        break
       case 'w':
         incrementTilt(step)
+        break
       case 's':
         incrementTilt(-step)
+        break
+      default:
+        continue
     }
   });
+  }, []);
+
+  // // CLICK & HOLD CONTROLS
+  // const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+  //   const getCenterCoordinates()=>{
+  //     //const rect = element.getBoundingClientRect();
+  //     const bounds = e.target.getBoundingClientRect() ?? {width: 1, height: 1};
+  //     e.target.
+  //     const centerX = bounds.left + (bounds.width / 2);
+  //     const centerY = bounds.top + (bounds.height / 2);
+  //   }
+  //   const getOriginX ={left: 50 %;}
+  //   const getOriginY ={top: 50%;}
+  //
+  //   const posX = position.x;
+  //   const posY = position.y;
+  //
+  //   // let coordsString = "Coordinates: (" + posX + "," + posY + ")";
+  //   // document.getElementById("demoCoords").innerHTML = coordsString;
+  //
+  //   const dispX = posX-getOriginX;
+  //   const dispY = posY-getOriginY;
+  //
+  //
+  // };
+
 
   // CLICK & DRAG Controls
   // const handleMouseDown = (e) => {
@@ -67,8 +104,9 @@ export const GimbalCamOverlay : React.FC<GimbalCamOverlayProps> = (props) => {
   // };
 
   return (
-    <div>
+    <div onMouseDown={handleMouseDown}>
+      <p id="demoCoords"></p>
 
-        </div>
+    </div>
   );
 };
