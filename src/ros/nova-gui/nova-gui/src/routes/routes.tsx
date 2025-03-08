72a535068eb84a959452ec3c42898eeec0a1c5dd
Tash Lee <tlee0160@student.monash.edu>
2025-03-08 14:25:19 +1100

    Working WASD (tested with vcan1)

--- src/ros/nova-gui/nova-gui/src/routes/routes.tsx
+++ src/ros/nova-gui/nova-gui/src/routes/routes.tsx
@@ -28,7 +28,6 @@ import URCRamanView from "../views/urc/URCRamanView.tsx";
 import { URCCartographerView } from "../views/urc/URCCartographerView.tsx";
 import TestStateView from "../views/test/TestStateView/TestStateView.tsx";
 import TestOverlayView from "../views/test/TestOverlayView/TestOverlayView.tsx";
-import {GimbalOverlayedCameraComponent} from "../components/CameraComponent/special/GimbalOverlayedCameraComponent.tsx";
 import TestScimbalCamView from "../views/test/TestOverlayView/TestScimbalCamView.tsx";
 
 export const arcRoutes: RouteObject[] = [
