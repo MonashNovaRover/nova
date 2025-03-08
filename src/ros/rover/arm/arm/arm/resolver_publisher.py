b3fd42f6b59009f393cbced6d4a8121cf8cd9737
Jono <53365342+jchin14@users.noreply.github.com>
2025-03-09 01:17:48 +1100

    Increase publishing speeds

--- src/ros/rover/arm/arm/arm/resolver_publisher.py
+++ src/ros/rover/arm/arm/arm/resolver_publisher.py
@@ -399,11 +399,11 @@ class ResolverPublisher(Node):
         Setup the node for the application. Create publishers, services, and initialize data members.
         """
         # Delay between each bus reading. In practice maxs out at 750+-50 us
-        self.receive_deadtime = 0.01
+        self.receive_deadtime = 0.001
         # Time to wait for a valid reading
-        self.receive_timeout = 0.5
+        self.receive_timeout = 0.1
         # Delay between each ROS publish. In practice maxs out at 15+-1 ms
-        resolver_pub_timer_period = 0.05
+        resolver_pub_timer_period = 0.015
 
         # Initialize the transceiver (keep it as originally structured)
         self.resolver_transceiver = ResolverTransceiver(
