33a9179fec2b57918d34cb05a1b54e54c7b1a85f
Felicity Matthews <felicitymatthews30@gmail.com>
2025-03-10 09:30:56 +1100

    update spinny part code

--- src/ros/rover/science/science/arc_spinny_part.py
+++ src/ros/rover/science/science/arc_spinny_part.py
@@ -20,10 +20,10 @@ EDITED:		02/03/2025
 """
 
 import rclpy
-from python_control.ControllerNode import ControllerNode
+from python_control.JoystickControllerNode import JoystickControllerNode
 from python_control.controls.Direction import Direction
 from input_interfaces.msg import InputJoystick
-from python_control.controls.ContinuousOneAxisPositionControl import ContinuousOneAxisPositionControl
+from python_control.controls.OneAxisPositionControl import OneAxisPositionControl
 from python_control.controllers.JonoPositionController import JonoPositionController
 
 
@@ -34,42 +34,38 @@ class SpinnyPartNode(JoystickControllerNode):
     # SENDING CARD IDS
     SERVO_ID = 0x0F1 # todo adjust accordingly
 
-    # CONTROL PARAMETERS
-    SERVO_MAX_POS = 360
-
     # SENDING COMMAND IDS
-    MOVE_SERVO_COMMAND = 0x10
+    SPIN_CLOCKWISE = 0x03 # todo adjust
+    SPIN_ANTICLOCKWISE = 0x04 # todo adjust
+
+    # CONTROL PARAMETERS
+    # Max Speed as a Percentage (0.0 to 1.0)
+    SPIN_MAX_PERCENT = 0.7
 
     # CONTROL DIRECTIONS
-    POSITION_CLOCKWISE = Direction.POSITIVE
-    POSITION_ANTICLOCKWISE = Direction.NEGATIVE
-
-    # BASE VELOCITY
-    DEFAULT_BASE_VELOCITY = 5
-    BASE_VELOCITY_PARAM = "base_velocity_param"
+    DIRECTION_CLOCKWISE = Direction.POSITIVE
+    DIRECTION_ANTICLOCKWISE = Direction.NEGATIVE
 
     def __init__(self):
         super().__init__(name="SpinnyPartNode", can_bus=self.CAN_BUS)
         logger = self.get_logger()
 
-        self.velocity_multiplier = 1
-
-        self.declare_parameter(self.BASE_VELOCITY_PARAM, self.DEFAULT_BASE_VELOCITY)
+        self.velocity = 0.5
 
         ## Create CONTROLS
-        self.spinny_part = ContinuousOneAxisPositionControl(
+        self.spinny_part = OneAxisPositionControl(
             logger=logger,
-            max_angle=self.SERVO_MAX_POS,
+            max_percent=self.SPIN_MAX_PERCENT,
         )
 
         ## Create CONTROLLERS
         self.spinny_part_controller = JonoPositionController(
             logger=logger,
             bus=self.bus,
-            pos_command=self.MOVE_SERVO_COMMAND,
             frame_id=self.SERVO_ID,
+            pos_command=self.SPIN_CLOCKWISE,
+            neg_command=self.SPIN_ANTICLOCKWISE,
             control=self.spinny_part,
-            max_value=self.SERVO_MAX_POS,
         )
 
         ## Add the CONTROLLERS to the node's controllers
@@ -83,22 +79,19 @@ class SpinnyPartNode(JoystickControllerNode):
         Updates the classes internal msg state
         :return: None
         """
-        self.velocity_multiplier = abs(joystick_l.ax_slider)
+        self.velocity = abs(joystick_l.ax_slider)
+        self.get_logger().debug(f"Velocity updated to {self.velocity}")
 
         if joystick_l.btn_thumb_l_state >= 1:
-            self.get_logger().debug("Spinny Part ANTICLOCKWISE")
-            self.spinny_part.displace(
-                self.velocity_multiplier
-                * self.get_parameter(self.BASE_VELOCITY_PARAM).value
-                * self.POSITION_ANTICLOCKWISE
-            )
+            self.get_logger().info(f"Spinny part moving ANTICLOCKWISE at velocity {self.velocity}")
+            self.spinny_part.update_direction(self.DIRECTION_ANTICLOCKWISE)
+            self.spinny_part.update_velocity(self.velocity)
         elif joystick_l.btn_thumb_r_state >= 1:
-            self.get_logger().debug("Spinny Part CLOCKWISE")
-            self.spinny_part.displace(
-                self.velocity_multiplier
-                * self.get_parameter(self.BASE_VELOCITY_PARAM).value
-                * self.POSITION_CLOCKWISE
-            )
+            self.get_logger().info(f"Spinny part moving CLOCKWISE at velocity {self.velocity}")
+            self.spinny_part.update_direction(self.DIRECTION_CLOCKWISE)
+            self.spinny_part.update_velocity(self.velocity)
+        else:
+            self.spinny_part.stop()
 
     def joystick_r(self, joystick_r: InputJoystick):
         """
