0bf8a7c11648cabe836b1e7630bb6d9999808772
Felicity Matthews <felicitymatthews30@gmail.com>
2025-03-09 15:02:17 +1100

    add new aliases

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -107,6 +107,8 @@ in
             launch-ec = "~/Builds/master/bin/ros2 launch nova_bringup ec_rover.launch.py";
             launch-science-arc = "~/Builds/master/bin/ros2 launch nova_bringup arc_science.launch.py";
             launch-science-urc = "~/Builds/master/bin/ros2 launch nova_bringup urc_science.launch.py";
+            launch-cameras = "~/Builds/master/bin/ros2 launch cameras2 camera_server_launch.py platform:=rover param-dir:='/home/nvidia/nova/src/ros/cameras2/cameras2/params'";
+            launch-cameras-all = "${launch-cameras} autostart:=true";
 
             # Rover setup aliases
             zero-arm = "${pkgs.bash}/bin/bash ${../../../scripts/zero-arm.sh}";
@@ -118,8 +120,9 @@ in
           
             # GUI aliases
             gui-shell = "nova-shell -A pkgs.ros.nova-gui";
-            gui-link = "ln -sf \"$ROS_TS_DEFINITIONS\" src/ros/rosTypes.ts";
+            gui-link = "ln -sf \"$ROS_TS_DEFINITIONS\" ~/nova/src/ros/nova-gui/nova-gui/src/ros/rosTypes.ts";
             gui-rosbridge = "~/Builds/master/bin/ros2 launch rosbridge_server rosbridge_websocket_launch.xml";
+            gui-run = "yarn --cwd ~/nova/src/ros/nova-gui/nova-gui dev";
 
             # Reolink camera
             reolink-low = "mpv --rtsp-transport=udp --no-cache --untimed --video-sync=display-resample --deinterlace=no --profile=low-latency --demuxer-max-bytes=512K --demuxer-max-back-bytes=512K rtsp://admin:Lab188b37@10.0.1.100:554/h264Preview_01_sub";
