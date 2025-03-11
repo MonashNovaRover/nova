8d631bb4c8760a59efda3cfb9293c3239a9402ed
Jono <53365342+jchin14@users.noreply.github.com>
2025-03-11 21:04:58 +1100

    Chat fixes code

--- nixfiles/modules/home/macros/can.sh
+++ nixfiles/modules/home/macros/can.sh
@@ -20,7 +20,7 @@
 #   can1 -  CAN 1 Line
 #   can2 -  CAN 2 Line
 #   all -   CAN 0, CAN 1 and CAN2 Line
-
+#
 #   vcan0 - Virtual CAN 0 Line
 #   vcan1 - Virtual CAN 1 Line
 #
@@ -31,150 +31,66 @@ ERROR='\033[0;31;1m'
 END='\033[0m'
 
 # Create an error function
-information () {
+information() {
     printf "%b%s%b\n" "$ERROR" "$1" "$END"
 }
 
 # Reset the failed flag
-failed="0"
+failed=0
 
-
-# Check if the first keyword command
-if [[ $1 = "start" ]]
-then
-    command="start"
-elif [[ $1 = "stop" ]] 
-then
-    command="stop"
+# Validate command argument
+if [[ "$1" == "start" || "$1" == "stop" ]]; then
+    command="$1"
 else
     information "Invalid Command! Please enter 'start' or 'stop'."
-    failed="1"
+    exit 1
 fi
 
+# Determine the CAN interface
+case "$2" in
+    can0|drive|ec) can="can0" ; bitrate=250000 ;;
+    can1|arm|sci) can="can1" ; bitrate=250000 ;;
+    can2) can="can2" ; bitrate=250000 ;;
+    vcan0) can="vcan0" ; bitrate=250000 ;;
+    vcan1) can="vcan1" ; bitrate=250000 ;;
+    all)
+        "$0" "$command" "can0"
+        "$0" "$command" "can1"
+        "$0" "$command" "can2"
+        exit 0 ;;
+    *)
+        information "Incorrect CAN line. Please enter one of:\n	['drive', 'ec', 'arm', 'sci', 'can0', 'can1', 'can2', 'all', 'vcan0', 'vcan1']"
+        exit 1 ;;
+esac
 
-# Get the CAN line
-if [[ $2 = "can0" ]]
-then
-    can="can0"
-elif [[ $2 = "can1" ]]
-then
-    can="can1"
-elif [[ $2 = "can2" ]]
-then
-    can="can2"
-elif [[ $2 = "drive" ]]
-then
-    can="can0"
-elif [[ $2 = "ec" ]]
-then
-    can="can0"
-elif [[ $2 = "arm" ]]
-then
-    can="can1"
-elif [[ $2 = "sci" ]]
-then
-    can="can1"
-elif [[ $2 = "vcan0" ]]
-then
-    can="vcan0"
-elif [[ $2 = "vcan1" ]]
-then
-    can="vcan1"
-
-# In the case of starting all CAN lines
-elif [[ $2 = "all" ]]
-then
-    can start can0
-    can start can1
-    can start can2
-    failed="1"
-
-# If invalid argument
-else
-    information "Incorrect CAN line. Please enter one of:\n\t['drive, 'ec', arm', 'sci', 'can0', 'can1', 'can2', 'all', 'vcan0', 'vcan1']"
-    failed="1"
+# If bitrate parameter is provided, override default
+if [[ -n "$3" ]]; then
+    bitrate="$3"
 fi
-
-
-# If the bitrate parameter exists
-if [[ -z "${3:-}" ]]; then
-    if [[ $2 = "can0" ]]
-    then
-        bitrate=250000
-    if [[ $2 = "can2" ]]
-    then
-        bitrate=250000
-    elif [[ $2 = "vcan0" ]]
-    then
-        bitrate=250000
-    elif [[ $2 = "sci" ]]
-    then
-        bitrate=250000
-    elif [[ $2 = "drive" ]]
-    then
-        bitrate=250000
-    elif [[ $2 = "ec" ]]
-    then
-        bitrate=250000
-    elif [[ $2 = "arm" ]]
-    then
-        bitrate=200000
-    else
-        bitrate=200000   # Default
-    fi
-else
-    bitrate=200000
-fi
-
 
 # Print message
-if [[ $failed != "1" ]]
-then
-    echo "Attempting to $command $can..."
+echo "Attempting to $command $can..."
+
+# Virtual CAN Handling
+if [[ "${can:0:1}" == "v" ]]; then
+    sudo modprobe vcan
+    if [[ "$command" == "start" ]]; then
+        sudo ip link add dev "$can" type vcan
+        sudo ip link set up "$can"
+    else
+        sudo ip link set down "$can"
+    fi
+    exit 0
 fi
 
-# Check if it is virtual CAN
-if [[ "$failed" != "1" && "${can:0:1}" == "v" ]]
-then
+# Real CAN Handling
+sudo modprobe can
+sudo modprobe can-raw
+sudo modprobe mttcan
 
-    # Run the Virtual CAN
-    sudo modprobe vcan
-
-    # If turning on CAN
-    if [[ $command == "start" ]]
-    then
-
-        # Create the CAN link
-        sudo ip link add dev "${can:1:5}" type vcan
-        sudo ip link set up "${can:1:5}"
-
-    # If turning off CAN
-    else
-
-        sudo ip link set down "${can:1:5}"
-
-    fi
-
-elif [[ $failed != "1" ]]
-then
-
-    # Run the CAN
-    sudo modprobe can
-    sudo modprobe can-raw
-    sudo modprobe mttcan
-
-    # If turning on CAN
-    if [[ $command == "start" ]]
-    then
-
-        # Create the CAN link
-        sudo ip link set "$can" type can bitrate "$bitrate" berr-reporting on 
-        sudo ip link set up "$can"
-
-    # If turning off CAN
-    else
-
-        sudo ip link set down "$can"
-
-    fi
+if [[ "$command" == "start" ]]; then
+    sudo ip link set "$can" type can bitrate "$bitrate" berr-reporting on
+    sudo ip link set up "$can"
+else
+    sudo ip link set down "$can"
 fi
