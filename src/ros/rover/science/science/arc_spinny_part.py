b3466fd62fef33079def5217d9fe63a3fbad6156
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-03-12 08:12:48 +1100

    Update arc_spinny_part.py

--- src/ros/rover/science/science/arc_spinny_part.py
+++ src/ros/rover/science/science/arc_spinny_part.py
@@ -35,8 +35,8 @@ class SpinnyPartNode(JoystickControllerNode):
     SERVO_ID = 0x0A0 # todo adjust accordingly
 
     # SENDING COMMAND IDS
-    SPIN_CLOCKWISE = 0x01 # todo adjust
-    SPIN_ANTICLOCKWISE = 0x02 # todo adjust
+    SPIN_CLOCKWISE = 0x03 # todo adjust
+    SPIN_ANTICLOCKWISE = 0x04 # todo adjust
     # CONTROL PARAMETERS
     # Max Speed as a Percentage (0.0 to 1.0)
     SPIN_MAX_PERCENT = 0.7
