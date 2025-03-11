b327bbae75ffe49f09ec4bc9c96f1818072af5a5
Tash <tlee0160@student.monash.edu>
2025-03-11 19:02:33 +1100

    Feat/scimbal cam v2 (#283)
    Co-authored-by: Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
    Co-authored-by: Felicity Matthews <felicitymatthews30@gmail.com>
    Co-authored-by: Monash Nova Rover <novaroverteam@monash.edu>
    
    FIX: Testing page route removed

--- src/ros/nova-gui/nova-gui/src/routes/routes.tsx
+++ src/ros/nova-gui/nova-gui/src/routes/routes.tsx
@@ -28,7 +28,7 @@ import URCRamanView from "../views/urc/URCRamanView.tsx";
 import { URCCartographerView } from "../views/urc/URCCartographerView.tsx";
 import TestStateView from "../views/test/TestStateView/TestStateView.tsx";
 import TestOverlayView from "../views/test/TestOverlayView/TestOverlayView.tsx";
-import TestScimbalCamView from "../views/test/TestOverlayView/TestScimbalCamView.tsx";
+import ScimbalCamView from "../views/shared/ScimbalCamPage.tsx";
 import NIRProbeWidget from "../components/NIRProbe/NIRProbeWidget.tsx";
 
 export const arcRoutes: RouteObject[] = [
@@ -61,6 +61,10 @@ export const arcRoutes: RouteObject[] = [
     element: <ARCAutonomousView />,
   },
   {
+    path: "/arc/scimbal-cam",
+    element: <ScimbalCamView />,
+  },
+  {
     path: "/arc/cameras",
     element: <CameraPage views={arcCameraSetup[ARCCompModes.ARC_POST_LANDING]} />,
   },
@@ -68,6 +72,7 @@ export const arcRoutes: RouteObject[] = [
     path: `/arc/cameras/${comp}`,
     element: <CameraPage views={arcCameraSetup[comp]} />,
   })),
+
 ];
 
 export const urcRoutes: RouteObject[] = [
@@ -102,6 +107,10 @@ export const urcRoutes: RouteObject[] = [
   {
     path: "/urc/uv-vis-spec",
     element: <URCUVVisSpecView/>
+  },
+  {
+    path: "/urc/scimbal-cam",
+    element: <ScimbalCamView />,
   },
   {
     path: "/urc/360cam",
