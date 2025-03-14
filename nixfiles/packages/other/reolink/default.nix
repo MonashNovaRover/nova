03f524f9a63000a3b338494d7096d7f9c779c58c
Orlando Chamberlain <ocha0025@student.monash.edu>
2025-03-15 00:07:53 +1100

    reolink-ctl: make nix config work with manual nix-build at least
    new testing instructions (on software side):
    
    ```
    cd /home/nova/nova/nixfiles/packages/other/reolink
    nix-build .
    ./result/bin/reolink-ctl
    ```
    
    im very much at a loss on how to get `nixos-rebuild switch` to even try
    to evaluate my default.nix

--- nixfiles/packages/other/reolink/default.nix
+++ nixfiles/packages/other/reolink/default.nix
@@ -1,9 +1,10 @@
-asdfds{ mkYarnPackage
-}:
-mkYarnPackage {
-  name = "reolink-ctl";
-  src = ./src;
-  packageJSON = ./src/package.json;
-  yarnLock = ./src/yarn.lock;
-  yarnNix = ./src/yarn.nix;
-};
+with (import <nixpkgs> {});
+rec {
+  reolink = mkYarnPackage {
+    name = "reolink-ctl";
+    src = ./src;
+    packageJSON = ./src/package.json;
+    yarnLock = ./src/yarn.lock;
+    yarnNix = ./src/yarn.nix;
+  };
+}
