fd30b8f674e8b6e9e269f35080b6640be165cd70
Tash Lee <tlee0160@student.monash.edu>
2025-03-16 09:18:14 +1100

    scimbal cam with wasd added to cameras dashboard

--- src/ros/nova-gui/nova-gui/src/views/shared/CamerasPage/CameraFilterConstants.ts
+++ src/ros/nova-gui/nova-gui/src/views/shared/CamerasPage/CameraFilterConstants.ts
@@ -43,6 +43,13 @@ export const defaultCamFilters: { [key: string]: CameraFilters } = {
     rotation: -90,
     contrast: 100,
     brightness: 100,
+  },
+  science_gimbal: {
+    flipCamera: false,
+    invertCamera: false,
+    rotation: 0,
+    contrast: 100,
+    brightness: 100,
   }
 }
 
