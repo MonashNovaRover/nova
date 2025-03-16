dc59416c1768d83a3b881ae115ee9799732c3f0c
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-03-16 13:12:44 +1100

    Update python node to take multiple readings (#320)

--- src/ros/rover/science/science/nir_probe_publisher.py
+++ src/ros/rover/science/science/nir_probe_publisher.py
@@ -27,6 +27,7 @@ The only commands to send this publisher are:
 """
 
 import logging
+import statistics
 import rclpy, jcan
 from rclpy.node import Node
 
@@ -62,7 +63,12 @@ class NIRProbePublisher(Node):
     ]
 
     # Timings
-    SEND_INTERVAL         = 0.01
+    SEND_INTERVAL = 0.1
+    TIMEOUT_PERIOD = 15
+    TIMEOUT_PARAM_NAME = "timeout"
+
+    DEFAULT_COUNT = 30
+    COUNT_PARAM_NAME = "count"
 
     def __init__(self):
 
@@ -71,6 +77,10 @@ class NIRProbePublisher(Node):
         self.get_logger().set_level(logging.INFO)
         self.get_logger().info("NIR Probe Publisher starting")
         self.publisher = self.create_publisher(NIRProbeData, '/science/nir_probe_data', 10)
+
+        # Initialise parameters
+        self.declare_parameter(self.TIMEOUT_PARAM_NAME, self.TIMEOUT_PERIOD)
+        self.declare_parameter(self.COUNT_PARAM_NAME, self.DEFAULT_COUNT)
 
         # Initialise CAN bus
         self.declare_parameter(self.CAN_BUS_PARAM, self.CAN_BUS)
@@ -87,13 +97,17 @@ class NIRProbePublisher(Node):
         self.command_service = self.create_service(TakeNIRProbeReading, '/science/take_nir_probe_reading', self.command_service_callback)
 
         # Initialise timers
+        self.timeout_timer = self.create_timer(self.get_parameter(self.TIMEOUT_PARAM_NAME).value, self.update_last_readings, autostart=False)
         self.last_read_timer = self.create_timer(0.1, self.publish_msg)
         self.timer_jcan_spin = self.create_timer(self.SEND_INTERVAL, self.bus.spin)
 
         # Internal State
+        self.readings = []
         self.active_photodiode = self.PHOTODIODE_OFF
         self.last_led_on = self.PHOTODIODE_OFF
-        self.last_reading = 0
+        self.last_average = 0
+        self.last_stdv = 0
+        self.last_count = 0
 
         self.get_logger().info(f"NIR Probe Publisher started on {self.get_parameter(self.CAN_BUS_PARAM).value}")
 
@@ -108,11 +122,70 @@ class NIRProbePublisher(Node):
         Publish data to GUI
         """
         msg = NIRProbeData()
-        msg.data = self.last_reading
+        msg.data = self.last_average
+        msg.stdv = self.last_stdv
+        msg.count = self.last_count
         msg.led = self.last_led_on
         msg.status = self.active_photodiode
         self.get_logger().debug(f"Publishing {msg}")
         self.publisher.publish(msg)
+
+    def request_reading(self, led):
+        """
+        Requests a reading from the NIR Probe
+        """
+        message = None
+        if led == self.PHOTODIODE1_ON:
+            message = self.TURN_LED1_ON
+        elif led == self.PHOTODIODE2_ON:
+            message = self.TURN_LED2_ON
+
+        if message is None:
+            self.get_logger().error(f"Invalid NIR probe request made: {led}")
+            return False
+
+        # Turn LED and photodiode on
+        self.get_logger().info(f"Turning on NIR probe LED and photodiode {led}")
+
+        frame = jcan.Frame(self.NIR_PROBE_ID, [message])
+        self.active_photodiode = led
+
+        try:
+            self.get_logger().debug(f"Sending {frame}")
+            self.bus.send(frame) # Turn lights on
+            return True
+
+        except Exception as e:
+            self.get_logger().error(f"Failed to send NIR probe command over CAN: {e}")
+
+        return False
+
+    def update_last_readings(self):
+        """
+        Do calculations on data and update last variables
+        """
+        self.timeout_timer.cancel()
+
+        # update last readings
+        self.last_count = len(self.readings)
+        self.last_led_on = self.active_photodiode
+
+        if len(self.readings) > 1:
+            self.last_average = statistics.mean(self.readings)
+            self.last_stdv = statistics.stdev(self.readings)
+        elif len(self.readings) == 1:
+            self.last_average = self.readings[0]
+            self.last_stdv = 0
+        else:
+            self.last_average = 0
+            self.last_stdv = 0
+
+        # reset variables
+        self.readings = []
+        self.active_photodiode = self.PHOTODIODE_OFF
+
+        self.publish_msg()
+
 
     """
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -129,45 +202,37 @@ class NIRProbePublisher(Node):
             self.get_logger().warn(f"Received unknown frame {frame}")
             return
 
+        if self.active_photodiode == self.PHOTODIODE_OFF:
+            self.get_logger().warn(f"Received frame when active_photodiode is off {frame}")
+            return
+
+        if frame.id == self.PHOTODIODE1_ID and self.active_photodiode != self.PHOTODIODE1_ON:
+            self.get_logger().warn(f"Received frame from wrong photodiode {frame}")
+            self.request_reading(self.active_photodiode)
+            return
+        if frame.id == self.PHOTODIODE2_ID and self.active_photodiode != self.PHOTODIODE2_ON:
+            self.get_logger().warn(f"Received frame from wrong photodiode {frame}")
+            self.request_reading(self.active_photodiode)
+            return
+
         self.get_logger().debug(f"Received {hex(frame.id)} {frame.data}")
 
-        self.last_reading = int.from_bytes(frame.data, "big")
-        self.last_led_on = self.active_photodiode
-        self.active_photodiode = self.PHOTODIODE_OFF
+        # add reading to list
+        self.readings.append(int.from_bytes(frame.data, "big"))
 
-        self.publish_msg()
+        # update last readings if reached end
+        if len(self.readings) >= self.get_parameter(self.COUNT_PARAM_NAME).value:
+            self.update_last_readings()
+            return
+
+        self.request_reading(self.active_photodiode)
 
     def command_service_callback(self, request, response):
         """
         Run commands from GUI on NIR Probe
         """
-        # Check which led state NIR Probe publisher is in
-        message = None
-        if request.led == self.PHOTODIODE1_ON:
-            message = self.TURN_LED1_ON
-        elif request.led == self.PHOTODIODE2_ON:
-            message = self.TURN_LED2_ON
-
-        if message is None:
-            self.get_logger().error(f"Invalid NIR probe request made: {request.led}")
-            response.success = False
-            return response
-
-        # Turn LED and photodiode on
-        self.get_logger().info(f"Turning on NIR probe LED and photodiode {request.led}")
-
-        frame = jcan.Frame(self.NIR_PROBE_ID, [message])
-        self.active_photodiode = request.led
-
-        try:
-            self.get_logger().debug(f"Sending {frame}")
-            self.bus.send(frame) # Turn lights on
-            response.success = True
-
-        except Exception as e:
-            self.get_logger().error(f"Failed to send NIR probe command over CAN: {e}")
-            response.success = False
-
+        response.success = self.request_reading(request.led)
+        self.timeout_timer.reset()
         return response
 
 # The main code that executes when starting
