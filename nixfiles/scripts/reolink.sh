cdb3f07efa4b9266f83bb3609b70e320b625dc5c
Orlando Chamberlain <ocha0025@student.monash.edu>
2025-03-22 22:44:49 +1100

    reolink: allow saving video to disk instead
    Mainly for auto to run on rover so that they can look at good video
    later and don't need to stream it while the auto stack is using most of
    the bandwidth.
    
    Use `reolink save`. The default filename will be
    ~/recordings/reolink_$CURRENTTIME.mp4, but you can specify a filename
    with `-o filename.mp4`.
    
    Once you copy the file from the rover to your laptop, you can play it with
    `ffplay /path/to/file.mp4` (or use your prefered video player program).
    
    Also you don't _need_ to use mp4, other formats will probably work too.
    
    Also added a --uri option for testing with other uris (eg for testing at
    home if you have an IP camera)

--- nixfiles/scripts/reolink.sh
+++ nixfiles/scripts/reolink.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 #TODO: remove once nix is working properly
-if which ffplay 2>/dev/null; then
+if which ffplay &>/dev/null; then
   :
 else
   echo "Please run me in a \`nix-shell -p ffmpeg\`"
@@ -19,7 +19,15 @@ NAME="Banksia Cam"
 STREAM_NAME="Preview_01_main"
 
 FLIP=""
+URI=""
+FILENAME=""
+OP="stream"
 
+# some of these might not really do anything.
+# there are some more mentioned in https://trac.ffmpeg.org/wiki/StreamingGuide that I haven't had time to test.
+LOW_LATENCY_ARGS="-flags low_delay -fflags nobuffer -framedrop -rc_lookahead 0 -probesize 32 -analyzeduration 0 -vf setpts=0 -tune zerolatency"
+
+FILENAME=""
 while test $# -gt 0; do
   case "$1" in
     high) STREAM_NAME="Preview_01_main"
@@ -28,17 +36,57 @@ while test $# -gt 0; do
       ;;
     180) FLIP="-vf hflip,vflip"
       ;;
-    *) echo Usage: $0 "[high|low] [180]"
+    "-o") FILENAME=$2
+      shift
+      ;;
+    stream) OP="stream"
+      ;;
+    save) OP="save"
+      ;;
+    "--uri") URI=$2
+      shift
+      ;;
+    *) echo Usage: $0 "[high|low] [save|stream] [180] [-o OUTPUT_FILENAME]"
       exit
       ;;
   esac
   shift
 done
 
-# some of these might not really do anything.
-LOW_LATENCY_ARGS="-flags low_delay -fflags nobuffer -framedrop -rc_lookahead 0 -probesize 32 -analyzeduration 0 -vf setpts=0 -tune zerolatency"
+if [ -e "$FILENAME" ]; then
+  echo "$FILENAME" already exists!
+  exit 1
+fi
 
-exec ffplay -rtsp_transport udp \
-  -i rtsp://$USERNAME:$PASSWORD@$IP:$PORT/$STREAM_NAME \
-  $FLIP $LOW_LATENCY_ARGS -window_title "$NAME"
+if [ -z "$FILENAME" ]; then
+  # set default timebased filename
+  FILENAME="$HOME/recordings/reolink_$(date -Iseconds | tr : _).mp4"
+fi
+
+if [ -z "$URI" ]; then
+  URI="rtsp://$USERNAME:$PASSWORD@$IP:$PORT/$STREAM_NAME"
+fi
+
+if [ "stream_" == "$OP"_ ]; then
+  # we are streaming to a window
+  exec ffplay -rtsp_transport udp -i "$URI" \
+    $FLIP $LOW_LATENCY_ARGS -window_title "$NAME"
+elif [ "save_" == "$OP"_ ]; then
+  # we are saving to a file
+  mkdir -p "$(dirname $FILENAME)"
+  echo "Saving to $FILENAME"
+  
+  # carefully handle errors here so we still print filename if user
+  # stops ffmpeg with ctrl-c instead of bash exiting immediately
+  set +e
+  ffmpeg -i "$URI" $FLIP "$FILENAME"
+  RET=$?
+  set -e
+  echo "Saved to $FILENAME"
+  exit $RET
+else
+  echo invalid op $OP
+  exit 1
+fi
+
 
