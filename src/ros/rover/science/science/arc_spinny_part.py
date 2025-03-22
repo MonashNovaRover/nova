f4ff941e489d45224ed7b180f514a0a454ad4349
Bailey Chessum <bailey.chessum1@gmail.com>
2025-03-22 21:46:01 +1100

    fix spinny param issue

--- src/ros/rover/science/science/arc_spinny_part.py
+++ src/ros/rover/science/science/arc_spinny_part.py
@@ -111,10 +111,10 @@ class SpinnyPartNode(JoystickControllerNode):
 
         # twitch/update offset
         if joystick_l.btn_bottom_r1_state == 1:
-            self.spinny_part.set_offset(min(self.MAX_OFFSET, self.spinny_part.get_offset() + self.get_parameter(self.OFFSET_STEP_PARAM_PARAM).value))
+            self.spinny_part.set_offset(min(self.MAX_OFFSET, self.spinny_part.get_offset() + self.get_parameter(self.OFFSET_STEP_PARAM).value))
             self.get_logger().info(f"OFFSET updated {self.spinny_part.get_offset()}")
         elif joystick_l.btn_bottom_r2_state == 1:
-            self.spinny_part.set_offset(max(self.MIN_OFFSET, self.spinny_part.get_offset() - self.get_parameter(self.OFFSET_STEP_PARAM_PARAM).value))
+            self.spinny_part.set_offset(max(self.MIN_OFFSET, self.spinny_part.get_offset() - self.get_parameter(self.OFFSET_STEP_PARAM).value))
             self.get_logger().info(f"OFFSET updated {self.spinny_part.get_offset()}")
 
     def joystick_r(self, joystick_r: InputJoystick):
