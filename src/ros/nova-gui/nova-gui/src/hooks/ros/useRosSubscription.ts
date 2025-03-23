0f40544553c57039ee8e06e17a1eaaada06bf639
Bailey Chessum <bailey.chessum1@gmail.com>
2025-03-23 15:33:55 +1100

    feat: autosaving from the ROS topic

--- src/ros/nova-gui/nova-gui/src/hooks/ros/useRosSubscription.ts
+++ src/ros/nova-gui/nova-gui/src/hooks/ros/useRosSubscription.ts
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useRef} from "react";
+import {DependencyList, useCallback, useContext, useEffect, useRef} from "react";
 import {RosContext} from "../../redux/context/ros/RosContext.ts";
 import {RosTopic} from "../../ros/topics/rosTopic.ts";
 import {Topic} from "roslib";
@@ -9,10 +9,15 @@ import {RosTopicInterfaces} from "../../ros/topics/rosTopicTypes.ts";
  * Simple hook for adding a callback for some ros topic subscription. Do not vary the topic.
  * @param topic The ROS topic to use
  * @param callback The callback function for when a new message is received
+ * @param extraDeps Dependencies for the callback function
  */
-export const useRosSubscription = (topic: RosTopic, callback: (message: RosTopicInterfaces[typeof topic]) => void) => {
+export const useRosSubscription = <T>(
+  topic: RosTopic, callback: (message: T & RosTopicInterfaces[typeof topic]) => void, extraDeps?: DependencyList
+) => {
   const ros = useContext(RosContext);
   const rosTopicRef = useRef<Topic | undefined>(undefined);
+
+  const cachedCallback = useCallback(callback, extraDeps ?? [callback]) // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     if (ros == undefined)
@@ -30,18 +35,18 @@ export const useRosSubscription = (topic: RosTopic, callback: (message: RosTopic
     const subscriptionCallback = () => {};
 
     rosTopicRef.current.subscribe(subscriptionCallback);
-    rosTopicRef.current.on("message", callback);
+    rosTopicRef.current.on("message", cachedCallback);
 
     return () => {
       rosTopicRef.current?.unsubscribe(subscriptionCallback);
-      rosTopicRef.current?.removeListener("message", callback);
-    }
-  }, [ros, callback, topic]);
+      rosTopicRef.current?.removeListener("message", cachedCallback);
+    };
+  }, [ros, cachedCallback, topic]);
 
   // Clean up at end
   useEffect(() => {
     return () => {
       rosTopicRef.current = undefined
-    }
+    };
   }, [ros]);
 }
