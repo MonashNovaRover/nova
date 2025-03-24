81857d38097bdde21b32b30f77db9db6f3aedbcd
Jared Landau <122423415+jaredlandau@users.noreply.github.com>
2025-03-24 16:11:17 +1100

    Added echo for build being run

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -25,12 +25,36 @@ in
       complete -F _complete_alias "''${!BASH_ALIASES[@]}"
 
       # Rover operator shell functions (act as aliases that can take arguments)
-      launch-base() { ~/Builds/"${1:-master}"/bin/ros2 launch nova_bringup base.launch.py; }
-      launch-drive() { ~/Builds/"${1:-master}"/bin/ros2 launch nova_bringup drive.launch.py; }
-      launch-arm() { ~/Builds/"${1:-master}"/bin/ros2 launch nova_bringup arm.launch.py; }
-      launch-ec() { ~/Builds/"${1:-master}"/bin/ros2 launch nova_bringup ec_rover.launch.py; }
-      launch-science-arc() { ~/Builds/"${1:-master}"/bin/ros2 launch nova_bringup arc_science.launch.py; }
-      launch-science-urc() { ~/Builds/"${1:-master}"/bin/ros2 launch nova_bringup urc_science.launch.py; }
+      launch-base() { 
+        local build="\${1:-master}"
+        echo "Running build: $build"
+        "${config.home.homeDirectory}/Builds/$build/bin/ros2" launch nova_bringup base.launch.py
+      }
+      launch-drive() { 
+        local build="\${1:-master}"
+        echo "Running build: $build"
+        "${config.home.homeDirectory}/Builds/$build/bin/ros2" launch nova_bringup drive.launch.py
+      }
+      launch-arm() { 
+        local build="\${1:-master}"
+        echo "Running build: $build"
+        "${config.home.homeDirectory}/Builds/$build/bin/ros2" launch nova_bringup arm.launch.py
+      }
+      launch-ec() { 
+        local build="\${1:-master}"
+        echo "Running build: $build"
+        "${config.home.homeDirectory}/Builds/$build/bin/ros2" launch nova_bringup ec_rover.launch.py
+      }
+      launch-science-arc() { 
+        local build="\${1:-master}"
+        echo "Running build: $build"
+        "${config.home.homeDirectory}/Builds/$build/bin/ros2" launch nova_bringup arc_science.launch.py
+      }
+      launch-science-urc() { 
+        local build="\${1:-master}"
+        echo "Running build: $build"
+        "${config.home.homeDirectory}/Builds/$build/bin/ros2" launch nova_bringup urc_science.launch.py
+      }
       
       # Ensure functions are available if shell ($-) is interactive (*i*)
       if [[ $- == *i* ]]; then
