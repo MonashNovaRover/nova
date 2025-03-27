c6e90541075e0e3d19912cfbc0c5462db9ec33ae
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-03-28 09:29:46 +1100

    Add NIR Reading count (#361)

--- src/ros/nova-gui/nova-gui/src/components/NIRProbe/NIRProbeFileTable/NIRProbeCalcTable.tsx
+++ src/ros/nova-gui/nova-gui/src/components/NIRProbe/NIRProbeFileTable/NIRProbeCalcTable.tsx
@@ -1,9 +1,8 @@
 import {CardProps, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
 import React from "react";
 import {useAverageReading} from "../NIRProbeCalibration/NIRCalibration.ts";
-import {
-  XYNames
-} from "../SpaceResourcesSiteType.tsx";
+import {NIRProbeReadingType, XYNames} from "../SpaceResourcesSiteType.tsx";
+import {useNIRSiteData} from "../useNIRSiteData.ts";
 
 export interface NIRProbeCalcTableProps extends CardProps {
 }
@@ -14,8 +13,9 @@ export interface NIRProbeCalcTableProps extends CardProps {
  */
 const NIRProbeCalcTable: React.FC<NIRProbeCalcTableProps> = () => {
   const [averageX, averageY, calibratedResult] = useAverageReading()
+  const [readings, _] = useNIRSiteData();
 
-  const tableRows = (
+  const tableRows = [
       <TableRow key="average">
         <TableCell key={1}>
           {averageX.toFixed(4)}
@@ -27,8 +27,18 @@ const NIRProbeCalcTable: React.FC<NIRProbeCalcTableProps> = () => {
           {calibratedResult}
         </TableCell>
         <TableCell key={0}>{""}</TableCell>
+      </TableRow>,
+      <TableRow>
+        <TableCell key={21}>
+          {readings[NIRProbeReadingType.WATER].length}
+        </TableCell>
+        <TableCell key={22}>
+          {readings[NIRProbeReadingType.ICE].length}
+        </TableCell>
+        <TableCell key={23}>{""}</TableCell>
+        <TableCell key={20}>{""}</TableCell>
       </TableRow>
-    )
+    ]
 
   return (
     <Table removeWrapper layout={"fixed"} className="table-fixed mb-3" aria-label="NIR probe readings table">
