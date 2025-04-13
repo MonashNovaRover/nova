83cdb6823f38ec8264f370d5b06e8517245f2d00
Bailey Chessum <52596359+BaileyChessum@users.noreply.github.com>
2025-04-13 16:52:06 +1000

    Make the IK controller chainable with a distinct Twistmapper controller (#373)
    Signed-off-by: Bailey Chessum <bailey.chessum1@gmail.com>

--- src/ros/rover/controllers/nova_ik_controller/include/nova_ik_controller/nova_ik_controller.hpp
+++ src/ros/rover/controllers/nova_ik_controller/include/nova_ik_controller/nova_ik_controller.hpp
@@ -10,16 +10,13 @@
 
 #include "visibility_control.h"
 #include "controller_interface/controller_interface.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "controller_interface/chainable_controller_interface.hpp"
 #include "hardware_interface/handle.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 #include "rclcpp/node.hpp"
 #include "realtime_tools/realtime_box.h"
-//#include "realtime_tools/realtime_buffer.h"
-//#include "realtime_tools/realtime_publisher.h"
 #include "tf2_msgs/msg/tf_message.hpp"
 #include "tf2/LinearMath/Scalar.h"
 #include "nova_ik_controller/speed_limiter.hpp"
@@ -35,7 +32,7 @@
 
 namespace nova_ik_controller
 {
-class NovaIKController : public controller_interface::ControllerInterface
+class NovaIKController : public controller_interface::ChainableControllerInterface
 {
 public:
   NovaIKController();
@@ -44,8 +41,12 @@ public:
 
   controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-  controller_interface::return_type update(
-    const rclcpp::Time &time, const rclcpp::Duration &period) override;
+  std::vector<hardware_interface::CommandInterface> on_export_reference_interfaces() override;
+
+  bool on_set_chained_mode(bool chained_mode) override;
+
+  controller_interface::return_type update_and_write_commands(
+    const rclcpp::Time& time, const rclcpp::Duration& period) override;
 
   controller_interface::CallbackReturn on_init() override;
 
@@ -66,6 +67,9 @@ public:
 
   controller_interface::CallbackReturn on_shutdown(
     const rclcpp_lifecycle::State &previous_state) override;
+
+  controller_interface::return_type update_reference_from_subscribers(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
   /// @brief Calculates the IK given a frame, pose and set of lengths.
   /// @brief See Keenan's IK notes.
@@ -93,11 +97,7 @@ protected:
   {
     std::string name;
     std::reference_wrapper<hardware_interface::LoanedCommandInterface> command;
-    // TODO: Add state interfaces to be used by the twistmapper for resetting on activation (or after activation upon receiving the first bit of meaningful data)
     // SpeedLimiter speed_limiter;
-    // float target_direction = 0.0;
-    // float best_effort_rotational_velocity = 0.0;
-    // store per joint odometry here maybe?
   };
 
   controller_interface::CallbackReturn configure_joints(
@@ -107,52 +107,13 @@ protected:
   // Helpers
   std::string joint_to_command_interface_name(const std::string& joint_name) const;
 
-  // TODO: change this message when we get one
-
   std::vector<JointHandle> registered_joint_handles_;
 
   // Parameters from ROS for nova_diff_drive_controller
   std::shared_ptr<ParamListener> param_listener_;
   Params params_;
 
-  // Twistmapper
-
-  // TODO: Initialize twist stamped topic and write callback
-  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_stamped_sub = nullptr;
-  realtime_tools::RealtimeBox<std::shared_ptr<geometry_msgs::msg::TwistStamped>> received_twist_stamped_ptr{nullptr};
-  /// Result of the twistmapper, and input to IK. Desired position and orientation of the end effector relative to the base.
-  tf2::Transform twistmapper_pose_ = tf2::Transform();
-  tf2::Vector3 twistmapper_pose_rpy_ = tf2::Vector3();
-  rclcpp::Time twistmapper_pose_update_time_ = rclcpp::Time();
-  /// True when initial state has been received to confirm that the value in _twistmapper_pose is safe and valid
-  // TODO: Make invalid when deactivated
-  bool twistmapper_pose_validated_ = false;
-
-  // broadcasting twistmapper
-  std::shared_ptr<tf2_ros::TransformBroadcaster> twistmapper_pose_tf_broadcaster_;
-  // FK for getting the initial state for twistmapper_pose_
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
-  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-
-  // TODO: Detection of initial state, setting initial twistmapper pose
-
-  // TODO: Timeout and halt for safety!
-
-  // TODO: Implement
-  void update_twistmapper(const rclcpp::Time &time, const rclcpp::Duration &period);
-
-  // Timeout to consider cmd_vel commands old
-  std::chrono::milliseconds cmd_vel_timeout_{500};
-  bool subscriber_is_active_ = false; // not sure what this is for yet
-  rclcpp::Time previous_update_timestamp_{0};
-
-  // publish rate limiter
-  double publish_rate_ = 50.0;
-  rclcpp::Duration publish_period_ = rclcpp::Duration::from_nanoseconds(0);
-  rclcpp::Time previous_publish_timestamp_{0, 0, RCL_CLOCK_UNINITIALIZED};
-  bool is_halted = false;
   bool reset();
-  void halt();
 };
 } // namespace nova_ik_controller
 #endif // NOVA_IK_CONTROLLER__NOVA_IK_CONTROLLER_HPP_
