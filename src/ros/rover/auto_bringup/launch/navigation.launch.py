25ac980a98ed17071acf927b1ff1556b25f372fc
Anthony <anthony@v01dbreaker.cc>
2025-04-25 18:35:09 +1000

    GPS Localisation in Sim (#458)
    Co-authored-by: Victor Bartlinski <vicbart08@gmail.com>

--- src/ros/rover/auto_bringup/launch/navigation.launch.py
+++ src/ros/rover/auto_bringup/launch/navigation.launch.py
@@ -1,16 +1,19 @@
-# Copyright (c) 2018 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Monash Nova Rover Team
+
+This launch file is responsible for navigation.
+It launches our nav2 stack.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODES:
+  -
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE: 	auto_bringup
+CREATION:	UNKNOWN
+EDITED:     24/04/2025
+EDITED BY:  Anthony Lew
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable, OpaqueFunction
@@ -290,7 +293,7 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
             name='nav2_params',
-            default_value=PathJoinSubstitution([auto_bringup_dir, 'params', 'nav2.yaml']),
+            default_value=PathJoinSubstitution([auto_bringup_dir, 'params', 'nav2_urc.yaml']),
             description='Full path to the ROS2 parameters file to use for all launched Nav2 nodes',
         ),
         DeclareLaunchArgument(
