830ad67e80bc41f8e9be4a8e93243598c1b45de8
Anthony Lew <anthony@v01dbreaker.cc>
2025-04-26 17:29:39 +1000

    feat: STVL builds using nixpkgs openvdb

--- nixfiles/packages/ros/spatio-temporal-voxel-layer/default.nix
+++ nixfiles/packages/ros/spatio-temporal-voxel-layer/default.nix
@@ -1,4 +1,37 @@
-{ lib, buildRosPackage, fetchFromGitHub, ament-cmake, ament-cmake-vendor-package, ament-lint-auto, boost, builtin-interfaces, c-blosc, git, geometry-msgs, laser-geometry, message-filters, nav2-costmap-2d, openexr, openvdb, pcl, pcl-conversions, pluginlib, rclcpp, rosidl-default-generators, rosidl-default-runtime, sensor-msgs, std-msgs, std-srvs, tbb_2021_11, tf2-geometry-msgs, tf2-ros, tf2-sensor-msgs, visualization-msgs, zlib }:
+{ lib, 
+ buildRosPackage, 
+ fetchFromGitHub, 
+ ament-cmake, 
+ pkg-config, 
+ ament-cmake-vendor-package, 
+ ament-lint-auto, 
+ boost, 
+ builtin-interfaces, 
+ c-blosc, 
+ git, 
+ geometry-msgs, 
+ laser-geometry, 
+ message-filters,
+ nav2-costmap-2d,
+ openexr, 
+ openvdb, 
+ pcl, 
+ pcl-conversions, 
+ pluginlib, 
+ rclcpp, 
+ rosidl-default-generators, 
+ rosidl-default-runtime, 
+ sensor-msgs, 
+ std-msgs, 
+ std-srvs, 
+ tbb,
+ tbb_2021_11, 
+ tf2-geometry-msgs, 
+ tf2-ros, 
+ tf2-sensor-msgs, 
+ visualization-msgs, 
+ zlib 
+}:
 let
 githubrepo = fetchFromGitHub {
   owner = "SteveMacenski";
@@ -6,42 +39,31 @@ githubrepo = fetchFromGitHub {
   rev = "2.5.5";
   hash = "sha256-Qk2k6aa+WDgXwz98l0MwB1LLb8yBULaue1u0mh6vVHc=";
 };
-openvdb-vendor = buildRosPackage rec {
-  pname = "ros-jazzy-openvdb-vendor";
-  version = "2.5.5";
+# openvdb-vendor = buildRosPackage rec {
+#   pname = "ros-jazzy-openvdb-vendor";
+#   version = "2.5.5";
 
-  src = githubrepo;
+#   src = githubrepo;
 
-  sourceRoot = "${src.name}/openvdb_vendor";
+#   sourceRoot = "${src.name}/openvdb_vendor";
 
-  buildType = "ament_cmake";
-  buildInputs = [ ament-cmake ament-cmake-vendor-package git ];
-  propagatedBuildInputs = [ boost c-blosc openvdb tbb_2021_11 zlib ];
-  nativeBuildInputs = [ ament-cmake ament-cmake-vendor-package git ];
+#   buildType = "ament_cmake";
+#   buildInputs = [ ament-cmake ament-cmake-vendor-package git ];
+#   propagatedBuildInputs = [ boost c-blosc openvdb tbb_2021_11 zlib ];
+#   nativeBuildInputs = [ ament-cmake ament-cmake-vendor-package git ];
 
-  meta = {
-    description = "Wrapper around OpenVDB, if not found on the system, will compile from source";
-    license = with lib.licenses; [ "LGPL-2.1-only" "MPL-2.0-license" ];
-    position = "./default.nix";
-  };
+#   meta = {
+#     description = "Wrapper around OpenVDB, if not found on the system, will compile from source";
+#     license = with lib.licenses; [ "LGPL-2.1-only" "MPL-2.0-license" ];
+#     position = "./default.nix";
+#   };
 
-  file = "CMakeLists.txt";
-
-  vendor = lib.tarSource {} (
-    fetchFromGitHub rec {
-      owner = "AcademySoftwareFoundation";
-      repo = "openvdb";
-      rev = "v10.0.1";
-      hash = "sha256-kaf5gpGYVWinmnRwR/IafE1SJcwmP2psfe/UZdtH1Og=";
-      name = rev;
-    });
-
-  postPatch = ''
-    substituteInPlace CMakeLists.txt\
-      --replace "https://github.com/AcademySoftwareFoundation/openvdb.git" "file://${vendor}"\
-      --replace "VCS_TYPE git" "VCS_TYPE tar"
-  '';
-};
+#   postPatch = ''
+#     substituteInPlace CMakeLists.txt\
+#       --replace "https://github.com/AcademySoftwareFoundation/openvdb.git" "file://${vendor}"\
+#       --replace "VCS_TYPE git" "VCS_TYPE tar"
+#   '';
+# };
 in
 buildRosPackage rec {
   pname = "ros-jazzy-spatio-temporal-voxel-layer";
@@ -54,11 +76,26 @@ buildRosPackage rec {
   buildType = "ament_cmake";
   buildInputs = [ ament-cmake rosidl-default-generators ];
   checkInputs = [ ament-lint-auto ];
-  propagatedBuildInputs = [ builtin-interfaces geometry-msgs laser-geometry message-filters nav2-costmap-2d openexr openvdb-vendor pcl pcl-conversions pluginlib rclcpp rosidl-default-runtime sensor-msgs std-msgs std-srvs tf2-geometry-msgs tf2-ros tf2-sensor-msgs visualization-msgs ];
-  nativeBuildInputs = [ ament-cmake ];
+  propagatedBuildInputs = [  boost builtin-interfaces c-blosc geometry-msgs laser-geometry message-filters nav2-costmap-2d openexr openvdb pcl pcl-conversions pluginlib rclcpp rosidl-default-runtime sensor-msgs std-msgs std-srvs tbb tf2-geometry-msgs tf2-ros tf2-sensor-msgs visualization-msgs ];
+  nativeBuildInputs = [ ament-cmake pkg-config ];
 
   meta = {
     description = "The spatio-temporal 3D obstacle costmap package";
     license = with lib.licenses; [ "LGPL-2.1-only" ];
   };
+
+  postPatch = ''
+    substituteInPlace CMakeLists.txt\
+      --replace "find_package(openvdb_vendor REQUIRED)" "find_package(OpenVDB REQUIRED)"\
+      --replace "OpenVDB:" "$"\
+      --replace ":openvdb" "{OpenVDB_LIBRARIES}"\
+      --replace 'PROJECT_SOURCE_DIR}/cmake/")' 'PROJECT_SOURCE_DIR}/cmake/" )'\
+      --replace "openvdb_vendor" ""
+    sed -i '/set(CMAKE_MODULE_PATH/alist(APPEND CMAKE_MODULE_PATH "${openvdb.dev}/lib/cmake/OpenVDB/")' CMakeLists.txt
+    sed -i '/find_package(OPENVDB REQUIRED)/afind_package(TBB REQUIRED)' CMakeLists.txt
+    sed -i '/{OpenVDB_LIBRARIES}/atbbprefix{TBB_LIBRARIES}' CMakeLists.txt
+    substituteInPlace CMakeLists.txt\
+      --replace "tbbprefix" "$"
+    cat CMakeLists.txt
+  '';
 }
