5323af88ddb7d0b95095d0ec5bd69666b75367a1
Anthony Lew <anthony@v01dbreaker.cc>
2025-04-27 17:08:30 +1000

    feat: Add cameras to test in URC auto gui

--- src/ros/nova-gui/nova-gui/src/views/urc/URCAutonomousNavigationView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/urc/URCAutonomousNavigationView.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import SerialMappedCameraComponent from "../shared/CamerasPage/SerialMappedCameraComponent.tsx";
 
 const URCAutonomousNavigationView: React.FC = () => {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className="grid grid-cols-2 gap-3">
+      <SerialMappedCameraComponent cameraSerial={"oak-rgb"} />
+      <SerialMappedCameraComponent cameraSerial={"oak-depth"} />
+      <SerialMappedCameraComponent cameraSerial={"bootie-rgb"} />
+      <SerialMappedCameraComponent cameraSerial={"bootie-depth"} />
     </div>
   );
 };
