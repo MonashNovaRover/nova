b3d58224e3d61f2518d8aef083fa75d0be590f25
Anthony Lew <anthony@v01dbreaker.cc>
2025-05-01 17:26:55 +1000

    fix: Add min depth to bootie pointcloud so that bootie is not seen in depth

--- src/ros/rover/auto_bringup/launch/camera.launch.py
+++ src/ros/rover/auto_bringup/launch/camera.launch.py
@@ -109,7 +109,11 @@ def launch_setup(context, *args, **kwargs):
                     name=f'{back_name}_point_cloud_xyz',
                     parameters=[{'decimation': 2,
                                  'max_depth': 10.0,
+<<<<<<< HEAD
                                  'min_depth': 1.1,
+=======
+                                 'min_depth': 1.0,
+>>>>>>> 23b9e3ef (fix: Add min depth to bootie pointcloud so that bootie is not seen in depth)
                                  'voxel_size': 0.1}],
                     remappings=[('depth/image', f'{back_name}/stereo/image_raw'),
                                 ('depth/camera_info', f'{back_name}/stereo/camera_info'),
