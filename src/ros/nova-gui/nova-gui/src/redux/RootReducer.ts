50382041e9db7e47a0083e6da11243a05d427aea
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-05-03 16:04:35 +1000

    Activated Node GUI (#472)

--- src/ros/nova-gui/nova-gui/src/redux/RootReducer.ts
+++ src/ros/nova-gui/nova-gui/src/redux/RootReducer.ts
@@ -5,13 +5,13 @@ import {RosTopic} from "../ros/topics/rosTopic";
 import {
   IRosCmdInterfacesCmdFeedback,
   IRosCmdInterfacesCmDsFeedback,
+  IRosNovaInterfacesHydraprobeData,
   IRosNovaInterfacesNirProbeDataConst,
   IRosSensorMsgsRange,
-  IRosNovaInterfacesHydraprobeData,
   IRosStdMsgsHeader
 } from "../ros/rosTypes";
-import { cartographerSlice } from "./slices/CartographerSlice";
-import { initialNavSatMessage } from "./models/CartographerState";
+import {cartographerSlice} from "./slices/CartographerSlice";
+import {initialNavSatMessage} from "./models/CartographerState";
 import {uiSlice} from "./slices/UISlice";
 import {cameraStreamerSlice} from "./slices/CameraStreamSlice";
 import {BLCMD_INDEX} from "../constants";
