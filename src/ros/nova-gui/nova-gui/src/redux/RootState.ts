a4e41b1291d6c8a702fc78563133c128178e585a
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-05-03 16:04:35 +1000

    Activated Node GUI (#472)

--- src/ros/nova-gui/nova-gui/src/redux/RootState.ts
+++ src/ros/nova-gui/nova-gui/src/redux/RootState.ts
@@ -25,7 +25,6 @@ import {
   IRosNovaInterfacesBmeSensor,
   IRosSensorMsgsBatteryState,
   IRosNovaInterfacesActiveNodeStatus,
-
 } from "../ros/rosTypes";
 
 import { BifrostStatus } from "./models/bifrost/BifrostTypes";
