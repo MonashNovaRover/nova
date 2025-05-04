efb3729ddaf70de1bc23a11589c96259c1c4e256
Monash Nova Rover <novaroverteam@monash.edu>
2025-05-04 11:53:45 +0000

    URC Cache using dynamic can id

--- src/ros/rover/nova_bringup/launch/urc_science.launch.py
+++ src/ros/rover/nova_bringup/launch/urc_science.launch.py
@@ -13,9 +13,10 @@ NODES:
   - control/analysis_platform.py        [analysis_platform]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 CREATION:   17/03/2024
-EDITED:     13/02/2025
+EDITED:     04/05/2025
 EDITED BY: Tristan Clark, Josh Leivenzon, 
-    Victor Bartlinski, Felicity Matthews
+    Victor Bartlinski, Felicity Matthews,
+    Brandon Chung
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 from launch import LaunchDescription
@@ -31,6 +32,8 @@ def launch_setup(context, *args, **kwargs):
     auger2_actuation_canid = LaunchConfiguration('auger2_actuation_canid')
     analysis_arm_cmd_canid = LaunchConfiguration('analysis_arm_cmd_canid')
     cbeam_actuation_canid = LaunchConfiguration('cbeam_actuation_canid')
+    cache1_canid = LaunchConfiguration('cache1_canid')
+    cache2_canid = LaunchConfiguration('cache2_canid')
 
     return [
         Node(
@@ -102,6 +105,18 @@ def launch_setup(context, *args, **kwargs):
             executable='urc_cache.py',
             output='screen',
             emulate_tty=True,
+            parameters=[{
+                "frame_id": cache1_canid,
+            }],
+        ),
+        Node(
+            package='science',
+            executable='urc_cache.py',
+            output='screen',
+            emulate_tty=True,
+            parameters=[{
+                "frame_id": cache2_canid,
+            }],
         ),
         Node(
             package='science',
@@ -161,6 +176,8 @@ def generate_launch_description():
         DeclareLaunchArgument("auger2_actuation_canid", default_value='0x0D2'),
         DeclareLaunchArgument("cbeam_actuation_canid", default_value='0x0A1'),
         DeclareLaunchArgument("analysis_arm_cmd_canid", default_value='0x0A2'),
+                DeclareLaunchArgument("cache1_canid", default_value='0x060'),
+                DeclareLaunchArgument("cache2_canid", default_value='0x061'),
     ]
 
     return LaunchDescription(
