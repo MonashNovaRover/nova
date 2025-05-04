5d10fe8ddc467e069db4bae0bcfee9937db1bbdf
Anthony <anthony@v01dbreaker.cc>
2025-05-04 16:24:47 +1000

    Streaming ROS Image topics to the GUI (#466)

---
+++ src/ros/nova-gui/nova-gui/src/views/urc/URCGazebo.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+import { Button } from "@nextui-org/react";
+import SerialMappedCameraComponent from "../shared/CamerasPage/SerialMappedCameraComponent.tsx";
+import { CameraControlPanelModal } from "../../components/CameraComponent/components/CamerasControlPanelModal";
+import { useCameraStreamer } from "../../components/CameraComponent/hooks/useCameraStreamer";
+
+const URCGazeboView: React.FC = () => {
+  const [controlPanelOpen, setControlPanelOpen] = useState(false);
+  const { refreshAvailabilities } = useCameraStreamer();
+  const closeControlPanel = () => setControlPanelOpen(false);
+  return (
+    <div className="p-3 flex flex-col gap-3">
+      <div>
+        <CameraControlPanelModal showModal={controlPanelOpen} closeModal={closeControlPanel} refreshAvailabilies={refreshAvailabilities} />
+        <Button size="md" color="primary" variant="ghost" className="w-36" onPress={() => setControlPanelOpen(true)}> Control Panel </Button>
+      </div>
+      <div className="grid grid-cols-2 gap-3">
+        <SerialMappedCameraComponent cameraSerial={"oak-rgb"} autostart={true}/>
+        <SerialMappedCameraComponent cameraSerial={"bootie-rgb"} autostart={true}/>
+      </div>
+    </div>
+  );
+};
+
+export default URCGazeboView;
