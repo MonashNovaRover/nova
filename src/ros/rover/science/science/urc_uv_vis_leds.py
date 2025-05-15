a1d660380095edfb5f74e9f1adb62c7d3af61af5
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-05-15 11:22:46 +1000

    Update Pumps and LED ROS nodes (#501)

--- src/ros/rover/science/science/urc_uv_vis_leds.py
+++ src/ros/rover/science/science/urc_uv_vis_leds.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
-
-import time
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Purpose: Control for the URC LEDs in the Vis Spec
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODE: URCUVVisLeds
+TOPICS: None
+SERVICES:
+    - "/science/uv_vis_led_1"
+    - "/science/uv_vis_led_2"
+ACTIONS: None
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE:    science
+AUTHOR(S):	???, Felicity Matthews
+CREATION:	???
+EDITED:		14/05/2025
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 from python_control.controls.ToggleControl import ToggleControl
 from python_control.controllers.ToggleController import ToggleController
-from python_control.sensors.ToggleCommandSensor import ToggleCommandSensor
-from rclpy.executors import MultiThreadedExecutor
 import rclpy
 from python_control.ControllerNode import ControllerNode
 from std_srvs.srv import SetBool
-
 
 class URCUVVisLeds(ControllerNode):
 
@@ -18,19 +30,8 @@ class URCUVVisLeds(ControllerNode):
 
     # SENDING CARD IDS
     # Add any CONTROL FRAME / CARD IDS here
-    LED_1_SEND_FRAME_ID = 0x050
-    LED_2_SEND_FRAME_ID = 0x050
-
-    # RECIEVING CARD IDS
-    # Add any CONTROL FRAME / CARD IDS here
-    LED_RECV_FRAME_ID = 0x451
-
-    # RECV COMMAND IDS
-    # Add any SENSOR command ids here
-    LED_1_COMMAND = 0x01
-    LED_2_COMMAND = 0x02
-    LED_ON_ID = 0x01
-    LED_OFF_ID = 0x00
+    LED_1_SEND_FRAME_ID = 0x0A0
+    LED_2_SEND_FRAME_ID = 0x0A0
 
     # ROS2 SERVICES
     LED_1_SERVICE = "/science/uv_vis_led_1"
@@ -48,10 +49,10 @@ class URCUVVisLeds(ControllerNode):
     
     # CONTROL COMMANDS
     # Add any CONTROL command ids here
-    LED_1_COMMAND_ON = 0x03
-    LED_1_COMMAND_OFF = 0x04
-    LED_2_COMMAND_ON = 0x05
-    LED_2_COMMAND_OFF = 0x06
+    LED_1_COMMAND = 0x04
+    LED_2_COMMAND = 0x05
+    LED_ON_COMMAND = 0x01
+    LED_OFF_COMMAND = 0x00
 
     TIMEOUT = 2.0
 
@@ -59,36 +60,9 @@ class URCUVVisLeds(ControllerNode):
         super(URCUVVisLeds, self).__init__(name="URCUVVisLeds", can_bus=self.CAN_BUS)
         logger = self.get_logger()
 
-        self.bus.set_id_filter([self.LED_RECV_FRAME_ID])
-
-
         # Create publishers
         self.create_service(SetBool, self.LED_1_SERVICE, self.led_1_callback)
         self.create_service(SetBool, self.LED_2_SERVICE, self.led_2_callback)
- 
-
-        # Create Sensors
-        self.led_1_sensor = ToggleCommandSensor(
-            logger=logger,
-            bus=self.bus,
-            frame_id=self.LED_RECV_FRAME_ID,
-            state_id_on=self.LED_ON_ID,
-            state_id_off=self.LED_OFF_ID,
-            control_id=self.LED_1_COMMAND,
-        )
-
-        self.led_2_sensor = ToggleCommandSensor(
-            logger=logger,
-            bus=self.bus,
-            frame_id=self.LED_RECV_FRAME_ID,
-            state_id_on=self.LED_ON_ID,
-            state_id_off=self.LED_OFF_ID,
-            control_id=self.LED_2_COMMAND,
-        )
-
-        # Add sensors to the node
-        self.add_sensor(self.LED_1_NAME, self.led_1_sensor)
-        self.add_sensor(self.LED_2_NAME, self.led_2_sensor)
 
         ## Create controls
         self.led_1 = ToggleControl(
@@ -98,29 +72,30 @@ class URCUVVisLeds(ControllerNode):
             logger=logger,
         )
 
-
         ## Create controllers
         self.led_1_controller = ToggleController(
             logger=logger,
             bus=self.bus,
             frame_id=self.LED_1_SEND_FRAME_ID,
             control=self.led_1,
-            toggle_command_on=self.LED_1_COMMAND_ON,
-            toggle_command_off=self.LED_1_COMMAND_OFF,
+            toggle_command_on=self.LED_ON_COMMAND,
+            toggle_command_off=self.LED_OFF_COMMAND,
+            control_id=self.LED_1_COMMAND,
         )
         self.led_2_controller = ToggleController(
             logger=logger,
             bus=self.bus,
             frame_id=self.LED_2_SEND_FRAME_ID,
             control=self.led_2,
-            toggle_command_on=self.LED_2_COMMAND_ON,
-            toggle_command_off=self.LED_2_COMMAND_OFF,
+            toggle_command_on=self.LED_ON_COMMAND,
+            toggle_command_off=self.LED_OFF_COMMAND,
+            control_id=self.LED_2_COMMAND,
         )
 
-        ## Start the CAN busget_name
+        ## Start the CAN bus
         self.start_can()
 
-    def led_callback(self, request, response, controller: ToggleController, sensor: ToggleCommandSensor, control_name: str):
+    def led_callback(self, request, response, controller: ToggleController, control_name: str):
         self.get_logger().info("LED Callback: {0}".format(request.data))
         control = controller.get_control()
         try:
@@ -129,24 +104,10 @@ class URCUVVisLeds(ControllerNode):
                 response.message = "{0} Started".format(control_name)
             else:
                 control.stop()
-                response.message = "{0} Stopped".format(control_name) 
-         
-  
-            start = time.time()
-            while time.time() - start < self.TIMEOUT and not sensor.get_sensor_value() == request.data:
-                try:
-                    controller.control_send_callback()
-                    frame = self.bus.receive_with_timeout(200)
-                    sensor.callback_function(frame)
-                except OSError as _:
-                    pass
-                time.sleep(0.1)
+                response.message = "{0} Stopped".format(control_name)
 
-            if sensor.get_sensor_value() == request.data:
-                response.success = True
-            else:
-                response.success = False
-                response.message = "Timeout"
+            controller.control_send_callback()
+            response.success = True
 
         except Exception as e:
             self.get_logger().error("Error in led_callback: {0}".format(e))
@@ -156,10 +117,10 @@ class URCUVVisLeds(ControllerNode):
         return response
     
     def led_1_callback(self, request, response):
-        return self.led_callback(request, response, self.led_1_controller, self.led_1_sensor, "LED 1")
+        return self.led_callback(request, response, self.led_1_controller, "LED 1")
 
     def led_2_callback(self, request, response):
-        return self.led_callback(request, response, self.led_2_controller, self.led_2_sensor, "LED 2")
+        return self.led_callback(request, response, self.led_2_controller,"LED 2")
         
             
 def main():
