5146d6fb0b9551d6109023eb3de63e6aac766286
Monash Nova Rover <novaroverteam@monash.edu>
2025-05-19 15:30:01 +1000

    Add ArePosesNearCondition node and update related configs, tested and seems to work on current_pose

--- src/ros/rover/nav2_autonomous/nova_behavior_tree/include/nova_behavior_tree/condition/are_poses_near_condition.hpp
+++ src/ros/rover/nav2_autonomous/nova_behavior_tree/include/nova_behavior_tree/condition/are_poses_near_condition.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Samsung Research America
+// Copyright (c) 2025 Open Navigation LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,85 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * @brief Condition node for checking whether two poses are nearby. If the input
- * poses are in different frames, it will automatically transform both to the global frame.
- * 
- * Ported over from Nav2 main branch.
- * 
- * @authors Terry Tian
- */
-
-#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__ARE_POSES_NEAR_CONDITION_HPP_
-#define NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__ARE_POSES_NEAR_CONDITION_HPP_
+#ifndef NOVA_BEHAVIOR_TREE__PLUGINS__CONDITION__ARE_POSES_NEAR_CONDITION_HPP_
+#define NOVA_BEHAVIOR_TREE__PLUGINS__CONDITION__ARE_POSES_NEAR_CONDITION_HPP_
 
 #include <string>
 #include <memory>
 
-#include "behaviortree_cpp/condition_node.h"
-#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "behaviortree_cpp/condition_node.h"
 #include "tf2_ros/buffer.h"
+#include "nav2_behavior_tree/bt_utils.hpp"
 
 namespace nova_behavior_tree
 {
 
-  class ArePosesNearCondition : public BT::ConditionNode
+/**
+ * @brief A BT::ConditionNode that returns SUCCESS when a specified goal
+ * is reached and FAILURE otherwise
+ */
+class ArePosesNearCondition : public BT::ConditionNode
+{
+public:
+  /**
+   * @brief A constructor for nav2_behavior_tree::ArePosesNearCondition
+   * @param condition_name Name for the XML tag for this node
+   * @param conf BT node configuration
+   */
+  ArePosesNearCondition(
+    const std::string & condition_name,
+    const BT::NodeConfiguration & conf);
+
+  /**
+   * @brief A destructor for nav2_behavior_tree::ArePosesNearCondition
+   */
+  ~ArePosesNearCondition() override = default;
+
+  /**
+   * @brief The main override required by a BT action
+   * @return BT::NodeStatus Status of tick execution
+   */
+  BT::NodeStatus tick() override;
+
+  /**
+   * @brief Function to read parameters and initialize class variables
+   */
+  void initialize();
+
+  /**
+   * @brief Checks if the current robot pose lies within a given distance from the goal
+   * @return bool true when goal is reached, false otherwise
+   */
+  bool arePosesNearby();
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing node-specific ports
+   */
+  static BT::PortsList providedPorts()
   {
-  public:
-    /**
-     * @brief A constructor for nova_behavior_tree::ARTagDetectedCondition
-     * @param condition_name Name for the XML tag for this node
-     * @param conf BT node configuration
-     */
-    ArePosesNearCondition(
-        const std::string &condition_name,
-        const BT::NodeConfiguration &conf);
+    return {
+      BT::InputPort<geometry_msgs::msg::PoseStamped>("ref_pose", "Destination"),
+      BT::InputPort<geometry_msgs::msg::PoseStamped>("target_pose", "Destination"),
+      BT::InputPort<std::string>("global_frame", "Global frame"),
+      BT::InputPort<double>("tolerance", 0.5, "Tolerance")
+    };
+  }
 
-    ArePosesNearCondition() = delete;
+private:
+  rclcpp::Node::SharedPtr node_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  double transform_tolerance_;
+  std::string global_frame_;
+};
 
-    /**
-     * @brief Function to initialize variables,
-     * called only once in the lifecycle of the BT
-     */
-    void initialize();
+}  // namespace nova_behavior_tree
 
-    /**
-     * @brief The main override required by a BT node
-     * @return BT::NodeStatus Status of tick execution
-     */
-    BT::NodeStatus tick() override;
-
-    /**
-     * @brief Creates list of BT ports
-     * @return BT::PortsList Containing node-specific ports
-     */
-    static BT::PortsList providedPorts()
-    {
-      return {
-        BT::InputPort<geometry_msgs::msg::PoseStamped>("from_pose", "From pose"),
-        BT::InputPort<geometry_msgs::msg::PoseStamped>("to_pose", "Second pose"),
-        BT::InputPort<std::string>("global_frame", "Global frame"),
-        BT::InputPort<double>("tolerance", "Tolerance"),
-      };
-    }
-
-  private:
-    /**
-     * @brief Checks if the current robot pose lies within a given distance from the goal
-     * @return bool true when goal is reached, false otherwise
-     */
-    bool are_poses_nearby();
-
-    rclcpp::Node::SharedPtr node_;
-    std::shared_ptr<tf2_ros::Buffer> tf_;
-
-    double transform_tolerance_;
-    std::string global_frame_;
-    
-    bool initialized_ = false;
-  };
-
-} // namespace nova_behavior_tree
-
-#endif // NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__ARE_POSES_NEAR_CONDITION_HPP_
+#endif  // NOVA_BEHAVIOR_TREE__PLUGINS__CONDITION__ARE_POSES_NEAR_CONDITION_HPP_
