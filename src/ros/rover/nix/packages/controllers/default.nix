53a6756df2984b288be03c48d0b38fa473497f40
Bailey Chessum <bailey.chessum1@gmail.com>
2025-05-20 17:59:27 +1000

    delete: nova_ik_controller

--- src/ros/rover/nix/packages/controllers/default.nix
+++ src/ros/rover/nix/packages/controllers/default.nix
@@ -7,7 +7,6 @@ with pkgs;
   nova-strafe-controller = callPackage ./strafe-controller { };
   nova-diff-drive-controller = callPackage ./nova-diff-drive-controller { };
   nova-arm-controller = callPackage ./nova-arm-controller { };
-  nova-ik-controller = callPackage ./nova-ik-controller { };
   nova-twistmapper = callPackage ./nova-twistmapper { };
   nova-path-planner = callPackage ./nova-path-planner { };
   nova-banksia-kinematics-plugin = callPackage ./nova-banksia-kinematics-plugin { };
