f7c79ed28b7e8f47a816bf5a84c837bd9cc3a483
Anthony Lew <anthony@v01dbreaker.cc>
2025-05-21 16:36:33 +1000

    GUI updates, including info screen

--- src/ros/nova-gui/nova-gui/src/components/AutoTyping/AutoTypingKeyEntryWidget.tsx
+++ src/ros/nova-gui/nova-gui/src/components/AutoTyping/AutoTypingKeyEntryWidget.tsx
@@ -1,44 +1,54 @@
 import React, {useCallback, useState} from "react";
-import {Button, Input, CardProps} from "@nextui-org/react";
+import {Button, Input, CardProps, Card} from "@nextui-org/react";
 import {useBifrost} from "../../redux/actions/bifrost/useBifrostAction.ts";
 import {RosService} from "../../ros/services/rosService.ts";
-import { Square, Power } from "react-feather";
+import { Square, Power, HelpCircle } from "react-feather";
 
 interface IAutoTypingKeyEntryWidgetProps extends CardProps {
+  showHelp: ()=>void;
 }
 
 /**
- * Widget controlling auto typing transforms on service "/arm/keyboard/transform_toggle"
+ * Widget which takes input and controls the auto typing sequencer node.
  * @param cardProps
  * @constructor
  */
-const AutoTypingKeyEntryWidget: React.FC<IAutoTypingKeyEntryWidgetProps> = () => {
+const AutoTypingKeyEntryWidget: React.FC<IAutoTypingKeyEntryWidgetProps> = (props) => {
   const startTypingBifrost = useBifrost({ service: RosService.START_AUTO_TYPING });
   const stopTypingBifrost = useBifrost({ service: RosService.STOP_AUTO_TYPING });
 
-  const startTyping = (sequence:string) => startTypingBifrost.callServiceToRedux({sequence: sequence});
+  const startTyping = (sequence:Array<string>) => startTypingBifrost.callServiceToRedux({sequence: sequence});
   const stopTyping = () => stopTypingBifrost.callServiceToRedux({});
   const onInput = useCallback((event: React.ChangeEvent<HTMLInputElement>)=>{
-    setSequence(event.target.value)
+    setSequence(event.target.value.replace(/\s+/g, ' ').trim().split(" ")) // split string by spaces
   }, [])
-  const [sequence, setSequence] = useState("");
+  const [sequence, setSequence] = useState([""]);
+
+
   return (
-    <div className="flex flex-row justify-between gap-5">
-      <Button className="w-1/3 text-h1 h-12" color="primary" 
-        onClick={() => startTyping(sequence)}>
-          START TYPING
-          <Power size="15"/>
-      </Button>
-      <Input size="lg" className="flex-grow w-1/3" 
-        placeholder="What do you want to type?" 
-        onChange={onInput}>
-      </Input>
-      <Button className="w-1/3 text-h1 h-12" color="danger"
-        onClick={stopTyping}>
-          STOP TYPING
-          <Square size="15" fill="white"/>
-      </Button>
-    </div>
+    <Card className="row-2 gap-2 p-2">
+      <div className="flex ">Separate keys with&nbsp;<b>spaces.</b>&nbsp;Type as seen on keyboard in&nbsp;<b>lowercase.</b>&nbsp;Click the&nbsp;<b>?</b>&nbsp;button for key info.</div> 
+      <div className="flex flex-row justify-between gap-2">
+        <Button className="w-3/12 text-h1 h-12" color="success" 
+          onClick={() => startTyping(sequence)}>
+            START TYPING
+            <Power size="15"/>
+        </Button>
+        <Input size="lg" className="flex-grow w-6/12" 
+          placeholder="What do you want to type?" 
+          onChange={onInput}>
+        </Input>
+        <Button className="w-3/12 text-h1 h-12" color="danger"
+          onClick={stopTyping}>
+            STOP TYPING
+            <Square size="15" fill="white"/>
+        </Button>
+        <Button className="w-1/12 text-h1 h-12" color="primary"
+          onClick={props.showHelp}>
+            <HelpCircle/>
+        </Button>
+      </div>
+    </Card>
   );
 }
 
---
+++ src/ros/nova-gui/nova-gui/src/components/AutoTyping/AutoTypingModal.tsx
@@ -0,0 +1,27 @@
+import { Modal, ModalBody, ModalContent, ModalHeader} from "@nextui-org/react";
+import guide from '../../assets/auto-typing-keyboard-guide.png';
+
+interface IAutoTypingModalProps {
+  showModal: boolean;
+  closeModal: () => void;
+}
+
+const AutoTypingModal: React.FC<IAutoTypingModalProps>= (props) => {
+  return (
+    <Modal
+      isOpen={props.showModal}
+      className="dark text-foreground"
+      onClose={props.closeModal}
+      size="5xl"
+    >
+      <ModalContent>
+        <ModalHeader className="flex flex-col gap-1">Auto Typing Keyboard Guide</ModalHeader>
+        <ModalBody>
+          <img src={guide} alt="Auto Typing Key Guide"/>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default AutoTypingModal;
