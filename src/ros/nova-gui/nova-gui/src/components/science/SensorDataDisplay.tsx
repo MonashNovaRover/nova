8058a9bf94d6bf3f31ef9cc98a5496b8f202a41d
Felicity Matthews <felicitymatthews30@gmail.com>
2025-05-24 09:21:25 +1000

    small refactor

--- src/ros/nova-gui/nova-gui/src/components/science/SensorDataDisplay.tsx
+++ src/ros/nova-gui/nova-gui/src/components/science/SensorDataDisplay.tsx
@@ -3,7 +3,7 @@ import {OverlayedProgress} from "../../OverlayedProgress/OverlayedProgress.tsx";
 import {zip} from "lodash";
 
 export interface NTCDataWidgetProps {
-  tempReadings: number[]
+  values: number[]
   labels: string[]
   suffixes: string[]
 }
@@ -12,22 +12,22 @@ const toTemp = (x: number) => 6 * x
 
 /**
  * Component to display data in the form of overlayed progress bars.
- * @param tempReadings a list of values to display
+ * @param values a list of values to display
  * @param labels labels of each value
  * @param suffixes suffixes to be displayed after each value (units eg "°C")
  * @constructor
  */
-const NTCData: React.FC<NTCDataWidgetProps> = ({tempReadings, labels, suffixes}) => {
-  const displayList = useMemo(() => zip(tempReadings, labels, suffixes), [tempReadings, labels, suffixes])
+const SensorDataDisplay: React.FC<NTCDataWidgetProps> = ({values, labels, suffixes}) => {
+  const displayList = useMemo(() => zip(values, labels, suffixes), [values, labels, suffixes])
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      {displayList.map(([reading, label, suffix]) => {
-        if (reading !== undefined && label && suffix) {
+      {displayList.map(([value, label, suffix]) => {
+        if (value !== undefined && label && suffix) {
           return (
             <div key={label} className="text-center">
-              <OverlayedProgress key={`progress-${label}`} aria-label={label} size="lg" label={label} value={toTemp(reading)}>
-                {toTemp(reading)} {suffix}
+              <OverlayedProgress key={`progress-${label}`} aria-label={label} size="lg" label={label} value={toTemp(value)}>
+                {toTemp(value)} {suffix}
               </OverlayedProgress>
             </div>
           );
@@ -38,4 +38,4 @@ const NTCData: React.FC<NTCDataWidgetProps> = ({tempReadings, labels, suffixes})
   )
 }
 
-export default NTCData
+export default SensorDataDisplay
