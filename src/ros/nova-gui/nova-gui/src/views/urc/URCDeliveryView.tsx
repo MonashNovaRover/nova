c61ee4fe28fa7160710a7ef2e29746ab2c57c0bd
Felicity Matthews <felicitymatthews30@gmail.com>
2025-05-26 09:54:21 +1000

    add large camera view

--- src/ros/nova-gui/nova-gui/src/views/urc/URCDeliveryView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/urc/URCDeliveryView.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import SerialMappedCameraComponent from "../shared/CamerasPage/SerialMappedCameraComponent.tsx";
+import {CameraSerials} from "../shared/CamerasPage/CameraPageConstants.tsx";
 
 const URCDeliveryView: React.FC = () => {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className="grid grid-cols-2 gap-3 m-3">
+      <SerialMappedCameraComponent cameraSerial={CameraSerials.ARM_END_PERISCOPE}/>
+      <SerialMappedCameraComponent cameraSerial={CameraSerials.ARM_END_TOP}/>
     </div>
   );
 };
