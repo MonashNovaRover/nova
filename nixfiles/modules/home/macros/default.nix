941cb0eb3f0b03873b510f69f8d9413361181562
Felicity Matthews <felicitymatthews30@gmail.com>
2025-05-27 10:18:37 +1000

    add alias

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -168,6 +168,9 @@ in
           launch-teleop = "~/Builds/auto/bin/ros2 launch telop_drive_joy teleop.launch.py";
           launch-rviz = "~/Builds/auto/bin/ros2 launch auto_bringup rviz.launch.py";
 
+          # GPS Alias
+          launch-gps = "~/Builds/master/bin/ros2 launch nova_bringup rover_gps.launch.py";
+
         }
       ];
 
