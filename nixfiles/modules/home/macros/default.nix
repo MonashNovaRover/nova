6bcb64a29556862942fd666fecbe7a6a13d8069d
Bailey Chessum <bailey.chessum1@gmail.com>
2025-05-28 14:18:47 +1000

    feat: launch improvements for path planning

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -183,15 +183,19 @@ in
           # Arm Aliases
           launch-typing = "~/Builds/master/bin/ros2 launch arm_bringup typing.launch.py";
           launch-arm-control = "~/Builds/master/bin/ros2 launch arm_bringup control.launch.py arm:=False old_arm:=True";
+          launch-path-control = "~/Builds/master/bin/ros2 launch arm_bringup path.control.launch.py arm:=False old_arm:=True";
           launch-arm-urdf = "~/Builds/master/bin/ros2 launch arm_bringup urdf.launch.py arm:=False old_arm:=True auto_camera:=False";
           launch-arm-teleop = "~/Builds/master/bin/ros2 launch teleop_arm_joy teleop.launch.py";
-          run-arm-teleop = "~/Builds/master/bin/ros2 run teleop_arm_joy teleop.launch.py";
+          run-arm-teleop = "~/Builds/master/bin/ros2 run teleop_arm_joy teleop_arm_joy_node";
+          run-arm-teleop-xbox = "~/Builds/master/bin/ros2 run teleop_arm_joy teleop_arm_joy_node ${cfg.sourceDir}/ros/rover/teleop_arm_joy/config/old.xbox.config.yaml";
           run-joy = "~/Builds/master/bin/ros2 run joy joy_node";
 
           # ros2_control Aliases
           controllers-list = "~/Builds/master/bin/ros2 control list_controllers";
           controllers-set = "~/Builds/master/bin/ros2 control set_controller_state";
-          
+          activate-path = "~/Builds/master/bin/ros2 control set_controller_state nova_path_planner active";
+          deactivate-path = "~/Builds/master/bin/ros2 control set_controller_state nova_path_planner inactive";
+
           # Ducket Aliases
           ducket-pos = "cansend can0 0C1#7000";
           ducket-neg = "cansend can0 0C1#9000";
