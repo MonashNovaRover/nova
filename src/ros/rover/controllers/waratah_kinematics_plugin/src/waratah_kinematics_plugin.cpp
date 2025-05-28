09fa06d91ea64c04d85ec0259373345dda1c9cb5
Orlando Chamberlain <ocha0025@student.monash.edu>
2025-05-28 23:37:34 +1000

    waratah_kinematics: fixes and logging
    a few fixes for the kinematics where I copied the formula wrong.
    Remember that MATLAB uses 1 indexed arrays!
    
    We print the requested transform matrix and resulting angles for
    debugging reasons, please change these from INFO to DEBUG once its
    working.
    
    Main issue is that the solution seems to be about 20cm higher in z than
    the request. Otherwise I think its working ok?

--- src/ros/rover/controllers/waratah_kinematics_plugin/src/waratah_kinematics_plugin.cpp
+++ src/ros/rover/controllers/waratah_kinematics_plugin/src/waratah_kinematics_plugin.cpp
@@ -66,6 +66,8 @@ namespace waratah_kinematics_plugin
     auto y = origin.getY();
     auto z = origin.getZ();
 
+    auto logger = node_.lock()->get_logger();
+
     tf2::Matrix3x3 rotated_basis = pose.getBasis() * ENDEFFECTOR_BASIS_INVERSE;
 
     double l1r = lengths[0];
@@ -89,6 +91,12 @@ namespace waratah_kinematics_plugin
     t07i(1, 3) = y;
     t07i(2, 3) = z;
 
+    RCLCPP_INFO(logger, "target:");
+    RCLCPP_INFO(logger, "[%+5.3f, %+5.3f, %+5.3f, %+5.3f; ...", t07i(0,0), t07i(0,1), t07i(0,2), t07i(0,3));
+    RCLCPP_INFO(logger, " %+5.3f, %+5.3f, %+5.3f, %+5.3f; ...", t07i(1,0), t07i(1,1), t07i(1,2), t07i(1,3));
+    RCLCPP_INFO(logger, " %+5.3f, %+5.3f, %+5.3f, %+5.3f; ...", t07i(2,0), t07i(2,1), t07i(2,2), t07i(2,3));
+    RCLCPP_INFO(logger, " %+5.3f, %+5.3f, %+5.3f, %+5.3f]",     t07i(3,0), t07i(3,1), t07i(3,2), t07i(3,3));
+
 #define T01(j1) sub_dh(0,       0,   0,   j1        )
 #define T12(j2) sub_dh(M_PI/2,  0,   0,   j2        )
 #define T23(j3) sub_dh(0,       l1r, 0,   j3        )
@@ -97,42 +105,11 @@ namespace waratah_kinematics_plugin
 #define T56(j6) sub_dh(M_PI/2,  0,   0,   j6        )
 #define T67()   sub_dh(0,       0,   a3r, 0         )
 
-    //Eigen::Matrix4d t67 { {1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, l3}, {0, 0, 0, 1} };
-    //Eigen::Matrix4d t67 = sub_dh(0, 0, a3, 0);
     Eigen::Matrix4d t0_wrist = t07i * T67().inverse();
 
     double wrist_x = t0_wrist(0, 3);
     double wrist_y = t0_wrist(1, 3);
-    double wrist_z = t0_wrist(2, 3);
-
-
-    /*double j3a = acos((pow(l, 2) + pow(wrist_z, 2) - pow(l1r, 2) - pow(l2r, 2)) / (2 * l1r * l2r));
-    double j3b = -j3a; // expands to -acos((l^2+wrist_z^2-L1r^2-L2r^2)/(2*L1r*L2r)) as per keenan's notes
-    double j3ao = j3a + M_PI / 2;
-    double j3bo = j3b + M_PI / 2;
-
-    double k1a = l1r + l2r * cos(j3a);
-    double k2a = l2r * sin(j3a);
-    double k1b = l1r + l2r * cos(j3b);
-    double k2b = l2r * sin(j3b);
-
-    double j2a = atan2(wrist_z, l) - atan2(k2a, k1a);
-    double j2b = atan2(wrist_z, l) - atan2(k2b, k1b);
-    double j2ao = j2a - M_PI / 2;
-    double j2bo = j2b - M_PI / 2;*/
-
-
-    /*Eigen::Matrix4d t12 = sub_dh(M_PI / 2, 0, 0, j2bo);
-    Eigen::Matrix4d t23 = sub_dh(0, l1r, 0, j3bo);
-    Eigen::Matrix4d t34 = sub_dh(0, l2r, a1, j4bo);
-    Eigen::Matrix4d t45 = sub_dh(-pi/2, 0, a2, j5+pi/2);
-    Eigen::Matrix4d t56 = sub_dh(pi/2, 0, a2, j6);*/
-    //Eigen::Matrix4d t02 = t01 * t12;
-    //Eigen::Matrix4d t03_wrist = t02 * t23;
-    //Eigen::Matrix3d r03_wrist = t03_wrist.topLeftCorner<3, 3>(); // R03_wrist = T03_wrist(1:3,1:3); in matlab
-    //Eigen::Matrix3d r07r = t07r.topLeftCorner<3, 3>(); // see above
-    //Eigen::Matrix3d r37r = r03_wrist.inverse() * r07r;
-
+    //double wrist_z = t0_wrist(2, 3); // unused
 
     // J1 Solution
     double beta = atan2(wrist_y, wrist_x);
@@ -143,41 +120,40 @@ namespace waratah_kinematics_plugin
     // Remove J1 now that it is solved
     Eigen::Matrix4d t01c = T01(j1);
     Eigen::Matrix4d t17c = t01c.inverse() * t07i;
-    //Eigen::Matrix4d t17 = t12*t23*t34*t45*t56*t67;
 
     // J56 Solution
-    double j5 = atan2((t17c(2,4)+a1r)/a3r,sqrt(pow(t17c(2,1),2)+pow(t17c(2,2),2)));
-    double j6 = atan2(-t17c(2,2),t17c(2,1));
+    double j5 = atan2((t17c(1,3)+a1r)/a3r,sqrt(pow(t17c(1,0),2)+pow(t17c(1,1),2)));
+    double j6 = atan2(-t17c(1,1),t17c(1,0));
 
     // Remove J56 now that they are solved
     Eigen::Matrix4d t47c = T45(j5)*T56(j6)*T67();
     Eigen::Matrix4d t14c = t01c.inverse() * t07i * t47c.inverse();
-    //Eigen::Matrix4d t14 = t12*t23*t34;;
-
 
     // J3 Solution
-    double b3 = pow(t14c(1,4),2)+pow(t14c(3,4),2) - pow(l2r,2) / (2*l1r*l2r); 
+    double b3 = (pow(t14c(0,3),2)+pow(t14c(2,3),2) - pow(l2r,2) - pow(l1r,2))  / (2*l1r*l2r); 
     double j3a = -atan2(sqrt(1-pow(b3,2)), b3);
     double j3b =  atan2(sqrt(1-pow(b3,2)), b3);
 
     // J2 Solution
-    double j2a = atan2(t14c(3,4),t14c(1,4))-atan2(l2r*sin(j3a),l1r+l2r*cos(j3a));
-    double j2b = atan2(t14c(3,4),t14c(1,4))-atan2(l2r*sin(j3b),l1r+l2r*cos(j3b));
+    double j2a = atan2(t14c(2,3),t14c(0,3))-atan2(l2r*sin(j3a),l1r+l2r*cos(j3a));
+    double j2b = atan2(t14c(2,3),t14c(0,3))-atan2(l2r*sin(j3b),l1r+l2r*cos(j3b));
 
     // J4 solution
     Eigen::Matrix4d t03ca = T01(j1)*T12(j2a)*T23(j3a);
     Eigen::Matrix4d t34ca = t03ca.inverse() * t07i * t47c.inverse();
-    double j4a = atan2(t34ca(2,1), t34ca(1,1));
+    double j4a = atan2(t34ca(1,0), t34ca(0,0));
     
     Eigen::Matrix4d t03cb = T01(j1)*T12(j2b)*T23(j3b);
     Eigen::Matrix4d t34cb = t03cb.inverse() * t07i * t47c.inverse();
-    double j4b = atan2(t34cb(2,1), t34cb(1,1));
+    double j4b = atan2(t34cb(1,0), t34cb(0,0));
 
-    // TODO: Needs to be in the same order as when they get put in a joint group
-    std::array<double, 6> new_joints_a = { j1, j2a, j3a, j4a, j5, j6 };
-    std::array<double, 6> new_joints_b = { j1, j2b, j3b, j4b, j5, j6 };
+    std::array<double, 6> new_joints_a = { j1, j2a - M_PI_2, j3a, j4a, j5, j6};
+    std::array<double, 6> new_joints_b = { j1, j2b - M_PI_2, j3b, j4b, j5, j6};
     // TODO: pick which solution to use smartly
-    // TODO: may need to add offsets  to some joints
+    
+#define DEG(x) x*360/(2*M_PI)
+    RCLCPP_INFO(logger, "solution: [%+07.2f %+07.2f %+07.2f  %+07.2f %+07.2f %+07.2f]", DEG(j1), DEG(j2a), DEG(j3a), DEG(j4a), DEG(j5), DEG(j6));
+
     return new_joints_a;
   }
 
