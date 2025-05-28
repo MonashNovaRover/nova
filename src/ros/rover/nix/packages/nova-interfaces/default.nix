5939389be74e70e9a6a36049dce4f541d12c1fd0
Felicity Matthews <felicitymatthews30@gmail.com>
2025-05-29 03:47:17 +1000

    update gui

--- src/ros/rover/nix/packages/nova-interfaces/default.nix
+++ src/ros/rover/nix/packages/nova-interfaces/default.nix
@@ -6,6 +6,7 @@
 , geometry-msgs
 , nav-msgs
 , action-msgs
+, sensor-msgs
 }:
 
 buildRosPackage {
@@ -23,6 +24,7 @@ buildRosPackage {
     std-msgs 
     nav-msgs 
     geometry-msgs 
-    action-msgs 
+    action-msgs
+    sensor-msgs
   ];
 }
