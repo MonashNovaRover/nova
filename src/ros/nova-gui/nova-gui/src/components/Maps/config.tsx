45f8215fafcc9b76433a95a55aefbd12741d347a
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-05-29 15:27:23 +1000

    Feat/maps config (#555)

--- src/ros/nova-gui/nova-gui/src/components/Maps/config.tsx
+++ src/ros/nova-gui/nova-gui/src/components/Maps/config.tsx
@@ -2,8 +2,13 @@
 
 // This is the Name that comes with the MBTiles File Itself
 export enum MapTile {
-  Hanksville = "Hanksville",
+  DeliveryHQ = "Delivery_HQ",
+  DeliveryLQ = "Delivery_LQ",
+  ESAHQ = "ESA_HQ",
+  ESALQ = "ESA_LQ",
   MDRS = "MDRS_Hi_Res",
+  MDRSBigBoy = "MDRS_HI_Res_Big_boi",
+  Hanksville = "Hanksville",
   Monash = "MonashClayton",
   PolicePaddocks = "PolicePaddocks",
   Mordiallic = "MordiallocHorseBeach",
@@ -16,6 +21,7 @@ export enum MapTile {
 export const MAP_BOUNDS:  {  [key in MapTile]: [number, number, number, number] }= {
   [MapTile.Hanksville]: [-110.747, 38.3092, -110.626, 38.3991],
   [MapTile.MDRS]: [-110.809, 38.3917, -110.765, 38.4177],
+  [MapTile.MDRSBigBoy]: [-110.839,38.3723,-110.713,38.4569],
   [MapTile.Monash]: [145.124,-37.9199,145.148,-37.9034],
   [MapTile.PolicePaddocks]: [145.223,-37.9713,145.268,-37.9387],
   [MapTile.Mordiallic]: [145.077,-38.0106,145.089,-38.0019],
