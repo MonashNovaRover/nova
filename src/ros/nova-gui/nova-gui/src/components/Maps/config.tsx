1b6efe539322e55f751c9831f858b2f04132038e
Felicity Matthews <116486687+FelicityMatt@users.noreply.github.com>
2025-05-29 11:44:41 +1000

    Add ED and ESA Map configs (#553)

--- src/ros/nova-gui/nova-gui/src/components/Maps/config.tsx
+++ src/ros/nova-gui/nova-gui/src/components/Maps/config.tsx
@@ -7,6 +7,10 @@ export enum MapTile {
   Monash = "MonashClayton",
   PolicePaddocks = "PolicePaddocks",
   Mordiallic = "MordiallocHorseBeach",
+  DeliveryHQ = "Delivery_HQ",
+  DeliveryLQ = "Delivery_LQ",
+  ESAHQ = "ESA_HQ",
+  ESALQ = "ESA_LQ",
 }
 
 export const MAP_BOUNDS:  {  [key in MapTile]: [number, number, number, number] }= {
@@ -15,4 +19,8 @@ export const MAP_BOUNDS:  {  [key in MapTile]: [number, number, number, number] 
   [MapTile.Monash]: [145.124,-37.9199,145.148,-37.9034],
   [MapTile.PolicePaddocks]: [145.223,-37.9713,145.268,-37.9387],
   [MapTile.Mordiallic]: [145.077,-38.0106,145.089,-38.0019],
+  [MapTile.DeliveryHQ]: [-110.793,38.4059,-110.786,38.4135],
+  [MapTile.DeliveryLQ]: [-110.797,38.4,-110.781,38.416],
+  [MapTile.ESAHQ]: [-110.797,38.4135,-110.778,38.4264],
+  [MapTile.ESALQ]: [-110.802,38.398,-110.772,38.4311],
 };
