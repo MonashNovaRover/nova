332c3a0c875f495864e5d3bea839ebf198c579a6
Victor Bartlinski <vicbart08@gmail.com>
2025-05-30 08:37:55 +1000

    fixing duplicate gps launch

--- src/ros/rover/auto_bringup/launch/hardware.launch.py
+++ src/ros/rover/auto_bringup/launch/hardware.launch.py
@@ -18,20 +18,12 @@ from launch_ros.substitutions import FindPackageShare
 
 def launch_setup(context, *args, **kwargs):
     auto_bringup_dir = FindPackageShare('auto_bringup')
-    nova_bringup_dir = FindPackageShare('nova_bringup')
 
-    gps_params = LaunchConfiguration('gps_params')
     controller_params = LaunchConfiguration('controller_params')
     front_params = LaunchConfiguration('front_params')
     back_params = LaunchConfiguration('back_params')
 
     return [
-        IncludeLaunchDescription(
-            launch_description_source=PythonLaunchDescriptionSource(PathJoinSubstitution([nova_bringup_dir, 'launch', 'gps_rover.launch.py'])),
-            launch_arguments={
-                'gps_params': gps_params,
-            }.items(),
-        ),
         IncludeLaunchDescription(
             launch_description_source=PythonLaunchDescriptionSource(PathJoinSubstitution([auto_bringup_dir, 'launch', 'control.launch.py'])),
             launch_arguments={
@@ -49,14 +41,8 @@ def launch_setup(context, *args, **kwargs):
 
 def generate_launch_description():
     auto_bringup_dir = FindPackageShare('auto_bringup')
-    nova_bringup_dir = FindPackageShare('nova_bringup')
 
     declared_arguments = [
-        DeclareLaunchArgument(
-            name='gps_params',
-            default_value=PathJoinSubstitution([nova_bringup_dir, 'params', 'gps.yaml']),
-            description='Absolute path to GPS params file',
-        ),
         DeclareLaunchArgument(
             name='controller_params',
             default_value=PathJoinSubstitution([auto_bringup_dir, 'params', 'controllers.yaml']),
