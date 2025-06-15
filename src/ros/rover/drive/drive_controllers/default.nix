e12aa920ecf2efa135e30c1ccaa1fa913051d074
Terry Tian <97772315+tytia@users.noreply.github.com>
2025-06-15 15:32:09 +1000

    new package nova_controller_common can't be found with cmake find_package for some reason

--- src/ros/rover/drive/drive_controllers/default.nix
+++ src/ros/rover/drive/drive_controllers/default.nix
@@ -3,6 +3,7 @@
 with pkgs;
 
 {
+  nova-controller-common = callPackage ./nova_controller_common { };
   nova-pivot-drive-controller = callPackage ./pivot_drive_controller { };
   nova-strafe-controller = callPackage ./strafe_controller { };
   nova-diff-drive-controller = callPackage ./nova_diff_drive_controller { };
