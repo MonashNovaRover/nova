1bab893afa780a5149b39df98092c108cdcbe16c
Bailey Chessum <bailey.chessum1@gmail.com>
2025-07-02 22:42:10 +1000

    fix: changing routes would need a manual refresh of the page
    Signed-off-by: Bailey Chessum <bailey.chessum1@gmail.com>

--- src/ros/nova-gui/nova-gui/src/root.tsx
+++ src/ros/nova-gui/nova-gui/src/root.tsx
@@ -1,21 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
-import configureRootStore from "./redux/store/configureRootStore";
-import { Provider } from "react-redux";
 import { RosRoot } from "./RosRoot";
-import { PersistGate } from "redux-persist/integration/react";
 
 export const Root = () => {
   const navigate = useNavigate();
-  const {store, persistor} = configureRootStore();
 
   return (
     <NextUIProvider navigate={navigate}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
           <RosRoot/>
-        </PersistGate>
-      </Provider>
     </NextUIProvider>
   );
 };
