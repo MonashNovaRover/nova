010bea8410879a795d53d750a297f8fd19c4a3f5
Monash Nova Rover <novaroverteam@monash.edu>
2025-07-18 21:11:23 +1000

    Nvidia drivers for users

--- nixfiles/modules/nixos/common/default.nix
+++ nixfiles/modules/nixos/common/default.nix
@@ -10,5 +10,6 @@
     ./users
     ./virtualisation
     ./workspace
+    ./nvidia
   ];
 }
--- nixfiles/modules/nixos/common/nvidia/default.nix
+++ nixfiles/modules/nixos/common/nvidia/default.nix
@@ -1,44 +1,51 @@
-{ pkgs, config, ... }:
+{ pkgs, config, libs, ... }:
 
 {
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [ nvidia-vaapi-driver ];
-  };
 
-  boot.kernelModules = [
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
-    "i2c-nvidia_gpu"
-  ];
+
+
+# Enable OpenGL
+  hardware.graphics.enable = true;
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
+
     # Modesetting is required.
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
+	# accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = { 
+		# Make sure to use the correct Bus ID values for your system!
+		  amdgpuBusId = "PCI:04:00:0";
+		  nvidiaBusId = "PCI:01:00:0";
+
+      sync.enable = true;
+      offload.enable = false;
+	};
+
   };
+
 }
