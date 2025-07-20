94ffa25ec4c5f3a0b3624ffbaa8e0bb85923c522
Bailey Chessum <bailey.chessum1@gmail.com>
2025-07-20 18:13:00 +1000

    feat: delete teleop_arm_joy
    Signed-off-by: Bailey Chessum <bailey.chessum1@gmail.com>

--- nixfiles/packages/ros/nova-workspace/default.nix
+++ nixfiles/packages/ros/nova-workspace/default.nix
@@ -41,7 +41,6 @@
 , nova-strafe-controller ? throw "nova-strafe-controller is needed, but not available!"
 , nova-diff-drive-controller ? throw "nova-diff-drive-controller is needed, but not available!"
 , nova-teleop-drive-joy ? throw "nova-teleop-drive-joy is needed, but not available!"
-, nova-teleop-arm-joy ? throw "nova-teleop-arm-joy is needed, but not available!"
 , nova-gazebo ? throw "nova-gazebo is needed, but not available!"
 , nova-python-control ? throw "python-control is needed, but not available!"
 , nova-excavation-construction ? throw "excavation-construction is needed, but not available!"
@@ -75,7 +74,6 @@
       nova-strafe-controller
       nova-diff-drive-controller
       nova-teleop-drive-joy
-      nova-teleop-arm-joy
       nova-gui
       nova-drive
       nova-drive-interfaces
