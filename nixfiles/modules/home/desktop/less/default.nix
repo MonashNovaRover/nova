853272eca2ec39979661b7a9a9746722498b93ad
Monash Nova Rover <novaroverteam@monash.edu>
2025-07-22 16:59:09 +1000

    Fixed yazi warning and less highlighting

--- nixfiles/modules/home/desktop/less/default.nix
+++ nixfiles/modules/home/desktop/less/default.nix
@@ -1,16 +1,19 @@
-{ lib, config, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   cfg = config.nova.desktop;
 in
 {
   config = lib.mkIf cfg.enable {
-    environment = {
-      systemPackages.pkgs.sourceHighlight = true;
-      variables = {
-        LESSOPEN = "/run/current-system/sw/bin/src-hilite-lesspipe.sh %s";
-        LESS = " -R ";
-      };
+    home.packages = with pkgs; [
+      sourceHighlight
+    ];
+    programs.less = {
+      enable = true;
+      package = pkgs.less;
+    };
+    home.sessionVariables = {
+      LESSOPEN = "${pkgs.sourceHighlight}/bin/src-hilite-lesspipe.sh %s"; # Syntax highlighting in less
     };
   };
 }
