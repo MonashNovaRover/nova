a6f31f0699cebbd6229a64207161a2bd83095e06
Bailey Chessum <52596359+BaileyChessum@users.noreply.github.com>
2025-07-28 11:18:36 +1000

    fix: changing routes would need a manual refresh of the page (#590)
    Co-authored-by: Felicity Matthews <felicitymatthews30@gmail.com>

--- src/ros/nova-gui/nova-gui/src/main.tsx
+++ src/ros/nova-gui/nova-gui/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routes } from "./routes/routes";
+import configureRootStore from "./redux/store/configureRootStore";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,6 +15,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 
 function App() {
   const router = createBrowserRouter(routes);
+  const {store, persistor} = configureRootStore();
 
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
+  );
+
 }
