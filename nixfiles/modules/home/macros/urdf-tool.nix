e629e5c9507673a3b13c679070a617cf796a0709
Anthony Lew <anthony@v01dbreaker.cc>
2025-08-06 23:32:27 +1000

    remove: urdfModifier from urdf-tool (unneeded)

--- nixfiles/modules/home/macros/urdf-tool.nix
+++ nixfiles/modules/home/macros/urdf-tool.nix
@@ -51,26 +51,6 @@ let
     };
   };
 
-  urdfModifier = pyPkgs.buildPythonApplication rec {
-    pname = "urdf-inertia-script";
-    version = "1.1";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "V01DBREAKER";
-      repo = pname;
-      rev = "ed09f00c6257cec36d8c3ed30fb50905cbcbb872";
-      hash = "sha256-dNhATy4ZtYQoj44O6VwtbB8k0hM3bSAp8W+nOFx9ik8=";
-    };
-
-    propagatedBuildInputs = [ pyPkgs.pymeshlab ];
-
-    meta = with pkgs.lib; {
-      description = "Modify generated URDFs and recalculate inertias.";
-      homepage = "https://github.com/V01DBREAKER/urdf-inertia-script";
-      license = licenses.mit;
-    };
-  };
-
   onshapeKeys = if builtins.pathExists "/etc/nixos/onshape_key/onshape-keys.nix" then
     import /etc/nixos/onshape_key/onshape-keys.nix
   else
@@ -84,12 +64,10 @@ pkgs.mkShell {
   packages = [
     pkgs.openscad
     onshapeToRobot
-    urdfModifier
     pyPkgs.pymeshlab
   ];
 
   shellHook = ''
-    export PATH=${onshapeToRobot}/bin:${urdfModifier}/bin:$PATH
     export PYTHONPATH=${onshapeToRobot}/${python.sitePackages}:$PYTHONPATH
     export ONSHAPE_API=https://cad.onshape.com
 
@@ -107,6 +85,5 @@ pkgs.mkShell {
     echo " ! Note: relative paths will not work for these scripts! Start any file path at /"
     echo "OpenSCAD usage: onshape-to-robot-edit-shape <stl-file-path>"
     echo "Oneshape-to-Robot usage: onshape-to-robot <config.json-folder-path>"
-    echo "Urdf-Inertia-Script usage: urdf-modifier <urdf-file-path> OR inertia-calc <stl-file-path>"
   '';
 }
