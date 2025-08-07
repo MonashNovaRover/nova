c491e00334196531c5df3cd11a22822de0db4e29
Monash Nova Rover <matthewheekh@gmail.com>
2025-08-07 14:32:45 +1000

    Fixed chip radius

--- src/ros/nova-gui/nova-gui/src/components/maps/Cartographer/components/CartographerGoalModal.tsx
+++ src/ros/nova-gui/nova-gui/src/components/maps/Cartographer/components/CartographerGoalModal.tsx
@@ -136,6 +136,7 @@ export const CartographerGoalModal: React.FC<{
                 : "bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-gray-200"
             }`}
             size="sm"
+            radius="sm"
           >
             {point.labelName}
           </Chip>
