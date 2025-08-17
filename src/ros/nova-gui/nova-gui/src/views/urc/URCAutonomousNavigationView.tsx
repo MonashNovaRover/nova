67895ec188f2d745406d2abe2b735084302eccbb
Monash Nova Rover <matthewheekh@gmail.com>
2025-08-17 21:59:04 +1000

    Fixed cartographer animations, moved auto components into new folder

--- src/ros/nova-gui/nova-gui/src/views/urc/URCAutonomousNavigationView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/urc/URCAutonomousNavigationView.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Cartographer } from "../../components/maps/Cartographer/Cartographer";
-import AutoStatus from "../../components/maps/Cartographer/components/AutoStatus";
+import AutoStatus from "../../components/auto/Cartographer/components/AutoStatus";
 import { GoalPublisher } from "../../components/maps/Cartographer/components/GoalPublisher";
 
 const URCAutonomousNavigationView: React.FC = () => {
   return (
-    <div>
+    <div className="w-full h-[92.1vh]">
       <Cartographer
         pointLabels={[
           { key: 0, text: "GNSS" },
