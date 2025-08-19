dd865fae28b611d1afceb6a19e4f19e8d8e65959
Victor Bartlinski <vicbart08@gmail.com>
2025-08-19 17:01:11 +1000

    hiding more secrets

--- nixfiles/modules/nixos/common/users/default.nix
+++ nixfiles/modules/nixos/common/users/default.nix
@@ -35,8 +35,7 @@
       {
         nova = lib.mkIf config.nova.users.nova.enable {
           description = "Monash Nova Rover";
-          hashedPassword =
-            "$y$j9T$l1uk9vWbRDWXWFjucvu6s1$C/GP.LwS.HI7ujVx8ZY/uqAMa4EYEZHLdpzlnsBb.tC";
+          hashedPassword = builtins.readFile ../../../../../secrets/nova-user-hashed-password.txt;
           extraGroups = with config.users.groups; [
             wheel.name
             video.name
