ac4b0c32c55108f3ce91bc31d5d80547a0440379
Joshua Leivenzon <jlei0014@student.monash.edu>
2025-09-08 16:26:57 +1000

    fix: nixos: Remove usage of lib.mdDoc
    This has been made redundant and removed in modern Nixpkgs revisions.

--- nixfiles/modules/common/nova/default.nix
+++ nixfiles/modules/common/nova/default.nix
@@ -3,7 +3,7 @@
 {
   options.nova.repos = lib.mkOption {
     type = with lib.types; nullOr (listOf path);
-    description = lib.mdDoc ''The Nova Rover repositories to pass to the main Nix function. Leave unset to search in the default locations.'';
+    description = ''The Nova Rover repositories to pass to the main Nix function. Leave unset to search in the default locations.'';
     default = null;
   };
 }
