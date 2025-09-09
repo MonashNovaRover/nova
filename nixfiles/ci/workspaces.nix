15a3254a51bdc6ccbf4c26d7e2ff09482788b5c4
Victor Bartlinski <118893280+vic-bart@users.noreply.github.com>
2025-09-09 15:54:56 +1000

    Removing old gui (#692)

--- nixfiles/ci/workspaces.nix
+++ nixfiles/ci/workspaces.nix
@@ -8,14 +8,7 @@ rec {
     # There is no easy way at this stage to determine if Hydra has access to
     # any real x86_64 machines, so these changes will apply indiscriminately.
     (lib.pkgs.lib.optionalAttrs (novaPkgs.hostPlatform.isx86_64 && (lib.pkgs.lib.systems.elaborate builtins.currentSystem).isAarch64) rec {
-      novaPackages = workspace.novaPackages // {
-        # The GUI frontend fails to build, but the output contains only
-        # static Web assets, and is architecture-independent. Use the
-        # Aarch64 version instead.
-        nova-gui-frontend = (lib.novaFor "aarch64-linux").pkgs.nova-gui-frontend;
-      };
-      extraPackages = workspace.extraPackages // {
-        nova-gui-frontend-server = novaPkgs.nova-gui-frontend-server.override { inherit (novaPackages) nova-gui-frontend; };
-      };
+      novaPackages = workspace.novaPackages;
+      extraPackages = workspace.extraPackages;
     });
 }
