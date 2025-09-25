98110d6b2e1f34c7493e4caf8d4860ae5f8f4249
Orlando Chamberlain <orlandoch.dev@gmail.com>
2025-09-25 23:37:08 +1000

    nixfiles: jobsets: isos: resolve name conflict
    make the iso name a lib.mkForce
    
    error: The option `image.baseName' has conflicting definition values:
    - In `/nix/store/081zspnjiwbq85jqhx89gkk2nbyrfg3y-source/nixos/modules/installer/cd-dvd/iso-image.nix': "nixos-graphical-workspace"
    - In `/nix/store/081zspnjiwbq85jqhx89gkk2nbyrfg3y-source/nixos/modules/installer/cd-dvd/iso-image.nix': "nixos-nova-nova-25.11pre-git-x86_64-linux"
    Use `lib.mkForce value` or `lib.mkDefault value` to change the priority on any of these definitions.

--- nixfiles/ci/jobsets/isos.nix
+++ nixfiles/ci/jobsets/isos.nix
@@ -41,7 +41,7 @@ let
               ];
 
               isoImage = {
-                isoBaseName = "nixos${lib.optionalString graphical "-graphical"}${lib.optionalString includeWorkspace "-workspace"}";
+                isoBaseName = lib.mkForce "nixos${lib.optionalString graphical "-graphical"}${lib.optionalString includeWorkspace "-workspace"}";
                 squashfsCompression = lib.mkIf (!enableCompression) "xz -noI -noD -noF -noX";
 
                 # Add workspace development dependencies, so that the live
