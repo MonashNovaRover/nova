d2b9f8a99f16152117ea9f17eb21ea5a41e91c4f
Felicity Matthews <felicitymatthews30@gmail.com>
2025-09-30 20:11:02 +1000

    fix: remove cmake files

--- src/ros/rover/science/science_control_modes/legacy_input_mode/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nova/nova/src/ros/rover/science/science_control_modes/legacy_input_mode
-# Build directory: /home/nova/nova/src/ros/rover/science/science_control_modes/legacy_input_mode/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
