8c100ddf20634a70b8a1c6ac0c550717bc41c3b8
Angel <angelloisaguinaldo@gmail.com>
2025-10-12 02:40:54 +1100

    Feat/can sleuth gui (#786)

---
+++ src/other/can_sleuth/can_sleuth/outputs/terminal_out.py
@@ -0,0 +1,18 @@
+from . import output
+
+"""
+    Output that prints atrributes to the terminal; Not pretty at all. Quick fix to use for debugging so errors weren't hidden.
+"""
+
+class TerminalOut(output.Output):
+    def __init__(self):
+        pass
+
+    def update(self, devices):
+        for device in devices:
+            print(f"Device: {device.name}")
+
+            for attr in device.attrs:
+                print(f"{attr.name}: {attr.value}")
+
+    
