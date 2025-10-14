638cec3c1a24657cc6bab366814938ae0f4866a6
Bailey Chessum <bailey.chessum1@gmail.com>
2025-10-14 21:49:57 +1100

    feat: further restructuring of base class
    Signed-off-by: Bailey Chessum <bailey.chessum1@gmail.com>

--- src/ros/rover/arm/arm_kinematics/arm_kinematics/include/arm_kinematics/kinematics_plugin_base.hpp
+++ src/ros/rover/arm/arm_kinematics/arm_kinematics/include/arm_kinematics/kinematics_plugin_base.hpp
@@ -10,26 +10,40 @@
 #include <geometry_msgs/msg/pose.hpp>
 #include <Eigen/Geometry>
 #include <rclcpp/logger.hpp>
+#include <rclcpp/node_interfaces/node_base_interface.hpp>
+#include <rclcpp/node_interfaces/node_logging_interface.hpp>
+#include "rclcpp/node_interfaces/node_interfaces.hpp"
+#include <optional>
 
 
 namespace arm_kinematics {
 
+/**
+ * Parameters used to configure kinematics plugins
+ */
+struct KinematicsParams {
+
+  /// The name of the link in the URDF to treat as the 'origin' for kinematics
+  std::string base_link_name;
+
+  /// The name of the link to use by default as the target in FK and IK calculations. Usually the end effector.
+  std::string ee_link_name;
+};
+
 class KinematicsPluginBase {
 public:
+  using KinematicsNodeInterfaces =
+    rclcpp::node_interfaces::NodeInterfaces<rclcpp::node_interfaces::NodeBaseInterface, rclcpp::node_interfaces::NodeLoggingInterface>;
+
   /**
    * Effectively replaces the constructor for the class, as we can only use a default constructor in plugins.
    *
    * \returns True if initialization was successful. False otherwise.
    */
   bool initialize(
+    KinematicsNodeInterfaces node_interfaces,
     std::string & robot_description,
-    const std::vector<std::string>& joint_names)
-  {
-    robot_description_ = &robot_description;
-    joint_names_ = joint_names;
-
-    return on_initialize();
-  }
+    const std::vector<std::string>& joint_names);
 
   virtual bool get_position_ik(
     const Eigen::Isometry3d & ik_pose,
@@ -73,21 +87,20 @@ public:
     */
   virtual bool get_position_fk(
     const std::vector<double> & joint_angles,
-    Eigen::Isometry3d & solution_pose) const {
-
-    return get_position_fk(joint_angles, ee_link_name_, solution_pose);
-  };
+    Eigen::Isometry3d & solution_pose) const;;
 
   // Accessors
 
   /// The URDF being used.
-  [[nodiscard]] const std::string & get_robot_description() const noexcept;
+  [[nodiscard]] const std::string & get_robot_description() const;
   /// The name of every joint considered in IK and FK calculations.
   [[nodiscard]] const std::vector<std::string> & get_joint_names() const noexcept;
-  /// The name of the link to use for IK and FK calculations by default
-  [[nodiscard]] const std::string & get_ee_link_name() const noexcept;
   /// Logger to use for logging
   [[nodiscard]] const rclcpp::Logger & get_logger() const noexcept;
+
+  [[nodiscard]] const KinematicsParams & get_kinematics_params() const noexcept;
+
+  [[nodiscard]] const KinematicsNodeInterfaces & get_node_interfaces() const;
 
 protected:
   /**
@@ -101,11 +114,14 @@ private:
   /// The URDF being used. You can get this from within a ros2_control controller
   std::string * robot_description_ = nullptr;
 
+  KinematicsParams kinematics_params_;
+
+private:
+
   /// The name of every joint considered in IK and FK calculations.
   std::vector<std::string> joint_names_{};
 
-  /// The name of the link to use for IK and FK calculations by default
-  std::string ee_link_name_;
+  std::optional<KinematicsNodeInterfaces> node_interfaces_ = std::nullopt;
 
   /// Logger to use for logging
   rclcpp::Logger logger_ = rclcpp::get_logger("arm_kinematics");
--- src/ros/rover/arm/arm_kinematics/arm_kinematics/src/kinematics_plugin_base.cpp
+++ src/ros/rover/arm/arm_kinematics/arm_kinematics/src/kinematics_plugin_base.cpp
@@ -4,9 +4,36 @@
 
 #include <arm_kinematics/kinematics_plugin_base.hpp>
 
+#include <urdf/model.h>
+#include <stdexcept>
+
 namespace arm_kinematics {
 
+bool KinematicsPluginBase::initialize(KinematicsPluginBase::KinematicsNodeInterfaces node_interfaces,
+                                      std::string &robot_description, const std::vector<std::string> &joint_names) {
+  robot_description_ = &robot_description;
+  joint_names_ = joint_names;
 
+  node_interfaces_ = node_interfaces;
+  logger_ = node_interfaces.get_node_logging_interface()->get_logger().get_child("kinematics");
+
+  return on_initialize();
+}
+
+bool KinematicsPluginBase::get_position_fk(const std::vector<double> &joint_angles,
+                                           const std::string &link_name,
+                                           Eigen::Isometry3d &solution_pose) const {
+  // Default implementation using KDL
+  return false;
+}
+
+bool KinematicsPluginBase::get_position_fk(const std::vector<double> &joint_angles,
+                                           Eigen::Isometry3d &solution_pose) const {
+  // Default implementation just uses the general case implementation
+  // But you could override this if you have an analytical solution
+
+  return get_position_fk(joint_angles, get_kinematics_params().ee_link_name, solution_pose);
+}
 
 bool KinematicsPluginBase::get_velocity_ik(const Eigen::Matrix<double, 6, 1> &ik_twist,
                                            const Eigen::Isometry3d &ik_seed_pose,
@@ -22,28 +49,38 @@ bool KinematicsPluginBase::get_velocity_ik(const Eigen::Matrix<double, 6, 1> &ik
                                            std::vector<double> &solution,
                                            double time_step) const
 {
-  get_position_fk()
+  // Just call the above method, getting ik_seed_pose from forward kinematics
+  Eigen::Isometry3d ik_seed_pose;
+  const auto fk_result = get_position_fk(ik_seed_state, ik_seed_pose);
+
+  return fk_result && get_velocity_ik(ik_twist, ik_seed_pose, ik_seed_state, solution, time_step);
 }
 
 const std::vector<std::string> &KinematicsPluginBase::get_joint_names() const noexcept {
   return joint_names_;
 }
 
-std::string & KinematicsPluginBase::get_robot_description() const noexcept {
+const std::string & KinematicsPluginBase::get_robot_description() const {
+  if (!robot_description_)
+    throw std::logic_error("Tried to use a KinematicsPlugin before calling initialize() or after initialize() failed.");
+
   return *robot_description_;
 }
 
-bool KinematicsPluginBase::get_position_fk(const std::vector<double> &joint_angles, const std::string &link_name,
-                                           Eigen::Isometry3d &solution_pose) const {
-  return false;
-}
 
 const rclcpp::Logger &KinematicsPluginBase::get_logger() const noexcept {
   return logger_;
 }
 
-const std::string &KinematicsPluginBase::get_ee_link_name() const noexcept {
-  return ee_link_name_;
+const KinematicsParams &KinematicsPluginBase::get_kinematics_params() const noexcept {
+  return kinematics_params_;
+}
+
+const KinematicsPluginBase::NodeInterfaces &KinematicsPluginBase::get_node_interfaces() const {
+  if (!node_interfaces_.has_value())
+    throw std::logic_error("Tried to use a KinematicsPlugin before calling initialize() or after initialize() failed.");
+
+  return *node_interfaces_;
 }
 
 
