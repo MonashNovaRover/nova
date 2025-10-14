51ad72268fdf02670fcbcbfdb8de158e59c6f675
Angel <angelloisaguinaldo@gmail.com>
2025-10-15 04:23:52 +1100

    create enums for led commands and add callback function

--- src/other/can_sleuth/can_sleuth/devices/led.py
+++ src/other/can_sleuth/can_sleuth/devices/led.py
@@ -2,30 +2,44 @@ from . import candevice
 import jcan
 
 class LEDStrip(candevice.CanDevice):
-    commands = {
+    class LedCommand(Enum):
+        BRIGHTNESS = 0x091
+        COLOUR = 0x095
+        PINK = 0x096
+
+    commandsDIct = {
         #Brightness
-        0x091: {
+        LedCommand.BRIGHTNESS: {
             0x8000: 1, #100%
             0x6000: 0.75, #75%
             0x4000: 0.5 #50%
+            0x0000: 0 # off
         },
         #colour
-        0x095: {
-            0x0100: "red"
-        }
+        LedCommand.COLOUR: {
+            0x0100: "red",
+            0x0200: "green",
+            0x0300: "blue"
+        },
+        LedCommand.PINK: "pink"
     }
+
     
     def __init__(self, name:str, interface:str):
         super.__init__(name, interface)
         self.brightness = 0
         self.colour=None
+        #add callback functions
+        #brightness
+        self.addCallback(LedCommand.BRIGHTNESS, self.set_brightness_cb)
+        self.addCallback(LedCommand.COLOUR, self.set_colour_cb)
+
+
     
     def set_brightness_cb(self, frame:jcan.Frame)
-        
-        pass
+        self.brightness = commandsDict[frame.id]
 
     def set_colour_cb(self, frame:jcan.Frame):
-        pass
-    
-    def spin():
-        pass
+        #TODO: Handle pink
+        #TODO: Handle data being integer list of hexes
+        self.colour = commandsDict[frame.id][frame.data]
