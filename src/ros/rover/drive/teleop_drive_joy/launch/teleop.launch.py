bf7eb3c6134a36a04f3d0e99d2705d651161ae11
Jonathan Jia <22513511+Glass2721@users.noreply.github.com>
2025-10-19 20:36:40 +1100

    make teleop launch file use game_controller_node instead

--- src/ros/rover/drive/teleop_drive_joy/launch/teleop.launch.py
+++ src/ros/rover/drive/teleop_drive_joy/launch/teleop.launch.py
@@ -1,26 +1,22 @@
 from launch import LaunchDescription
-from launch.actions import OpaqueFunction, DeclareLaunchArgument, LogInfo
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import OpaqueFunction, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def launch_setup(context, *args, **kwargs):
-    teleop_drive_joy_dir = FindPackageShare('teleop_drive_joy')
 
     # Launch configurations
     device_id = LaunchConfiguration('device_id')
     device_name = LaunchConfiguration('device_name')
     joy_vel = LaunchConfiguration('joy_vel')
-    config = LaunchConfiguration('config').perform(context)
-    params_file = PathJoinSubstitution([teleop_drive_joy_dir, 'config', f'{config}.config.yaml'])
 
     return [
         # Add Nodes
         Node(
             package='joy',
-            executable='joy_node',
-            name='joy_node',
+            executable='game_controller_node',
+            name='game_controller_node',
             parameters=[
                 {'device_id': device_id,
                  'deadzone': 0.1,
@@ -31,13 +27,10 @@ def launch_setup(context, *args, **kwargs):
             package='teleop_drive_joy',
             executable='teleop_drive_joy_node',
             name='teleop_drive_joy_node',
-            parameters=[params_file],
             remappings=[
                 ('/cmd_vel', joy_vel),
             ],
         ),
-        # Log Information
-        LogInfo(msg=['Joystick Loaded: ', config]),
     ]
 
 def generate_launch_description():
@@ -53,10 +46,6 @@ def generate_launch_description():
         DeclareLaunchArgument(
             name='joy_vel', 
             default_value='cmd_vel'
-        ),
-        DeclareLaunchArgument(
-            name='config',
-            default_value='xbox', #Defined in launch_setup due to requiring another launch argument
         ),
     ]
 
