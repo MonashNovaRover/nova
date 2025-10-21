2e9280285b2f55c2d7f9b9c0c570d66d206dd7f6
Jackson <83764855+jacksonmacc@users.noreply.github.com>
2025-10-21 17:05:12 +1100

    wip: wrote out the rest of the end effector controller

--- src/ros/rover/arm/arm_controllers/nova_end_effector_controller/include/nova_end_effector_controller/nova_end_effector_controller.hpp
+++ src/ros/rover/arm/arm_controllers/nova_end_effector_controller/include/nova_end_effector_controller/nova_end_effector_controller.hpp
@@ -7,17 +7,22 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 #include "realtime_tools/realtime_buffer.hpp"
+#include "realtime_tools/realtime_box.hpp"
+#include "joint_limits/joint_saturation_limiter.hpp"
+#include "trajectory_msgs/msg/joint_trajectory_point.hpp"
+
+#include <nova_interfaces/msg/arm_fk_velocity_targets.hpp>
 
 #include <nova_end_effector_controller/nova_end_effector_controller_parameters.hpp>
 
 namespace nova_end_effector_controller {
     class NovaEndEffectorController : public controller_interface::ChainableControllerInterface {
     public:
-        NovaEndEffectorController();
-        
         controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
         controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+        std::vector<hardware_interface::CommandInterface> on_export_reference_interfaces() override;
 
         controller_interface::CallbackReturn on_init() override;
 
@@ -26,6 +31,10 @@ namespace nova_end_effector_controller {
 
         controller_interface::CallbackReturn on_activate(
             const rclcpp_lifecycle::State & previous_state) override;
+
+        controller_interface::return_type update_and_write_commands(
+            const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
 
         controller_interface::CallbackReturn on_deactivate(
             const rclcpp_lifecycle::State & previous_state) override;
@@ -47,22 +56,39 @@ namespace nova_end_effector_controller {
             std::reference_wrapper<hardware_interface::LoanedCommandInterface> command;
         };
 
+        controller_interface::return_type update_reference_from_subscribers(
+            const rclcpp::Time & time, const rclcpp::Duration & period) override;
+        controller_interface::return_type update_velocity_reference_from_subscribers();
+        
+
         const char *joint_command_type() const;
+        const char *joint_feedback_type() const;
+        bool reset();
+        void halt();
+
+        joint_limits::JointSaturationLimiter<trajectory_msgs::msg::JointTrajectoryPoint> joint_limiter;
 
         controller_interface::CallbackReturn configure_joints(
             const std::vector<std::string> &joint_names,
             std::vector<JointHandle> &registered_handles);
 
+        // Subscription
+        rclcpp::Subscription<nova_interfaces::msg::ArmFkVelocityTargets>::SharedPtr input_subscriber_ = nullptr;
+        realtime_tools::RealtimeBox<std::shared_ptr<nova_interfaces::msg::ArmFkVelocityTargets>> received_msg_ptr_{nullptr};
         bool subscriber_is_active_ = false; // not sure what this is for yet
+
         bool is_halted = false;
 
-    private:
-        // Joints being used by the controller. Order should match that of the joint name definitions in parameters
-        std::vector<JointHandle> registered_joint_handles_;
-        
+        rclcpp::Time previous_update_timestamp_{0};
+
         // Parameters from ROS for nova_end_effector_controller
         std::shared_ptr<ParamListener> param_listener_{};
-        Params params_{}; // TODO: Find the include for this
+        Params params_{};
+
+        void get_joint_states(trajectory_msgs::msg::JointTrajectoryPoint&);
+
+        // Joints being used by the controller. Order should match that of the joint name definitions in parameters
+        std::vector<JointHandle> registered_joint_handles_;
     };
 };
 #endif
