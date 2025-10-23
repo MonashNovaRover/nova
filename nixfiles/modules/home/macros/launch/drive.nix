d8b11005fd5b6c749d4b7f9cb19107f23e2a1978
Anthony <anthony@v01dbreaker.cc>
2025-10-23 21:26:20 +1100

    Terminal Launcher (#735)
    Launch scripts!

---
+++ nixfiles/modules/home/macros/launch/drive.nix
@@ -0,0 +1,30 @@
+# simple drive with rover and controller with base station
+# by default it will run with current drive, but old drive can be specified with -A drive.old
+{ 
+    base,
+    rover,
+    pre-shell,
+    bashBuilder,
+}:
+
+let 
+  drive-setup = {
+    pre = pre-shell {payload-name="Drive"; need-rover=true;};
+    terminals = [
+      {name = "Base:Teleop"; platform=base; cmd="./ros2 launch teleop_drive_joy teleop.launch.py";}
+      {name = "Rover:Drive"; platform=rover; cmd="launch-drive";}
+    ];
+  };
+  old-drive-setup = {
+    pre = pre-shell {payload-name="Old Drive"; need-rover=true;};
+    terminals = [
+      {name = "Base:Base"; platform=base; cmd="./ros2 launch nova_bringup base.launch.py";}
+      {name = "Rover:Old Drive"; platform=rover; cmd="launch-old-drive";}
+    ];
+  };
+in
+{
+  # by default, use current drive
+  drive = bashBuilder drive-setup "run-drive";
+  old-drive = bashBuilder old-drive-setup "run-old-drive";
+}
