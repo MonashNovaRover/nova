7c92e421ceea5766d9bc57a762e15ab4b283b9fa
Anthony Lew <anthony@v01dbreaker.cc>
2025-11-30 18:45:41 +1100

    feat: Templates for tasks

---
+++ nixfiles/modules/home/macros/launch/urc/delivery.nix
@@ -0,0 +1,38 @@
+{ 
+    base,
+    rover,
+    pre-shell,
+    post-shell,
+    bashBuilder
+}:
+
+let 
+  task-name = "de";
+  one = {
+    pre = pre-shell {payload-name=task-name + " one"; need-rover=true;};
+    terminals = [
+      {name = "Base:Rviz"; platform=base; cmd="ros2 launch auto_bringup rviz.launch.py";}
+    ];
+    post = post-shell;
+  };
+
+  two = {
+    pre = pre-shell {payload-name=task-name + " two"; need-rover=true; };
+    terminals = [
+      {name="Base:Teleop"; platform=base; cmd="ros2 launch teleop_drive_joy teleop.launch.py";}
+      {name = "Rover:Drive"; platform=rover; cmd="launch-drive";}
+    ];
+    post = post-shell;
+  };
+
+  combined = {
+    pre = pre-shell {payload-name=task-name+" combined"; need-rover=true; };
+    terminals = one.terminals ++ two.terminals;
+    post = post-shell;
+  };
+in
+{
+  de-one = bashBuilder one ("run-"+task-name+"-one");
+  de-two = bashBuilder two ("run-"+task-name+"-two");
+  de-combined = bashBuilder combined ("run-"+task-name+"-combined");
+}
