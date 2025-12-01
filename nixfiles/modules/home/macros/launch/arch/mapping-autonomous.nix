95b7a6794e546776e9171fd9ac6c36cc9435276d
Anthony <anthony@v01dbreaker.cc>
2025-12-01 19:42:24 +1100

    Launch Templates (#753)

---
+++ nixfiles/modules/home/macros/launch/arch/mapping-autonomous.nix
@@ -0,0 +1,38 @@
+{ 
+    base,
+    rover,
+    pre-shell,
+    post-shell,
+    bashBuilder
+}:
+
+let 
+  task-name = "ma";
+  one = {
+    pre = pre-shell {payload-name=task-name + " one"; need-rover=true;};
+    terminals = [
+      {name = "Base:Rviz"; platform=base; cmd="ros2 launch auto_bringup rviz.launch.py";}
+    ];
+    post = post-shell;
+  };
+
+  two = {
+    pre = pre-shell {payload-name=task-name + " two"; need-rover=true; };
+    terminals = [
+      {name="Base:Teleop"; platform=base; cmd="ros2 launch teleop_drive_joy teleop.launch.py";}
+      {name = "Rover:Drive"; platform=rover; cmd="launch-drive";}
+    ];
+    post = post-shell;
+  };
+
+  combined = {
+    pre = pre-shell {payload-name=task-name+" combined"; need-rover=true; };
+    terminals = one.terminals ++ two.terminals;
+    post = post-shell;
+  };
+in
+{
+  ma-one = bashBuilder one (task-name+"-one");
+  ma-two = bashBuilder two (task-name+"-two");
+  ma-combined = bashBuilder combined (task-name+"-combined");
+}
