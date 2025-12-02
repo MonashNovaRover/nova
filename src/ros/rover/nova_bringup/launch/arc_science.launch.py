70addfafea98c764c1fd14ee0c764f2886d50968
Monash Nova Rover <novaroverteam@monash.edu>
2025-12-02 13:37:57 +1100

    fix: params and names

--- src/ros/rover/nova_bringup/launch/arc_science.launch.py
+++ src/ros/rover/nova_bringup/launch/arc_science.launch.py
@@ -31,6 +31,7 @@ def launch_setup(context, *args, **kwargs):
 
     return [
         Node(
+            name='nir_probe_publisher',
             package='science',
             executable='nir_probe_publisher.py',
             output='screen',
@@ -40,6 +41,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
+            name='kiln_server',
             package='science',
             executable='kiln_server.py',
             output='screen',
@@ -49,6 +51,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
+            name='auger',
             package='science',
             executable='auger.py',
             output='screen',
@@ -58,6 +61,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
+            name='analysis_arm',
             package='science',
             executable='analysis_arm.py',
             output='screen',
@@ -67,7 +71,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
-            name='AnalysisArm',
+            name='c_beam',
             package='science',
             executable='analysis_arm.py',
             output='screen',
@@ -77,16 +81,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
-            name='CBeam',
-            package='science',
-            executable='analysis_arm.py',
-            output='screen',
-            emulate_tty=True,
-            parameters=[
-                science_params,
-            ],
-        ),
-        Node(
+            name='arc_sweeper_servo',
             package='science',
             executable='arc_sweeper_servo.py',
             output='screen',
@@ -96,6 +91,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
+            name='arc_spinny_part',
             package='science',
             executable='arc_spinny_part.py',
             output='screen',
@@ -105,6 +101,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
+            name='scimbal_cam',
             package='science',
             executable='scimbal_cam.py',
             output='screen',
