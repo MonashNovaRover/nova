510f4ef5c101a0d9491fb99642507fe323f33d2a
V01DBREAKER <anthony@v01dbreaker.cc>
2025-12-05 23:31:34 +0000

    feat: auto urc launch draft

--- nixfiles/modules/home/macros/launch/urc/autonomous-navigation.nix
+++ nixfiles/modules/home/macros/launch/urc/autonomous-navigation.nix
@@ -1,17 +1,19 @@
 { 
     base,
     rover,
+    mast,
     pre-shell,
     post-shell,
     bashBuilder
 }:
 
 let 
-  task-name = "an";
+  task-name = "an"; # we might need to add delays to some of these commands?
   one = {
     pre = pre-shell {payload-name=task-name + " one"; need-rover=true;};
     terminals = [
-      {name = "Base:Rviz"; platform=base; cmd="ros2 launch auto_bringup rviz.launch.py";}
+      {name="Base:Rviz"; platform=base; cmd="ros2 launch auto_bringup rviz.launch.py";}
+      {name="Rover:Software"; platform=rover; cmd="ros2 launch auto_bringup software.launch.py";}
     ];
     post = post-shell;
   };
@@ -20,7 +22,17 @@ let
     pre = pre-shell {payload-name=task-name + " two"; need-rover=true; };
     terminals = [
       {name="Base:Teleop"; platform=base; cmd="ros2 launch teleop_drive_joy teleop.launch.py";}
-      {name = "Rover:Drive"; platform=rover; cmd="launch-drive";}
+      {name="Rover:Drive"; platform=rover; cmd="ros2 launch drive_bringup drive.launch.py auto:=True";}
+      {name="Rover:Cameras"; platform=rover; cmd="ros2 launch auto_bringup camera.launch.py";}
+      {name="Run:Gui+Maps"; platform=base; cmd="run-gui-maps";} # need to test if this works
+    ];
+    post = post-shell;
+  };
+
+  mast-setup = { # this won't be added to combined
+      pre = pre-shell {payload-name=task-name + " mast"; need-mast=true; };
+      terminals = [
+        {name="Mast:GPS"; platform=mast; cmd="ros2 launch nova_bringup gps_base.launch.py gps_params:=/home/nova/gps.yaml";}
     ];
     post = post-shell;
   };
@@ -35,4 +47,5 @@ in
   an-one = bashBuilder one (task-name+"-one");
   an-two = bashBuilder two (task-name+"-two");
   an-combined = bashBuilder combined (task-name+"-combined");
+  an-mast = bashBuilder mast-setup (task-name+"-mast");
 }
