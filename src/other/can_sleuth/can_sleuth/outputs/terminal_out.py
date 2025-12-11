684d0ce67a5d30ed3e8085a8de57818ab1e18409
Angel <angelloisaguinaldo@gmail.com>
2025-12-11 15:07:38 +1100

    fix commandsDict key values

--- src/other/can_sleuth/can_sleuth/outputs/terminal_out.py
+++ src/other/can_sleuth/can_sleuth/outputs/terminal_out.py
@@ -16,3 +16,22 @@ class TerminalOut(output.Output):
                 print(f"{attr.name}: {attr.value}")
 
     
+
+from . import output
+
+"""
+    Output that prints atrributes to the terminal; to use for debugging.
+"""
+
+class TerminalOut(output.Output):
+    def __init__(self):
+        pass
+
+    def update(self, devices):
+        for device in devices:
+            print(f"Device: {device.name}")
+
+            for attr in device.attrs:
+                print(f"{attr.name}: {attr.value}")
+
+    
