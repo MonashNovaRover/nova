a6ba719421145ecf4b8943f6bd171bc61f84ce79
Ivan Li <ivanli10252006@gmail.com>
2025-12-17 00:56:04 +1100

    Add outer div to suit title and etc implementation

--- src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
+++ src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
@@ -8,36 +8,38 @@ export const HomePage = (props: HomePageProps) => {
   const navigationData = props.navigationData
 
   return (
-    <div className="flex flex-wrap justify-center items-center h-screen gap-10 p-3">
-      {Object.keys(navigationData).map((item) => {
-        return (
-          <Card key={item} className="w-[20%]">
-            <CardHeader className="flex justify-center">
-              <div className="text-lg font-semibold text-white">{item}</div>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-              <div className="flex flex-col gap-2 p-3">
-                {navigationData[item].map((mode) => {
-                  return (
-                    <Button
-                      onPress={() => navigate(mode.route)}
-                      size="md"
-                      variant="ghost"
-                      color="default"
-                      className="transition-transform duration-300 ease-in-out hover:scale-105"
-                      key={mode.route}
-                      startContent={mode.icon}
-                    >
-                        {mode.title}
-                    </Button>
-                  );
-                })}
-              </div>
-            </CardBody>
-          </Card>
-        );
-      })}
+    <div className="flex flex-col h-screen justify-center">
+      <div className="flex flex-wrap justify-center items-center w-screen gap-10 p-3">
+        {Object.keys(navigationData).map((item) => {
+          return (
+            <Card key={item} className="w-[20%]">
+              <CardHeader className="flex justify-center">
+                <div className="text-lg font-semibold text-white">{item}</div>
+              </CardHeader>
+              <Divider />
+              <CardBody>
+                <div className="flex flex-col gap-2 p-3">
+                  {navigationData[item].map((mode) => {
+                    return (
+                      <Button
+                        onPress={() => navigate(mode.route)}
+                        size="md"
+                        variant="ghost"
+                        color="default"
+                        className="transition-transform duration-300 ease-in-out hover:scale-105"
+                        key={mode.route}
+                        startContent={mode.icon}
+                      >
+                          {mode.title}
+                      </Button>
+                    );
+                  })}
+                </div>
+              </CardBody>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 }
