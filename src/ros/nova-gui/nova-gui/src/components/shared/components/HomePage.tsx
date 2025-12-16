7a93f33ca90f10d8c46462599f6d7c9da6eea3a8
Ivan Li <ivanli10252006@gmail.com>
2025-12-16 16:19:18 +1100

    Add card format from heroui

--- src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
+++ src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { HomePageProps } from "../../../views/shared/HomePageView";
 
@@ -11,37 +11,43 @@ export const HomePage = (props: HomePageProps) => {
   const currentPath = location.pathname;
 
   return (
-    <div>
+    <div className="flex justify-evenly items-center h-screen">
       {Object.keys(navigationData).map((item) => {
         return (
-          <div key={item} className="flex flex-col pb-5">
-            <div className="text-sm font-light">{item}</div>
-            <div className="flex flex-col gap-2 mt-2">
-              {navigationData[item].map((mode) => {
-                const isCurrentSelected = currentPath === mode.route;
-                return (
-                  <Button
-                    onPress={() => navigate(mode.route)}
-                    size="md"
-                    variant={isCurrentSelected ? "solid" : "light"}
-                    color={isCurrentSelected ? "primary" : "default"}
-                    fullWidth
-                    className="pl-3"
-                    key={mode.route}
-                  >
-                    <div
-                      className={`w-full flex flex-row justify-start gap-3 items-center m-0 ${
-                        !isCurrentSelected && "text-gray-400"
-                      }`}
+          // TEMP: className values setting width and height in card are temporary!!
+          <Card key={item} className="w-[400px] h-[400px]">
+            <CardHeader className="flex gap-3">
+              <div className="text-sm font-light">{item}</div>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <div className="flex flex-col gap-2 mt-2">
+                {navigationData[item].map((mode) => {
+                  const isCurrentSelected = currentPath === mode.route;
+                  return (
+                    <Button
+                      onPress={() => navigate(mode.route)}
+                      size="md"
+                      variant={isCurrentSelected ? "solid" : "light"}
+                      color={isCurrentSelected ? "primary" : "default"}
+                      fullWidth
+                      className="pl-3"
+                      key={mode.route}
                     >
-                      <div>{mode.icon}</div>
-                      <div>{mode.title}</div>
-                    </div>
-                  </Button>
-                );
-              })}
-            </div>
-          </div>
+                      <div
+                        className={`w-full flex flex-row justify-start gap-3 items-center m-0 ${
+                          !isCurrentSelected && "text-gray-400"
+                        }`}
+                      >
+                        <div>{mode.icon}</div>
+                        <div>{mode.title}</div>
+                      </div>
+                    </Button>
+                  );
+                })}
+              </div>
+            </CardBody>
+          </Card>
         );
       })}
     </div>
