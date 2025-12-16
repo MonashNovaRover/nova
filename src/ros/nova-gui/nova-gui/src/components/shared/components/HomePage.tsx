ca591d32664397176cbb1f85c76b5d2c2ec16d09
Ivan Li <ivanli10252006@gmail.com>
2025-12-17 00:39:38 +1100

    Remove temporary width values and clean code

--- src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
+++ src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
@@ -1,34 +1,31 @@
 import { Button, Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { HomePageProps } from "../../../views/shared/HomePageView";
 
 export const HomePage = (props: HomePageProps) => {
-  const location = useLocation();
   const navigate = useNavigate();
 
   const navigationData = props.navigationData
 
   return (
-    <div className="flex justify-center items-center h-screen gap-5">
+    <div className="flex flex-wrap justify-center items-center h-screen gap-10 p-3">
       {Object.keys(navigationData).map((item) => {
         return (
-          // TEMP: className values setting width and height in card are temporary!!
-          <Card key={item} className="w-[400px] h-[400px]">
-            <CardHeader className="flex gap-3 justify-center">
-              <div className="text-sm font-light">{item}</div>
+          <Card key={item} className="w-[20%]">
+            <CardHeader className="flex justify-center">
+              <div className="text-lg font-semibold text-white">{item}</div>
             </CardHeader>
             <Divider />
             <CardBody>
-              <div className="flex flex-col gap-2 mt-2">
+              <div className="flex flex-col gap-2 p-3">
                 {navigationData[item].map((mode) => {
                   return (
                     <Button
                       onPress={() => navigate(mode.route)}
                       size="md"
-                      variant="bordered"
+                      variant="ghost"
                       color="default"
-                      fullWidth
-                      className="pl-3"
+                      className="transition-transform duration-300 ease-in-out hover:scale-105"
                       key={mode.route}
                       startContent={mode.icon}
                     >
