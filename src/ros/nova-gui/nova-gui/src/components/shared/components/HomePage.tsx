42076890d1c078d06f8142c5ad3c48265aed9363
Ivan Li <ivanli10252006@gmail.com>
2025-12-18 11:39:35 +1100

    Improve UI design

--- src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
+++ src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Button, Card } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import { HomePageProps } from "../../../views/shared/HomePageView";
 
@@ -8,32 +8,28 @@ export const HomePage = (props: HomePageProps) => {
   const navigationData = props.navigationData
 
   return (
-    <div className="flex flex-col items-center h-screen gap-10 p-3 pt-15 pb-20">
+    <div className="flex flex-col items-center h-[calc(100vh-5rem)] gap-10 py-5 px-10">
       {Object.keys(navigationData).map((item) => {
         return (
-          <Card key={item} className="w-[90%] flex-1">
-            <CardHeader className="pl-4">
-              <div className="text-lg font-semibold text-white">{item}</div>
-            </CardHeader>
-            <CardBody>
-              <div className="flex gap-10 p-5 h-full">
-                {navigationData[item].map((mode) => {
-                  return (
-                    <Button
-                      onPress={() => navigate(mode.route)}
-                      size="lg"
-                      variant="ghost"
-                      color="default"
-                      className="flex-1 h-full transition-transform duration-300 ease-in-out hover:scale-105"
-                      key={mode.route}
-                      startContent={mode.icon}
-                    >
-                        {mode.title}
-                    </Button>
-                  );
-                })}
-              </div>
-            </CardBody>
+          <Card key={item} fullWidth className="flex-1 p-5">
+            <div className="text-lg font-semibold text-white">{item}</div>
+            <div className="flex gap-10 p-5 h-full">
+              {navigationData[item].map((mode) => {
+                return (
+                  <Button
+                    onPress={() => navigate(mode.route)}
+                    size="lg"
+                    variant="ghost"
+                    color="default"
+                    className="flex-1 h-full transition-transform duration-300 ease-in-out hover:scale-105"
+                    key={mode.route}
+                    startContent={mode.icon}
+                  >
+                      {mode.title}
+                  </Button>
+                );
+              })}
+            </div>
           </Card>
         );
       })}
