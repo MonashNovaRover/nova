9a06aa28544e7811eedd01c864a681044cc21469
Ivan Li <ivanli10252006@gmail.com>
2025-12-20 18:25:24 +1100

    Change padding and gap size in card

--- src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
+++ src/ros/nova-gui/nova-gui/src/components/shared/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import { HomePageProps } from "../../../views/shared/HomePageView";
 
@@ -8,28 +8,32 @@ export const HomePage = (props: HomePageProps) => {
   const navigationData = props.navigationData
 
   return (
-    <div className="flex flex-col items-center h-[calc(100vh-6rem)] gap-5">
+    <div className="flex flex-col items-center h-[calc(100vh-6rem)] gap-3">
       {Object.keys(navigationData).map((item) => {
         return (
-          <Card key={item} fullWidth className="flex-1 p-2">
-            <div className="text-lg font-semibold text-white px-3">{item}</div>
-            <div className="flex gap-10 p-3 px-5 h-full">
-              {navigationData[item].map((mode) => {
-                return (
-                  <Button
-                    onPress={() => navigate(mode.route)}
-                    size="lg"
-                    variant="ghost"
-                    color="default"
-                    className="flex-1 h-full transition-transform duration-300 ease-in-out hover:scale-105"
-                    key={mode.route}
-                    startContent={mode.icon}
-                  >
+          <Card key={item} fullWidth className="flex-1">
+            <CardHeader className="text-lg font-semibold text-white">
+              {item}
+            </CardHeader>
+            <CardBody>
+              <div className="flex gap-3 p-0 h-full">
+                {navigationData[item].map((mode) => {
+                  return (
+                    <Button
+                      onPress={() => navigate(mode.route)}
+                      size="lg"
+                      variant="ghost"
+                      color="default"
+                      className="flex-1 h-full text-3xl font-medium gap-3"
+                      key={mode.route}
+                      startContent={mode.icon}
+                    >
                       {mode.title}
-                  </Button>
-                );
-              })}
-            </div>
+                    </Button>
+                  );
+                })}
+              </div>
+            </CardBody>
           </Card>
         );
       })}
