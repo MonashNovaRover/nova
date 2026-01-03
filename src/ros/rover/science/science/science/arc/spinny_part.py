ff5c62d0aeb00c2e820c2aa5e415f0fbe69c15bb
binudakal <bkal0007@student.monash.edu>
2026-01-03 21:36:33 +1100

    Add SpinnyPartHardware

--- src/ros/rover/science/science/science/arc/spinny_part.py
+++ src/ros/rover/science/science/science/arc/spinny_part.py
@@ -120,6 +120,35 @@ class SpinnyPartController(Controller):
         self.pos_cmd.value = float(clamped_position)
 
 
+class SpinnyPartHardware(HardwareInterface):
+    pos_cmd: Interface
+
+    def __init__(self, contexts: Contexts):
+        super().__init__(contexts)
+        self.logger.info("SpinnyPartHardware -- I have been __init__ialized")
+        self.bus = contexts[jcan.Bus]
+
+        self.max_angle = float(self.get_parameter("max_angle").value)
+
+        self.frame_id = int(self.declare_parameter("frame_id", 0x0A0).value)
+        self.function_id = int(self.declare_parameter("function_id", 0x04).value)
+
+    def on_configure(self, command_interfaces: InterfaceCollection, state_interfaces: InterfaceCollection) -> Optional[bool]:
+        self.pos_cmd = command_interfaces["spinny/position"]
+
+        if not self.pos_cmd:
+            self.logger.error("pos_cmd not provided!")
+            return False
+
+    def on_read(self, now: float, period: float):
+        pass
+
+    def on_write(self, now: float, period: float):
+        position = self.pos_cmd.value if self.pos_cmd else 0.0
+        can_value = int((position / self.max_angle) * 255) if self.max_angle > 0 else 0
+        self.bus.send(jcan.Frame(self.frame_id, [self.function_id, can_value]))
+        
+
 if __name__ == "__main__":
     print("Setting up!")
 
@@ -130,7 +159,7 @@ if __name__ == "__main__":
 
     PythonControl(node, update_rate=5, can_bus="can1") \
         .with_controller("spinny_controller", SpinnyPartController) \
-        .with_hardware("spinny_hw", TestHardware) \
+        .with_hardware("spinny_hw", SpinnyPartHardware) \
         .with_teleop(inputs) \
         .with_jcan() \
         .spin()
