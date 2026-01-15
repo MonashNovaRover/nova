f281726c4a662efd9e0125a00cb89785cf2b8858
Victor Bartlinski <vicbart08@gmail.com>
2026-01-15 20:35:30 +1100

    added lidar launch file

--- src/ros/rover/auto/auto_bringup/launch/lidar.launch.py
+++ src/ros/rover/auto/auto_bringup/launch/lidar.launch.py
@@ -1,54 +1,55 @@
-import os
-from ament_index_python.packages import get_package_share_directory
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Monash Nova Rover Team
+
+This launch file is responsible for LiDAR.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODES:
+  - livox_lidar_publisher
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE: 	auto_bringup
+CREATION:	15/01/2026
+EDITED:     15/01/2026
+EDITED BY:  Kabilan Velmurugan Sujatha, Bailey 
+    Chessum, Victor Bartlinski
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-import launch
+from launch_ros.substitutions import FindPackageShare
 
-################### user configure parameters for ros2 start ###################
-xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
-multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
-data_src      = 0    # 0-lidar, others-Invalid data src
-publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
-output_type   = 0
-frame_id      = 'livox_frame'
-lvx_file_path = '/home/livox/livox_test.lvx'
-cmdline_bd_code = 'livox0000000001'
+def launch_setup(context, *args, **kwargs):
+    lidar_config = LaunchConfiguration('lidar_config').perform(context)
+    lidar_params = LaunchConfiguration('lidar_params')
 
-cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
-cur_config_path = cur_path + '../params'
-user_config_path = os.path.join(cur_config_path, 'MID360_config.json')
-################### user configure parameters for ros2 end #####################
-
-livox_ros2_params = [
-    {"xfer_format": xfer_format},
-    {"multi_topic": multi_topic},
-    {"data_src": data_src},
-    {"publish_freq": publish_freq},
-    {"output_data_type": output_type},
-    {"frame_id": frame_id},
-    {"lvx_file_path": lvx_file_path},
-    {"user_config_path": user_config_path},
-    {"cmdline_input_bd_code": cmdline_bd_code}
-]
-
+    return [
+        Node(
+            package='livox_ros_driver2',
+            executable='livox_ros_driver2_node',
+            name='livox_lidar_publisher',
+            output='screen',
+            parameters=[lidar_params, {'user_config_path': lidar_config}],
+        ),
+    ]
 
 def generate_launch_description():
-    livox_driver = Node(
-        package='livox_ros_driver2',
-        executable='livox_ros_driver2_node',
-        name='livox_lidar_publisher',
-        output='screen',
-        parameters=livox_ros2_params
-        )
+    auto_bringup_dir = FindPackageShare('auto_bringup')
 
-    return LaunchDescription([
-        livox_driver,
-        # launch.actions.RegisterEventHandler(
-        #     event_handler=launch.event_handlers.OnProcessExit(
-        #         target_action=livox_rviz,
-        #         on_exit=[
-        #             launch.actions.EmitEvent(event=launch.events.Shutdown()),
-        #         ]
-        #     )
-        # )
-    ])
+    declared_arguments = [
+        DeclareLaunchArgument(
+            name='lidar_config',
+            default_value=PathJoinSubstitution([auto_bringup_dir,'params','lidar_config.json']),
+            description='',
+        ),
+        DeclareLaunchArgument(
+            name='lidar_params',
+            default_value=PathJoinSubstitution([auto_bringup_dir,'params','lidar.yaml']),
+            description='',
+        ),
+    ]
+
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
