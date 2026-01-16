9cd7de025777257fe1a65f4e3e7a7cc0b7f848d5
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-01-16 18:00:08 +1100

    working

--- src/ros/rover/drive/drive_bringup/launch/drive.launch.py
+++ src/ros/rover/drive/drive_bringup/launch/drive.launch.py
@@ -69,11 +69,16 @@ def launch_setup(context, *args, **kwargs):
     params = IfElseSubstitution(auto, auto_params, nova_params)
 
     return [
+        # Node(
+        #     package='controller_manager',
+        #     executable='spawner',
+        #     arguments=['pivot_drive_controller', '--switch-timeout', '10',
+        #         '--ros-args', '--log-level', log_level]
+        # ),
         Node(
             package='controller_manager',
             executable='spawner',
-            arguments=['pivot_drive_controller', '--switch-timeout', '10',
-                '--ros-args', '--log-level', log_level]
+            arguments=['pivot_drive_controller', '--inactive']
         ),
         Node(
             package='controller_manager',
@@ -84,6 +89,12 @@ def launch_setup(context, *args, **kwargs):
             package='controller_manager',
             executable='spawner',
             arguments=['diff_drive_controller', '--inactive']
+        ),
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['ackermann_steering_controller', '--switch-timeout', '10',
+                '--ros-args', '--log-level', log_level]
         ),
         GroupAction(
             condition=UnlessCondition(gazebo),
@@ -97,7 +108,10 @@ def launch_setup(context, *args, **kwargs):
                     package='controller_manager',
                     executable='ros2_control_node',
                     parameters=[params],
-                    remappings=[('/controller_manager/robot_description', '/robot_description')],
+                    remappings=[
+                        ('/controller_manager/robot_description', '/robot_description'),
+                        ('/ackermann_steering_controller/reference', '/cmd_vel'),
+                    ],
                 ),
                 IncludeLaunchDescription(
                     condition=IfCondition(auto),
