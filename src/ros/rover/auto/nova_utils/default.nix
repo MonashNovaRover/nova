97f695e9a0f72ad39b58470784767ea45d1a4215
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-01-19 20:30:30 +1100

    LiDAR and Nav2 changes for Auto (#832)
    Co-authored-by: Monash Nova Rover <novaroverteam@monash.edu>
    Co-authored-by: Kabilan VS <kabiniki4849@gmail.com>
    Co-authored-by: Victor Bartlinski <vicbart08@gmail.com>

--- src/ros/rover/auto/nova_utils/default.nix
+++ src/ros/rover/auto/nova_utils/default.nix
@@ -1,17 +1,21 @@
-{ 
-  buildRosPackage, 
-  ament-cmake, 
-  std-msgs, 
-  geometry-msgs, 
+{
+  buildRosPackage,
+  ament-cmake,
+  std-msgs,
+  geometry-msgs,
+  sensor-msgs,
+  sensor-msgs-py,
   geographic-msgs,
-  robot-localization, 
-  nav-msgs, 
-  vision-msgs, 
-  visualization-msgs, 
-  yolo-msgs, 
-  tf2-ros, 
-  tf-transformations, 
-  pythonPackages, 
+  robot-localization,
+  nav-msgs,
+  vision-msgs,
+  cv-bridge,
+  visualization-msgs,
+  yolo-msgs,
+  tf2-ros,
+  tf-transformations,
+  rerun,
+  pythonPackages,
 }:
 
 buildRosPackage rec {
@@ -22,19 +26,27 @@ buildRosPackage rec {
     name = "nova-utils-source";
     path = ./.;
   };
-  
+
   nativeBuildInputs = [ ament-cmake ];
-  buildInputs = [ std-msgs nav-msgs ];
-  propagatedBuildInputs = with pythonPackages; [ 
-    vision-msgs 
-    visualization-msgs 
-    geometry-msgs 
-    geographic-msgs 
-    robot-localization 
-    std-msgs 
-    yolo-msgs 
-    tf2-ros 
-    tf-transformations 
+  buildInputs = [
+    std-msgs
+    nav-msgs
+  ];
+  propagatedBuildInputs = with pythonPackages; [
+    vision-msgs
+    visualization-msgs
+    geometry-msgs
+    geographic-msgs
+    robot-localization
+    std-msgs
+    yolo-msgs
+    tf2-ros
+    tf-transformations
+    rerun
+    cv-bridge
+    sensor-msgs
+    sensor-msgs-py
     pythonPackages.geographiclib
+    pythonPackages.rerun-sdk
   ];
 }
