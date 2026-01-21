351624a8cbde51df5828cf6bf71b7120c148ffe5
Monash Nova Rover <novaroverteam@monash.edu>
2026-01-21 12:30:23 +0000

    Revert "Research on performance of cuda vs vulkan vs cpu performance for ai"
    This reverts commit c08f09c7a56f2930c525a6e9fa40267d5557631d.

--- nixfiles/modules/nixos/devices/jetson/devices/orin-nano/default.nix
+++ nixfiles/modules/nixos/devices/jetson/devices/orin-nano/default.nix
@@ -24,7 +24,7 @@ in
       (final: prev: { cudaPackages = prev.cudaPackages_12_6; })
     ];
 
-    # Add cuda capabilities. Ensure cudaVersion and overlays match version from nvidia-smi
+    # Add cuda capabilities
     nixpkgs.config = {
       allowUnfree = true;
       cudaSupport = true;
