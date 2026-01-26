f1d1e6cc493a5e5c7f7ea09652fd04886afdd6ce
Jonathan Jia <22513511+Glass2721@users.noreply.github.com>
2026-01-26 11:44:53 +1100

    feat: migrate kiln to PC2 (#823)
    Also adds GenericSensorHardware PC2 hardware interface.

--- src/ros/rover/nova_generic/python_control2/python_control2/hardware_interfaces/__init__.py
+++ src/ros/rover/nova_generic/python_control2/python_control2/hardware_interfaces/__init__.py
@@ -2,3 +2,4 @@ from .HardwareInterface import HardwareInterface
 from .Direction import Direction
 from .CMDHardware import CMDHardware
 from .QCMDHardware import QCMDHardware
+from .GenericSensorHardware import GenericSensorHardware
