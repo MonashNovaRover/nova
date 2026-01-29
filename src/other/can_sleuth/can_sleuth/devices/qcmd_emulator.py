c439afcb9e7e51183d5c782d5578e533d4fbe6b0
Angel <angelloisaguinaldo@gmail.com>
2026-01-29 14:49:22 +1100

    define on message callback function

--- src/other/can_sleuth/can_sleuth/devices/qcmd_emulator.py
+++ src/other/can_sleuth/can_sleuth/devices/qcmd_emulator.py
@@ -3,39 +3,41 @@ import jcan
 
 class QCMDEmulator(candevice.CanDevice):
 
+
     def __init__(self, name, bus, canIdList):
         super.__init__(name, bus)
         self.idList = canIdList
-        #add callback functions 
+
+        self.motors = {}
+        #add callback functions and motors to motor dictionary
         for i in range(len(self.idList)):
             self.addCallback(self.idList[i], self.on_message)
+            self.motors[str(self.idList[i] & 0xf0)][1 if ]
 
     def on_message(self, message:jcan.Frame):
         """
             Process can frame
         """
-
         cmd = message.id & 0xf
+        controllerId = str(message.id & 0xf0)
+        self.control(controllerId,cmd, message.data)
 
-        match cmd:
-            case 0x1:
-                self.control(1, message.data)
-            case 0x2:
-                self.control(2, message.data)
-            case 0x3:
-                self.set_current_limit(1, message.data):
-            case 0x4:
-                self.set_current_limit(2, message.data):
-        
-    
-
-    def control(motor:int, data: list[int])
+    def control(controller:str, motorCmd:int, data: list[int])
         """
             Process qcmd control
         """
+        speed = self.pack_can_data(data)
         pass
 
-    def set_current_limit(motor, data: list[int])
+    def set_speed(speed, motor):
+        pass
+
+    def set_current_limit(currentLim, motor):
+        pass
+    
+    def pack_can_data(data:list[int]):
+        return int.from_bytes(data, signed = True, byteorder='big')
+
 
     def spin(self):
         super.spin()
