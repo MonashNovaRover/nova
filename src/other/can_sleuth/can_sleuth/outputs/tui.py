9bc778754e1a0f76b755b2ac4060a355e32a6d09
Monash Nova Rover <novaroverteam@monash.edu>
2026-01-31 16:14:33 +1100

    fix issue with disconnected BLCMDs

--- src/other/can_sleuth/can_sleuth/outputs/tui.py
+++ src/other/can_sleuth/can_sleuth/outputs/tui.py
@@ -66,7 +66,7 @@ class TUI(output.Output):
                 max_y, max_x = win.getmaxyx()
 
                 # clearly display when a device is disconnected (no telemetry)
-                if all(attr.value() is None for attr in dev.attrs):
+                if dev.attrs[0].value() is None:
                     text = "Disconnected"
                     y = max_y // 2
                     x = max(1, (max_x - len(text)) // 2)
