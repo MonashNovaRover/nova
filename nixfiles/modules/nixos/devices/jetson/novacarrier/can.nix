4beb42392f4f3604b57e609261d00dc563d3e316
Monash Nova Rover <novaroverteam@monash.edu>
2026-02-09 06:21:35 +0000

    Revert "nixfiles: can: add systemd networkd config to name canbusses correctly"
    This reverts commit 4fc0e6c6f7b5c208ac2cf84f4c2354139b3d73d3.

--- nixfiles/modules/nixos/devices/jetson/novacarrier/can.nix
+++ nixfiles/modules/nixos/devices/jetson/novacarrier/can.nix
@@ -13,9 +13,10 @@
       };
     };
 
+    # TODO: get the exact ID_PATH from `udevadm info -p $(udevadm info --query=path --path=/sys/class/net/can1)`
     systemd.network.links."20-can1" = {
       matchConfig = {
-        Path = "platform-3210000.spi-cs-0";
+        Path = "*spi0*";
         Driver = "mcp251xfd";
       };
       linkConfig = {
@@ -23,9 +24,10 @@
       };
     };
 
+    # TODO: get the exact ID_PATH from `udevadm info -p $(udevadm info --query=path --path=/sys/class/net/can2)`
     systemd.network.links."20-can2" = {
       matchConfig = {
-        Path = "platform-3230000.spi-cs-0";
+        Path = "*spi1*";
         Driver = "mcp251xfd";
       };
       linkConfig = {
