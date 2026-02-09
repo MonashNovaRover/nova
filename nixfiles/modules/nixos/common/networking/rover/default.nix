0bd9071c9a10a63f41e8491e15e73593430c0350
Orlando Chamberlain <orlandoch.dev@gmail.com>
2026-02-09 06:26:10 +0000

    fix can names

--- nixfiles/modules/nixos/common/networking/rover/default.nix
+++ nixfiles/modules/nixos/common/networking/rover/default.nix
@@ -39,6 +39,39 @@ in
           };
         };
       };
+
+      links = {
+        "20-can0" = {
+          matchConfig = {
+            Path = "platform-c310000.mttcan";
+            Driver = "mttcan";
+          };
+          linkConfig = {
+            Name = "can0";
+          };
+        };
+
+        "20-can1" = {
+          matchConfig = {
+            Path = "platform-3210000.spi-cs-0";
+            Driver = "mcp251xfd";
+          };
+          linkConfig = {
+            Name = "can1";
+          };
+        };
+
+        "20-can2" = {
+          matchConfig = {
+            Path = "platform-3230000.spi-cs-0";
+            Driver = "mcp251xfd";
+          };
+          linkConfig = {
+            Name = "can2";
+          };
+        };
+      };
+
       networks = {
         "40-br0" = {
           matchConfig.Name = "br0";
