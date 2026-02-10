6b71422adb0e56f3b10e7c4cb0d4d6627a23b5bb
Orlando Chamberlain <orlandoch.dev@gmail.com>
2026-02-10 20:42:58 +1100

    gui: nix: add script for serving built gui
    eg
    
    result/bin/serve-gui -p 8080
    
    to run on port 8080.

--- src/ros/nova-gui/nix/packages/gui/default.nix
+++ src/ros/nova-gui/nix/packages/gui/default.nix
@@ -12,6 +12,7 @@
 , nova-interfaces
 , nova-camera-msgs
 , nova-science-interfaces
+, static-web-server
 }:
 
 let
@@ -57,7 +58,7 @@ mkYarnPackage {
     # without this, the built css file is missing 2/3rds of the content
     # maybe the deps we pull in aren't specifically marked as deps of nova-gui
     rm deps/nova-gui/node_modules
-    ln -s $PWD/node_modules deps/nova-gui/node_modules
+    ln -s "$PWD/node_modules" deps/nova-gui/node_modules
 
     yarn --offline build
 
@@ -69,6 +70,19 @@ mkYarnPackage {
 
     mkdir -p "$out/share/nova-gui"
     cp -r deps/nova-gui/dist "$out/share/nova-gui/www"
+
+    mkdir -p "$out/bin/"
+
+    echo "#!/bin/bash
+    ${static-web-server}/bin/static-web-server --root $out/share/nova-gui/www --page-fallback $out/share/nova-gui/www/index.html --cache-control-headers false \$@
+    err=\$?
+    if [ \$err -ne 0 ]; then
+      echo If it says permission denied, maybe you want to specify a non-default port, e.g.
+      echo -e \\\\t \$0 -p 8080
+    fi
+    exit \$err
+    " > "$out/bin/serve-gui"
+    chmod +x "$out/bin/serve-gui"
 
     runHook postInstall
   '';
