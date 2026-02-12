45c64e0cf302c9ed885e13b862b6e7d6a20368c5
Orlando Chamberlain <orlandoch.dev@gmail.com>
2026-02-13 02:08:35 +1100

    gui: vite: use relative path

--- src/ros/nova-gui/nova-gui/vite.config.ts
+++ src/ros/nova-gui/nova-gui/vite.config.ts
@@ -5,6 +5,7 @@ import glsl from 'vite-plugin-glsl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), glsl()],
+  base: "",
   build: {
     assetsDir: "assets",
   },
