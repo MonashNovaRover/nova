0c3a07eccafefefcbe7a7ff27f1ce8b5163815b7
Tash Lee <tlee0160@student.monash.edu>
2026-02-18 14:48:43 +1100

    fix launch file Node syntax

--- src/ros/rover/science/science_bringup/launch/arc.launch.py
+++ src/ros/rover/science/science_bringup/launch/arc.launch.py
@@ -39,6 +39,16 @@ def launch_setup(context, *args, **kwargs):
     return [
         # Analysis Arm - Nodes for components on the analysis arm
         Node(
+            name='time_of_flight_sensor',
+            package='science',
+            executable='time_of_flight.py',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                science_params
+            ],
+        ),
+        Node(
             name='analysis_arm',
             package='science',
             executable='analysis_arm_stepper.py',
@@ -58,16 +68,6 @@ def launch_setup(context, *args, **kwargs):
                 science_params,
             ],
         ),
-        Node(
-            name='time_of_flight_sensor',
-            package='science',
-            executable='time_of_flight.py',
-            output='screen',
-            emulate_tty=True,
-            parameters=[
-                science_params
-            ],
-        )
 
         # CBeam - Nodes for components on the CBeam
         Node(
@@ -141,6 +141,7 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
 
+        
         # Misc - Nodes for misc components
         Node(
             name='scimbal_cam',
