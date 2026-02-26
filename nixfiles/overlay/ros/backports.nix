c3880a1786b38da065ff7fd367d85bbaa2ad2f15
Monash Nova Rover <novaroverteam@monash.edu>
2026-02-26 21:37:01 +1100

    Backport to add route_tool to nav2-rviz-plugins.

--- nixfiles/overlay/ros/backports.nix
+++ nixfiles/overlay/ros/backports.nix
@@ -69,6 +69,19 @@ self: super:
           ];
         });
 
+        # nav2_rviz_plugins: Add route_tool
+        # nav2-rviz-plugins = rosSuper.nav2-rviz-plugins.overrideAttrs ({ patches ? [ ], ... }: {
+        #   patchFlags = [ "-p2" ];
+        #   patches = patches ++ [
+        #     # Backport of nav2_rviz_plugins::RouteTool plugin to Jazzy
+        #     # https://github.com/ros-navigation/navigation2/issues/5663
+        #     (self.fetchpatch {
+        #       url = "https://github.com/ros-navigation/navigation2/commit/617ede9c16665eaf59bb4a55969ff44496e58617.diff";
+        #       hash = "sha256-2SFH6gtQFWuultdMrdxLyZ//jr6tFEuCYsKihVd7QdM=";
+        #     })
+        #   ];
+        # });
+
         # nav2-smoother = rosSuper.nav2-smoother.overrideAttrs ({ patches ? [ ], ... }: {
         #   patchFlags = [ "-p2" ];
         #   patches = patches ++ [
