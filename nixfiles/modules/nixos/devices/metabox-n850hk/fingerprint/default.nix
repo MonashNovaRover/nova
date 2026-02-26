1227620ff06b7525431cc69951ce98bd435a3d04
rotaryViper <93423444+rotaryViper@users.noreply.github.com>
2026-02-26 15:12:31 +1100

    [WIP] Added configs for new devices (#908)
    Co-authored-by: Monash Nova Rover <novaroverteam@monash.edu>

--- nixfiles/modules/nixos/devices/metabox-n850hk/fingerprint/default.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, ... }:
-
-{
-  config = lib.mkIf config.devices.metabox-n850hk.enable {
-    services.fprintd.enable = lib.mkDefault true;
-  };
-}
--- nixfiles/modules/nixos/devices/metabox-v158pnh/apps/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  config = lib.mkIf config.devices.metabox-v158pnh.enable {
-    environment.systemPackages = with pkgs; [
-      (blender.override { cudaSupport = true; })
-    ];
-  };
-}
