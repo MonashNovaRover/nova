0fdbf2fbf77f32487fc556b8355c21540d21e914
Victor Bartlinski <vicbart08@gmail.com>
2026-03-03 20:48:29 +1100

    nav2-behavior-tree and nav2-rviz-plugins building (moved nav2-route from packages/ros to a nix-ros-overlay patch)

--- nixfiles/default.nix
+++ nixfiles/default.nix
@@ -42,19 +42,12 @@ let
       inherit (revisions.nix-ros-overlay) rev hash;
     };
     patches = [
-      # # fix: gz vendor
-      # # https://github.com/lopsided98/nix-ros-overlay/pull/472
-      # ./overlay/ros/patches/nix-ros-workspace.patch
-
-      # # Some more Gazebo improvements
-      # # https://github.com/muellerbernd/nix-ros-overlay/pull/2
-      # (pkgs.fetchpatch {
-      #   url = "https://github.com/lopsided98/nix-ros-overlay/compare/6d04148eac0727be34e5333f6e12cfc7e86673c3...eca9687ce15335bbb2d4b7b14fbf74ce0e957f43.patch";
-      #   hash = "sha256-c6DD2U6Lo2dcs0APxEHg9l0bz1Ioa5aX5FoATajXYAc=";
-      # })
-
       # speed up ws-build by avoiding wrapping qt apps twice.
       ./overlay/ros/patches/0001-don-t-wrap-qt-apps-for-the-whole-env.patch
+
+      # adds nav2-route to rosSuper before ./overlay is evaluated so it can 
+      # be added to nav2-msgs propogatedBuildInputs
+      ./overlay/ros/patches/add-nav2-route-overlay.patch
     ];
   });
 
