dad2f62f25e44afca4e087372611115ff8cc22c2
V01DBREAKER <anthony@v01dbreaker.cc>
2026-03-05 11:40:32 +0000

    fix: remove old shell aliases from cameras2

--- src/ros/cameras2/default.nix
+++ src/ros/cameras2/default.nix
@@ -3,9 +3,4 @@
     nova-camera-msgs = callPackage ./nix/packages/camera-msgs { };
     nova-cameras2 = callPackage ./nix/packages/cameras2 { };
   };
-
-  shellAliases = rec {
-    cameras = "ros2 launch cameras2 camera_server_launch.py platform:=rover";
-    cameras_all = "${cameras} autostart:=true";
-  };
 }
