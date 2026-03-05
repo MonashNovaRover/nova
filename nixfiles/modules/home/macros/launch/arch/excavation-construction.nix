4600b903dcd537132fcb0ecaa34e8bb86ded45e3
Tash Lee <tlee0160@student.monash.edu>
2026-03-05 16:00:45 +1100

    add relative path ./ros2

--- nixfiles/modules/home/macros/launch/arch/excavation-construction.nix
+++ nixfiles/modules/home/macros/launch/arch/excavation-construction.nix
@@ -11,9 +11,9 @@ let
   one = {
     pre = pre-shell {payload-name=task-name + " one"; need-rover=true;};
     terminals = [
-      {name = "Base:ecTeleop"; platform=base; cmd="ros2 launch teleop_ec teleop.launch.py log_inputs:=true";}
-      {name = "Rover:EC"; platform=rover; cmd="ros2 launch nova_bringup ec_rover.launch.py";}
-      {name = "Rover:Cameras"; platform=rover; cmd="~/Builds/cameras2legacy/bin/gst-nova-launcher ros2 launch cameras2 camera_server_launch.py platform:=orin param-dir:='\''/home/nova/nova/src/ros/cameras2/cameras2/params'\'' payload:=ec";}
+      {name = "Base:ecTeleop"; platform=base; cmd="./ros2 launch teleop_ec teleop.launch.py log_inputs:=true";}
+      {name = "Rover:EC"; platform=rover; cmd="./ros2 launch nova_bringup ec_rover.launch.py";}
+      {name = "Rover:Cameras"; platform=rover; cmd="cameras-orin payload:=ec";}
     ];
     post = post-shell;
   };
@@ -21,8 +21,8 @@ let
   two = {
     pre = pre-shell {payload-name=task-name + " two"; need-rover=true; };
     terminals = [
-      {name="Base:driveTeleop"; platform=base; cmd="ros2 launch teleop_drive_joy teleop.launch.py";}
-      {name = "Rover:Drive"; platform=rover; cmd="ros2 launch drive_bringup drive.launch.py";}
+      {name="Base:driveTeleop"; platform=base; cmd="./ros2 launch teleop_drive_joy teleop.launch.py";}
+      {name = "Rover:Drive"; platform=rover; cmd="./ros2 launch drive_bringup drive.launch.py";}
       {name="Base:Reolink"; platform=base; cmd="reolink";}
     ];
     post = post-shell;
