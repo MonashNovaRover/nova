be753b0d200f4785abc4494f9c88b15c08243cd8
Angel <angelloisaguinaldo@gmail.com>
2026-03-07 14:01:18 +1100

    add terminals to science launch script

--- nixfiles/modules/home/macros/launch/arch/space-resources.nix
+++ nixfiles/modules/home/macros/launch/arch/space-resources.nix
@@ -3,7 +3,8 @@
     rover,
     pre-shell,
     post-shell,
-    bashBuilder
+    bashBuilder, 
+    base-nix
 }:
 
 let 
@@ -11,7 +12,10 @@ let
   one = {
     pre = pre-shell {payload-name=task-name + " one"; need-rover=true;};
     terminals = [
-      {name = "Base:Rviz"; platform=base; cmd="ros2 launch auto_bringup rviz.launch.py";}
+      {name = "Base:scienceTeleop"; platform=base; cmd="./ros2 launch teleop_science teleop.launch.py";}
+      {name = "Base:guiRosbridge"; platform=base; cmd="./ros2 launch rosbridge_server rosbridge_websocket_launch.xml";}
+      {name = "Base:guiRun"; platform=base-nix "nix-shell ~/nova/src/../nixfiles -A pkgs.ros.nova-gui"; cmd= "gui-run";}
+      {name = "Rover:science"; platform=rover; cmd="./ros2 launch science_bringup arc.launch.py";}
     ];
     post = post-shell;
   };
@@ -19,8 +23,12 @@ let
   two = {
     pre = pre-shell {payload-name=task-name + " two"; need-rover=true; };
     terminals = [
-      {name="Base:Teleop"; platform=base; cmd="ros2 launch teleop_drive_joy teleop.launch.py";}
-      {name = "Rover:Drive"; platform=rover; cmd="launch-drive";}
+      {name = "Base:driveTeleop"; platform=base; cmd="./ros2 launch teleop_drive_joy teleop.launch.py";}
+      {name = "Rover:drive"; platform=rover; cmd="./ros2 launch drive_bringup drive.launch.py";}
+      {name = "Rover:scienceCameras"; platform=rover; cmd="cameras-orin payload:=science";}
+      {name = "Rover:reolink"; platform=base-nix "nix-shell -p ffmpeg"; cmd="reolink";}
+      {name = "Rover:reolinkCtl"; platform=rover "nix-shell -p ffmpeg"; cmd="~/Builds/master/bin/reolink-ctl";}
+
     ];
     post = post-shell;
   };
