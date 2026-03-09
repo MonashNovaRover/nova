803745429c450ba77966ef8eac5253b4c1222364
Aiken <chikitlee2001@gmail.com>
2026-03-09 20:15:18 +1100

    add scale to microscope camera

--- src/ros/nova-gui/nova-gui/src/components/science/MicroscopeThresholdWidget/MicroscopeCamerasWidget.tsx
+++ src/ros/nova-gui/nova-gui/src/components/science/MicroscopeThresholdWidget/MicroscopeCamerasWidget.tsx
@@ -1,7 +1,7 @@
-import {CameraComponent} from "../../cameras/CameraComponent/CameraComponent.tsx";
 import {CameraSerials} from "../../../views/shared/CamerasPage/CameraPageConstants.tsx";
 import SiteSelectWidget from "../SiteSelectWidget/SiteSelectWidget.tsx";
 import SerialMappedCameraComponent from "../../../views/shared/CamerasPage/SerialMappedCameraComponent.tsx";
+import MicroscopeScaleOverlayedCameraComponent from "../../cameras/CameraComponent/special/MicroscopeScaleOverlayedCameraComponent.tsx";
 
 /**
  * Widget for microscope camera feed and ML output
@@ -10,7 +10,7 @@ import SerialMappedCameraComponent from "../../../views/shared/CamerasPage/Seria
 function MicroscopeCamerasWidget () {
   return (
     <div className="p-0">
-      <CameraComponent cameraSerial={CameraSerials.SCIENCE_MICROSCOPE}/>
+      <MicroscopeScaleOverlayedCameraComponent cameraSerial={CameraSerials.SCIENCE_MICROSCOPE}/>
       <SiteSelectWidget pickerClassName="mt-4 mb-1"/>
       <div className="grid grid-cols-2 gap-3 mt-3">
         <SerialMappedCameraComponent cameraSerial={CameraSerials.SCIENCE_GIMBAL}/>
