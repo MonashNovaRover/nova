080d0b77d560dd3653b5939177cfc83cc8cbb578
Tash <tlee0160@student.monash.edu>
2026-03-11 10:32:30 +1100

    Ec launch scripts (#925)
    Signed-off-by: Tash Lee <tashkeni.c@gmail.com>
    Co-authored-by: Tash Lee <tashkeni.c@gmail.com>
    Co-authored-by: Jonathan Jia <22513511+Glass2721@users.noreply.github.com>

--- nixfiles/modules/home/macros/launch/arch/excavation-construction.nix
+++ nixfiles/modules/home/macros/launch/arch/excavation-construction.nix
@@ -1,5 +1,8 @@
 { 
     base,
+    base-nix,
+    base-window,
+    base-window-nix,
     rover,
     pre-shell,
     post-shell,
@@ -11,7 +14,9 @@ let
   one = {
     pre = pre-shell {payload-name=task-name + " one"; need-rover=true;};
     terminals = [
-      {name = "Base:Rviz"; platform=base; cmd="ros2 launch auto_bringup rviz.launch.py";}
+      {name = "Base:ecTeleop"; platform=base; cmd="./ros2 launch teleop_ec teleop.launch.py log_inputs:=true";}
+      {name = "Rover:EC"; platform=rover; cmd="./ros2 launch nova_bringup ec_rover.launch.py";}
+      {name = "Rover:Cameras"; platform=rover; cmd="cameras-orin payload:=ec";}
     ];
     post = post-shell;
   };
@@ -19,8 +24,10 @@ let
   two = {
     pre = pre-shell {payload-name=task-name + " two"; need-rover=true; };
     terminals = [
-      {name="Base:Teleop"; platform=base; cmd="ros2 launch teleop_drive_joy teleop.launch.py";}
-      {name = "Rover:Drive"; platform=rover; cmd="launch-drive";}
+      {name="Base: drive-teleop"; platform=base; cmd="./ros2 launch teleop_drive_joy teleop.launch.py";}
+      {name = "Rover: drive"; platform=rover; cmd="./ros2 launch drive_bringup drive.launch.py";}
+      {name="Base: reolink"; platform=base-nix "nix-shell -p ffmpeg"; cmd="reolink";}
+      {name="Base: reolink-ctl"; platform=base-window; cmd="./reolink-ctl";}
     ];
     post = post-shell;
   };
