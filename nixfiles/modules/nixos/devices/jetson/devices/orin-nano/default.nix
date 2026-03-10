c1464ef3054c91165f21b914cf945bee86679f1b
Orlando Chamberlain <orlandoch.dev@gmail.com>
2026-03-10 16:52:39 +1100

    nixfiles: networking: fix rover to not use bridges (#936)

--- nixfiles/modules/nixos/devices/jetson/devices/orin-nano/default.nix
+++ nixfiles/modules/nixos/devices/jetson/devices/orin-nano/default.nix
@@ -23,6 +23,40 @@ in
       ethernetInterface = "enP8p1s0";
     };
 
+    systemd.network = {
+      links = {
+        "20-can0" = {
+          matchConfig = {
+            Path = "platform-c310000.mttcan";
+            Driver = "mttcan";
+          };
+          linkConfig = {
+            Name = "can0";
+          };
+        };
+
+        "20-can1" = {
+          matchConfig = {
+            Path = "platform-3210000.spi-cs-0";
+            Driver = "mcp251xfd";
+          };
+          linkConfig = {
+            Name = "can1";
+          };
+        };
+
+        "20-can2" = {
+          matchConfig = {
+            Path = "platform-3230000.spi-cs-0";
+            Driver = "mcp251xfd";
+          };
+          linkConfig = {
+            Name = "can2";
+          };
+        };
+      };
+    };
+
     # Apply global cuda overlays
     nixpkgs.overlays = [
       (final: _: { inherit (final.nvidia-jetpack) cudaPackages; })
