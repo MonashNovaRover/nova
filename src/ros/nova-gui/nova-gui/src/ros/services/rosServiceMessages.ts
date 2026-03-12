a61df3b0846b9b0a2ff7ecb55f343a02265da164
Monash Nova Rover <novaroverteam@monash.edu>
2026-03-12 18:32:11 +1100

    Adding a power cycle button

--- src/ros/nova-gui/nova-gui/src/ros/services/rosServiceMessages.ts
+++ src/ros/nova-gui/nova-gui/src/ros/services/rosServiceMessages.ts
@@ -42,6 +42,7 @@ export const rosServiceMessages = {
   [RosService.RESET_TOF]: "std_srvs/srv/Trigger",
   [RosService.TOOL_ROTATOR_PRESETS]: "science_interfaces/srv/SetPositionPresets",
   [RosService.TOOL_ROTATOR_POSITION]:"science_interfaces/srv/SetPosition",
+  [RosService.POWER_CYCLE_SCIENCE]: "science_interfaces/srv/PowerCycleScience",
 
   // General
   [RosService.RGBInput]: "nova_interfaces/srv/RGBInput",
