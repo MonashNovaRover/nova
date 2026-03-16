a6ac1ed8ea6af82f9dff6ced967ea3633a523746
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-03-17 03:08:47 +1100

    Disabled desktop idle time (laptops won't dim automatically) (#976)

--- nixfiles/modules/home/desktop/default.nix
+++ nixfiles/modules/home/desktop/default.nix
@@ -36,7 +36,7 @@ in
       };
 
       "org/gnome/desktop/session" = {
-        idle-delay = lib.hm.gvariant.mkUint32 (15 * 60); # 15 minutes before screen off
+        idle-delay = lib.hm.gvariant.mkUint32 0; # disabled because auto can't touch the laptops
       };
 
       "org/gnome/desktop/interface" = {
