a28a85dc702aa1ece96106a4abc233d0a36f9c36
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-03-17 21:04:12 +1100

    Configure ptpd service (#977)
    Co-authored-by: Monash Nova Rover <novaroverteam@monash.edu>

--- nixfiles/modules/nixos/common/services/ptpd/default.nix
+++ nixfiles/modules/nixos/common/services/ptpd/default.nix
@@ -10,18 +10,27 @@ in
         type = lib.types.str;
         description = "network interface name that PTP should run on";
       };
+      priority = lib.mkOption {
+        type = lib.types.int;
+        description = "priority of the PTP daemon (lower is higher priority, default is 128, max is 248)";
+        default = 128;
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
     systemd.services.ptpd = {
       enable = true;
-      after = [ "network.target" ];
-      wantedBy = [ "default.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
       description = "Precision Time Protocol Daemon";
+      startLimitIntervalSec = 0;
       serviceConfig = {
         Type = "simple";
-        ExecStart = ''${pkgs.nova.ptpd}/bin/ptpd2 -C -m -i ${cfg.interfaceName}'';
+        ExecStart = ''${pkgs.nova.ptpd}/bin/ptpd2 -C -m -i ${cfg.interfaceName} --ptpengine:priority1=${toString cfg.priority}'';
+        Restart = "on-failure";
+        RestartSec = 2;
       };
     };
   };
