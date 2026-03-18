4485e57ec69ea80776058265a32186ffa2883ec1
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-03-19 00:54:31 +1100

    prioritise CPU power for fastlivo2

--- src/ros/rover/auto/auto_bringup/launch/lidar.launch.py
+++ src/ros/rover/auto/auto_bringup/launch/lidar.launch.py
@@ -112,6 +112,8 @@ def launch_setup(context, *args, **kwargs):
         executable='fastlivo_mapping',
         name='fastlivo2',
         output='screen',
+        # gives 66.67% of CPU under 100% load assuming all other processes have default weight of 100
+        prefix='systemd-run --scope -p CPUWeight=200 --unit=fastlivo2',
         parameters=[fastlivo2_rewritten_params, extrinsics_params,
                     {'save_folder': output_dir}],
     )
