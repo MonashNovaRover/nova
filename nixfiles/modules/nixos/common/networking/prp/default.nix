d09b15c09ce588776c50bbac2274cbdf3f421aab
Monash Nova Rover <novaroverteam@monash.edu>
2026-03-19 16:33:57 +1100

    fail to get vxlan to work
    on orin it can't seem to have vxlan as the slave1 (netlink says device
    already exists?)
    
    i can't convince it to set the mac address manually of prp0 on orin too,
    it seems to only be able to get it from the eth.
    
    maybe this could work but i will give up on vxlan for now

--- nixfiles/modules/nixos/common/networking/prp/default.nix
+++ nixfiles/modules/nixos/common/networking/prp/default.nix
@@ -74,6 +74,7 @@ in {
           netdevConfig = {
             Kind = "vxlan";
             Name = "vxlan0";
+            MACAddress = "9e:06:05:54:78:07";
           };
           vxlanConfig = {
             VNI = 7; # Kevin's choice
@@ -140,10 +141,11 @@ in {
       [NetDev]
       Kind = hsr
       Name = prp0
+      MACAddress=9e:06:05:54:78:07
       [HSR]
       Protocol = prp
-      Ports = vxlan0
       Ports = ${netcfg.ethernetInterface}
+      Ports = vxlan0
     '';
       #Ports = ${netcfg.secondaryEthernetInterface}
 
