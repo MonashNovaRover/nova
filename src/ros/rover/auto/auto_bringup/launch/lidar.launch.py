a10384700e2b1055e91700b7dc5ed61bb9040b5f
Monash Nova Rover <novaroverteam@monash.edu>
2026-03-19 12:14:18 +1100

    run systemd as user for fastlivo2

--- src/ros/rover/auto/auto_bringup/launch/lidar.launch.py
+++ src/ros/rover/auto/auto_bringup/launch/lidar.launch.py
@@ -101,7 +101,7 @@ def launch_setup(context, *args, **kwargs):
         name='fastlivo2',
         output='screen',
         # gives 66.67% of CPU under 100% load assuming all other processes have default weight of 100
-        prefix='systemd-run --scope -p CPUWeight=200 --unit=fastlivo2',
+        prefix='systemd-run --scope --user -p CPUWeight=200 --unit=fastlivo2',
         parameters=[fastlivo2_params, extrinsics_params,
                     {'use_sim_time': sim,
                      'save_folder': output_dir,
