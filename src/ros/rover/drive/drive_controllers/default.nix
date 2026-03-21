7fa952808d85fb14590d8dc827243aa420a87078
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-03-21 11:03:31 +1100

    Fix diff-drive-controller collision warning

--- src/ros/rover/drive/drive_controllers/default.nix
+++ src/ros/rover/drive/drive_controllers/default.nix
@@ -7,4 +7,6 @@ with pkgs;
   nova-pivot-drive-controller = callPackage ./pivot_drive_controller { };
   nova-strafe-drive-controller = callPackage ./strafe_drive_controller { };
   nova-diff-drive-controller = callPackage ./diff_drive_controller { };
+  # Override the upstream diff-drive-controller so only one version ends up in the workspace.
+  diff-drive-controller = callPackage ./diff_drive_controller { };
 }
