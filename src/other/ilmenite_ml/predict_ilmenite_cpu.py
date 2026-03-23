3301557f4c2336b7a8ee0425aef0d953f794d8c6
angelaguinaldo <126725144+angelaguinaldo@users.noreply.github.com>
2026-03-23 16:37:37 +1100

    Fix: change normalisation values of predict-ilmenite-cpu (#999)
    Co-authored-by: Monash Nova Rover <novaroverteam@monash.edu>

--- src/other/ilmenite_ml/predict_ilmenite_cpu.py
+++ src/other/ilmenite_ml/predict_ilmenite_cpu.py
@@ -53,7 +53,7 @@ model.eval()
 transform = transforms.Compose([
     transforms.Resize((IMG_SIZE, IMG_SIZE)), 
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 # Load the images from the directory
