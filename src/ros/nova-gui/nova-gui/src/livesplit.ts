12bbdb6f501fc24641745a6e89141efd6a0a0619
Monash Nova Rover <novaroverteam@monash.edu>
2026-03-29 01:18:09 +1030

    added auto-finish at the end of the task

--- src/ros/nova-gui/nova-gui/src/livesplit.ts
+++ src/ros/nova-gui/nova-gui/src/livesplit.ts
@@ -1,5 +1,15 @@
 const ws = new WebSocket("ws://localhost:16834/livesplit");
 
+const intervalId = setInterval(() => {
+  console.log("checking time");
+  const now = new Date();
+  if (now.getHours() === 9 && now.getMinutes() === 55) {
+    for (let i = 0; i < 16; i++) {
+      ws.send("startorsplit");
+  };
+  }
+}, 10000);
+
 export default (taskname: string) => {
   if (taskname == "clear") {
     localStorage.clear();
