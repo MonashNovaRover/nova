a945532a53e30756279f26182ca438662a669bb3
V01DBREAKER <anthony@v01dbreaker.cc>
2026-04-10 09:12:19 +0000

    fix: cartographer goal linting error for real this time

--- src/ros/nova-gui/nova-gui/src/components/auto/Cartographer/components/CartographerGoalModal.tsx
+++ src/ros/nova-gui/nova-gui/src/components/auto/Cartographer/components/CartographerGoalModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffectEvent, useState } from "react";
 import {
   DndContext,
   closestCenter,
@@ -84,9 +84,9 @@ export const CartographerGoalModal: React.FC<{
   };
 
   // idk how to fix this linting error cause i dont get what this is meant to do @Felicity
-  useEffect(() => {
+  useEffectEvent(() => {
     setItems(points.map((point) => ({ ...point, selected: false })));
-  }, [isOpen, points]);
+  });
 
   const renderPoints = (points: MapPoint[], isSortable: boolean) => {
     return points.map((point) => (
