98a42e98c9dde2b6a524aa0e91898a15a62d54da
rotaryViper <93423444+rotaryViper@users.noreply.github.com>
2026-04-15 23:25:21 +1000

    Cameras3 upgrade 2 (#1023)
    Co-authored-by: Monash Nova Rover <novaroverteam@monash.edu>

---
+++ src/ros/cameras/cameras3/src/pipelines/h264passthrough.cpp
@@ -0,0 +1,130 @@
+#include <string>
+
+#include <gst/gst.h>
+#include "rclcpp/rclcpp.hpp"
+#include <camera_msgs/msg/camera.hpp>
+
+#include "pipelines/properties.hpp"
+#include "pipelines/pipelines.hpp"
+#include "properties/common.hpp"
+
+#include "properties/sources.hpp"
+#include "properties/sinks.hpp"
+
+#include "properties/capsfilters.hpp"
+#include "properties/cpufilters.hpp"
+
+#include "properties/h264.hpp"
+#include "cameras/colors.hpp"
+
+/*
+ * V4l camera (h264) to webrtc pipeline (direct)
+ * Enforces alignment from h264 v4l camera and feeds directly to webrtc 
+ * gst-launch-1.0 v4l2src device={props->node} ! {props->mime},width={props->width},height={props->height},framerate={props->framerate}/1,alignment={props->alignment},stream-format={props->stream_format},format={props->format}! webrtcsink meta='meta, serial=(string){props->serial}' video-caps=video/x-h264
+ */
+
+GstElement* h264passthrough_pipeline(rclcpp::Node* streamer_node, h264passthroughPipelineProperties* props)
+{
+  // 1. Create the elements
+  GstElement* gst_pipeline = gst_pipeline_new(props->serial.c_str());
+  GstElement* source = gst_element_factory_make("v4l2src", "video-source");
+  GstElement* srcfilter = gst_element_factory_make("capsfilter", "srcfilter");
+  GstElement* parse = gst_element_factory_make("h264parse", "parser");
+  GstElement* webrtc = gst_element_factory_make("webrtcsink", "webrtc");
+  GstElement* payload = (props->payload_quirk) ? gst_element_factory_make("rtph264pay", "payloader") : nullptr;
+  GstElement* depayload = (props->payload_quirk) ? gst_element_factory_make("rtph264depay", "depayloader") : nullptr;
+
+  if (!gst_pipeline || !source || !srcfilter || !parse || !webrtc || (props->payload_quirk && !payload) || (props->payload_quirk && !depayload)) {
+      RCLCPP_ERROR(streamer_node->get_logger(), "%sCould not create pipeline for %s%s%s", C_FAIL, C_TITLE, props->serial.c_str(), C_RESET);
+      return nullptr;
+  }
+
+  // 2. Set element properties
+  set_v4lsource(source, props->device, props->io_mode);
+  set_srcfilter(srcfilter, props->mime, props->width, props->height, props->framerate, props->framerate_denominator, props->downrate, props->brightness, props->contrast);
+  if (props->payload_quirk) set_h264payload(payload);
+  set_h264parse(parse);
+  set_webrtcsink(webrtc, props->serial, props->video_caps, props->do_fec, props->do_retransmission, props->congestion_control, props->bitrate);
+
+  // 3. Add elements to pipeline
+  gst_bin_add_many(GST_BIN(gst_pipeline), source, srcfilter, parse, webrtc, NULL);
+  if (props->payload_quirk) gst_bin_add_many(GST_BIN(gst_pipeline), payload, depayload, NULL);
+
+  // 4. Link elements
+  if (!link_elements(streamer_node, source, srcfilter, props->serial)) return nullptr;
+
+  if (props->payload_quirk) {
+    if (!link_elements(streamer_node, srcfilter, payload, props->serial)) return nullptr;
+    if (!link_elements(streamer_node, payload, depayload, props->serial)) return nullptr;
+    if (!link_elements(streamer_node, depayload, parse, props->serial)) return nullptr;
+  } else {
+    if (!link_elements(streamer_node, srcfilter, parse, props->serial)) return nullptr;
+  }
+  if (!link_elements(streamer_node, parse, webrtc, props->serial)) return nullptr;
+
+  return gst_pipeline;
+}
+
+
+/*
+ * Retrieve ros2 parameters for h264passthrough pipeline or sets defaults
+*/
+
+h264passthroughPipelineProperties* get_h264passthrough_pipeline_properties(rclcpp::Node* streamer_node, camera_msgs::msg::Camera* camera, const std::string pipeline_type, const std::string profile)
+{
+  // 0. Initialize constants
+  h264passthroughPipelineProperties* props = new h264passthroughPipelineProperties;
+  props->serial = camera->serial;
+  props->node = camera->node;
+  props->original_serial = camera->original_serial;
+
+  // 1. Define default properties
+  std::string default_string;
+
+  // source
+  props->device = camera->node;
+
+  props->io_mode = 4; // dmabuf
+
+  props->verify_resolution = set_property(streamer_node, camera->serial, profile, camera->original_serial, "verify_resolution", false);
+
+  // rate
+  props->downrate = 1; // Do not change framerate
+
+  // filter
+  props->mime = "video/x-h264";
+
+  props->brightness = set_property(streamer_node, camera->serial, profile, camera->original_serial, "brightness", 0);
+  props->contrast = set_property(streamer_node, camera->serial, profile, camera->original_serial, "contrast", 0);
+  props->framerate = set_property(streamer_node, camera->serial, profile, camera->original_serial, "framerate", 30);
+  props->framerate_denominator = set_property(streamer_node, camera->serial, profile, camera->original_serial, "framerate_denominator", 1);
+  props->height = set_property(streamer_node, camera->serial, profile, camera->original_serial, "height", 720);
+  props->width = set_property(streamer_node, camera->serial, profile, camera->original_serial, "width", 1280);
+
+  // payloader
+  props->payload_quirk = set_property(streamer_node, camera->serial, profile, camera->original_serial, "payload_quirk", false);
+
+  // webrtc
+  default_string = "gcc";
+  props->congestion_control = set_property(streamer_node, camera->serial, profile, camera->original_serial, "congestion_control", default_string);
+  props->video_caps = "video/x-h264";
+
+  props->bitrate = set_property(streamer_node, camera->serial, profile, camera->original_serial, "bitrate", 4096);
+
+  props->do_fec = set_property(streamer_node, camera->serial, profile, camera->original_serial, "do_fec", false);
+  props->do_retransmission = set_property(streamer_node, camera->serial, profile, camera->original_serial, "do_retransmission", false);
+
+  // 2. Finalize props
+  if (props->verify_resolution) {
+    if (verify_v4lresolution(props->device, &props->mime, &props->width, &props->height, &props->framerate, &props->framerate_denominator)) {
+        RCLCPP_INFO(streamer_node->get_logger(), "%sInitialized pipeline: %s%s%s for %s%s%s with profile: %s%s %dx%d@%dfps%s", C_QUIET, C_INPUT, pipeline_type.c_str(), C_QUIET, C_TITLE, props->serial.c_str(), C_QUIET, C_MODE, profile.c_str(), props->width, props->height, props->framerate/props->framerate_denominator, C_RESET);
+    } else {
+        RCLCPP_ERROR(streamer_node->get_logger(), "%sWrong resolution!%s Fallback pipeline: %s%s%s for %s%s%s with profile: %s%s %dx%d@%dfps%s", C_FAIL, C_QUIET, C_INPUT, pipeline_type.c_str(), C_QUIET, C_TITLE, props->serial.c_str(), C_QUIET, C_MODE, profile.c_str(), props->width, props->height, props->framerate/props->framerate_denominator, C_RESET);
+    }
+  } else {
+      RCLCPP_INFO(streamer_node->get_logger(), "%sInitialized pipeline: %s%s%s for %s%s%s with profile: %s%s %dx%d@%dfps%s", C_QUIET, C_INPUT, pipeline_type.c_str(), C_QUIET, C_TITLE, props->serial.c_str(), C_QUIET, C_MODE, profile.c_str(), props->width, props->height, props->framerate/props->framerate_denominator, C_RESET);
+  }
+
+  return props;
+}
+
---
+++ src/ros/cameras/cameras3/src/properties/common.cpp
@@ -0,0 +1,152 @@
+#include <string>
+#include <stdlib.h>
+#include <cstring>
+#include <gst/gst.h>
+#include "rclcpp/rclcpp.hpp"
+#include <camera_msgs/msg/camera.hpp>
+#include <systemd/sd-device.h>
+
+#include "properties/common.hpp"
+#include "pipelines/pipelines.hpp"
+
+bool link_elements(rclcpp::Node* streamer_node, GstElement* first_element, GstElement* second_element, const std::string serial) {
+   if (!gst_element_link(first_element, second_element)) {
+      RCLCPP_ERROR(streamer_node->get_logger(), "Could not link %s to %s for %s", gst_object_get_name(GST_OBJECT(first_element)), gst_object_get_name(GST_OBJECT(second_element)), serial.c_str());
+      return false;
+   }
+   return true;
+}
+
+std::string set_property(rclcpp::Node* streamer_node, const std::string serial, const std::string profile, const std::string original_serial, const std::string element, const std::string default_value) {
+    std::string value;
+    // Check serial for property
+    if (streamer_node->get_parameter<std::string>((std::string(PIPELINE_PREFIX) + "." + serial + "." + element).c_str(), value)) return value;
+    // Check profile for property
+    if (!profile.empty()) {
+      if (streamer_node->get_parameter<std::string>((std::string(PROFILE_PREFIX) + "." + original_serial + "." + profile + "." + element).c_str(), value)) return value;
+      if (streamer_node->get_parameter<std::string>((std::string(PROFILE_PREFIX) + "." + std::string(UNKNOWN_PROFILE_PREFIX) + "." + profile + "." + element).c_str(), value)) return value;
+    }
+    // Check default for property
+    if (streamer_node->get_parameter<std::string>((std::string(DEFAULT_PREFIX) + "." + original_serial + "." + element).c_str(), value)) return value;
+    return default_value;
+}
+
+int set_property(rclcpp::Node* streamer_node, const std::string serial, const std::string profile, const std::string original_serial, const std::string element, const int default_value) {
+    int value;
+    // Check serial for property
+    if (streamer_node->get_parameter((std::string(PIPELINE_PREFIX) + "." + serial + "." + element).c_str(), value)) return value;
+    // Check profile for property
+    if (!profile.empty()) {
+      if (streamer_node->get_parameter((std::string(PROFILE_PREFIX) + "." + original_serial + "." + profile + "." + element).c_str(), value)) return value;;
+      if (streamer_node->get_parameter((std::string(PROFILE_PREFIX) + "." + std::string(UNKNOWN_PROFILE_PREFIX) + "." + profile + "." + element).c_str(), value)) return value;
+
+    }
+    // Check default for property
+    if (streamer_node->get_parameter((std::string(DEFAULT_PREFIX) + "." + original_serial + "." + element).c_str(), value)) return value;
+    return default_value;
+}
+
+bool set_property(rclcpp::Node* streamer_node, const std::string serial, const std::string profile, const std::string original_serial, const std::string element, const bool default_value) {
+    bool value;
+    // Check serial for property
+    if (streamer_node->get_parameter((std::string(PIPELINE_PREFIX) + "." + serial + "." + element).c_str(), value)) return value;
+    // Check profile for property
+    if (!profile.empty()) {
+      if (streamer_node->get_parameter((std::string(PROFILE_PREFIX) + "." + original_serial + "." + profile + "." + element).c_str(), value)) return value;
+      if (streamer_node->get_parameter((std::string(PROFILE_PREFIX) + "." + std::string(UNKNOWN_PROFILE_PREFIX) + "." + profile + "." + element).c_str(), value)) return value;
+    }
+    // Check default for property
+    if (streamer_node->get_parameter((std::string(DEFAULT_PREFIX) + "." + original_serial + "." + element).c_str(), value)) return value;
+    return default_value;
+}
+
+bool verify_v4lresolution(const std::string device_name, std::string* mime, int* width, int* height, int* framerate, int* framerate_denominator) {
+  GstDeviceMonitor *monitor = gst_device_monitor_new();
+  gst_device_monitor_add_filter(monitor, "Video/Source", NULL);
+
+  GList *devices = gst_device_monitor_get_devices(monitor);
+  for (GList *l = devices; l != NULL; l = l->next) {
+      GstDevice *device = (GstDevice *)l->data;
+      GstStructure *device_props = gst_device_get_properties(device);
+      const gchar *path = gst_structure_get_string(device_props, "device.path");
+      int valid_width = 1280, valid_height = 720, framerate_n = 30, framerate_d = 1;
+      std::string valid_mime;
+
+      if (std::string(path) == device_name) {
+          GstCaps* caps = gst_device_get_caps(device);
+          for (guint i = 0; i < gst_caps_get_size(caps); i++) {
+              const GstStructure* str = gst_caps_get_structure(caps, i);
+              valid_mime = std::string(gst_structure_get_name(str));
+
+              // Width
+              const GValue* width_val = gst_structure_get_value(str, "width");
+              bool width_ok = false;
+
+              if (G_VALUE_HOLDS_INT(width_val)) {
+                  width_ok = (valid_width == *width);
+                  valid_width = g_value_get_int(width_val);
+              } else if (GST_VALUE_HOLDS_INT_RANGE(width_val)) {
+                  width_ok = (*width >= gst_value_get_int_range_min(width_val) &&
+                              *width <= gst_value_get_int_range_max(width_val));
+                  valid_width = gst_value_get_int_range_min(width_val);
+              } else if (GST_VALUE_HOLDS_LIST(width_val)) {
+                  for (guint j = 0; j < gst_value_list_get_size(width_val); ++j) {
+                      const GValue* v = gst_value_list_get_value(width_val, j);
+                      if (g_value_get_int(v) == *width) {
+                          width_ok = true;
+                          valid_width = g_value_get_int(v);
+                          break;
+                      }
+                  }
+              }
+
+              // Height
+              const GValue* height_val = gst_structure_get_value(str, "height");
+              bool height_ok = false;
+
+              if (G_VALUE_HOLDS_INT(height_val)) {
+                  height_ok = (valid_height == *height);
+                  valid_height = g_value_get_int(height_val);
+              } else if (GST_VALUE_HOLDS_INT_RANGE(height_val)) {
+                  height_ok = (*height >= gst_value_get_int_range_min(height_val) &&
+                               *height <= gst_value_get_int_range_max(height_val));
+                  valid_height = gst_value_get_int_range_max(height_val);
+              } else if (GST_VALUE_HOLDS_LIST(height_val)) {
+                  for (guint j = 0; j < gst_value_list_get_size(height_val); ++j) {
+
+                      const GValue* v = gst_value_list_get_value(height_val, j);
+                      if (g_value_get_int(v) == *height) {
+                          height_ok = true;
+                          valid_height = g_value_get_int(v);
+                          break;
+                      }
+                  }
+              }
+
+              // Framerate
+              gst_structure_get_fraction(str, "framerate", &framerate_n, &framerate_d);
+
+              if ((valid_mime == *mime) && width_ok && height_ok && (framerate_n == *framerate) && (framerate_d == *framerate_denominator)) {
+                g_list_free_full(devices, gst_object_unref);
+                gst_object_unref(monitor);
+                return true;
+              }
+          }
+          g_list_free_full(devices, gst_object_unref);
+          gst_object_unref(monitor);
+          gst_caps_unref(caps);
+
+          *mime = valid_mime;
+          *width = valid_width;
+          *height = valid_height;
+          *framerate = framerate_n;
+          *framerate_denominator = framerate_d;
+
+          return false;
+      }
+  }
+  g_list_free_full(devices, gst_object_unref);
+  gst_object_unref(monitor);
+  return true;
+}
+
