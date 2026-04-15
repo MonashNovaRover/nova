a34406010bb4729a58db7402c642380e4d1e48fc
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-04-15 17:50:45 +1000

    package wmm-calculator python package (untested)

---
+++ nixfiles/packages/python/geomaglib/default.nix
@@ -0,0 +1,25 @@
+{ buildPythonPackage
+, fetchurl
+, pythonOlder
+, setuptools
+, python3Packages
+}:
+
+buildPythonPackage rec {
+  pname = "geomaglib";
+  version = "1.2.3";
+  pyproject = true;
+
+  src = fetchurl {
+    url = "https://files.pythonhosted.org/packages/ed/cf/815c4ead29d70670ae35097b3cc5e8d2a7d7bad95619c28e0092e283c59b/geomaglib-1.2.3.tar.gz";
+    hash = "sha256-cdNFiE/lghmQDjr38GLDAgI6MdMlQyYEyx0OpLHjA6o=";
+  };
+
+  build-system = [ setuptools ];
+
+  propagatedBuildInputs = with python3Packages; [
+    numpy
+  ];
+
+  pythonImportsCheck = [ "geomaglib" ];
+}
