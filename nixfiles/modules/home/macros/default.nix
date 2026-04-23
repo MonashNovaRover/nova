b9443980e2fb8b3ee21fb4af8b69cbc019056132
binudakal <bkal0007@student.monash.edu>
2026-04-23 12:49:23 +1000

    fix launch files

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -301,7 +301,7 @@ in
           # Arm
           launch-typing = "~/Builds/master/bin/ros2 launch arm_bringup typing.launch.py";
           launch-arm-control = "~/Builds/master/bin/ros2 launch arm_bringup control.launch.py arm:=False old_arm:=True";
-          launch-path-control = "~/Builds/master/bin/ros2 launch arm_bringup path.control.launch.py arm:=False old_arm:=True";
+          launch-path-control = "~/Builds/master/bin/ros2 launch arm_bringup path.control.launch.py";
           launch-arm-urdf = "~/Builds/master/bin/ros2 launch arm_bringup urdf.launch.py arm:=False old_arm:=True auto_camera:=False";
           launch-arm-teleop = "~/Builds/master/bin/ros2 launch teleop_arm teleop.launch.py";
           run-arm-teleop = "~/Builds/master/bin/ros2 run teleop_arm_joy teleop_arm_joy_node";
