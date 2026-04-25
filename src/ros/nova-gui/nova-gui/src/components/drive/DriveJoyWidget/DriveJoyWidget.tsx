7d6a82a10f6cab89d7134fc2b1b013901b43b278
V01DBREAKER <anthony@v01dbreaker.cc>
2026-04-25 12:06:18 +0000

    fix: update joy msg to reflect teleop drive joy

--- src/ros/nova-gui/nova-gui/src/components/drive/DriveJoyWidget/DriveJoyWidget.tsx
+++ src/ros/nova-gui/nova-gui/src/components/drive/DriveJoyWidget/DriveJoyWidget.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, CardBody, CardHeader, CardProps } from "@nextui-org/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useEffectEvent } from "react";
 import { Info, Lock } from "react-feather";
 import { useBifrost } from "../../../redux/actions/bifrost/useBifrostAction";
 import { RootState } from "../../../redux/RootState";
@@ -59,7 +59,7 @@ const DriveJoyWidget: React.FC<IDriveJoyWidgetProps> = (props) => {
     locked: boolean | undefined
     driveMode: DriveMode | undefined
     controlMode: ControlMode | undefined
-    speed: number
+    speed: Speed
   }
   const [joyState, setJoyState] = useState<IJoyState>({
     left: {
@@ -73,7 +73,7 @@ const DriveJoyWidget: React.FC<IDriveJoyWidgetProps> = (props) => {
     locked: undefined,
     driveMode: undefined,
     controlMode: undefined,
-    speed: 0    // [-2, 2]
+    speed: 0
   });
 
   // update state whenever joystick values change
@@ -123,10 +123,10 @@ const DriveJoyWidget: React.FC<IDriveJoyWidgetProps> = (props) => {
 
   /* send joy messages when state changes
      must convert state to joy message axes and buttons
-     joy msg format on xbox:
-     axes: number[-1to1] = [left_x, left_y, left_trigger, right_x, right_y, right_trigger, dpad_x, dpad_y]
-     buttons: number[0:1] = [a, b, x, y, left_bumper, right_bumper, screenshot, menu, xbox, left_stick_press, right_stick_press, upload]
-     x axes are inverted e.g right = -1
+
+     Teleop Drive Joy Msg
+      axes: [left horizontal, left vertical, right horizontal, right vertical, stop autolock, speed down linear]
+      buttons: [pivot, ackermann, strafe, tank, lock, none, unlock, none, none, auto mode, manual mode, speed up big, speed down big, speed down small, speed up small]
   */
   useEffect(()=>{
     const joyMsg: IRosSensorMsgsJoy = {
@@ -136,48 +136,39 @@ const DriveJoyWidget: React.FC<IDriveJoyWidgetProps> = (props) => {
       },
       axes: [-joyState.left.distance * Math.cos(joyState.left.angle * Math.PI / 180), 
              joyState.left.distance * Math.sin(joyState.left.angle * Math.PI / 180),
-             1.0, // left trigger
              -joyState.right.distance * Math.cos(joyState.right.angle * Math.PI / 180), 
              joyState.right.distance * Math.sin(joyState.right.angle * Math.PI / 180),
-             1.0, // right trigger
-             Math.abs(joyState.speed) == 1 ? joyState.speed : 0, // dpad x
-             Math.abs(joyState.speed) == 2 ? joyState.speed / 2 : 0, // dpad y
+             1, // always stop autolock
+             0, // speed down linear (handbrake) unused in gui
             ],
 
-      buttons: [joyState.controlMode == ControlMode.AUTONOMOUS ? 1 : 0, // auto mode
-                joyState.controlMode == ControlMode.MANUAL ? 1 : 0, // manual mode
-                0, // x button
-                joyState.driveMode == DriveMode.TANK ? 1 : 0,   // tank mode
-                joyState.driveMode == DriveMode.STRAFE ? 1 : 0, // strafe mode
-                joyState.driveMode == DriveMode.PIVOT ? 1 : 0,  // pivot mode
-                joyState.locked ? 1 : 0, // lock game pad
-                !joyState.locked && joyState.locked !== undefined ? 1 : 0, // unlock game pad
-                0, // xbox button
-                0, // left stick button 
-                0, // right stick button
-                0], // upload button
+      buttons: [
+        joyState.driveMode == DriveMode.PIVOT ? 1 : 0,
+        joyState.driveMode == DriveMode.ACKERMANN ? 1 : 0,
+        joyState.driveMode == DriveMode.STRAFE ? 1 : 0,
+        joyState.driveMode == DriveMode.TANK ? 1 : 0,
+        joyState.locked ? 1 : 0, // lock
+        0, 
+        !joyState.locked && joyState.locked !== undefined ? 1 : 0, // unlock
+        0,
+        0,
+        joyState.controlMode == ControlMode.AUTONOMOUS ? 1 : 0,
+        joyState.controlMode == ControlMode.MANUAL ? 1 : 0,
+        joyState.speed == Speed.COARSEUP ? 1 : 0,
+        joyState.speed == Speed.COARSEDOWN ? 1 : 0,
+        joyState.speed == Speed.FINEDOWN ? 1 : 0,
+        joyState.speed == Speed.FINEUP ? 1 : 0,
+      ]
     }
     bifrostJoy.publishToTopic(joyMsg)
+    console.log(joyState)
   }, [joyState, bifrostJoy])
 
   // Joystick component properties
   const Stick = (side: JoystickSide) => <div className="relative m-8">
     <Joystick onChange={(val)=>updateJoystickState(side, val)} />
   </div>
-  
-  useEffect(() => {
-    const preventPinchZoom = (e: TouchEvent) => {
-      if (e.touches.length > 1) {
-        e.preventDefault();
-      }
-    };
-  
-    document.addEventListener('touchmove', preventPinchZoom, { passive: false });
-  
-    return () => {
-      document.removeEventListener('touchmove', preventPinchZoom);
-    };
-  }, []);
+
 
   // return a card with all components
   // its a big blob of code i'm sorry if anyone decides to read this
@@ -213,21 +204,26 @@ const DriveJoyWidget: React.FC<IDriveJoyWidgetProps> = (props) => {
               Unlock
             </Button>
           </div>
-          <div className="grid grid-cols-3 gap-3">
+          <div className="grid grid-cols-4 gap-3">
             <Button className="col-start-1 min-w-0"
-              onPressStart={()=>setDriveMode(DriveMode.TANK)}
+              onPressStart={()=>setDriveMode(DriveMode.PIVOT)}
               onPressEnd={()=>setDriveMode(undefined)}>
-              Tank
+              Pivot
             </Button>
             <Button className="col-start-2 min-w-0"
+              onPressStart={()=>setDriveMode(DriveMode.ACKERMANN)}
+              onPressEnd={()=>setDriveMode(undefined)}>
+              Ackermann
+            </Button>
+            <Button className="col-start-3 min-w-0"
               onPressStart={()=>setDriveMode(DriveMode.STRAFE)}
               onPressEnd={()=>setDriveMode(undefined)}>
               Strafe
             </Button>
-            <Button className="col-start-3 min-w-0"
-              onPressStart={()=>setDriveMode(DriveMode.PIVOT)}
+            <Button className="col-start-4 min-w-0"
+              onPressStart={()=>setDriveMode(DriveMode.TANK)}
               onPressEnd={()=>setDriveMode(undefined)}>
-              Pivot
+              Tank
             </Button>
           </div>
           <div className="space-y-2">
@@ -236,23 +232,23 @@ const DriveJoyWidget: React.FC<IDriveJoyWidgetProps> = (props) => {
             </div>
             <div className="grid grid-cols-4 gap-3">
               <Button className="col-start-1 min-w-0"
-                onPressStart={()=>updateSpeedState(-2)}
-                onPressEnd={()=>updateSpeedState(0)}>
+                onPressStart={()=>updateSpeedState(Speed.COARSEDOWN)}
+                onPressEnd={()=>updateSpeedState(Speed.NONE)}>
                 --
               </Button>
               <Button className="col-start-2 min-w-0"
-                onPressStart={()=>updateSpeedState(-1)}
-                onPressEnd={()=>updateSpeedState(0)}>
+                onPressStart={()=>updateSpeedState(Speed.FINEDOWN)}
+                onPressEnd={()=>updateSpeedState(Speed.NONE)}>
                 -
               </Button>
               <Button className="col-start-3 min-w-0"
-                onPressStart={()=>updateSpeedState(1)}
-                onPressEnd={()=>updateSpeedState(0)}>
+                onPressStart={()=>updateSpeedState(Speed.FINEUP)}
+                onPressEnd={()=>updateSpeedState(Speed.NONE)}>
                 +
               </Button>
               <Button className="col-start-4 min-w-0"
-                onPressStart={()=>updateSpeedState(2)}
-                onPressEnd={()=>updateSpeedState(0)}>
+                onPressStart={()=>updateSpeedState(Speed.COARSEUP)}
+                onPressEnd={()=>updateSpeedState(Speed.NONE)}>
                 ++
               </Button>
             </div>
@@ -272,6 +268,14 @@ enum ControlMode {
 enum JoystickSide {
   LEFT,
   RIGHT,
+}
+
+enum Speed {
+  COARSEUP,
+  FINEUP,
+  NONE,
+  FINEDOWN,
+  COARSEDOWN
 }
 
 export default DriveJoyWidget;
