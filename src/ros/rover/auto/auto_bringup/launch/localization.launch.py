e94f2e1c035702a74954f461c1c57d1ce80d89ff
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-04-25 23:15:09 +1000

    minor post field-test cleanup

--- src/ros/rover/auto/auto_bringup/launch/localization.launch.py
+++ src/ros/rover/auto/auto_bringup/launch/localization.launch.py
@@ -112,7 +112,7 @@ def launch_setup(context, *args, **kwargs):
                     parameters=[rl_params, {'use_sim_time': gazebo}],
                     remappings=[('odometry/filtered', 'odometry/global'),
                                 ('gps/fix', 'gps_rover/fix'),
-                                ('imu', 'imu')],
+                                ('imu', 'gps_rover/heading_imu')],
                 ),
             ],
         ),
