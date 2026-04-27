5ef41c65693157003fb10242292373473aeb23f7
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-04-27 14:09:01 +1000

    feat: dds tuning (#1091)

--- nixfiles/modules/home/macros/default.nix
+++ nixfiles/modules/home/macros/default.nix
@@ -112,14 +112,6 @@ in
                             mkdir -p $HOME/.config/nova;
                             echo 'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp' > $HOME/.config/nova/ros_dds";
 
-          # ROS Discovery Server
-          base_dds_client = "FASTRTPS_DEFAULT_PROFILES_FILE=${./ros_discovery/base_client_configuration.xml}";
-          rover_dds_client = "FASTRTPS_DEFAULT_PROFILES_FILE=${./ros_discovery/rover_client_configuration.xml}";
-          base_pi_dds_client = "FASTRTPS_DEFAULT_PROFILES_FILE=${./ros_discovery/base_pi_client_configuration.xml}";
-          base_dds_super_client = "FASTRTPS_DEFAULT_PROFILES_FILE=${./ros_discovery/base_super_client_configuration.xml}";
-          rover_dds_super_client = "FASTRTPS_DEFAULT_PROFILES_FILE=${./ros_discovery/rover_super_client_configuration.xml}";
-          base_pi_dds_super_client = "FASTRTPS_DEFAULT_PROFILES_FILE=${./ros_discovery/base_pi_super_client_configuration.xml}";
-
           # Comp Selection (for auto)
           set_arch = "export COMP=ARCh;
                       mkdir -p $HOME/.config/nova;
