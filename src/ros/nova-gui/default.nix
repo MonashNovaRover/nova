27981523113e78682172647198866add1d2c6016
V01DBREAKER <anthony@v01dbreaker.cc>
2026-04-27 07:46:39 +0000

    feat: nova-gui dev shell

--- src/ros/nova-gui/default.nix
+++ src/ros/nova-gui/default.nix
@@ -1,5 +1,6 @@
 {
   rosPackages = pkgs: with pkgs; {
     nova-gui = callPackage ./nix/packages/gui { };
+    nova-gui-dev-shell = callPackage ./nix/packages/gui/dev-shell.nix { };
   };
 }
---
+++ src/ros/nova-gui/nix/packages/gui/dev-shell.nix
@@ -0,0 +1,58 @@
+{ mkShell
+, config
+, nova-gui
+, pkgs
+}:
+
+# This shell is designed for use in development and it is assumed the developer will run the gui using yarn
+# If looking for a shell with the gui fully built, use the default.nix instead (e.g for gui-serve)
+
+let 
+
+gui = nova-gui.overrideAttrs(oldAttrs :{
+  # disable building of gui-serve to make it faster
+  postUnpack = "";
+  buildPhase = ''
+    runHook preBuild
+    runHook postBuild
+  '';
+  installPhase = ''
+    runHook preInstall
+    runHook postInstall
+  '';
+
+  # add path to node_modules to output to be used in dev shell
+  configurePhase = oldAttrs.configurePhase + ''
+    mkdir -p $out
+    echo $node_modules > $out/node_modules
+    echo $ROS_TS_DEFINITIONS > $out/ROS_TS_DEFINITIONS
+  '';
+
+  passthru.ROS_TS_DEFINITIONS = oldAttrs.ROS_TS_DEFINITIONS;
+});
+
+diff_ignore=''-x ".vite" -x ".vite-temp" -x "nova-gui"'';
+
+in
+
+mkShell {
+  preBuild = ''
+    echo "Entering GUI shell..."
+  '';
+  buildInputs = [ gui ];
+
+  shellHook = ''
+    if ! diff -rq $(cat ${gui}/node_modules) \${builtins.toString ../../../nova-gui/node_modules} ${diff_ignore} >/dev/null; then
+      echo "Copying nix-store gui modules $(cat ${gui}/node_modules) to ${builtins.toString ../../../nova-gui/node_modules}"
+      cp -rf $(cat ${gui}/node_modules) ${builtins.toString ../../../nova-gui}
+      chmod -R 777 ${builtins.toString ../../../nova-gui/node_modules}
+    else
+      echo "Node modules has not changed, skipping copy..."
+    fi
+
+    echo "Linking Typescript Definitions... "
+    ln -sf $(cat ${gui}/ROS_TS_DEFINITIONS) ${builtins.toString ../../../nova-gui/src/ros/rosTypes.ts}
+
+    echo -e "Done! Run \e[40m\e[1;93mgui-run\e[0m to run the GUI, or to change to nova-gui's directory run \e[40m\e[1;93mgui\e[0m"
+  '';
+}
