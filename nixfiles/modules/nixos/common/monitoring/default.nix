56002affa5f66d27f24cd971e47346b2f3353d1c
Terry Tian <97772315+tytia@users.noreply.github.com>
2026-05-05 23:56:03 +1000

    add nethogs, glances and usbtop

---
+++ nixfiles/modules/nixos/common/monitoring/default.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.nova.monitoring;
+in
+{
+  options.nova.monitoring.enable = lib.mkEnableOption "Install monitoring tools";
+
+  config = lib.mkIf cfg.enable {
+    # USB bandwidth monitor
+    programs = {
+      usbtop.enable = true;
+    };
+
+    # General purpose monitor with network support
+    services = {
+      glances.enable = true;
+    };
+
+    # Network bandwidth by process/application monitor
+    environment.systemPackages = with pkgs; [
+      nethogs
+    ];
+
+    # Security wrappers for tools that require elevated capabilities
+    # Enables running usbtop and nethogs without sudo while still maintaining security
+    security.wrappers.usbtop = {
+      source = "${pkgs.usbtop}/bin/usbtop";
+      capabilities = "cap_dac_read_search+ep";
+      owner = "root";
+      group = "root";
+    };
+    security.wrappers.nethogs = {
+      source = "${pkgs.nethogs}/bin/nethogs";
+      capabilities = "cap_net_admin,cap_net_raw,cap_dac_read_search,cap_sys_ptrace+ep";
+      owner = "root";
+      group = "root";
+    };
+  };
+}
