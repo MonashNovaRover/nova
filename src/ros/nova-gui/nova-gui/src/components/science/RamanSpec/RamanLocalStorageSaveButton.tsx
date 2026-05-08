c545eea41c36c39460e78092c3b8cf64887e1c1c
Felicity Matthews <felicitymatthews30@gmail.com>
2026-05-08 12:58:36 +1000

    feat: auto populate graph names

--- src/ros/nova-gui/nova-gui/src/components/science/RamanSpec/RamanLocalStorageSaveButton.tsx
+++ src/ros/nova-gui/nova-gui/src/components/science/RamanSpec/RamanLocalStorageSaveButton.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Button, Input} from "@nextui-org/react";
 
 
@@ -12,6 +12,13 @@ export interface RamanLocalStorageSaveButtonProps {
 // Uses key prop from parent to reset when suggestedName changes
 const RamanLocalStorageSaveButton: React.FC<RamanLocalStorageSaveButtonProps> = (props) => {
   const [graphName, setGraphName] = useState<string>(props.suggestedName ?? "");
+
+  // Update input when suggestedName changes (pre-fill with cuvette name)
+  useEffect(() => {
+    if (props.suggestedName !== undefined) {
+      setGraphName(props.suggestedName);
+    }
+  }, [props.suggestedName]);
 
   return (
     <div className="flex flex-row gap-3 my-3 mb-0 flex-grow">
