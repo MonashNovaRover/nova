03f6d686951ac3c859d87b6106bcc33a90ffbc13
Tash Lee <tlee0160@student.monash.edu>
2026-05-18 17:30:25 +1000

    Revert "clean up"
    This reverts commit 15a8139a63cb46301453b6e09fede47607fe2fe6.

--- src/ros/nova-gui/nova-gui/src/components/maps/Cartographer/maptiler/MapTilerMap.tsx
+++ src/ros/nova-gui/nova-gui/src/components/maps/Cartographer/maptiler/MapTilerMap.tsx
@@ -16,7 +16,8 @@ export const MapTilerMap = (props: { overlay: React.ReactNode, mapTile: MapTile,
   const mapRef = useRef<maptilersdk.Map | null>(null);
   
   const { updateMousePosition, handleMapClickEvent } = useCartographerActions();
-  
+
+
   useEffect(()=>{
     if (mapRef.current || !mapContainer.current) return;
     const newMap = new maptilersdk.Map({
