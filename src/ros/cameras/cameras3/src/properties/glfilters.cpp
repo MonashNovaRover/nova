444cf32ac997f3059b9bee88be994ce713f60232
rotaryViper <93423444+rotaryViper@users.noreply.github.com>
2026-05-19 22:54:14 +1000

    feat/cameras3-upgrade6 (#1114)
    Co-authored-by: Monash Nova Rover <novaroverteam@monash.edu>

--- src/ros/cameras/cameras3/src/properties/glfilters.cpp
+++ src/ros/cameras/cameras3/src/properties/glfilters.cpp
@@ -1,10 +1,20 @@
-#include <string>
 #include <gst/gst.h>
 #include "properties/glfilters.hpp"
 
-void set_glgreyscale(GstElement* glgreyscale) { 
-  g_object_set(glgreyscale,
-    "saturation", 0.0, // Turn off colour
+void set_glgreyscale(GstElement* element) { 
+  g_object_set(element,
+    "saturation", 0.0, // turn off colour
   NULL);
 };
 
+void set_no_glgreyscale(GstElement* element) { 
+  g_object_set(element,
+    "saturation", 1.0, // turn on colour
+  NULL);
+};
+
+void set_no_glcrop43(GstElement* element) { 
+  g_object_set(element,
+    "scale-x", 1.0,
+  NULL);
+};
