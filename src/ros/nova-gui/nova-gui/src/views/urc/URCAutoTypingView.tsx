df26994d2bacb84d70f2fa64a89e1ecfb5a315c6
Monash Nova Rover <novaroverteam@monash.edu>
2026-05-22 12:22:06 +1000

    fix: use arm end finger

--- src/ros/nova-gui/nova-gui/src/views/urc/URCAutoTypingView.tsx
+++ src/ros/nova-gui/nova-gui/src/views/urc/URCAutoTypingView.tsx
@@ -19,7 +19,7 @@ const URCAutoTypingView: React.FC = () => {
     <div className="flex flex-col lg:flex-row gap-3">
       {/* Left Column (Col 1) */}
       <div className="flex flex-col gap-3 w-full lg:w-1/2">
-        <KeyboardOverlayedCameraComponent cameraSerial={CameraSerials.ARM_END_PERISCOPE}/>
+        <KeyboardOverlayedCameraComponent cameraSerial={CameraSerials.ARM_END_FINGER}/>
         <AutoTypingKeyEntryWidget showHelp={() => setInfoPanelOpen(true)}/>
       </div>
 
