e96458bbc38bdac39fad424ef9e056bd28bd5aa5
Monash Nova Rover <novaroverteam@monash.edu>
2026-05-27 12:50:05 +0000

    Reapply "feat/cameras3-upgrade6 (#1114)"
    This reverts commit 9c244867b958a6d7819042b267bf67e45cded1c3.

--- src/ros/cameras/cameras3/include/properties/glfilters.hpp
+++ src/ros/cameras/cameras3/include/properties/glfilters.hpp
@@ -6,173 +6,158 @@
 
 void set_glgreyscale(GstElement* glgreyscale);
 
-template<typename properties> void set_gldenoise(GstElement* gldenoise, const properties& props) {
-  // fxaa antiliasing
+void set_no_glgreyscale(GstElement* glgreyscale);
+
+void set_no_glshader(GstElement* glshader);
+
+template<typename properties> void set_glshaders(GstElement* element, const properties& props) {
   const std::string shader = R"(#version 100
-precision lowp float;
+precision mediump float;
 
 uniform sampler2D tex;
-uniform float width;
-uniform float height;
-uniform float factor;     // default 1
-uniform float sigma;      // spatial sigma (e.g. 2.0)
-uniform float threshold;  // color similarity threshold (e.g. 0.1)
-uniform int radius;     // Default 3
-
 varying vec2 v_texcoord;
 
-float gaussian(float x, float s) {
-    return exp(-(x * x) / (2.0 * s * s));
+uniform float width;
+uniform float height;
+
+uniform float undistort_k1;  // radial distortion coefficient
+uniform float undistort_k2;  // optional higher-order term
+uniform float undistort_scale; // zoom adjustment
+
+uniform float denoise_factor;     // default 0.5
+uniform float denoise_sigma;      // spatial sigma (e.g. 2.0)
+uniform float denoise_threshold;  // color similarity threshold (e.g. 0.1)
+uniform int denoise_radius;       // Default 3
+
+uniform float sharpen_radius;   // Default 0.2
+uniform float sharpen_strength; // Default 4.0
+
+uniform int do_undistort;
+uniform int do_greyscale;
+uniform int do_denoise;
+uniform int do_sharpen;
+
+// 1D Bilateral Function
+vec3 computeBlur(vec3 centerColor, vec2 direction) {
+  vec3 sum = vec3(0.0);
+  float weightSum = 0.0;
+
+  for (int i = -7; i <= 7; i++) {
+    if (i < -denoise_radius) continue;
+    if (i > denoise_radius) break;
+
+    vec2 offset = direction * float(i);
+    vec3 sampleColor = texture2D(tex, v_texcoord + offset).rgb;
+
+    float spatialDist = abs(float(i));
+    float colorDist = length(sampleColor - centerColor);
+
+    // Simple Gaussian approximation
+    float w = exp(-(spatialDist * spatialDist) / (2.0 * denoise_sigma * denoise_sigma)) * exp(-(colorDist * colorDist) / (2.0 * denoise_threshold * denoise_threshold));
+
+    sum += sampleColor * w;
+    weightSum += w;
+  }
+  return sum / weightSum;
 }
 
 void main() {
-    vec2 texel = factor / vec2(width, height);
 
-    vec3 centerColor = texture2D(tex, v_texcoord).rgb;
+  vec2 uv = v_texcoord;
 
-    vec3 sum = vec3(0.0);
-    float weightSum = 0.0;
+  if (do_undistort == 1) {
+    uv = v_texcoord * 2.0 - 1.0;
+    uv /= undistort_scale;
+    float r2 = dot(uv, uv);   // r^2
+    float undistort_factor = 1.0 + undistort_k1 * r2 + undistort_k2 * r2 * r2;
 
-    for (int x = -radius; x <= radius; x++) {
-        for (int y = -radius; y <= radius; y++) {
+    uv *= undistort_factor;
+    uv = uv * 0.5 + 0.5;
+  }
 
-            vec2 offset = vec2(float(x), float(y)) * texel;
-            vec3 sampleColor = texture2D(tex, v_texcoord + offset).rgb;
+  vec3 centerColor = texture2D(tex, uv).rgb;
+  vec3 result = centerColor;
 
-            float spatialDist = length(vec2(float(x), float(y)));
-            float colorDist = length(sampleColor - centerColor);
+  if (do_denoise == 1) {
+    
+    // Direction vectors
+    vec2 hDir = vec2(1.0 / width, 0.0);
+    vec2 vDir = vec2(0.0, 1.0 / height);
 
-            float wSpatial = gaussian(spatialDist, sigma);
-            float wColor   = gaussian(colorDist, threshold);
+    // Apply both
+    vec3 hBlur = computeBlur(centerColor, hDir);
+    vec3 vBlur = computeBlur(centerColor, vDir);
 
-            float weight = wSpatial * wColor;
+    // Average them or combine
+    result = ((hBlur + vBlur) * 0.5) * denoise_factor + result * (1.0 - denoise_factor);
+  }
 
-            sum += sampleColor * weight;
-            weightSum += weight;
-        }
-    }
+  if (do_sharpen == 1) {
+    vec2 texOffset = sharpen_radius / vec2(width, height);
 
-    vec3 result = sum / weightSum;
-
-    gl_FragColor = vec4(result, 1.0);
-})";
-  const std::string uniforms = "uniforms";
-  GstStructure *str = gst_structure_new(
-    uniforms.c_str(),
-    "factor", G_TYPE_FLOAT, props->denoise_factor,
-    "width", G_TYPE_FLOAT, ((float) props->width/ (float) props->downscale),
-    "height", G_TYPE_FLOAT, ((float) props->height/ (float) props->downscale),
-    "factor", G_TYPE_FLOAT, props->denoise_factor,
-    "sigma", G_TYPE_FLOAT, props->denoise_factor,
-    "threshold", G_TYPE_FLOAT, props->denoise_factor,
-    "radius", G_TYPE_INT, props->denoise_radius,
-  NULL);
-  g_object_set(gldenoise,
-    "fragment", shader.c_str(),
-    "uniforms", str,
-  NULL);
-  gst_structure_free(str);
-}
-
-
-template<typename properties> void set_gledgedetect(GstElement* gledgedetect, const properties& props) {
-  // sobel edge detection
-  const std::string shader = R"(#version 100
-precision lowp float;
-
-uniform sampler2D tex;
-uniform float factor;
-uniform float width;
-uniform float height;
-varying vec2 v_texcoord;
-
-void main() {
-    vec2 texOffset = factor / vec2(width, height);
+    vec3 blur = vec3(0.0);
 
     // Sample the 3x3 neighborhood
-    vec3 c00 = texture2D(tex, v_texcoord + texOffset * vec2(-1.0, -1.0)).rgb;
-    vec3 c10 = texture2D(tex, v_texcoord + texOffset * vec2( 0.0, -1.0)).rgb;
-    vec3 c20 = texture2D(tex, v_texcoord + texOffset * vec2( 1.0, -1.0)).rgb;
+    blur += texture2D(tex, uv + texOffset * vec2(-1.0, -1.0)).rgb;
+    blur += 2.0 * texture2D(tex, uv + texOffset * vec2( 0.0, -1.0)).rgb;
+    blur += texture2D(tex, uv + texOffset * vec2( 1.0, -1.0)).rgb;
 
-    vec3 c01 = texture2D(tex, v_texcoord + texOffset * vec2(-1.0,  0.0)).rgb;
-    vec3 c11 = texture2D(tex, v_texcoord + texOffset * vec2( 0.0,  0.0)).rgb;
-    vec3 c21 = texture2D(tex, v_texcoord + texOffset * vec2( 1.0,  0.0)).rgb;
+    blur += 2.0 * texture2D(tex, uv + texOffset * vec2(-1.0,  0.0)).rgb;
+    blur += 4.0 * texture2D(tex, uv + texOffset * vec2( 0.0,  0.0)).rgb;
+    blur += 2.0 * texture2D(tex, uv + texOffset * vec2( 1.0,  0.0)).rgb;
 
-    vec3 c02 = texture2D(tex, v_texcoord + texOffset * vec2(-1.0,  1.0)).rgb;
-    vec3 c12 = texture2D(tex, v_texcoord + texOffset * vec2( 0.0,  1.0)).rgb;
-    vec3 c22 = texture2D(tex, v_texcoord + texOffset * vec2( 1.0,  1.0)).rgb;
+    blur += texture2D(tex, uv + texOffset * vec2(-1.0,  1.0)).rgb;
+    blur += 2.0 * texture2D(tex, uv + texOffset * vec2( 0.0,  1.0)).rgb;
+    blur += texture2D(tex, uv + texOffset * vec2( 1.0,  1.0)).rgb;
 
-    // Sobel kernels
-    vec3 gx = -c00 - 2.0*c01 - c02 + c20 + 2.0*c21 + c22;
-    vec3 gy = -c00 - 2.0*c10 - c20 + c02 + 2.0*c12 + c22;
+    blur /= 16.0;
 
-    vec3 edge = sqrt(gx*gx + gy*gy);
+    // Edge
+    vec3 edge = result - blur;
 
-    gl_FragColor = vec4(edge, 1.0);
+    // Apply sharpening
+    result += edge * sharpen_strength;
+  }
+
+  if (do_greyscale == 1) {
+    float grey = dot(result, vec3(0.299, 0.587, 0.114));
+    result = vec3(grey);
+  }
+
+  gl_FragColor = vec4(result, 1.0);
 })";
   const std::string uniforms = "uniforms";
   GstStructure *str = gst_structure_new(
     uniforms.c_str(),
-    "factor", G_TYPE_FLOAT, props->edgedetect_factor,
     "width", G_TYPE_FLOAT, ((float) props->width/ (float) props->downscale),
     "height", G_TYPE_FLOAT, ((float) props->height/ (float) props->downscale),
+    "undistort_k1", G_TYPE_FLOAT, props->undistort_k1,
+    "undistort_k2", G_TYPE_FLOAT, props->undistort_k2,
+    "undistort_scale", G_TYPE_FLOAT, props->undistort_scale,
+    "denoise_factor", G_TYPE_FLOAT, props->denoise_factor,
+    "denoise_sigma", G_TYPE_FLOAT, props->denoise_sigma,
+    "denoise_threshold", G_TYPE_FLOAT, props->denoise_threshold,
+    "denoise_radius", G_TYPE_INT, props->denoise_radius,
+    "sharpen_radius", G_TYPE_FLOAT, props->sharpen_radius,
+    "sharpen_strength", G_TYPE_FLOAT, props->sharpen_strength,
+    "do_undistort", G_TYPE_INT, (int) props->undistort,
+    "do_greyscale", G_TYPE_INT, (int) props->greyscale,
+    "do_denoise", G_TYPE_INT, (int) props->denoise,
+    "do_sharpen", G_TYPE_INT, (int) props->sharpen,
   NULL);
-  g_object_set(gledgedetect,
+  g_object_set(element,
     "fragment", shader.c_str(),
     "uniforms", str,
   NULL);
   gst_structure_free(str);
 }
 
-template<typename properties> void set_glundistort(GstElement* glundistort, const properties& props) {
-  const std::string shader = R"(#version 100
-precision lowp float;
-
-uniform sampler2D tex;
-varying vec2 v_texcoord;
-
-// Distortion parameters
-uniform float k1;  // radial distortion coefficient
-uniform float k2;  // optional higher-order term
-uniform float scale; // zoom adjustment
-
-void main()
-{
-    // Convert [0,1] → [-1,1]
-    vec2 uv = v_texcoord * 2.0 - 1.0;
-
-    // Apply scaling (useful to zoom in/out after correction)
-    uv /= scale;
-
-    // Inverse radial distortion (approximate)
-    float r2 = dot(uv, uv);   // r^2
-    float factor = 1.0 + k1 * r2 + k2 * r2 * r2;
-
-    uv = uv * factor;
-
-    // Convert back to [0,1]
-    vec2 corrected_uv = uv * 0.5 + 0.5;
-
-    gl_FragColor = texture2D(tex, corrected_uv);
-})";
-  const std::string uniforms = "uniforms";
-  GstStructure *str = gst_structure_new(
-    uniforms.c_str(),
-    "k1", G_TYPE_FLOAT, props->undistort_k1,
-    "k2", G_TYPE_FLOAT, props->undistort_k2,
-    "scale", G_TYPE_FLOAT, props->undistort_scale,
-  NULL);
-  g_object_set(glundistort,
-    "fragment", shader.c_str(),
-    "uniforms", str,
-  NULL);
-  gst_structure_free(str);
-}
-
-template<typename properties> void set_glcrop43(GstElement* glcrop, const properties& props) { 
-  g_object_set(glcrop,
+template<typename properties> void set_glcrop43(GstElement* element, const properties& props) { 
+  g_object_set(element,
     "scale-x", (double) props->width/(props->height/3*4),
   NULL);
 };
+
+void set_no_glcrop43(GstElement* element);
 
 #endif
--- src/ros/cameras/cameras3/src/properties/h264.cpp
+++ src/ros/cameras/cameras3/src/properties/h264.cpp
@@ -2,16 +2,16 @@
 #include <gst/gst.h>
 #include "properties/h264.hpp"
 
-void set_h264payload(GstElement* payload) {
+void set_h264payload(GstElement* element) {
   // Apply patch for gc2093
-  g_object_set(payload,
+  g_object_set(element,
       "aggregate-mode", 1,
       "config-interval", -1,
       NULL);
 }
 
-void set_h264parse(GstElement* parse, const int interval) {
-    g_object_set(parse,
+void set_h264parse(GstElement* element, const int interval) {
+    g_object_set(element,
         "config-interval", interval,
         NULL);
 }
