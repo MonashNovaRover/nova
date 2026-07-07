bd52c41d86ad81d90bbbdf4bf3745071467d3551
Orlando Chamberlain <orlandoch.dev@gmail.com>
2026-07-07 17:12:22 +1000

    f mocking can

--- nixfiles/modules/nixos/common/mocking/can/default.nix
+++ nixfiles/modules/nixos/common/mocking/can/default.nix
@@ -13,9 +13,10 @@ in
       default = "-e drive";
     };
     # TODO: configure vcan here?
+    # TODO: allow injecting a different package in place of can sleuth
     file = lib.mkOption {
       type = with lib.types; string;
-      description = "The file for the state of the mock can devices to be reported. Use commands like `jq .J6.BLCMDEmulator.Pos /run/can_sleuth_state` to get values from there."
+      description = "The file for the state of the mock can devices to be reported. Use commands like `jq .J6.BLCMDEmulator.Pos /run/can_sleuth_state` to get values from there.";
       default = "/run/can_sleuth_state";
     };
   };
@@ -32,7 +33,7 @@ in
           in
           # The awk script overwrites the file every time it gets an update https://unix.stackexchange.com/a/356398
           ''
-            ${can_sleuth} ${systems} -o json | ${awk} -vfile=${cfg.file} '{print $0 > file; close(file)}'
+            ${can_sleuth} ${cfg.systems} -o json | ${awk} -vfile=${cfg.file} '{print $0 > file; close(file)}'
           '';
       };
   };
