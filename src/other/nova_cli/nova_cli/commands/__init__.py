2f0c3dddd00b64556e5cc7993fb9c17cc541e413
Felicity Matthews <felicitymatthews30@gmail.com>
2026-07-07 13:59:16 +1000

    feat: add can sleuth

--- src/other/nova_cli/nova_cli/commands/__init__.py
+++ src/other/nova_cli/nova_cli/commands/__init__.py
@@ -3,10 +3,12 @@
 from nova_cli.commands.env import EnvCommand
 from nova_cli.commands.launch import LaunchCommand
 from nova_cli.commands.run import RunCommand
+from nova_cli.commands.sleuth import SleuthCommand
 
 # Command registry - add new commands here
 COMMANDS = {
     'env': EnvCommand,
     'launch': LaunchCommand,
     'run': RunCommand,
+    'sleuth': SleuthCommand,
 }
