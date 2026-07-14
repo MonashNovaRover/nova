3a28753d8bb9229e0da8fa6ba050dc809e50a625
Orlando Chamberlain <orlandoch.dev@gmail.com>
2026-07-14 20:49:57 +1000

    Feat/faster git metadata (#1163)

--- nixfiles/packages/ros/nova-workspace/default.nix
+++ nixfiles/packages/ros/nova-workspace/default.nix
@@ -1,90 +1,8 @@
 { lib
 , pkgs
+, git-metadata
 , buildROSWorkspace
 , buildEnv
-, rviz2
-, ros-gz
-, gz-ros2-control
-, rqt
-, rqt-common-plugins
-, gdb
-, pythonPackages
-, gps-umd
-, tf2-tools
-, reolink
-, moveit-core
-, moveit-kinematics
-, ublox-dgnss
-, teleop-modular
-, teleop-modular-core
-, teleop-modular-twist
-, teleop-modular-control-mode
-, teleop-modular-input-source
-, teleop-modular-node
-, teleop-modular-python-utils
-, livox-ros-driver2
-, mbtileserver
-, rmw-cyclonedds-cpp
-
-, nova-dgnss ? throw "nova-dgnss is needed, but not available!"
-, nova-electronics ? throw "electronics is needed, but not available!"
-, nova-science ? throw "science is needed, but not available!"
-, nova-cameras2 ? throw "cameras2 is needed, but not available!"
-, nova-gui ? throw "gui is needed, but not available!"
-, nova-drive ? throw "drive is needed, but not available!"
-, nova-drive-interfaces ? throw "drive-interfaces is needed, but not available!"
-, nova-blcmd-interfaces ? throw "blcmd-interfaces is needed, but not available!"
-, nova-blcmd-utils ? throw "blcmd-utils is needed, but not available!"
-, nova-arm-interfaces ? throw "arm-interfaces is needed, but not available!"
-, nova-arm ? throw "arm is needed, but not available!"
-, nova-input-interfaces ? throw "input-interfaces is needed, but not available!"
-, nova-inputs ? throw "inputs is needed, but not available!"
-, nova-cmd-interfaces ? throw "cmd-interfaces is needed, but not available!"
-, nova-cmd-utils ? throw "cmd-utils is needed, but not available!"
-, nova-gimbal-cam ? throw "gimbal-cam is needed, but not available!"
-, nova-interfaces ? throw "nova-interfaces is needed, but not available!"
-, nova-bringup ? throw "nova-bringup is needed, but not available!"
-, nova-auto-bringup ? throw "auto-bringup is needed, but not available!"
-, nova-arm-bringup ? throw "arm-bringup is needed, but not available!"
-, nova-drive-bringup ? throw "drive-bringup is needed, but not available!"
-, nova-rover-description ? throw "rover-description is needed, but not available!"
-, nova-blcmd-hardware ? throw "nova-blcmd-hardware is needed, but not available!"
-, nova-blcmd-hardware2 ? throw "nova-blcmd-hardware2 is needed, but not available!"
-, nova-cmd-hardware ? throw "nova-cmd-hardware is needed, but not available!"
-, nova-qcmd-hardware ? throw "nova-qcmd-hardware is needed, but not available!"
-, nova-controller-common ? throw "nova-controller-common is needed, but not available!"
-, nova-drive-controller-base ? throw "nova-drive-controller-base is needed, but not available!"
-, nova-pivot-drive-controller ? throw "nova-pivot-drive-controller is needed, but not available!"
-, nova-strafe-drive-controller ? throw "nova-strafe-drive-controller is needed, but not available!"
-, nova-diff-drive-controller ? throw "nova-diff-drive-controller is needed, but not available!"
-, nova-teleop-drive-joy ? throw "nova-teleop-drive-joy is needed, but not available!"
-, nova-teleop-arm-joy ? throw "nova-teleop-arm-joy is needed, but not available!"
-, nova-gazebo ? throw "nova-gazebo is needed, but not available!"
-, nova-python-control ? throw "python-control is needed, but not available!"
-, nova-python-control2 ? throw "python-control2 is needed, but not available!"
-, nova-excavation-construction ? throw "excavation-construction is needed, but not available!"
-, nova-teleop-ec ? throw "teleop-ec is needed, but not available!"
-, nova-utils ? throw "nova-utils is needed, but not available!"
-, nova-arm-controller ? throw "nova-arm-controller is needed, but not available!"
-, nova-twistmapper ? throw "nova-twistmapper is needed, but not available!"
-, nova-path-planner ? throw "nova-path-planner is needed, but not available!"
-, nova-banksia-kinematics-plugin ? throw "nova-banksia-kinematics-plugin is needed, but not available!"
-, nova-waratah-kinematics-plugin ? throw "nova-waratah-kinematics-plugin is needed, but not available!"
-, gpsd-client ? throw "gpsd-client is needed, but not available!"
-, nova-joint-space-control-mode ? throw "nova-joint-space-control-mode is need, but not available!"
-, nova-teleop-arm ? throw "nova-teleop-arm is need, but not available!"
-, nova-launch-scripts
-, nova-git-metadata
-, nova-legacy-input-mode ? throw "nova-joint-space-control-mode is need, but not available!"
-, nova-teleop-science ? throw "nova-teleop-science is needed, but not available!"
-, nova-science-interfaces ? throw "nova-science-interfaces is needed, but not available!"
-, nova-science-bringup ? throw "nova-science-bringup is needed, but not available!"
-, nova-arm-kinematics ? throw "nova-arm-kinematics is needed, but not available!"
-, nova-locked-publisher
-, nova-cameras
-, nova-camera-msgs
-, mission-planner
-, mavproxy
   # Configuration options
   ## Include graphical applications in the workspace.
 , graphical ? true
@@ -96,7 +14,7 @@
   ## Note that some packages may have dependencies on others that will be
   ## implicitly included.
 , novaPackages ? {
-    inherit
+    inherit (pkgs.ros)
       nova-dgnss
       nova-electronics
       nova-science
@@ -141,11 +59,8 @@
       nova-path-planner
       nova-banksia-kinematics-plugin
       nova-waratah-kinematics-plugin
-      reolink
       nova-joint-space-control-mode
       nova-teleop-arm
-      nova-launch-scripts
-      nova-git-metadata
       nova-legacy-input-mode
       nova-teleop-science
       nova-science-interfaces
@@ -155,12 +70,19 @@
       nova-camera-msgs
       nova-locked-publisher
       ;
-    nova-can-sleuth = pythonPackages.nova-can-sleuth;
+    inherit (pkgs)
+      nova-launch-scripts
+      reolink
+      ;
+    inherit (pkgs.python3Packages)
+      nova-can-sleuth
+      ;
+    nova-git-metadata = (pkgs.writeTextDir "nova-git-metadata" git-metadata);
   }
 
   ## Extra packages to add to the workspace.
 , extraPackages ? { 
-    inherit
+    inherit (pkgs.ros)
       tf2-tools
       moveit-core   # needed to dynamically load the kinematics_solver plugin for nova_twistmapper
       moveit-kinematics
@@ -173,12 +95,15 @@
       teleop-modular-node
       teleop-modular-python-utils
       livox-ros-driver2
-      mbtileserver
       rmw-cyclonedds-cpp
+      rqt-controller-manager
       ;
-    rqt-controller-manager = pkgs.ros.rqt-controller-manager;
-    
-    ros2-unbag = pythonPackages.ros2-unbag;
+    inherit (pkgs)
+      mbtileserver
+      ;
+    inherit (pkgs.python3Packages)
+      ros2-unbag
+      ;
 }
 }:
 
@@ -199,18 +124,20 @@ in
   inherit interactive;
   devPackages = novaPackages;
   prebuiltPackages = (lib.optionalAttrs graphical {
-    inherit
+    inherit (pkgs.ros)
       rviz2
-      mission-planner
-      mavproxy
       ros-gz
       gz-ros2-control
       gps-umd
       rqt rqt-common-plugins
       ublox-dgnss;
+    inherit (pkgs)
+      mission-planner
+      mavproxy
+      ;
   }) // extraPackages;
   prebuiltShellPackages = {
-    inherit
+    inherit (pkgs)
       gdb;
   };
 }).overrideAttrs ({ passthru ? { }, ... }: {
