0cf748233d8f79831bde081c9e49d066c6c08848
V01DBREAKER <anthony@v01dbreaker.cc>
2026-08-21 01:57:40 +0000

    missed one whoops

--- src/other/nova_cli/nova_cli/commands/__init__.py
+++ src/other/nova_cli/nova_cli/commands/__init__.py
@@ -13,7 +13,7 @@ EDITED:         09/07/2026
 from nova_cli.commands.build import BuildCommand
 from nova_cli.commands.env import EnvCommand
 from nova_cli.commands.launch import LaunchCommand
-from nova_cli.commands.rebuild_main import RebuildMasterCommand
+from nova_cli.commands.rebuild_main import RebuildMainCommand
 from nova_cli.commands.run import RunCommand
 from nova_cli.commands.sleuth import SleuthCommand
 from nova_cli.commands.start import StartCommand
