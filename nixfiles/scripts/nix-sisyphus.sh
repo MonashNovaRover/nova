74087b388c8860254cf96ddb76d502cef223ee34
Victor Bartlinski <vicbart08@gmail.com>
2026-08-31 14:01:27 +1000

    changing target branch

--- nixfiles/scripts/nix-sisyphus.sh
+++ nixfiles/scripts/nix-sisyphus.sh
@@ -12,7 +12,7 @@ export GIT_AUTHOR_NAME=github-actions[bot]
 export GIT_AUTHOR_EMAIL=github-actions[bot]@users.noreply.github.com
 export GIT_COMMITTER_NAME=github-actions[bot]
 export GIT_COMMITTER_EMAIL=github-actions[bot]@users.noreply.github.com
-export GIT_BRANCH=test/update-nix2
+export GIT_BRANCH=test/update-nix
 
 # Checkout the branch if it already exists
 git pull
