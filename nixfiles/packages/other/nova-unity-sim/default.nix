a2a641944d29b6a59b7d14abbb5589b0f10c522a
Victor Bartlinski <vicbart08@gmail.com>
2026-09-03 11:03:01 +1000

    replaced steam-run-free with makeWrapper

--- nixfiles/packages/other/nova-unity-sim/default.nix
+++ nixfiles/packages/other/nova-unity-sim/default.nix
@@ -1,37 +1,22 @@
 { 
-  lib, 
-  stdenv,
-  makeWrapper, 
   fetchurl,
-  autoPatchelfHook, 
-  autoAddDriverRunpath, 
-  zlib, 
-  alsa-lib, 
-  pulseaudio, 
+  lib, 
   libGL, 
-  glib, 
-  gtk3, 
-  cairo, 
-  pango, 
-  gdk-pixbuf, 
-  fontconfig, 
-  freetype, 
-  libxkbcommon, 
-  minizip, 
-  xorg, 
-  libpulseaudio, 
   libudev0-shim, 
-  libxcursor, 
+  libx11,
   libxi, 
-  libxinerama, 
   libxrandr, 
-  libxscrnsaver, 
-  libxxf86vm, 
+  makeWrapper, 
+  stdenv,
   vulkan-loader, 
-  wayland, 
+  zlib, 
 }:
 
-# https://nixos.wiki/wiki/Packaging/Binaries
+# https://discourse.nixos.org/t/makewrapper-vs-buildinputs/21474
+# Used Claude
+# The unity build requires several of the dependencies listed below to be
+# available at runtime, which is achieved by linking them to the build executable.
+# The dependencies were determined by muntzing.
 stdenv.mkDerivation rec {
   name = "nova-unity-sim";
   version = "1.0.0";
@@ -42,45 +27,9 @@ stdenv.mkDerivation rec {
     hash = "sha256-Kv7CSyNJQKMuEATjBTDXIpn/u9sjAvPVJOlm7/SETN8=";
   };
 
-  buildInputs = [
-    stdenv.cc.cc.lib
-    zlib
-    alsa-lib
-    alsa-lib
-    libGL
-    libpulseaudio
-    libudev0-shim
-    libxcursor
-    libxi
-    libxinerama
-    libxrandr
-    libxscrnsaver
-    libxxf86vm
-    vulkan-loader
-    zlib
-    pulseaudio
-    libGL
-    glib
-    gtk3
-    cairo
-    pango
-    gdk-pixbuf
-    fontconfig
-    freetype
-    libxkbcommon
-    minizip
-    wayland
-  ] ++ (with xorg; [
-    libX11
-    libXext
-    libXxf86vm
-  ]);
-
   sourceRoot = ".";
 
   nativeBuildInputs = [
-    autoPatchelfHook
-    autoAddDriverRunpath
     makeWrapper
   ];
 
@@ -88,29 +37,17 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     mkdir -p $out/bin $out/share
-
-    cp -r ${build}/build_Data $out/share/
-    cp ${build}/build.x86_64 $out/share/
-    cp ${build}/libdecor-0.so.0 $out/share/
-    cp ${build}/libdecor-cairo.so $out/share/
-    cp ${build}/UnityPlayer.so $out/share/
-    chmod +x $out/share/build.x86_64
+    cp -r ${build}/* $out/share/
 
     makeWrapper $out/share/build.x86_64 $out/bin/${name} \
-      --chdir $out/share \
-      --set LIBDECOR_PLUGIN_DIR $out/share \
-      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXinerama
-        xorg.libXi
-        xorg.libXext
-        xorg.libXxf86vm
-        libxkbcommon
-        wayland
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ 
         libGL
+        libudev0-shim
+        libx11
+        libxi
+        libxrandr
         vulkan-loader
+        zlib
       ]}
 
     runHook postInstall
