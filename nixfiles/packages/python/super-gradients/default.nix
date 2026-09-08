da87a61316f18f31401b3c51ae18b46b24763576
Victor Bartlinski <vicbart08@gmail.com>
2026-09-08 17:42:42 +1000

    temporarily fixing super-gradients build errors, unknown if runtime errors exist

--- nixfiles/packages/python/super-gradients/default.nix
+++ nixfiles/packages/python/super-gradients/default.nix
@@ -1,65 +1,57 @@
-{ buildPythonPackage
-, fetchFromGitHub
-, callPackage
-, albumentationsx
-, boto3
-, deprecated
-, einops
-, fonttools
-, hydra-core
-, imagesize
-, json-tricks
-, jsonschema
-, matplotlib
-, onnx
-, onnxruntime
-, packaging
-, pillow
-, pip-tools
-, psutil
-, pygments
-, torch
-, rapidfuzz
-, scipy
-, setuptools
-, stringcase
-, tensorboard
-, termcolor
-, torchmetrics
-, torchvision
-, treelib
-, tqdm
-, werkzeug
-, wheel
+{ 
+  albumentationsx, 
+  boto3, 
+  buildPythonPackage, 
+  callPackage, 
+  deprecated, 
+  einops, 
+  fetchFromGitHub, 
+  fonttools, 
+  hydra-core, 
+  imagesize, 
+  json-tricks, 
+  jsonschema, 
+  matplotlib, 
+  onnx, 
+  onnxruntime, 
+  packaging, 
+  pillow, 
+  pip-tools, 
+  psutil, 
+  pygments, 
+  rapidfuzz, 
+  scipy, 
+  setuptools, 
+  sphinx, 
+  sphinx-rtd-theme, 
+  stringcase, 
+  tensorboard, 
+  termcolor, 
+  torch, 
+  torchmetrics, 
+  torchvision, 
+  tqdm, 
+  treelib, 
+  werkzeug, 
+  wheel, 
 }:
 
 let
-  pytorch = torch;
-  albumentations = albumentationsx;
   data-gradients = callPackage ./data-gradients.nix { };
-  # onnx-simplifier = callPackage ./onnx-simplifier.nix { };
 in
 buildPythonPackage rec {
   pname = "super-gradients";
-  version = "8.0.146";
-
+  version = "3.7.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Deci-AI";
     repo = pname;
-
-    rev = "d7152a4d3b92f1be339f71493135627f9a3529c8";
+    rev = version;
     hash = "sha256-51TWJatypEkTnh+0VsQSt9UFHIh0f7Lp/bKhnyjijeE=";
   };
 
-  prePatch = ''
-    echo "${version}" > version.txt
-    substituteInPlace src/super_gradients/__init__.py \
-      --replace-warn '__version__ = "3.7.1"' '__version__ = "${version}"'
-  '';
-
-  propagatedBuildInputs = [
+  buildInputs = [
     albumentationsx
     boto3
     data-gradients
@@ -72,27 +64,38 @@ buildPythonPackage rec {
     jsonschema
     matplotlib
     onnx
-    # onnx-simplifier i hate this dependency chain so i deleted it lol
     onnxruntime
     packaging
     pillow
     pip-tools
     psutil
     pygments
-    torch
     rapidfuzz
     scipy
     setuptools
+    sphinx
+    sphinx-rtd-theme
     stringcase
     tensorboard
     termcolor
+    torch
     torchmetrics
     torchvision
-    treelib
     tqdm
+    treelib
     werkzeug
     wheel
   ];
 
+  patches = [
+    ../../../overlay/ros/patches/super-gradients.patch
+  ];
+
+  # This derivation has not been maintained; significant patching needs to occur to the source files.
+  # This is a temporary fix.
+  # To resolve this issue properly, there are 3 (or more) options:
+  # 1. Convert this to a flake and use an earlier version of nixpkgs as an input
+  # 2. Manually override each dependency to an earlier version (this isn't hard, just tedious)
+  # 3. Open a PR against upstream source files with new versions and fixes, then patch the PR into the source files above.
   dontCheckRuntimeDeps = true;
 }
