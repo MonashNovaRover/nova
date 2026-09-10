2317f272985df1f6c744a9591bfaf14a6d240041
Victor Bartlinski <vicbart08@gmail.com>
2026-09-10 10:44:55 +1000

    fixing reformatting

--- src/other/default.nix
+++ src/other/default.nix
@@ -3,11 +3,8 @@
     ./can_sleuth
     ./nova_cli
     ./nova_testing
-  ] ++ (
-    builtins.filter (p: builtins.pathExists (p + "/default.nix")) [
-      ./coms_utils
-      ./libblcmd
-      ./libcanmd
-    ]
-  );
+    ./coms_utils
+    ./libblcmd
+    ./libcanmd
+  ];
 }
