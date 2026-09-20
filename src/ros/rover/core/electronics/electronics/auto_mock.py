72bfea72dcfd2d620979753c0c5cbb682ad53d07
applewendy12 <wendy.huangxi87@outlook.com>
2026-09-20 15:47:29 +1000

    autonomous navigation code

---
+++ src/ros/rover/core/electronics/electronics/auto_mock.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+<insert purpose here>
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+NODE: Auto
+TOPICS:
+  - publisher: <topic> [<msg type>]
+SERVICES:
+	- service: <service> [<srv type>]
+ACTIONS: None
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PACKAGE:        <package>
+AUTHOR(S):      <insert your name>
+CREATION:       <current date>
+EDITED:         <current date>
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+import rclpy
+from rclpy.node import Node
+from nova_interfaces.msg import GPSData
+from nova_interfaces.srv import CartographerCommand
+
+class Auto(Node):
+
+    SPEED_PARAM = "speed"
+
+    def __init__(self):
+        super().__init__("Auto")
+        
+        # define variables
+        self.declare_parameter(self.SPEED_PARAM, 0.0000005)
+
+        self.PERIOD = 0.5
+        self.speed = self.get_parameter(self.SPEED_PARAM).value
+        self.current_pos = (38.406787449642906, -110.79183322459582)
+        self.target_pos = self.current_pos
+
+        # add publishers
+
+        self.gps_publisher = self.create_publisher(GPSData, '/gps_rover/fix_custom', 10)
+        
+        # add services
+        self.update_timer = self.create_timer(self.PERIOD, lambda: self.update(self.PERIOD))
+        self.cartographer_service = self.create_service(CartographerCommand, '/autonomous/cartographer_command', self.cartographer_callback)
+
+        self.get_logger().info(f"{self.get_name()} started.")
+    
+
+    def update(self, delta_time):
+
+        displacement = (
+            self.target_pos[0] - self.current_pos[0], 
+            self.target_pos[1] - self.current_pos[1]
+        )
+        distance = (displacement[0] ** 2 + displacement[1] ** 2) ** 0.5
+        
+        if distance > 0:
+	    	# normalise displacement vector
+            direction = (
+                displacement[0] / distance, 
+                displacement[1] / distance
+            )
+    
+	    	# get the velocity to move
+            velocity = (
+                direction[0] * self.speed, 
+                direction[1] * self.speed
+            )
+        
+	    	# calculate movement for this update
+            delta_pos = (
+	    		velocity[0] * delta_time,
+	    		velocity[1] * delta_time
+	    	)
+    
+	    	# make sure we don't overshoot
+            delta_pos_dist = (delta_pos[0] ** 2 + delta_pos[1] ** 2) ** 0.5
+	    	
+            if delta_pos_dist > distance:
+                delta_pos = displacement
+    
+	    	# update current position
+            self.current_pos = (
+	    		self.current_pos[0] + delta_pos[0],
+	    		self.current_pos[1] + delta_pos[1]
+	    	)
+    
+        msg = GPSData()
+        msg.latitude, msg.longitude =  self.current_pos[0], self.current_pos[1]
+
+        self.gps_publisher.publish(msg)
+
+    def cartographer_callback(self, request, response):
+        if len(request.goals) <= 0:
+            response.success = False
+            return response
+
+        goal_pos = request.goals[-1]
+
+        self.target_pos = (goal_pos.latitude, goal_pos.longitude)
+        self.get_logger().info(f"Target position updated to: {self.target_pos}")
+
+        response.success = True
+        return response
+
+def main():
+    rclpy.init()
+    node = Auto()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
