c3fd49bb78706e78dd71b984290a93f545ef8fd7
Monash Nova Rover <novaroverteam@monash.edu>
2026-09-23 13:48:24 +0000

    don't do slack on arm

--- nixfiles/modules/home/desktop/slack.nix
+++ nixfiles/modules/home/desktop/slack.nix
@@ -7,9 +7,9 @@ in
   options.nova.desktop.slack.enable = lib.mkEnableOption "Nova Rover user Slack configuration";
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
+    home.packages = with pkgs; builtins.filter (lib.meta.availableOn stdenv.hostPlatform) ([
       slack
-    ];
+    ]);
 
     dconf.settings."org/gnome/shell".favorite-apps = lib.mkAfter [
       "slack.desktop"
