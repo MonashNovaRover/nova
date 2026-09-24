9cf56d98b15e7581d6eb00f3c72cb01a9cb77764
Monash Nova Rover <novaroverteam@monash.edu>
2026-09-24 01:22:09 +0000

    nixfiles: ci: enable kvm capability for hydra

--- nixfiles/modules/nixos/common/ci/master/default.nix
+++ nixfiles/modules/nixos/common/ci/master/default.nix
@@ -71,7 +71,7 @@ in
       buildMachinesFiles =
         (lib.optional (config.nix.buildMachines != [ ]) "/etc/nix/machines") ++
         (lib.optional (cfg.hydra.localMaxJobs != 0) (pkgs.writeText "hydra-build-machines" ''
-          localhost ${builtins.concatStringsSep "," ([ builtins.currentSystem ] ++ config.nix.settings.extra-platforms or [ ])} - ${toString cfg.hydra.localMaxJobs} ${toString cfg.hydra.localSpeedFactor} ${builtins.concatStringsSep "," [ "nixos-test" "big-parallel" ]}
+          localhost ${builtins.concatStringsSep "," ([ builtins.currentSystem ] ++ config.nix.settings.extra-platforms or [ ])} - ${toString cfg.hydra.localMaxJobs} ${toString cfg.hydra.localSpeedFactor} ${builtins.concatStringsSep "," [ "kvm" "nixos-test" "big-parallel" ]}
         ''));
       logo = pkgs.nova.nova-icons + /share/icons/hicolor/512x512/apps/nova-logo-white-and-orange.png;
       #maxServers = 2; # Max number of hydra web workers
